3cc9ee2e6280b4a9df52c33a40c73a646f385f55
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-05-20 01:27:18 +0530

    Time: 11 ms (70.59%), Space: 13.1 MB (30.48%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int n=prices.size();
+        
+        if(n<=1 || k==0){
+            return 0;
+        }
+        
+        vector<vector<int>>dp(k+1,vector<int>(n,0));
+        
+        dp[0][0]=0;
+        
+        for(int i=1;i<=k;i++){
+            
+           vector<int>v=dp[i-1];
+            
+           vector<int>prefix(n,0);
+            
+           prefix[0]=v[0]-prices[0];
+            
+           for(int j=1;j<n;j++){
+               
+              prefix[j]=max(prefix[j-1],v[j]-prices[j]);
+               
+           }
+            
+           for(int j=1;j<n;j++){
+               
+               dp[i][j]=max(dp[i][j-1],prefix[j-1]+prices[j]);
+               
+           } 
+            
+        }
+        
+        
+        // for(int i=0;i<=k;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }cout<<endl;
+        // }cout<<endl;
+        
+        return dp[k][n-1];
+        
+    }
+};
