680c45f7cc248916cbf01a63bf254effbfc25f23
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-05-23 11:16:11 +0530

    Time: 286 ms (75.53%), Space: 9.9 MB (67.86%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,57 +1,37 @@
 class Solution {
 public:
-    
-    vector<pair<int,int>>vec;
-    
-    int dp[605][105][105];
-    
-    int func(int i,int len,int m,int n,int cntm,int cntn){
-        
-        if(i>=len){
-            return 0;
-        }
-        
-        if(dp[i][cntm][cntn]!=-1){
-            return dp[i][cntm][cntn];
-        }
-        
-        int currval=func(i+1,len,m,n,cntm,cntn);
-        
-        int curr0=vec[i].first;
-        int curr1=vec[i].second;
-        
-        if(cntm+curr0<=m && cntn+curr1<=n){
-            currval=max(currval,1+func(i+1,len,m,n,cntm+curr0,cntn+curr1));
-        }
-        
-        return dp[i][cntm][cntn]=currval;
-        
-    }
-    
     int findMaxForm(vector<string>& strs, int m, int n) {
         
-      for(int i=0;i<strs.size();i++){
-          
-          int cnt0=0,cnt1=0;
-          
-          for(int j=0;j<strs[i].length();j++){
-              if(strs[i][j]=='0'){
-                  cnt0++;
-              }else{
-                  cnt1++;
-              }
-          }
-          
-          vec.push_back({cnt0,cnt1});
-          
-      }
+        int len=strs.size();
         
-      
-        memset(dp,-1,sizeof(dp));
+        vector<vector<int>>dp(m+5,vector<int>(n+5,0));
         
-        int ans=func(0,strs.size(),m,n,0,0);
+        for(string str:strs){
+            
+            int cnt0=0,cnt1=0;
+            
+            for(char ch:str){
+                if(ch=='0'){
+                    cnt0++;
+                }else{
+                    cnt1++;
+                }
+            }
+            
+            
+            for(int i=m;i>=cnt0;i--){
+                
+                for(int j=n;j>=cnt1;j--){
+                    
+                    dp[i][j]=max(dp[i][j],dp[i-cnt0][j-cnt1]+1);
+                    
+                }
+                
+            }
+            
+        }
         
-        return ans;
+        return dp[m][n];
         
     }
 };
