44d5f00d15934e9eab58bef258cff90b0939682e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-05-23 11:21:33 +0530

    Time: 357 ms (54.58%), Space: 9.9 MB (70.82%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        
+        int len=strs.size();
+        
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        
+        for(string str:strs){
+            
+            int cnt0=0,cnt1=0;
+            
+            for(char ch:str){
+                if(ch=='0'){
+                    cnt0++;
+                }else{
+                    cnt1++;
+                }
+            }
+            
+            for(int i=m;i>=cnt0;i--){
+                for(int j=n;j>=cnt1;j--){
+                    dp[i][j]=max(dp[i][j],dp[i-cnt0][j-cnt1]+1);
+                }
+            }
+            
+        }
+        
+        
+        return dp[m][n];
+        
+    }
+};
