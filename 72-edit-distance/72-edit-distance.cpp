d7af9d1a23b3f686b9bc344c525ff0479af81ab6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-05-25 23:48:04 +0530

    Time: 18 ms (54.90%), Space: 9.1 MB (20.45%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        
+        int m=s1.length();
+        int n=s2.length();
+        
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        
+        s1="*"+s1;
+        s2="*"+s2;
+        
+        for(int i=1;i<=n;i++){
+            dp[0][i]=i;
+        }
+        
+        for(int i=1;i<=m;i++){
+            dp[i][0]=i;
+        }
+        
+        for(int i=1;i<=m;i++){
+            
+            for(int j=1;j<=n;j++){
+                
+                if(s1[i]==s2[j]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+                }
+                
+            }
+            
+        }
+        
+        return dp[m][n];
+        
+    }
+};
