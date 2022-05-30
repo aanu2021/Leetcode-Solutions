585f3d413f87f16d22c5bdf1600c559a787ef66a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-05-30 21:37:43 +0530

    Time: 1082 ms (28.06%), Space: 229.4 MB (5.14%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -4,48 +4,62 @@ public:
         
         int n=s.length();
         
-        map<char,vector<int>>mp;
+        vector<vector<int>>prefix(n+1,vector<int>(26,0));
         
         for(int i=0;i<n;i++){
-            mp[s[i]].push_back(i);
+            
+            int j=s[i]-'a';
+            
+            prefix[i][j]++;
+            
         }
         
+        for(int i=1;i<n;i++){
+            
+            for(int j=0;j<26;j++){
+                
+                prefix[i][j]+=prefix[i-1][j];
+                
+            }
+            
+        }
+        
+        
         int ans=0;
+        
+        map<char,int>firstidx;
+        map<char,int>lastidx;
+        
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+            if(firstidx.find(ch)==firstidx.end()){
+                firstidx[ch]=i;
+            }
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            char ch=s[i];
+            if(lastidx.find(ch)==lastidx.end()){
+                lastidx[ch]=i;
+            }
+        }
+        
         
         for(char ch1='a';ch1<='z';ch1++){
             
             for(char ch2='a';ch2<='z';ch2++){
                 
-                int extreme_size=mp[ch1].size();
-                int middle_size=mp[ch2].size();
+                int i=firstidx[ch1];
+                int j=lastidx[ch1];
                 
-                if(ch1==ch2 && extreme_size>=3){
-                    
-                    ans++;
+                if(i==j){
                     continue;
-                    
                 }
                 
-                else if(ch1!=ch2 && extreme_size>=2 && middle_size>=1){
-                    
-int idx=upper_bound(mp[ch2].begin(),mp[ch2].end(),mp[ch1][0])-mp[ch2].begin();
-                    
-                    if(idx==middle_size){
-                        
-                        continue;
-                        
-                    }
-                    
-                    else{
-                        
-                        int currval=mp[ch2][idx];
-                        
-                        if(currval>mp[ch1][0] && currval<mp[ch1].back()){
-                            ans++;
-                        }
-                        
-                    }
-                    
+                int middle_occ=(j-1>=0 ? prefix[j-1][ch2-'a'] : 0)-(i>=0 ? prefix[i][ch2-'a'] : 0);
+                
+                if(middle_occ>=1){
+                    ans++;
                 }
                 
             }
