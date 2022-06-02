5ada69cbbc17b014d8a865b6ff112c032aa1e2f2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-02 11:50:54 +0530

    Time: 45 ms (55.86%), Space: 13.2 MB (70.13%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<vector<pair<int,int>>>graph(n);
+        
+        for(int i=0;i<flights.size();i++){
+            graph[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+        }
+        
+        vector<int>dist(n,1e9);
+        
+        dist[src]=0;
+        
+        k++;
+        
+        queue<pair<int,int>>q;
+        
+        q.push({src,0});
+        
+        
+        while(!q.empty() && k>0){
+            
+            int sz=q.size();
+            
+            while(sz--){
+                
+                auto curr=q.front();
+                q.pop();
+                
+                int node=curr.first;
+                int wt=curr.second;
+                
+                for(auto j:graph[node]){
+                    
+                    if(dist[j.first]>wt+j.second){
+                        dist[j.first]=wt+j.second;
+                        q.push({j.first,dist[j.first]});
+                    }
+                    
+                }
+                
+            }
+            
+            k--;
+            
+        }
+        
+        
+        if(dist[dst]<1e9){
+            return dist[dst];
+        }else{
+            return -1;
+        }
+        
+    }
+};
