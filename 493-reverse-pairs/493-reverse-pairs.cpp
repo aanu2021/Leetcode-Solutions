03eda21f8b101a050258349a2984672d1f1a572c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-09 20:40:50 +0530

    Time: 735 ms (53.47%), Space: 45.1 MB (89.91%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    int merge(vector<int>&nums,int low,int mid,int high){
+        
+        int i=low,j=mid+1;
+        
+        int cnt=0;
+        
+        for(i=low , j=mid+1 ;i<=mid;i++){
+            
+            while(j<=high && nums[i]/2.0>nums[j]){
+                j++;
+            }
+            
+            cnt+=(j-(mid+1));
+            
+        }
+        
+        sort(nums.begin()+low,nums.begin()+high+1);
+        
+        return cnt;
+        
+    }
+    
+    int mergesort(vector<int>&nums,int low,int high){
+        
+        int cnt=0;
+        
+        if(low<high){
+            
+            int mid=(low+high)/2;
+            
+            cnt+=mergesort(nums,low,mid);
+            cnt+=mergesort(nums,mid+1,high);
+            
+            cnt+=merge(nums,low,mid,high);
+            
+        }
+        
+        return cnt;
+        
+    }
+    
+    int reversePairs(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        return mergesort(nums,0,n-1);
+        
+    }
+};
