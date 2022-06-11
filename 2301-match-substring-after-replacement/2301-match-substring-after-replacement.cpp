3194f4842fad0a119400de474c456c1810a1951c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-11 21:32:53 +0530

    Time: 994 ms (40.00%), Space: 18.7 MB (40.00%) - LeetHub

---
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool matchReplacement(string s, string t, vector<vector<char>>& mappings) {
+        
+        int n=s.length();
+        int m=t.length();
+        
+        map<char,map<char,int>>mp;
+        
+        for(int i=0;i<mappings.size();i++){
+            mp[mappings[i][0]][mappings[i][1]]++;
+        }
+        
+        bool flag=false;
+        
+        for(int i=0;i<=n-m;i++){
+            
+            flag=true;
+            
+            for(int j=0;j<m;j++){
+                
+                if(t[j]==s[i+j]){
+                    continue;
+                }
+                
+                else{
+                    
+                    if(mp[t[j]][s[i+j]]>0){
+                        continue;
+                    }else{
+                        flag=false;
+                        break;
+                    }
+                    
+                }
+                
+            }
+            
+            if(flag==true){
+                break;
+            }
+            
+        }
+        
+        return flag;
+        
+    }
+};
