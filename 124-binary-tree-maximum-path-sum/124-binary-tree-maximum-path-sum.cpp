4bb8fcf230dc630109ea0f33306e5218d59daa5f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-11 11:52:32 +0530

    Time: 31 ms (70.37%), Space: 28.8 MB (6.17%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     
@@ -28,17 +17,11 @@ public:
         pair<int,int>left=func(root->left);
         pair<int,int>right=func(root->right);
         
-        maxval=max(maxval,max(left.first,left.second));
-        maxval=max(maxval,max(right.first,right.second));
-        
         pair<int,int>curr={root->val,root->val};
         
-        curr.first=root->val+max(left.first,right.first);
-        curr.second=root->val+left.first+right.first;
+        curr.first=max(curr.first,root->val+max(left.first,right.first));
         
-        curr.first=max(curr.first,root->val);
-        curr.second=max(curr.second,root->val);
-        
+        curr.second=max(curr.second,root->val+left.first+right.first);
         curr.second=max(curr.second,max(left.second,right.second));
         
         maxval=max(maxval,max(curr.first,curr.second));
@@ -53,7 +36,7 @@ public:
             return 0;
         }
         
-        pair<int,int>ans=func(root);
+        func(root);
         
         return maxval;
         
