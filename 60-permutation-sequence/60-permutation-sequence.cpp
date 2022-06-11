baa6fc1cac093adfdd556627fd834aa71174f86a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-11 12:25:10 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.36%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void solve(int n,int k,vector<int>&nums,vector<int>&fact,string &ans){
+    void func(int n,int k,vector<int>&nums,vector<int>&fact,string&ans){
         
         if(n==1){
             ans+=to_string(nums[0]);
@@ -16,35 +16,32 @@ public:
         
         k-=(idx*fact[n-1]);
         
-        solve(n-1,k,nums,fact,ans);
+        func(n-1,k,nums,fact,ans);
         
     }
     
     string getPermutation(int n, int k) {
         
         if(n==1){
-            return "1";
+           return "1";
         }
         
         k--;
         
         string ans="";
         
+        vector<int>nums;
+        
         vector<int>fact(n+1);
-        
-        vector<int>nums(n);
-        
-        for(int i=0;i<n;i++){
-            nums[i]=i+1;
-        }
         
         fact[0]=1;
         
         for(int i=1;i<=n;i++){
+            nums.push_back(i);
             fact[i]=fact[i-1]*i;
         }
         
-        solve(n,k,nums,fact,ans);
+        func(n,k,nums,fact,ans);
         
         return ans;
         
