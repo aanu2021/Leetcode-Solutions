5de6a074a4d32eff6f5438c865fe9c0b3c8467ee
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-13 10:17:44 +0530

    Time: 12 ms (28.50%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -4,36 +4,30 @@ public:
         
         int n=triangle.size();
         
-        vector<vector<int>>dp(n,vector<int>(n,1e9));
+        vector<int>dp(n,1e9);
         
-        dp[0][0]=triangle[0][0];
+        dp[0]=triangle[0][0];
         
-        for(int i=1;i<n;i++){
+        for(int i=1;i<n;++i){
             
-            for(int j=0;j<=i;j++){
+            for(int j=i;j>=0;--j){
                 
+                dp[j]=dp[j]+triangle[i][j];
                 
                 if(j-1>=0){
-                    
-                    dp[i][j]=min(dp[i][j],dp[i-1][j-1]+triangle[i][j]);
-                    
+                    dp[j]=min(dp[j],dp[j-1]+triangle[i][j]);
                 }
-                
-                if(j<n){
-                    
-                    dp[i][j]=min(dp[i][j],dp[i-1][j]+triangle[i][j]);
-                    
-                }
-                
                 
             }
             
+            
         }
         
+       
         int ans=1e9;
         
         for(int i=0;i<n;i++){
-            ans=min(ans,dp[n-1][i]);
+            ans=min(ans,dp[i]);
         }
         
         return ans;
