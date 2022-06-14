e8546877abb88a51e853934fa4976f092864ece0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-14 10:25:57 +0530

    Time: 36 ms (38.03%), Space: 12.3 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -7,34 +7,18 @@ public:
         
         vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         
-        for(int i=1;i<=n;i++){
-            dp[0][i]=i;
-        }
-        
         for(int i=1;i<=m;i++){
-            dp[i][0]=i;
-        }
-        
-        s1="*"+s1;
-        s2="*"+s2;
-        
-        for(int i=1;i<=m;i++){
-            
             for(int j=1;j<=n;j++){
-                
-                if(s1[i]==s2[j]){
-                    dp[i][j]=dp[i-1][j-1];
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
-                
-                else{
-                    dp[i][j]=min(dp[i-1][j],dp[i][j-1])+1;
-                }
-                
             }
-            
         }
+      
         
-        return dp[m][n];
+        return (m+n-(2*dp[m][n]));
         
     }
 };
