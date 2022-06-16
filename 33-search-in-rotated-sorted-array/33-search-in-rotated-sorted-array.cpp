9231490e88d0e956a5bf09e4b32bcdb163fba9da
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-16 13:16:51 +0530

    Time: 11 ms (17.06%), Space: 11.1 MB (74.96%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
+        // Here we have to use the property , that if we are at index i , then either its left part or its right part would be sorted.
+        
         int n=nums.size();
         
         int low=0,high=n-1;
@@ -14,10 +16,9 @@ public:
                 return mid;
             }
             
-            
             if(nums[mid]>=nums[low]){
                 
-                if(target>=nums[low] && target<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>=target){
                     high=mid-1;
                 }else{
                     low=mid+1;
@@ -27,7 +28,7 @@ public:
             
             else{
                 
-                if(target>=nums[mid] && target<=nums[high]){
+                if(nums[mid]<=target && nums[high]>=target){
                     low=mid+1;
                 }else{
                     high=mid-1;
