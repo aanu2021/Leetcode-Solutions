b69e7c42b5492040caf6a04d16b9f8503b9d604d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-17 13:17:53 +0530

    Time: 69 ms (29.28%), Space: 89.3 MB (62.19%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,50 +1,84 @@
 class Solution {
 public:
+    
+    double avg(int a,int b){
+        
+        return (a+b)/2.0;
+        
+    }
+    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        int m=nums1.size();
-        int n=nums2.size();
+        int x=nums1.size();
+        int y=nums2.size();
         
-        vector<int>v(m+n);
-        
-        int i=0,j=0;
-        
-        int k=0;
-        
-        while(i<m && j<n){
+        if(x>y){
             
-            if(nums1[i]<=nums2[j]){
-                v[k++]=nums1[i++];
-            }else{
-                v[k++]=nums2[j++];
+            return findMedianSortedArrays(nums2,nums1);
+            
+        }
+        
+        int low=0,high=x;
+        
+        while(low<=high){
+            
+            int partX=(low+high)/2;
+            int partY=(x+y+1)/2;
+            partY-=partX;
+            
+            int maxLeftX=INT_MIN;
+            int maxLeftY=INT_MIN;
+            int minRightX=INT_MAX;
+            int minRightY=INT_MAX;
+            
+            if(partX!=0){
+                maxLeftX=nums1[partX-1];
+            }
+            
+            if(partY!=0){
+                maxLeftY=nums2[partY-1];
+            }
+            
+            if(partX!=x){
+                minRightX=nums1[partX];
+            }
+            
+            if(partY!=y){
+                minRightY=nums2[partY];
+            }
+            
+            
+            if(maxLeftX<=minRightY && maxLeftY<=minRightX){
+                
+                if((x+y)%2!=0){
+                    
+                    return max(maxLeftX,maxLeftY);
+                    
+                }
+                
+                else{
+                    
+                    return avg(max(maxLeftX,maxLeftY),min(minRightX,minRightY));
+                    
+                }
+                
+            }
+            
+            if(maxLeftY>minRightX){
+                
+                low=partX+1;
+                
+            }
+            
+            else{
+                
+                high=partX-1;
+                
             }
             
         }
         
-        
-        while(i<m){
-            
-            v[k++]=nums1[i++];
-            
-        }
-        
-        while(j<n){
-            
-            v[k++]=nums2[j++];
-            
-        }
-        
-        if(k%2!=0){
-            
-            return (1.0*v[k/2]);
-            
-        }
-        
-        else{
-            
-            return (v[k/2]+v[k/2-1])/2.0;
-            
-        }
+        return -1.0;
         
     }
 };
