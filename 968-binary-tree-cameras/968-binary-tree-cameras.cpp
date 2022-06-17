9219b78a1915a93da6aadad4037aba926e26b7c2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-17 11:54:55 +0530

    Time: 94 ms (5.10%), Space: 31.7 MB (5.10%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,5 +1,3 @@
-//Approach : DP on Trees
-
 class Solution {
 public:
     
@@ -14,12 +12,16 @@ public:
             }else{
                 return 0;
             }
-           
+            
         }
         
+        
         if(dp.find({root,{placed,parentCam}})!=dp.end()){
+            
             return dp[{root,{placed,parentCam}}];
+            
         }
+        
         
         int ans=1e9;
         
@@ -27,39 +29,40 @@ public:
             
             if(parentCam==0){
                 
-                int curr1=func(root->left,1,0)+min(func(root->right,1,0),func(root->right,0,0)) ;
+                int curr1=func(root->left,1,0) + min(func(root->right,1,0),func(root->right,0,0));
                 
-                int curr2=func(root->right,1,0)+min(func(root->left,1,0),func(root->left,0,0));
+                int curr2=func(root->right,1,0) + min(func(root->left,1,0),func(root->left,0,0));
                 
-                ans = min(curr1,curr2);
+                ans=min(curr1,curr2);
                 
             }
             
             else{
                 
-                ans = min(func(root->left,0,0),func(root->left,1,0)) + min(func(root->right,0,0),func(root->right,1,0));
+                ans = min(func(root->left,1,0),func(root->left,0,0)) + min(func(root->right,1,0),func(root->right,0,0));
                 
             }
             
         }
+        
         
         else{
             
             if(parentCam==0){
                 
-               ans = 1 + min(func(root->left,0,1),func(root->left,1,1)) + min(func(root->right,0,1),func(root->right,1,1));   
+                ans = 1 + min(func(root->left,1,1),func(root->left,0,1)) + min(func(root->right,1,1),func(root->right,0,1));
                 
             }
             
             else{
                 
-                ans = 1 + min(func(root->left,0,1),func(root->left,1,1)) + min(func(root->right,0,1),func(root->right,1,1));
+                ans = 1 + min(func(root->left,1,1),func(root->left,0,1)) + min(func(root->right,1,1),func(root->right,0,1));
                 
             }
             
         }
         
-        return dp[{root,{placed,parentCam}}]=ans;
+        return dp[{root,{placed,parentCam}}] = ans;
         
     }
     
@@ -67,19 +70,9 @@ public:
         
         if(root==NULL)return 0;
         
-        if(root==NULL && root->left==NULL && root->right==NULL)return 1;
+        if(!root->left && !root->right)return 1;
         
-        
-       /*  
-       
-       
-       dp[u][a][b] --> gives us the minimum number of cameras required to monitor the entire subtree rooted at vertex u , given that a(0/1) be the state of placing a camera or not , and b(0/1) be the state of presence of a camera at its parent node.
-       
-       
-       */
-        
-        
-        int ans=min(func(root,1,0),func(root,0,0));
+        int ans=min( func(root,1,0) , func(root,0,0) );
         
         return ans;
         
