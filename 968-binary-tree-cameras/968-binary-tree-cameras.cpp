d6f9a4a520a84f6df5775a9a67a1a69cfa0b23b6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-17 12:00:56 +0530

    Time: 16 ms (57.04%), Space: 24 MB (13.58%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,80 +1,47 @@
 class Solution {
 public:
     
-    map<pair<TreeNode*,pair<bool,bool>>,int>dp;
+    set<TreeNode*>S;
+    int cam=0;
     
-    int func(TreeNode*root,int placed,int parentCam){
+    void func(TreeNode*root,TreeNode*parent){
         
-        if(root==NULL){
+        if(root!=NULL){
             
-            if(placed==1){
-                return 1e9;
-            }else{
-                return 0;
-            }
+            func(root->left,root);
+            func(root->right,root);
             
-        }
-        
-        
-        if(dp.find({root,{placed,parentCam}})!=dp.end()){
-            
-            return dp[{root,{placed,parentCam}}];
-            
-        }
-        
-        
-        int ans=1e9;
-        
-        if(placed==0){
-            
-            if(parentCam==0){
+            if((parent==NULL && S.find(root)==S.end()) || S.find(root->left)==S.end() || S.find(root->right)==S.end()){
                 
-                int curr1=func(root->left,1,0) + min(func(root->right,1,0),func(root->right,0,0));
+                cam++;
                 
-                int curr2=func(root->right,1,0) + min(func(root->left,1,0),func(root->left,0,0));
+                S.insert(root);
+                S.insert(parent);
                 
-                ans=min(curr1,curr2);
-                
-            }
-            
-            else{
-                
-                ans = min(func(root->left,1,0),func(root->left,0,0)) + min(func(root->right,1,0),func(root->right,0,0));
+                S.insert(root->left);
+                S.insert(root->right);
                 
             }
             
         }
-        
-        
-        else{
-            
-            if(parentCam==0){
-                
-                ans = 1 + min(func(root->left,1,1),func(root->left,0,1)) + min(func(root->right,1,1),func(root->right,0,1));
-                
-            }
-            
-            else{
-                
-                ans = 1 + min(func(root->left,1,1),func(root->left,0,1)) + min(func(root->right,1,1),func(root->right,0,1));
-                
-            }
-            
-        }
-        
-        return dp[{root,{placed,parentCam}}] = ans;
         
     }
     
     int minCameraCover(TreeNode* root) {
         
-        if(root==NULL)return 0;
+        if(root==NULL){
+            return 0;
+        }
         
-        if(!root->left && !root->right)return 1;
+        if(!root->left && !root->right){
+            return 1;
+        }
         
-        int ans=min( func(root,1,0) , func(root,0,0) );
+        S.insert(NULL);
         
-        return ans;
+        func(root,NULL);
+        
+        return cam;
         
     }
 };
