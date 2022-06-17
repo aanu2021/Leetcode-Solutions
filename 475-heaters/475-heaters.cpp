a6e672caa664612eb97d6667aea499dfdc318e8b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-17 20:43:13 +0530

    Time: 221 ms (9.62%), Space: 25.3 MB (85.99%) - LeetHub

---
+++ 475-heaters/475-heaters.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    
+    bool isPossible(vector<int>&houses,vector<int>&heaters,int radius){
+        
+       int i=0,j=0,m=houses.size(),n=heaters.size();
+        
+        while(i<m){
+            
+            if(j==n){
+                return false;
+            }
+            
+            int l=heaters[j]-radius;
+            int r=heaters[j]+radius;
+            
+            if(houses[i]<l || houses[i]>r){
+                j++;
+            }else{
+                i++;
+            }
+            
+        }
+        
+        return true;
+        
+    }
+    
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+        
+        int low=0,high=1e9;
+        
+        int ans=-1;
+        
+        while(low<=high){
+            
+            int mid=(low+high)/2;
+            
+            if(isPossible(houses,heaters,mid)==true){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+};
