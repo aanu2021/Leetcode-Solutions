d4bc284919fb32323a8c14eabb16f588452864da
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-19 10:37:57 +0530

    Time: 62 ms (81.35%), Space: 23.7 MB (94.80%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        
+        vector<vector<string>>result;
+        
+        int n=products.size();
+        
+        sort(products.begin(),products.end());
+        
+        int bstart=0;
+        
+        string prefix="";
+        
+        for(char c:searchWord){
+            
+            prefix+=c;
+            
+            int start=lower_bound(products.begin()+bstart,products.end(),prefix)-products.begin();
+            
+            result.push_back({});
+            
+            for(int i=start;i<min(start+3,n) && !products[i].compare(0, prefix.length(), prefix) ;++i){
+                result.back().push_back(products[i]);
+            }
+            
+            bstart=start;
+            
+        }
+        
+        return result;
+        
+    }
+};
