5b51af5054386824c85265025d7c0d38cb96ee3c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-23 10:29:49 +0530

    Time: 404 ms (37.38%), Space: 56.3 MB (58.52%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     
-    bool static comp(vector<int>&x,vector<int>&y){
+    bool static comp(const vector<int>&x,const vector<int>&y){
+        
         return x[1] < y[1];
-    }
+        
+    } 
     
     int scheduleCourse(vector<vector<int>>& c) {
         
         sort(c.begin(),c.end(),comp);
         
-        priority_queue<int>pq;
+        int sum=0;
         
-        int total=0;
+        priority_queue<int>pq;
         
         int ans=0;
         
         for(int i=0;i<c.size();i++){
             
-            if(total+c[i][0]<=c[i][1]){
+            if(sum+c[i][0]<=c[i][1]){
                 
-                total+=c[i][0];
-                
+                sum+=c[i][0];
                 pq.push(c[i][0]);
-                
                 ans++;
                 
             }
             
-            else{
+            else if(!pq.empty() && pq.top()>c[i][0]){
                 
-                if(!pq.empty() && pq.top()>c[i][0]){
-                    
-                    total-=pq.top();
-                    pq.pop();
-                    
-                    total+=c[i][0];
-                    pq.push(c[i][0]);
-                    
-                }
+                sum-=pq.top();
+                pq.pop();
+                
+                sum+=c[i][0];
+                
+                pq.push(c[i][0]);
+                
                 
             }
             
