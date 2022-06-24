7d4ae992d34f1a808ff33cc5863cb853bdb45bbe
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-24 11:41:13 +0530

    Time: 16 ms (16.51%), Space: 6 MB (89.86%) - LeetHub

--- 920-number-of-music-playlists/920-number-of-music-playlists.cpp
+++ 920-number-of-music-playlists/920-number-of-music-playlists.cpp
@@ -17,28 +17,45 @@ public:
         return mod(mod(a)+mod(b));
     }
     
+    ll dp[105][105];
     
-    int numMusicPlaylists(int N, int L, int K) {
+    ll func(ll n,ll l,ll K,ll N,ll L){
         
-        vector<vector<ll>>dp(L+1,vector<ll>(N+1,0LL));
-        
-        dp[0][0]=1LL;
-        
-        for(ll i=1;i<=L;i++){
+        if(l==0){
             
-            for(ll j=1;j<=N;j++){
-                
-                ll left=mul(dp[i-1][j-1],N-j+1);
-                
-                ll right=mul(dp[i-1][j],max(0LL,j-K));
-                
-                dp[i][j]=add(left,right);
-                
+            if(n==0){
+                return 1;
+            }
+            
+            else{
+                return 0;
             }
             
         }
         
-        return mod(dp[L][N]);
+        if(l<0 || n<0){
+            return 0;
+        }
+        
+        if(dp[n][l]!=-1){
+            return dp[n][l];
+        }
+        
+        ll intro=mul(func(n-1,l-1,K,N,L),(ll)(N-n+1));
+        
+        ll again=mul(func(n,l-1,K,N,L),max(0LL,(ll)n-K));
+        
+        return dp[n][l] = add(intro,again);
+        
+    }
+    
+    int numMusicPlaylists(int N, int L, int K) {
+        
+        memset(dp,-1,sizeof(dp));
+        
+        ll ans=func(N,L,K,N,L);
+        
+        return mod(ans);
         
     }
 };
