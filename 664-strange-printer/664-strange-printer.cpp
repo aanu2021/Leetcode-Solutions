59bde72496dc5be0681b25f5101a1396e0155242
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-24 11:24:49 +0530

    Time: 55 ms (61.07%), Space: 9.6 MB (22.61%) - LeetHub

--- 664-strange-printer/664-strange-printer.cpp
+++ 664-strange-printer/664-strange-printer.cpp
@@ -2,19 +2,7 @@ class Solution {
 public:
     int strangePrinter(string s) {
         
-        string res="";
-        
-        res+=s[0];
-        
-        for(int i=1;i<s.length();++i){
-            if(res.back()!=s[i]){
-                res+=s[i];
-            }
-        }
-        
-        int n=res.length();
-        
-        s=res;
+        int n=s.length();
         
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
@@ -23,32 +11,30 @@ public:
         }
         
         for(int i=0;i<n-1;++i){
-            dp[i][i+1]=2;
+            if(s[i]==s[i+1]){
+                dp[i][i+1]=1;
+            }else{
+                dp[i][i+1]=2;
+            }
         }
         
         for(int L=3;L<=n;L++){
             
-            for(int i=0;i<n-L+1;i++){
+            for(int i=0;i<n-L+1;++i){
                 
                 int j=i+L-1;
                 
-                dp[i][j]=1e9;
-                
-                dp[i][j]=min(dp[i][j],1+dp[i+1][j]);
+                dp[i][j]=1+dp[i+1][j];
                 
                 for(int k=i+1;k<=j;k++){
                     
-                   if(s[i]==s[k]){
-                       
-                       dp[i][j]=min(dp[i][j],dp[i+1][k]+dp[k+1][j]);
-                       
-                   }
+                    if(s[i]==s[k]){
+                        
+                        dp[i][j]=min(dp[i][j],dp[i+1][k]+dp[k+1][j]);
+                        
+                    }
                     
                 }
-                
-                // if(s[i]==s[j]){
-                //     dp[i][j]--;
-                // }
                 
             }
             
