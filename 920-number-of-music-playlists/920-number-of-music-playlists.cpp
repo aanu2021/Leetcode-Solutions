d0b03d878e07d2759b58b01b449a6a9d50d17633
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-24 11:45:30 +0530

    Time: 28 ms (6.13%), Space: 9.1 MB (44.58%) - LeetHub

--- 920-number-of-music-playlists/920-number-of-music-playlists.cpp
+++ 920-number-of-music-playlists/920-number-of-music-playlists.cpp
@@ -17,45 +17,59 @@ public:
         return mod(mod(a)+mod(b));
     }
     
-    ll dp[105][105];
+//     ll dp[105][105];
     
-    ll func(ll n,ll l,ll K,ll N,ll L){
+//     ll func(ll n,ll l,ll K,ll N,ll L){
         
-        if(l==0){
+//         if(l==0){
             
-            if(n==0){
-                return 1;
-            }
+//             if(n==0){
+//                 return 1;
+//             }
             
-            else{
-                return 0;
-            }
+//             else{
+//                 return 0;
+//             }
             
-        }
+//         }
         
-        if(l<0 || n<0){
-            return 0;
-        }
+//         if(l<0 || n<0){
+//             return 0;
+//         }
         
-        if(dp[n][l]!=-1){
-            return dp[n][l];
-        }
+//         if(dp[n][l]!=-1){
+//             return dp[n][l];
+//         }
         
-        ll intro=mul(func(n-1,l-1,K,N,L),(ll)(N-n+1));
+//         ll intro=mul(func(n-1,l-1,K,N,L),(ll)(N-n+1));
         
-        ll again=mul(func(n,l-1,K,N,L),max(0LL,(ll)n-K));
+//         ll again=mul(func(n,l-1,K,N,L),max(0LL,(ll)n-K));
         
-        return dp[n][l] = add(intro,again);
+//         return dp[n][l] = add(intro,again);
         
-    }
+//     }
     
     int numMusicPlaylists(int N, int L, int K) {
         
-        memset(dp,-1,sizeof(dp));
+        vector<vector<ll>>dp(L+1,vector<ll>(N+1,0LL));
         
-        ll ans=func(N,L,K,N,L);
+        dp[0][0]=1LL;
         
-        return mod(ans);
+        for(ll i=1;i<=L;++i){
+            
+            for(ll j=1;j<=N;++j){
+                
+                ll intro=mul(dp[i-1][j-1],(ll)(N-j+1));
+                
+                ll again=mul(dp[i-1][j],max(0LL,j-K));
+                
+                dp[i][j]=add(intro,again);
+                
+            }
+            
+        }
+        
+        return mod(dp[L][N]);
         
     }
 };
