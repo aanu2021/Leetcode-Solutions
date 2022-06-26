87a841314a2fe2d4603855479d37cc72e3df2c26
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-26 19:20:24 +0530

    Time: 681 ms (33.33%), Space: 16.6 MB (33.33%) - LeetHub

--- 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
+++ 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
@@ -3,28 +3,19 @@ public:
     
     vector<vector<int>>graph;
     
-    int t=0;
-    
-    vector<int>tin;
-    
-    vector<int>tout;
-    
     vector<int>a;
-    
-    vector<int>xs;
     
     vector<vector<bool>>visited;
     
-    int N;
+    vector<int>xs;
     
+    int N;
     
     void dfs(int u,int p){
         
-        tin[u]=t++;
+        visited[u][u]=true;
         
         xs[u]=a[u];
-        
-        visited[u][u]=true;
         
         for(int v:graph[u]){
             
@@ -40,23 +31,8 @@ public:
                     
                 }
                 
-                // tout[u]=t++;
-                
             }
             
-            tout[u]=t++;
-            
-        }
-        
-    }
-    
-    
-    bool isAncestor(int u,int v){
-        
-        if(tin[u]<=tin[v] && tout[v]<=tout[u]){
-            return true;
-        }else{
-            return false;
         }
         
     }
@@ -68,36 +44,23 @@ public:
         N=n;
         
         graph.resize(n);
-        
-        tin.resize(n);
-        
-        tout.resize(n);
-        
+        a.resize(n);
         xs.resize(n);
         
-        a.resize(n);
-        
-        a=nums;
-        
-        visited=vector<vector<bool>>(n,vector<bool>(n,false));
+        for(int i=0;i<n;i++){
+            a[i]=nums[i];
+        }
         
         for(int i=0;i<edges.size();++i){
             graph[edges[i][0]].push_back(edges[i][1]);
             graph[edges[i][1]].push_back(edges[i][0]);
         }
         
-        
-        dfs(0,-1);
-        
-//         for(int i=0;i<n;i++){
-//             cout<<tin[i]<<" ";
-//         }cout<<endl;
-        
-//         for(int i=0;i<n;i++){
-//             cout<<tout[i]<<" ";
-//         }cout<<endl;
+        visited=vector<vector<bool>>(n,vector<bool>(n,false));
         
         int ans=INT_MAX;
+        
+        dfs(0,-1);
         
         for(int i=1;i<n;i++){
             
@@ -108,42 +71,26 @@ public:
                 if(visited[u][v]==true || visited[v][u]==true){
                     
                     if(visited[v][u]==true){
-                        swap(u,v);
+                        
+                        swap(v,u);
+                        
                     }
                     
                     int val1=xs[v];
                     int val2=xs[u]^xs[v];
                     int val3=xs[0]^xs[u];
                     
-                     ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
+                    ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
                     
                 }
-                
-//                 if(isAncestor(u,v) || isAncestor(v,u)){
-                    
-//                     if(isAncestor(v,u)){
-//                         swap(u,v);
-//                     }
-                    
-//                     int val1=xs[v];
-                    
-//                     int val2=xs[u]^xs[v];
-                    
-//                     int val3=xs[0]^xs[u];
-                    
-//                     ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
-                    
-//                 }
                 
                 else{
                     
                     int val1=xs[v];
-                    
                     int val2=xs[u];
-                    
                     int val3=xs[0]^xs[u]^xs[v];
                     
-                    ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
+                     ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
                     
                 }
                 
