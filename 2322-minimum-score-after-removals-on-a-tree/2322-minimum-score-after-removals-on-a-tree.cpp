55a30eb4deb4dbf19aaa5b3dedec361c437b07d9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-06-26 13:14:00 +0530

    Time: 480 ms (33.33%), Space: 16.6 MB (33.33%) - LeetHub

--- 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
+++ 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
@@ -13,12 +13,18 @@ public:
     
     vector<int>xs;
     
+    vector<vector<bool>>visited;
+    
+    int N;
+    
     
     void dfs(int u,int p){
         
         tin[u]=t++;
         
         xs[u]=a[u];
+        
+        visited[u][u]=true;
         
         for(int v:graph[u]){
             
@@ -27,6 +33,12 @@ public:
                 dfs(v,u);
                 
                 xs[u]^=xs[v];
+                
+                for(int j=0;j<N;j++){
+                    
+                    visited[u][j]=visited[u][j] || visited[v][j];
+                    
+                }
                 
                 // tout[u]=t++;
                 
@@ -53,6 +65,8 @@ public:
         
         int n=nums.size();
         
+        N=n;
+        
         graph.resize(n);
         
         tin.resize(n);
@@ -65,6 +79,7 @@ public:
         
         a=nums;
         
+        visited=vector<vector<bool>>(n,vector<bool>(n,false));
         
         for(int i=0;i<edges.size();++i){
             graph[edges[i][0]].push_back(edges[i][1]);
@@ -74,6 +89,14 @@ public:
         
         dfs(0,-1);
         
+//         for(int i=0;i<n;i++){
+//             cout<<tin[i]<<" ";
+//         }cout<<endl;
+        
+//         for(int i=0;i<n;i++){
+//             cout<<tout[i]<<" ";
+//         }cout<<endl;
+        
         int ans=INT_MAX;
         
         for(int i=1;i<n;i++){
@@ -82,21 +105,35 @@ public:
                 
                 int u=i,v=j;
                 
-                if(isAncestor(u,v) || isAncestor(v,u)){
+                if(visited[u][v]==true || visited[v][u]==true){
                     
-                    if(isAncestor(v,u)){
+                    if(visited[v][u]==true){
                         swap(u,v);
                     }
                     
                     int val1=xs[v];
-                    
                     int val2=xs[u]^xs[v];
-                    
                     int val3=xs[0]^xs[u];
                     
-                    ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
+                     ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
                     
                 }
+                
+//                 if(isAncestor(u,v) || isAncestor(v,u)){
+                    
+//                     if(isAncestor(v,u)){
+//                         swap(u,v);
+//                     }
+                    
+//                     int val1=xs[v];
+                    
+//                     int val2=xs[u]^xs[v];
+                    
+//                     int val3=xs[0]^xs[u];
+                    
+//                     ans=min(ans,max({val1,val2,val3})-min({val1,val2,val3}));
+                    
+//                 }
                 
                 else{
                     
