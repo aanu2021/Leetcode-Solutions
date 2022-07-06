6a30b9ea11dbeb1d80694c1132c10fbf1152f1f3
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-06 10:45:03 +0530

    Time: 4 ms (52.45%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n==0){
+            return 0;
+        }
+        
+        if(n<=2){
+            return 1;
+        }
+        
+        int dp[n+1];
+        
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        
+    }
+};
