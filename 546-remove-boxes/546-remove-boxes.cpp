5e6c83ca7fe1c809d1bcf2f0e92546bba396a2b2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-07 10:57:18 +0530

    Time: 137 ms (76.79%), Space: 12.4 MB (45.36%) - LeetHub

--- 546-remove-boxes/546-remove-boxes.cpp
+++ 546-remove-boxes/546-remove-boxes.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     
-    int dp[105][105][105];
+    int dp[102][102][102];
     
     vector<pair<int,int>>groups;
     
-    int sqr(int n){
-        return (n*n);
+    int sqr_func(int n){
+        
+        return n*n;
+        
     }
     
     int func(int i,int j,int extra){
         
         if(i>j){
+            
             return 0;
+            
         }
         
         if(dp[i][j][extra]!=-1){
+            
             return dp[i][j][extra];
+            
         }
         
-        int curr_grp_sz=groups[i].second+extra;
         
-        int ans=sqr(curr_grp_sz)+func(i+1,j,0);
+        int ans=sqr_func(groups[i].second+extra)+func(i+1,j,0);
         
-        for(int group=i+1;group<=j;group++){
+        for(int k=i+1;k<=j;k++){
             
-            if(groups[i].first==groups[group].first){
+            if(groups[i].first==groups[k].first){
                 
-                ans=max(ans,func(i+1,group-1,0)+func(group,j,extra+groups[i].second));
+                ans=max(ans,func(i+1,k-1,0) + func(k,j,extra+groups[i].second));
                 
             }
             
@@ -41,41 +46,43 @@ public:
         
         int n=boxes.size();
         
-        for(int i=0;i<=n+1;++i){
-            for(int j=0;j<=n+1;++j){
-                for(int k=0;k<=n+1;++k){
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                for(int k=0;k<=n;k++){
                     dp[i][j][k]=-1;
                 }
             }
         }
         
-        int cnt=0;
-        
-        int prev=boxes[0];
+        int prev=boxes[0],cnt=0;
         
         for(int i=0;i<n;++i){
             
             if(boxes[i]==prev){
+                
                 cnt++;
+                
             }
             
             else{
+                
                 groups.push_back({prev,cnt});
-                cnt=1;
+                
                 prev=boxes[i];
+                
+                cnt=1;
+                
             }
             
         }
         
-        if(cnt!=0){
+        if(cnt>0){
+            
             groups.push_back({prev,cnt});
+            
         }
         
         n=groups.size();
-        
-        // for(int i=0;i<n;++i){
-        //     cout<<groups[i].first<<" "<<groups[i].second<<endl;
-        // }cout<<"\n";
         
         int ans=func(0,n-1,0);
         
