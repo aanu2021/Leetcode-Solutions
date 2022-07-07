8f17a343673ce7b08ad66f06a1cfcc961d237afa
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-07 12:33:18 +0530

    Time: 193 ms (55.62%), Space: 55.1 MB (73.27%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,49 +1,27 @@
 class Solution {
 public:
-    
-    int dp[50005][2];
-    
-    int func(vector<int>&prices,int i,int n,int fee,bool bought){
-        
-        if(i>=n){
-            return 0;
-        }
-        
-        if(dp[i][bought]!=-1){
-            return dp[i][bought];
-        }
-        
-        int ans=func(prices,i+1,n,fee,bought);
-        
-        if(bought){
-            
-            ans=max(ans,-prices[i] + func(prices,i+1,n,fee,false));
-            
-        }
-        
-        else{
-            
-            ans=max(ans,prices[i]-fee+func(prices,i+1,n,fee,true));
-            
-        }
-        
-        return dp[i][bought]=ans;
-        
-    }
-    
     int maxProfit(vector<int>& prices, int fee) {
         
         int n=prices.size();
         
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<2;j++){
-                dp[i][j]=-1;
-            }
+        int oldbuy=-prices[0];
+        int oldsold=0;
+        
+        for(int i=1;i<n;++i){
+            
+            int newbuy,newsold;
+            
+            newbuy=max(oldsold-prices[i],oldbuy);
+            
+            newsold=max(oldsold,oldbuy+prices[i]-fee);
+            
+            oldbuy=newbuy;
+            
+            oldsold=newsold;
+            
         }
         
-        int ans=func(prices,0,n,fee,true);
-        
-        return ans;
+        return max(oldbuy,oldsold);
         
     }
 };
