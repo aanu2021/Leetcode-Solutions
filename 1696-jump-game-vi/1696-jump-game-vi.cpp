a52a948d02346db2ec6c9bc674c74053f39f6b05
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-09 10:19:41 +0530

    Time: 350 ms (41.18%), Space: 93.8 MB (40.42%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int maxResult(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        
+        vector<int>dp(n);
+        
+        dp[n-1]=nums[n-1];
+        
+        priority_queue<pair<int,int>>pq;
+        
+        pq.push({dp[n-1],n-1});
+        
+        for(int i=n-2;i>=0;i--){
+            
+            while(!pq.empty() && pq.top().second>i+k){
+                pq.pop();
+            }
+            
+            dp[i]=pq.top().first+nums[i];
+            
+            pq.push({dp[i],i});
+            
+        }
+        
+        return dp[0];
+        
+    }
+};
