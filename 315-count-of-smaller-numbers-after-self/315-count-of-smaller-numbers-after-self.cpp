d10b6201a0b1271470a7b6f1def8ffcc55bf7232
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-13 00:46:46 +0530

    Time: 327 ms (83.84%), Space: 71.6 MB (97.76%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,121 @@
+class Solution {
+public:
+    
+    int seg[400005];
+    
+    void BuildSeg(int idx,int low,int high){
+        
+        if(low>high){
+            return;
+        }
+        
+        if(low==high){
+            seg[idx]=0;
+            return;
+        }
+        
+        else{
+            
+            int mid=(low+high)/2;
+            
+            BuildSeg(2*idx+1,low,mid);
+            
+            BuildSeg(2*idx+2,mid+1,high);
+            
+        }
+        
+    }
+    
+    void Update(int idx,int low,int high,int pos){
+        
+        if(low>high){
+            return;
+        }
+        
+        if(low==high){
+            seg[idx]++;
+            return;
+        }
+        
+        else{
+            
+            int mid=(low+high)/2;
+            
+            if(pos<=mid){
+                
+                Update(2*idx+1,low,mid,pos);
+                
+            }
+            
+            else{
+                
+                Update(2*idx+2,mid+1,high,pos);
+                
+            }
+            
+            seg[idx]=seg[2*idx+1]+seg[2*idx+2];
+            
+        }
+        
+    }
+    
+    int query(int idx,int low,int high,int l,int r){
+        
+        if(low>high || low>r || high<l){
+            return 0;
+        }
+        
+        if(low==high){
+            return seg[idx];
+        }
+        
+        if(low>=l && high<=r){
+            return seg[idx];
+        }
+        
+        else{
+            
+            int mid=(low+high)/2;
+            
+            int left=query(2*idx+1,low,mid,l,r);
+            
+            int right=query(2*idx+2,mid+1,high,l,r);
+            
+            return left+right;
+            
+        }
+        
+    }
+    
+    vector<int> countSmaller(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        int mini=*min_element(nums.begin(),nums.end());
+        
+        for(int i=0;i<n;i++){
+            
+            nums[i]-=mini;
+            
+        }
+        
+        int maxi=*max_element(nums.begin(),nums.end());
+        
+        int m=maxi+1;
+        
+        BuildSeg(0,0,maxi);
+        
+        vector<int>ans(n,0);
+        
+        for(int i=n-1;i>=0;i--){
+            
+            ans[i]=query(0,0,m,0,nums[i]-1);
+            
+            Update(0,0,m,nums[i]);
+            
+        }
+        
+        return ans;
+        
+    }
+};
