136ba1acb57867f5eabc701cbfa1bbadc84445cd
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-13 00:28:55 +0530

    Time: 567 ms (68.05%), Space: 45 MB (95.55%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -3,40 +3,51 @@ public:
     
     int merge(vector<int>&nums,int low,int mid,int high){
         
-        int cnt=0;
+        int ans=0;
         
-        for(int i=low,j=mid+1;i<=mid;i++){
+        int j=mid+1;
+        
+        for(int i=low;i<=mid;i++){
             
-            while(j<=high && nums[i]/2.0>nums[j]){
+            while(j<=high && (nums[i]/2.0)>nums[j]){
+                
                 j++;
+                
             }
             
-            cnt+=(j-mid-1);
+            ans+=(j-mid-1);
             
         }
         
         sort(nums.begin()+low,nums.begin()+high+1);
         
-        return cnt;
+//         for(int i=low;i<=high;++i){
+//             cout<<nums[i]<<" ";
+//         }cout<<"\n";
+        
+//         cout<<ans<<"\n";
+        
+        return ans;
         
     }
     
     int mergesort(vector<int>&nums,int low,int high){
         
-        int cnt=0;
+        int ans=0;
         
         if(low<high){
             
             int mid=(low+high)/2;
             
-            cnt+=mergesort(nums,low,mid);
-            cnt+=mergesort(nums,mid+1,high);
+            ans+=mergesort(nums,low,mid);
             
-            cnt+=merge(nums,low,mid,high);
+            ans+=mergesort(nums,mid+1,high);
+            
+            ans+=merge(nums,low,mid,high);
             
         }
         
-        return cnt;
+        return ans;
         
     }
     
