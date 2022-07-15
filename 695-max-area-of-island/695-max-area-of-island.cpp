481b21bc59a9dbaa8a225137dc1e67a77f531d23
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-15 10:43:54 +0530

    Time: 47 ms (16.26%), Space: 27.1 MB (11.23%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     
-    int currarea=0;
     
     void dfs(vector<vector<int>>&grid,int i,int j){
         
@@ -9,7 +8,7 @@ public:
             return;
         }
         
-        currarea++;
+       // currarea++;
         
         grid[i][j]=0;
         
@@ -25,17 +24,55 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        
         int maxarea=0;
+        
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
         
         for(int i=0;i<m;i++){
             
             for(int j=0;j<n;j++){
                 
-                if(grid[i][j]==1){
+                if(grid[i][j]==1 && visited[i][j]==false){
                     
-                    currarea=0;
+                    int currarea=0;
                     
-                    dfs(grid,i,j);
+                    stack<pair<int,int>>st;
+                    
+                    st.push({i,j});
+                    
+                    while(!st.empty()){
+                        
+                        auto curr=st.top();
+                        st.pop();
+                        
+                        int x=curr.first;
+                        int y=curr.second;
+                        
+                        if(visited[x][y]==true){
+                            continue;
+                        }
+                        
+                        visited[x][y]=true;
+                        
+                        currarea++;
+                        
+                        for(int dir=0;dir<4;dir++){
+                            
+                            int nx=x+dx[dir];
+                            int ny=y+dy[dir];
+                            
+                            if(nx>=0 && ny>=0 && nx<m && ny<n && visited[nx][ny]==false && grid[nx][ny]==1){
+                                
+                                st.push({nx,ny});
+                                
+                            }
+                            
+                        }
+                        
+                    }
                     
                     maxarea=max(maxarea,currarea);
                     
