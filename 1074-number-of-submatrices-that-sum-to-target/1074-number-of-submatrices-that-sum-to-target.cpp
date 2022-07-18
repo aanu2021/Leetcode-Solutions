af988d7efb8c1087ec9a7befe04c6d47de4400a0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-18 10:32:58 +0530

    Time: 1660 ms (17.53%), Space: 274.3 MB (18.69%) - LeetHub

---
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& grid, int target) {
+        
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        for(int i=0;i<m;i++){
+            for(int j=1;j<n;j++){
+                grid[i][j]+=grid[i][j-1];
+            }
+        }
+        
+        int ans=0;
+        
+        for(int c1=0;c1<n;c1++){
+            
+            for(int c2=c1;c2<n;c2++){
+                
+                int sum=0;
+                map<int,int>mp;
+                mp[0]=1;
+                
+                for(int r=0;r<m;r++){
+                    
+                    sum+=grid[r][c2]-(c1-1>=0 ? grid[r][c1-1] : 0);
+                    
+                    ans+=mp[sum-target];
+                    
+                    mp[sum]++;
+                    
+                }
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+};
