a76ca2e3b2c92dcc44d5d9487486be8204cd3ab1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-18 20:40:59 +0530

    Time: 551 ms (14.31%), Space: 109.2 MB (56.65%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -9,25 +9,29 @@ public:
         
         vector<ll>P(n,0LL);
         
+        P[0]=(ll)nums[0];
+        
         ll ans=n+1;
+        
+        for(ll i=1;i<n;i++){
+            
+            P[i]=P[i-1]+(ll)nums[i];
+            
+        }
         
         for(ll i=0;i<n;i++){
             
-            P[i]=(i-1>=0 ? P[i-1] : 0LL)+(ll)nums[i];
-            
             if(P[i]>=k){
-                
                 ans=min(ans,i+1);
-                
             }
             
         }
         
         deque<ll>q;
         
-        for(ll i=0;i<n;++i){
+        for(ll i=0;i<n;i++){
             
-            while(!q.empty() && P[i]<=P[q.back()]){
+            while(!q.empty() && P[q.back()]>=P[i]){
                 
                 q.pop_back();
                 
