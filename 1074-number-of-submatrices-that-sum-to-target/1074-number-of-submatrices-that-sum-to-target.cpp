508a99231bc86be554d551bb8b4755c43ca31ca6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-18 10:45:16 +0530

    Time: 826 ms (84.05%), Space: 164.7 MB (44.18%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -6,9 +6,13 @@ public:
         int n=grid[0].size();
         
         for(int i=0;i<m;i++){
+            
             for(int j=1;j<n;j++){
+                
                 grid[i][j]+=grid[i][j-1];
+                
             }
+            
         }
         
         int ans=0;
@@ -17,13 +21,15 @@ public:
             
             for(int c2=c1;c2<n;c2++){
                 
-                int sum=0;
                 map<int,int>mp;
+                
                 mp[0]=1;
                 
-                for(int r=0;r<m;r++){
+                int sum=0;
+                
+                for(int row=0;row<m;row++){
                     
-                    sum+=grid[r][c2]-(c1-1>=0 ? grid[r][c1-1] : 0);
+                    sum+=grid[row][c2]-(c1-1>=0 ? grid[row][c1-1] : 0);
                     
                     if(mp.find(sum-target)!=mp.end()){
                         
