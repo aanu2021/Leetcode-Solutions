574a5e2b53941ee388ecc30ee5ac393e1c6a9fea
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-23 19:00:34 +0530

    Time: 479 ms (56.04%), Space: 20.5 MB (65.93%) - LeetHub

---
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maximumANDSum(vector<int>& nums, int numSlots) {
+        
+        int limit=2*numSlots;
+        
+        while(nums.size()<limit){
+            nums.push_back(0);
+        }
+        
+        int n=nums.size();
+        
+        // for(int i=0;i<n;i++){
+        //     cout<<nums[i]<<" ";
+        // }cout<<"\n";
+        
+        vector<int>dp((1<<n),0);
+        
+        dp[0]=0;
+        
+        for(int mask=1;mask<(1<<n);mask++){
+            
+            int cnt=__builtin_popcount(mask);
+            
+            int slot=(cnt+1)/2;
+            
+            for(int i=0;i<n;++i){
+                
+                if((mask&(1<<i))!=0){
+                    
+        dp[mask]=max(dp[mask],dp[mask^(1<<i)]+(nums[i]&slot));
+                    
+                }
+                
+            }
+            
+        }
+        
+        return dp[(1<<n)-1];
+        
+    }
+};
