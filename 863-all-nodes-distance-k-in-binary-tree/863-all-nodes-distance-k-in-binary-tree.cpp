879bbb9f9ca8770a87f9a719f6bac0fbd6516b4c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-26 21:13:40 +0530

    Time: 10 ms (55.24%), Space: 12.8 MB (68.05%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    
+    map<TreeNode*,TreeNode*>parent;
+    
+    map<TreeNode*,bool>visited;
+    
+    void dfs(TreeNode*&root){
+        
+        if(root==NULL){
+            return;
+        }
+        
+        if(root->left!=NULL){
+            
+            parent[root->left]=root;
+            
+        }
+        
+        if(root->right!=NULL){
+            
+            parent[root->right]=root;
+            
+        }
+        
+        dfs(root->left);
+        
+        dfs(root->right);
+        
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+        vector<int>ans;
+        
+        int level=0;
+        
+        dfs(root);
+        
+        queue<TreeNode*>q;
+        
+        q.push(target);
+        
+        while(!q.empty()){
+            
+            int sz=q.size();
+            
+            while(sz--){
+                
+                auto curr=q.front();
+                q.pop();
+                
+                if(visited[curr]==true){
+                    
+                    continue;
+                    
+                }
+                
+                visited[curr] = true;
+                
+                if(level==k){
+                    
+                    ans.push_back(curr->val);
+                    continue;
+                    
+                }
+                
+                if(parent.find(curr)!=parent.end()){
+                    
+           //  cout<<curr->val<<" "<<parent[curr]->val<<"\n";
+                    
+                    q.push(parent[curr]);
+                    
+                }
+                
+                if(curr->left!=NULL){
+                    
+                    q.push(curr->left);
+                    
+                }
+                
+                if(curr->right!=NULL){
+                    
+                    q.push(curr->right);
+                    
+                }
+                
+            }
+            
+            level++;
+            
+        }
+        
+        return ans;
+        
+    }
+};
