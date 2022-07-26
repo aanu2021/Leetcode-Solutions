d54b62f79604dc5e67831a3c85cb5724552dc9e6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-26 19:50:15 +0530

    Time: 38 ms (13.78%), Space: 15.6 MB (15.26%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     
-    
-    bool func(TreeNode*root,TreeNode* target, vector<TreeNode*>&path){
+    bool func(TreeNode*&root,TreeNode*&target,vector<TreeNode*>&path){
         
         if(root==NULL){
+            
             return false;
+            
         }
         
         if(root==target){
+            
             path.push_back(root);
+            
             return true;
+            
         }
         
-        
-        if(func(root->left,target,path)){
+        if(func(root->left,target,path)==true){
             
             path.push_back(root);
             
         }
         
-        else if(func(root->right,target,path)){
+        else if(func(root->right,target,path)==true){
             
             path.push_back(root);
             
         }
         
-        
-        if(!path.empty()){
-            return true;
-        }else{
-            return false;
-        }
+        return !path.empty();
         
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(root==NULL){
+            return NULL;
+        }
         
         vector<TreeNode*>path1;
         
@@ -45,37 +47,41 @@ public:
         
         func(root,q,path2);
         
-        reverse(path1.begin(),path1.end());
-        
-        reverse(path2.begin(),path2.end());
-        
-        int sz1=path1.size();
-        
-        int sz2=path2.size();
-        
-        int i=0;
-        
-        for(i=0;i<min(sz1,sz2);++i){
-            
-            if(path1[i]!=path2[i]){
-                
-                break;
-                
-            }
-            
-        }
-        
-      //  cout<< path1[i-1] <<endl;
-        
 //         for(int i=0;i<path1.size();++i){
-//             cout<<path1[i]<<" ";
+            
+//             cout<<path1[i]->val<<" ";
+            
 //         }cout<<"\n";
         
 //         for(int i=0;i<path2.size();++i){
-//             cout<<path2[i]<<" ";
+            
+//             cout<<path2[i]->val<<" ";
+            
 //         }cout<<"\n";
         
-        return path1[i-1];
+           reverse(path1.begin(),path1.end());
         
+           reverse(path2.begin(),path2.end());
+        
+           int sz1=path1.size();
+        
+           int sz2=path2.size();
+        
+           int sz=min(sz1,sz2);
+        
+           int i=0;
+         
+           for(i=0;i<sz;++i){
+               
+               if(path1[i]!=path2[i]){
+                   
+                   break;
+                   
+               }
+               
+           }
+        
+           return path1[i-1]; 
+       
     }
 };
