ece711e1514419e63ff3adf750b33a2e8613bd6c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-07-30 00:24:09 +0530

    Time: 157 ms (20.68%), Space: 11.5 MB (49.52%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -7,25 +7,38 @@ public:
         
         vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
         
+        dp[0][0]=true;
+        
         s="*"+s;
         p="*"+p;
         
-        dp[0][0]=true;
         
         for(int i=1;i<=n;i++){
+         
             if(p[i]=='*'){
                 dp[0][i]=dp[0][i-1];
             }
+            else{
+                dp[0][i]=false;
+            }
+            
         }
         
-        for(int i=1;i<=m;i++){
+        
+        for(int i=1;i<=m;++i){
             
-            for(int j=1;j<=n;j++){
+            for(int j=1;j<=n;++j){
                 
                 if(s[i]==p[j] || p[j]=='?'){
+                    
                     dp[i][j]=dp[i-1][j-1];
-                }else if(p[j]=='*'){
+                    
+                }
+                
+                else if(p[j]=='*'){
+                    
                     dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                    
                 }
                 
             }
