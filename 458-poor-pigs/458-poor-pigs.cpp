c4fd3a82abfe8d03d759bc1df0e761cc2a0a313d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-06 10:17:16 +0530

    Time: 4 ms (25.82%), Space: 6 MB (58.91%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+         int T = minutesToTest/minutesToDie;   //Trial count          
+        return (int)ceil(log(buckets)/log(T+1));
+        
+    }
+};
