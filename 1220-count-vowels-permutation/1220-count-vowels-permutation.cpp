ffa82f9f0fc2bd8c951eac2dac0bac5180ab927a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-07 10:16:17 +0530

    Time: 189 ms (24.45%), Space: 27.4 MB (24.61%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,3 +1,36 @@
+ /*
+        
+        dp[i][0] --> stores count of i-length string , ends with character 'a'        
+        
+         dp[i][1] --> stores count of i-length string , ends with character 'e'
+        
+         dp[i][2] --> stores count of i-length string , ends with character 'i'
+        
+         dp[i][3] --> stores count of i-length string , ends with character 'o'
+        
+         dp[i][4] --> stores count of i-length string , ends with character 'u'
+        
+        
+         Required Recurrences for solving the problem ......
+        
+        
+        According to the question Each character 'a' may only be followed by 'e' .
+        
+        That means if character at i-th index be 'e' , then its one of the previous characters might be 'a'
+        
+        'a' --next--> 'e'
+        'a' <--prev-- 'e'
+        
+        Now , the recurrence boils down to ---->
+        
+        for the character at i-th index , try to figure out all the possible (i-1)-th indexed characters.
+        
+        e.g : dp[i]['e'] = dp[i-1]['a'] + dp[i-1]['i']
+        
+        
+        */
+
+
 class Solution {
 public:
     
@@ -6,7 +39,7 @@ public:
     const ll M = 1e9 + 7;
     
     ll mod(ll a){
-       return ((a%M)+M)%M;
+        return ((a%M)+M)%M;
     }
     
     ll mul(ll a,ll b){
@@ -19,11 +52,9 @@ public:
     
     int countVowelPermutation(int n) {
         
-        if(n==1){
-            return 5;
-        }
-        
         vector<vector<ll>>dp(n+1,vector<ll>(5,0LL));
+       
+        // Base Case :
         
         dp[1][0]=1LL;
         dp[1][1]=1LL;
@@ -31,52 +62,43 @@ public:
         dp[1][3]=1LL;
         dp[1][4]=1LL;
         
-        for(ll i=2;i<=n;i++){
+        // Transitions for recurrence :
+        
+        for(ll i=2;i<=n;++i){
             
-            ll curr=0LL;
+            // For character 'a' 
             
-            curr=add(curr,dp[i-1][1]);
-            curr=add(curr,dp[i-1][2]);
-            curr=add(curr,dp[i-1][4]);
+            dp[i][0] = add(add(dp[i-1][1],dp[i-1][2]),dp[i-1][4]);
             
-            dp[i][0]=curr;
+            // For character 'e'
             
-            curr=0LL;
+            dp[i][1] = add(dp[i-1][0],dp[i-1][2]);
             
-            curr=add(curr,dp[i-1][0]);
-            curr=add(curr,dp[i-1][2]);
+            // For character 'i'
             
-            dp[i][1]=curr;
+            dp[i][2] = add(dp[i-1][1],dp[i-1][3]);
             
-            curr=0LL;
+            // For character 'o'
             
-            curr=add(curr,dp[i-1][1]);
-            curr=add(curr,dp[i-1][3]);
+            dp[i][3] = dp[i-1][2];
             
-            dp[i][2]=curr;
+            // For character 'u'
             
-            curr=0LL;
-            
-            curr=add(curr,dp[i-1][2]);
-            
-            dp[i][3]=curr;
-            
-            curr=0LL;
-            
-            curr=add(curr,dp[i-1][2]);
-            curr=add(curr,dp[i-1][3]);
-            
-            dp[i][4]=curr;
+            dp[i][4] = add(dp[i-1][2],dp[i-1][3]);
             
         }
         
-        ll ans=0LL;
+        ll total = 0LL;
         
-        for(ll i=0;i<5;i++){
-            ans=add(ans,dp[n][i]);
-        }
+        for(ll i=0;i<5;++i){
+            
+            total=add(total,dp[n][i]);
+            
+          //  cout<<dp[n][i]<<" ";
+            
+        }cout<<"\n";
         
-        return ans;
+        return total;
         
     }
 };
