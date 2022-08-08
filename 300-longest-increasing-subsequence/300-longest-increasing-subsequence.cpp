9dba249e055191e8924c25af1b2c310faffc15ed
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-08 10:24:22 +0530

    Time: 358 ms (58.32%), Space: 10.7 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,59 +1,32 @@
 class Solution {
 public:
-    
-    int dp[2502];
-    
-    int func(vector<int>&nums,int i,int n){
-        
-        if(i>=n){
-            return 0;
-        }
-        
-        if(dp[i]!=-1){
-            
-            return dp[i];
-            
-        }
-        
-        int maxLen = 1;
-        
-        for(int j=i+1;j<n;++j){
-            
-            if(nums[i]<nums[j]){
-                
-                maxLen=max(maxLen , 1 + func(nums,j,n) );
-                
-            }
-            
-        }
-        
-        return dp[i]=maxLen;
-        
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         
         int n=nums.size();
         
-        for(int i=0;i<=n;++i){
-            dp[i]=-1;
+        vector<int>dp(n,1);
+        
+        for(int i=1;i<n;++i){
+            
+            for(int j=0;j<i;++j){
+                
+                if(nums[j]<nums[i]){
+                    
+                    dp[i]=max(dp[i],1+dp[j]);
+                    
+                }
+                
+            }
+            
         }
         
         int maxLen=0;
         
         for(int i=0;i<n;++i){
             
-            if(dp[i]==-1){
-                
-                maxLen=max(maxLen,func(nums,i,n));
-                
-            }
+            maxLen=max(maxLen,dp[i]);
             
         }
-        
-        // for(int i=0;i<n;++i){
-        //     cout<<dp[i]<<" ";
-        // }cout<<"\n";
         
         return maxLen;
         
