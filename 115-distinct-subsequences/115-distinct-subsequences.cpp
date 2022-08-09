13b0357725bd4c73d7f4da00c118bddce05f0432
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-09 11:04:53 +0530

    Time: 85 ms (23.80%), Space: 30.2 MB (7.03%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,81 +3,50 @@ public:
     
     /*
     
+    Tabulation DP
     
-    s[0...i]
-    t[0...j]
-    
-    dp[i][j] --> number of distinct subsequences present in s[0...i] in the form of t[0...j]
-    
-    dp[i][j] --> 
-    
-    Case 1. s[i]==t[j]
-    
-    (i) We can pick the i-th character and find the remaining t[0...j-1] inside s[0...i-1]
-    
-    (ii) We leave the i-th character and find the t[0...j] substring inside s[0...i-1] 
-    
-    
-    Case 2. s[i]!=t[j]
-    
-    We leave the i-th character and find the t[0...j] substring inside s[0...i-1] 
-    
-    
-    Case 1 : dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
-    
-    Case 2 : dp[i][j] = dp[i-1][j]
-    
+    Time Complexity  : O(M*N)
+    Space Complexity : O(M*N)
     
     */
     
-    
-    int dp[1002][1002];
-    
-    int func(string &s,string &t,int i,int j){
-        
-        if(i<0 && j<0){
-            return 1;
-        }
-        
-        if(i>=0 && j<0){
-            return 1;
-        }
-        
-        if(i<0){
-            return 0;
-        }
-        
-        if(dp[i][j]!=-1){
-            
-            return dp[i][j];
-            
-        }
-        
-        
-        if(s[i]==t[j]){
-            
-           return dp[i][j]=func(s,t,i-1,j-1)+func(s,t,i-1,j);
-            
-        }
-        
-        else{
-            
-           return dp[i][j]=func(s,t,i-1,j);   
-            
-        }
-        
-    }
+    typedef unsigned long long ll;
     
     int numDistinct(string s, string t) {
         
         int m=s.length();
         int n=t.length();
         
-        memset(dp,-1,sizeof(dp));
+        vector<vector<ll>>dp(m+1,vector<ll>(n+1,0LL));
         
-        int ans=func(s,t,m-1,n-1);
+        s="*"+s;
+        t="*"+t;
         
-        return ans;
+        for(ll i=0;i<=m;++i){
+            dp[i][0]=1LL;
+        }
+        
+        for(ll i=1;i<=m;++i){
+            
+            for(ll j=1;j<=n;++j){
+                
+                if(s[i]==t[j]){
+                    
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    
+                }
+                
+                else{
+                    
+                    dp[i][j]=dp[i-1][j];
+                    
+                }
+                
+            }
+            
+        }
+        
+        return dp[m][n];
         
     }
 };
