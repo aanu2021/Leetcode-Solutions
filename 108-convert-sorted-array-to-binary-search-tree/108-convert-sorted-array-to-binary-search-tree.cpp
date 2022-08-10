861f382a2f6623b3f3ef2e589357e6e0afaac28e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-10 10:59:01 +0530

    Time: 16 ms (81.21%), Space: 20.6 MB (97.86%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,28 +1,80 @@
+
 class Solution {
 public:
     
-    TreeNode*BuildBST(vector<int>&nums,int low,int high){
+    struct Node{
+      
+        int low;
+        int high;
+        TreeNode*node;
         
-        if(low>high){
-            return NULL;
+        Node(int l,int h,TreeNode*n){
+            
+            low=l;
+            high=h;
+            node=n;
+            
         }
         
-        int mid=(low+high)/2;
+    };
+    
+    int getMid(int low,int high){
         
-        TreeNode*root=new TreeNode(nums[mid]);
-        
-        root->left =BuildBST(nums,low,mid-1);
-        root->right=BuildBST(nums,mid+1,high);
-        
-        return root;
+        return ((low+high)/2);
         
     }
     
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         
-      int n=nums.size();
+        int n=nums.size();
         
-        TreeNode*root=BuildBST(nums,0,n-1);
+        if(n==0){
+            return NULL;
+        }
+        
+        
+        int midIndex=getMid(0,n-1);
+        
+        TreeNode*root=new TreeNode(nums[midIndex]);
+        
+        
+        stack<Node>S;
+        
+        S.push({0,n-1,root});
+        
+        
+        while(!S.empty()){
+            
+            auto curr_entity=S.top();
+            S.pop();
+            
+            int low       = curr_entity.low;
+            int high      = curr_entity.high;
+            TreeNode*node = curr_entity.node;
+            
+            midIndex=getMid(low,high);
+            
+            if(midIndex-1>=low){
+                
+            int midIndex_for_left_sub=getMid(low,midIndex-1);
+                
+        node->left=new TreeNode(nums[midIndex_for_left_sub]);
+                
+                S.push({low,midIndex-1,node->left});
+                
+            }
+            
+            if(midIndex+1<=high){
+                
+                int midIndex_for_right_sub=getMid(midIndex+1,high);
+                
+        node->right=new TreeNode(nums[midIndex_for_right_sub]);
+                
+                S.push({midIndex+1,high,node->right});
+                
+            }
+            
+        }
         
         return root;
         
