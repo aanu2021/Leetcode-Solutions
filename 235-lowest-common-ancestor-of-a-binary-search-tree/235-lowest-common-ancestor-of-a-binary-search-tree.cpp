af3f2f48ed68cc36628a0994244047d1ec73af91
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-12 11:56:39 +0530

    Time: 52 ms (37.58%), Space: 23.3 MB (12.34%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -3,83 +3,32 @@ public:
     
     /*
     
-    But in the above mentioned approach , we are not able to use
-    thr super cool properties of BST not even a single time ,
-    can we use that to reduce time and space complexities......
-    
-    Definitely yes , look for a valid BST , every node of that
-    BST should maintain a property ,
-    
-       a. node->val > node->left->val (all nodes in left sub)
-       b. node->val < node->right->val (all nodes in right sub)
-       
-       
-        Now in these question , there are given that all the
-        node values are unique , and node p and q should exist 
-        in the BST, that clearly implies , if we are at node (u)
-        , and never ever explore (p) and (q) beforehand , then 
-        there are three possibilities of exploring them 
-        afterwards ------>
-        
-        
-        Case 1 --> (p->val < node->val) && (q->val < node->val)
-        
-        Case 2 --> (p->val > node->val) && (q->val > node->val)
-        
-        Case 3 --> All the other cases
-        
-        
-        Now if we observe carefully , we can figure out that
-        
-        For case1 , node p and q should lie in the left
-        subtree of the current node , that is why the ancestor 
-        might be current root node or some node in its left sub.
-        
-        For case2 , node p and q should lie in the right
-        subtree of the current node , that is why the ancestor
-    might be current root node or some node in its right sub.
-    
-        For case3 , ancestor would ne definitely the current
-        root node.
-    
-    */
-    
-    
-    /*
-    
-    Time Complexity : O(H) --> where H is the height of the BST
-    
-        Average Case : O(logN) [Balanced BST]
-        Worst Case   : O(N)    [Skewed BST]
-    
-    Space Complexity : Recursion Stack Space (more or less O(H))
-    
-       Average Case : O(logN)  [Balanced BST]
-       Worst Case   : O(N)     [Skewed BST]
+     Though it is not an optimization , but we can come up with
+     an iterative version. of these problem. 
     
     */
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        // Handling Case1
+        TreeNode*curr=root;
         
-        if((root->val>p->val) && (root->val>q->val)){
+        while(curr){
             
-            return lowestCommonAncestor(root->left,p,q);
+            if((curr->val>p->val) && (curr->val>q->val)){
+                curr=curr->left;
+            }
+            
+            else if((curr->val<p->val) && (curr->val<q->val)){
+                curr=curr->right;
+            }
+            
+            else{
+                break;
+            }
             
         }
         
-        // Handling Case2
-        
-        if((root->val<p->val) && (root->val<q->val)){
-            
-            return lowestCommonAncestor(root->right,p,q);
-            
-        }
-        
-        // Handling Other Cases
-        
-        return root;
+        return curr;
         
     }
 };
