3e67e45d1b4f894050974c87923da9f090a1293e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-13 11:14:28 +0530

    Time: 55 ms (89.36%), Space: 17 MB (85.52%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    
+    int n,k,wordLen,substringSize;
+    
+    vector<int>ans;
+    
+    map<string,int>wordCount;
+    
+    void func(string &s,int left){
+        
+        map<string,int>wordFound;
+        
+        int total=0;
+        
+        bool excess=false;
+        
+        
+        for(int right=left;right<=n-wordLen;right+=wordLen){
+            
+            string sub=s.substr(right,wordLen);
+            
+            if(wordCount.find(sub)==wordCount.end()){
+                
+                total=0;
+                excess=false;
+                wordFound.clear();
+                left=right+wordLen;
+                
+            }
+            
+            else{
+                
+                while((right-left==substringSize) || excess==true){
+                    
+                    string left_word=s.substr(left,wordLen);
+                    
+                    left+=wordLen;
+                    
+                    wordFound[left_word]--;
+                    
+                          if(wordFound[left_word]==wordCount[left_word]){
+                        
+                       excess=false; 
+                        
+                    }
+                    
+                    else{
+                        
+                        total--;
+                        
+                    }
+                    
+                }
+                
+                wordFound[sub]++;
+                
+                if(wordFound[sub]<=wordCount[sub]){
+                    total++;
+                }else{
+                    excess=true;
+                }
+                
+                if(total==k && excess==false){
+                    
+                    ans.push_back(left);
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+   vector<int> findSubstring(string s, vector<string>& words) {
+   
+      n=s.length();
+      k=words.size();
+      wordLen=words[0].length();
+      substringSize=(wordLen*k);
+       
+      for(string str:words){
+          wordCount[str]++;
+      } 
+       
+      for(int i=0;i<wordLen;++i){
+          
+          func(s,i);
+          
+      }  
+       
+       
+      return ans; 
+      
+    }
+};
