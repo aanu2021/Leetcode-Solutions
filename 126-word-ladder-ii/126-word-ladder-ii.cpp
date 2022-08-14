61cf9640c80c1c0d8c9d592c318acbe078bd3515
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-14 19:28:52 +0530

    Time: 29 ms (53.66%), Space: 9.6 MB (70.31%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    // These utility helps us to determine whether two different words can be the neighbour of each other or not.
+    // Check whether two strings are neighbour to each other , by checking only the different characters at each index.
     
     bool isNeighbour(string &s1,string &s2){
         
@@ -10,11 +10,9 @@ public:
         int diff=0;
         
         for(int i=0;i<n;++i){
-            
             if(s1[i]!=s2[i]){
                 diff++;
             }
-            
         }
         
         return diff==1;
@@ -22,19 +20,28 @@ public:
     }
     
     
+    
+    
     void BFS(int src,vector<vector<int>>&graph,vector<vector<int>>&parent,int n){
+        
+        // dist[i] --> shortest distance of node i from node src
         
         vector<int>dist(n,1e9);
         
         dist[src]=0;
         
+        /* Base Case --> for the src node , there is no node 
+        possible which can be parent of it , therefore just assign
+        an offset value as its parent node.*/
+        
+        parent[src].push_back(-1);
+        
+        
+        // Queue DS will surely the best to do a BFS traversal
+        
         queue<int>q;
         
         q.push(src);
-        
-        // By default there is no parent of our source , as it is our starting point.
-        
-        parent[src]={-1};
         
         
         while(!q.empty()){
@@ -42,21 +49,20 @@ public:
             int node=q.front();
             q.pop();
             
+            // Explore all its potential neighbouring nodes.
+            
             for(int nbr:graph[node]){
                 
-                if(dist[nbr]>dist[node] + 1){
+                if(dist[nbr] > dist[node] + 1){
                     
+                    dist[nbr] = dist[node] + 1;
                     q.push(nbr);
-                    
-                    dist[nbr]=dist[node] + 1;
-                    
                     parent[nbr].clear();
-                    
                     parent[nbr].push_back(node);
                     
                 }
                 
-                else if(dist[nbr]==dist[node] + 1){
+                else if(dist[nbr] == dist[node] + 1){
                     
                     parent[nbr].push_back(node);
                     
@@ -66,25 +72,37 @@ public:
             
         }
         
+        /* Finally we are done with the BFS traversal and now we
+        have all the required relations between a node and its
+        immediate parents , which will be a part of atleast one 
+        shortest path.*/
         
     }
     
     
-    void AllShortestPaths(int node,vector<vector<int>>&parent,vector<int>&path,vector<vector<int>>&allpath){
+    
+    void DFS(int node,vector<vector<int>>&parent,vector<int>&path,vector<vector<int>>&allpath){
+        
+        /* i.e. we able to reach the parent node of the src node,
+        that is set to (-1) previously , for now include it in our 
+        path , in future in order to calculate the words we will 
+        not gonna count it.*/
         
         if(node==-1){
             
             allpath.push_back(path);
-            
             return;
             
         }
+        
+        
+        // Explore all its parent nodes.
         
         for(int par:parent[node]){
             
             path.push_back(par);
             
-            AllShortestPaths(par,parent,path,allpath);
+            DFS(par,parent,path,allpath);
             
             path.pop_back();
             
@@ -93,24 +111,30 @@ public:
     }
     
     
-    vector<vector<string>> findLadders(string bW, string eW, vector<string>& wordList) {
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
         
-        vector<vector<string>>ans;
+        /* 
+        
+        As all the strings in wordList dictionary unique to
+        each other , therefore it is efficient to access them by
+        there indices , by doing these there is no need to store
+        map<string,map<string,int>> kind of data structure which
+        is space inefficient as well as increase the time 
+        complexity indirectly in case of find or erase operations. */
+        
+        int src=-1,dest=-1;
         
         int n=wordList.size();
         
-        
-         int src=-1,dest=-1;
-        
         for(int i=0;i<n;++i){
             
-            if(wordList[i]==bW){
+            if(wordList[i]==beginWord){
                 
                 src=i;
                 
             }
             
-            if(wordList[i]==eW){
+            if(wordList[i]==endWord){
                 
                 dest=i;
                 
@@ -119,9 +143,15 @@ public:
         }
         
         
-       // cout<<src<<" "<<dest<<"\n";
+        /* Data structure needed to store all the valid yet 
+           shortest paths from src to dest node.*/
         
-        // When the endWord is not found , then definitely there doesn't exist any path from beginWord to it.
+        vector<vector<string>>ans;
+        
+        
+        /* Edge Case : When destination string is not found inside
+        the wordList dictionary.Then there is no path exist from 
+        src to dest. */
         
         if(dest==-1){
             
@@ -129,85 +159,129 @@ public:
             
         }
         
-        // Same Logic goes here.
+        
+        /*
+        
+         Edge Case : When src node is not found inside the given
+         dictionary , then we need to insert it inside the 
+         dictionary and as a result total size (n) of the 
+         dictionay gets increased.
+        
+        */
+        
         
         if(src==-1){
             
-            wordList.push_back(bW);
-            
+            wordList.push_back(beginWord);
             src=n;
-            
             n++;
             
         }
         
         
-        vector<vector<int>>graph(n);
-        
-        vector<int>path;
-        
-        
-        
-        
-        // Create a graph of words's indices , providing wordList[i] and wordList[j] are neighbour of each other.
-        
-        for(int i=0;i<n;++i){
-            
-            for(int j=i+1;j<n;++j){
-                
-                if(isNeighbour(wordList[i],wordList[j])){
-                    
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                    
-                }
-                
-            }
-            
-        }
-        
-        
-        // Stores all the parent nodes of the current node.
+        /* These Data Structure will store all the possible parent
+        nodes of the current node , which are required to build 
+        any of the the shortest path */
         
         vector<vector<int>>parent(n);
         
-       
+        
+        /*
+        
+        These DS will store the inner links b/w two words , if 
+        they are differing only in one character.
+        
+        */
+        
+        vector<vector<int>>graph(n);
         
         
-        // Otherwise we have to find the parent-child relationship first using BFS traversal .
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;++j){
+                if(isNeighbour(wordList[i],wordList[j])){
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
+                }
+            }
+        }
+        
+        
+        
+        /* Now call the BFS utility function , that will help us 
+        to explore all the shortest paths from src to dest node ,
+        by maintaining the relationship b/w current node and its 
+        immediate parents . As we want to explore all the paths ,
+        so it is mandatory to access its parent , then parent of 
+        its parent  , and all the way upto src . Here we access 
+        the paths from dest to src  , you can also do the reverse
+        of it , if you want.*/
+        
         
         BFS(src,graph,parent,n);
         
         
-        // After formulating the parent array , now it is the time to explore all possible paths , but from destination node towards source node.
+        /*
         
-        vector<vector<int>>paths;
+        Once we have all the relations in our pouch , all we need
+        to do simply explore all the paths recursively , and for 
+        doing these DFS traversal is most efficient one.
         
-        AllShortestPaths(dest,parent,path,paths);
+        */
         
-       // cout<<paths.size()<<"\n";
+        /*
         
-        for(auto curr:paths){
+        As we discussed previously , we are start our journey from 
+        the end(i.e. the dest node) and keep exploring all the 
+        paths upto src node. 
+        
+        */
+        
+        /*
+        
+         To store all the paths's nodes we need to maintain a DS ,
+         that will store them , and again a DS that will store all 
+         the paths at the end of DFS traversal.
+        
+        */
+        
+        vector<int>path;
+        
+        vector<vector<int>>allpath;
+        
+        
+        DFS(dest,parent,path,allpath);
+        
+        
+        /*
+        
+    Now at that moment we have all the shortest paths with us.
+        
+        */
+        
+        
+        for(auto paths:allpath){
             
-            vector<string>currpath;
+            vector<string>current_path;
             
-            int sz=curr.size();
+            int size=paths.size();
             
-            for(int i=0;i<sz-1;++i){
+            for(int i=0;i<size-1;++i){
                 
-                currpath.push_back(wordList[curr[i]]);
+                current_path.push_back(wordList[paths[i]]);
                 
             }
             
-            reverse(currpath.begin(),currpath.end());
+            reverse(current_path.begin(),current_path.end());
             
-            currpath.push_back(wordList[dest]);
+            current_path.push_back(endWord);
             
-            ans.push_back(currpath);
+            ans.push_back(current_path);
             
         }
         
+        
         return ans;
+        
         
     }
 };
