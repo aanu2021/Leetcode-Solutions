f7b374ad2fc0a77cb7f33f8edcaee240d11923ff
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-15 12:22:22 +0530

    Time: 24 ms (45.70%), Space: 8.4 MB (69.83%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -6,29 +6,41 @@ public:
         int m=s1.length();
         int n=s2.length();
        
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<int>prev(n+1,0);
         
-        dp[0][0]=0;
+        prev[0]=0;
         
         for(int i=1;i<=n;++i){
-            dp[0][i]=i;
+            prev[i]=i;
         }
         
-        for(int i=1;i<=m;++i){
-            dp[i][0]=i;
-        }
          
         for(int i=1;i<=m;++i){
+            
+            vector<int>curr(n+1,0);
+            
+            curr[0]=i;
+            
             for(int j=1;j<=n;++j){
+                
                 if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }else{
-                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                    
+                    curr[j]=prev[j-1];
+                    
                 }
+                else{
+                    
+                    curr[j]=1+min({prev[j-1],curr[j-1],prev[j]});
+                    
+                }
+                
             }
+            
+            prev=curr;
+            
         }
         
-        return dp[m][n];
+        return prev[n];
         
     }
 };
