a31fd3a737ece1cb0e06660ec5f0e479c0312082
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-15 11:50:28 +0530

    Time: 79 ms (28.59%), Space: 15.2 MB (69.04%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,55 +1,62 @@
 class Solution {
 public:
     
-    /*
+    typedef long long ll;
     
-    1-D DP Optimization
+    ll dp[1002][1002];
     
-    Time Complexity  : O(M*N)
-    Space Complexity : O(N)
-    
-    */
-    
-    typedef unsigned long long ll;
+    ll func(string &s,string &t,ll i,ll j){
+        
+        if(i<0 && j<0){
+            return 1LL;
+        }
+        
+        if(i>=0 && j<0){
+            return 1LL;
+        }
+        
+        if(i<0 && j>=0){
+            return 0LL;
+        }
+        
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+        
+        ll num_subseq=0LL;
+        
+        if(s[i]==t[j]){
+            
+            num_subseq=func(s,t,i-1,j-1)+func(s,t,i-1,j);
+            
+        }
+        
+        else{
+            
+            num_subseq=func(s,t,i-1,j);
+            
+        }
+        
+        return dp[i][j]=num_subseq;
+        
+    }
     
     int numDistinct(string s, string t) {
         
         ll m=s.length();
         ll n=t.length();
-
         
-        vector<ll>curr(n+1,0LL);
-        
-        
-        curr[0]=1LL;
-
-        
-        s="*"+s;
-        t="*"+t;
-        
-        
-        for(ll i=1;i<=m;++i){
-            
-            for(ll j=n;j>=1;--j){
-                
-                if(s[i]==t[j]){
-                    
-                    curr[j]=curr[j-1]+curr[j];
-                    
-                }
-                
-                else{
-                    
-                    curr[j]=curr[j];
-                    
-                }
-                
+        for(ll i=0;i<=m;++i){
+            for(ll j=0;j<=n;++j){
+                dp[i][j]=-1LL;
             }
-        
-            
         }
         
-        return curr[n];
+        ll ans=func(s,t,m-1,n-1);
         
+        return ans;
+               
     }
 };
