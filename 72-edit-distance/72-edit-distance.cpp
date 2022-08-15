7dd3022aa57eb5a013934f7f0806619d5de141d4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-15 12:19:54 +0530

    Time: 22 ms (51.21%), Space: 9.1 MB (21.81%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,71 +1,34 @@
 class Solution {
 public:
-    
-    int dp[502][502];
-    
-    int func(string &s1,string &s2,int i,int j){
-        
-        if(i<0 && j<0){
-            return 0;
-        }
-        
-        if(i<0 && j>=0){
-            return j+1;
-        }
-        
-        if(i>=0 && j<0){
-            return i+1;
-        }
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        
-        int num_ops=0;
-        
-        if(s1[i]==s2[j]){
-            
-            num_ops=func(s1,s2,i-1,j-1);
-            
-        }
-        
-        else{
-            
-            int insert=func(s1,s2,i,j-1);
-            int deletion=func(s1,s2,i-1,j);
-            int replace=func(s1,s2,i-1,j-1);
-            
-            num_ops=1+min({insert,deletion,replace});
-            
-        }
-        
-        return dp[i][j]=num_ops;
-        
-    }
-    
+   
     int minDistance(string s1, string s2) {
         
         int m=s1.length();
         int n=s2.length();
+       
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         
-        /*
+        dp[0][0]=0;
         
-        Case 1: s1[i]!=s2[j] 
-            
-                func(i,j) = 1 + min(func(i+1,j),func(i,j+1),func(i+1,j+1));
-            
-        Case 2: s1[i]==s2[j] 
-            
-                func(i,j) = func(i+1,j+1)
-                
-                */
+        for(int i=1;i<=n;++i){
+            dp[0][i]=i;
+        }
         
+        for(int i=1;i<=m;++i){
+            dp[i][0]=i;
+        }
+         
+        for(int i=1;i<=m;++i){
+            for(int j=1;j<=n;++j){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                }
+            }
+        }
         
-        memset(dp,-1,sizeof(dp));
-        
-        int ans=func(s1,s2,m-1,n-1);
-        
-        return ans;
+        return dp[m][n];
         
     }
 };
