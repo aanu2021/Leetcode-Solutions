d7f0268bd442163a1f2001c97ea692a50f8b0b28
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-18 13:14:43 +0530

    Time: 20 ms (21.37%), Space: 19 MB (14.22%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -14,13 +14,48 @@ public:
         
     }
     
+    int depth2(TreeNode*&root){
+        
+        queue<TreeNode*>q;
+        
+        q.push(root);
+        
+        int level=0;
+        
+        while(!q.empty()){
+            
+            int sz=q.size();
+            
+            while(sz--){
+                
+                auto node=q.front();
+                q.pop();
+                
+                if(node->left){
+                    q.push(node->left);
+                }
+                
+                if(node->right){
+                    q.push(node->right);
+                }
+                
+            }
+            
+            level++;
+            
+        }
+        
+        return level;
+        
+    }
+    
     int maxDepth(TreeNode* root) {
         
         if(root==NULL){
             return 0;
         }
         
-        int ht=depth(root);
+        int ht=depth2(root);
         
         return ht;
         
