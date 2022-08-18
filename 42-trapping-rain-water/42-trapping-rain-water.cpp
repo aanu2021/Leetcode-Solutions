a58b6c320bd35409b6deee4900c413406c8ead4f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-18 11:32:20 +0530

    Time: 29 ms (37.63%), Space: 19.9 MB (61.26%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,32 +3,37 @@ public:
     int trap(vector<int>& height) {
         
         int n=height.size();
-       
-        int water_stored=0;
         
-        vector<int>prefix(n,0);
+        int l=0,r=n-1;
         
-        vector<int>suffix(n,0);
+        int left_max=0,right_max=0;
         
-        prefix[0]=height[0];
+        int ans=0;
         
-        suffix[n-1]=height[n-1];
-        
-        for(int i=1;i<n;++i){
-            prefix[i]=max(prefix[i-1],height[i]);
-        }
-        
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],height[i]);
-        }
-        
-        for(int i=0;i<n;++i){
+        while(l<=r){
             
-            water_stored+=min(prefix[i],suffix[i])-height[i];
+            if(height[l]<=height[r]){
+                
+                left_max=max(left_max,height[l]);
+                
+                ans+=(left_max-height[l]);
+                
+                l++;
+                
+            }else{
+                
+                right_max=max(right_max,height[r]);
+                
+                ans+=(right_max-height[r]);
+                
+                r--;
+                
+            }
             
         }
+           
         
-        return water_stored;
+        return ans;
         
     }
 };
