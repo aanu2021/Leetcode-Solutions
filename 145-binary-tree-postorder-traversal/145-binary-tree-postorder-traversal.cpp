023bf7d39539e8523caeb871ff9a7a866249defa
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-18 13:08:35 +0530

    Time: 8 ms (9.89%), Space: 8.4 MB (86.62%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -1,56 +1,88 @@
 class Solution {
 public:
-    
     vector<int> postorderTraversal(TreeNode* root) {
-       
+        
+        if(!root){
+            return {};
+        }
+        
+        stack<pair<TreeNode*,int>>S;
+        
+        S.push({root,1});
+        
+        vector<int>preorder;
+        vector<int>inorder;
         vector<int>postorder;
         
-        if(root==NULL){return {};}
         
-        stack<TreeNode*>S;
-       
-        TreeNode*curr=root;
-        
-        while(curr!=NULL || !S.empty()){
+        while(!S.empty()){
             
-            if(curr){
+            auto curr=S.top();
+            S.pop();
+            
+            if(curr.second==1){
+                
+                curr.second++;
+                
+                preorder.push_back(curr.first->val);
                 
                 S.push(curr);
-                curr=curr->left;
                 
-            }
-            
-            else{
-                
-                TreeNode*temp=S.top()->right;
-                
-                if(temp==NULL){
+                if(curr.first->left){
                     
-                    temp=S.top();
-                    S.pop();
-                    
-                    postorder.push_back(temp->val);
-                    
-                    while(!S.empty() && temp==S.top()->right){
-                        
-                        temp=S.top();
-                        S.pop();
-                        
-                        postorder.push_back(temp->val);
-                        
-                    }
-                    
-                }else{
-                    
-                    curr=temp;
+                    S.push({curr.first->left,1});
                     
                 }
                 
             }
             
+            else if(curr.second==2){
+                
+                curr.second++;
+                
+                inorder.push_back(curr.first->val);
+                
+                S.push(curr);
+                
+                if(curr.first->right){
+                    
+                    S.push({curr.first->right,1});
+                    
+                }
+                
+            }
+            
+            else{
+                
+                postorder.push_back(curr.first->val);
+                
+            }
+            
         }
         
+        int n=preorder.size();
+        
+        for(int i=0;i<n;++i){
+            
+            cout<<preorder[i]<<" ";
+            
+        }cout<<"\n";
+        
+        for(int i=0;i<n;++i){
+            
+            cout<<inorder[i]<<" ";
+            
+        }cout<<"\n";
+        
+        for(int i=0;i<n;++i){
+            
+            cout<<postorder[i]<<" ";
+            
+        }cout<<"\n";
+        
         return postorder;
+        
+        
         
     }
 };
