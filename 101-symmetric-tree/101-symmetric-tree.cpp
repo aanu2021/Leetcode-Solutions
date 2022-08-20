c29b3621de593bb47ec4160d0889cfe6263d0d43
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-20 19:58:36 +0530

    Time: 3 ms (94.38%), Space: 16.6 MB (17.60%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    bool isCheck(TreeNode*&root1,TreeNode*root2){
+        
+        if(root1==NULL && root2==NULL){
+            return true;
+        }
+        
+        if(root1==NULL || root2==NULL || root1->val!=root2->val){
+            return false;
+        }
+        
+        return isCheck(root1->left,root2->right) && isCheck(root1->right,root2->left);
+        
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        
+        if(!root){
+            return true;
+        }
+        
+        return isCheck(root->left,root->right);
+        
+    }
+};
