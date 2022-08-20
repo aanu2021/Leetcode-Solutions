e4e8b32305c72a76a68a7195ff7b0635dc9a91f5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-20 11:27:37 +0530

    Time: 185 ms (11.06%), Space: 16.2 MB (51.25%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,71 +1,48 @@
 class Solution {
 public:
     
-    /*
-    
-     Optimized Approach - Using Heap
-     
-     The Explanation in the solution section is itself 
-     amazing , so you can check that out.
-    
-    */
-    
-     /*
-     
-     Time Complexity  : O(N*logN)
-     Space Complexity : O(N)
-     
-     */
-    
-     typedef long long ll;
+    typedef long long ll;
     
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         
-       int n=stations.size();
+        ll n=stations.size();
         
-       ll distance=0LL;
+        vector<ll>dp(n+1,0LL);
         
-       priority_queue<ll>pq; /* Maxheap that will gonna 
-       store the maximum amount of fuel from a particular 
-       gas station , provided it is in our range (i.e we can 
-       reach the gas-station from our current state using 
-       currently stored fuel in our gas tank) */
+        dp[0]=(ll)startFuel;
         
         
-       int num_of_refuel_stations=0; 
+        for(ll i=0;i<n;++i){
+            
+            for(ll j=i;j>=0;j--){
+                
+                if(dp[j]>=(ll)stations[i][0]){
+                    
+                    dp[j+1]=max(dp[j+1],dp[j]+(ll)stations[i][1]);
+                    
+                }
+                
+            }
+            
+        }
         
-       distance=(ll)startFuel;
+        if(dp[n]<target){
+            
+            return -1;
+            
+        }
         
-       int i=0;
+        for(ll i=0;i<=n;++i){
+            
+            if(dp[i]>=target){
+                
+                return i;
+                
+            }
+            
+        }
         
-        
-       while(distance<target){
-           
-           while(i<n && distance>=(ll)stations[i][0]){
-               
-               pq.push(stations[i][1]);
-               
-               i++;
-               
-           }
-           
-           if(pq.size()==0){ // We are running out of fuel.
-               
-               return -1;
-               
-           }
-           
-           // Otherwise , pick the maximum fuel 
-           
-           distance+=(ll)pq.top();
-           
-           pq.pop(); // Once a station's fuel is used , it will never considerable in future.
-           
-           num_of_refuel_stations++;
-           
-       } 
-        
-       return num_of_refuel_stations; 
+        return -1;
         
     }
 };
