0ba5a6c9921e04420c3ea9d87ed29f2f3e7793a6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-22 01:22:55 +0530

    Time: 43 ms (75.90%), Space: 30.8 MB (63.58%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -1,12 +1,63 @@
 class Solution {
 public:
+    
+    /*
+    
+    Complete Binary Tree --> Combination of a lot of Perfect Binary trees(internal nodes have exactly 2 children && all the leaf nodes must be present at the same level)
+    
+    T.C : O(logN*logN)
+    
+    For exploring total height of the tree and findLeftHeight and findRightHeight function they will take O(logN) time in both the cases.
+    
+    S.C : O(logN)
+    
+    */
+    
+    
+    int findLeftHeight(TreeNode*&root){
+        
+        int ht=0;
+        
+        TreeNode*curr=root;
+        
+        while(curr){
+            ht++;
+            curr=curr->left;
+        }
+        
+        return ht;
+        
+    }
+    
+    int findRightHeight(TreeNode*&root){
+        
+        int ht=0;
+        
+        TreeNode*curr=root;
+        
+        while(curr){
+            ht++;
+            curr=curr->right;
+        }
+        
+        return ht;
+        
+    }
+    
     int countNodes(TreeNode* root) {
         
         if(!root){
             return 0;
         }
         
-        return 1+countNodes(root->left)+countNodes(root->right);
+        int lh=findLeftHeight(root);
+        int rh=findRightHeight(root);
+        
+        if(lh==rh){
+            return (1<<lh)-1;
+        }else{
+            return 1+countNodes(root->left)+countNodes(root->right);
+        }
         
     }
 };
