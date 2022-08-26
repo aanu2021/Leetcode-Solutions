9e5e87d99b2570ba9a07ab61326cb1bcb967daf7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-26 20:30:31 +0530

    Time: 703 ms (76.21%), Space: 13.1 MB (51.54%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -4,51 +4,45 @@ public:
     struct Node{
       
         Node*links[26];
-        string word;
         int endword;
-        
+        string eword;
+    
         bool containsKey(char ch){
-            
             return (links[ch-'a']!=NULL);
-            
         }
         
         Node*get(char ch){
-            
             return links[ch-'a'];
-            
         }
         
         void put(char ch,Node*node){
-            
             links[ch-'a']=node;
-            
         }
-      
+        
+        
     };
-    
     
     Node*root=new Node();
     
     vector<string>ans;
     
-    void insert(string &Word){
+    void insert(string &word){
         
         Node*node=root;
         
-        for(int i=0;i<Word.length();++i){
+        for(int i=0;i<word.length();++i){
             
-            if(!node->containsKey(Word[i])){
-                node->put(Word[i],new Node());
+            if(!node->containsKey(word[i])){
+                node->put(word[i],new Node());
             }
             
-            node=node->get(Word[i]);
+            node=node->get(word[i]);
             
         }
         
         node->endword++;
         
-        node->word=Word;
+        node->eword=word;
         
     }
     
@@ -68,38 +62,24 @@ public:
             
             curr->endword--;
             
-            ans.push_back(curr->word);
+            ans.push_back(curr->eword);
             
         }
-        
         
         char temp=board[i][j];
-        
         board[i][j]='*';
         
-        if(i>0){
-            
+        if(i>0)
             func(board,i-1,j,m,n,curr);
-            
-        }
         
-        if(i<m-1){
-            
+        if(i<m-1)
             func(board,i+1,j,m,n,curr);
-            
-        }
         
-        if(j>0){
-            
+        if(j>0)
             func(board,i,j-1,m,n,curr);
-            
-        }
         
-        if(j<n-1){
-            
+        if(j<n-1)
             func(board,i,j+1,m,n,curr);
-            
-        }
         
         board[i][j]=temp;
         
@@ -111,21 +91,15 @@ public:
         int n=board[0].size();
         
         for(string str:words){
-            
             insert(str);
-            
         }
         
         Node*curr=root;
         
         for(int i=0;i<m;++i){
-            
             for(int j=0;j<n;++j){
-                
                 func(board,i,j,m,n,curr);
-                
             }
-            
         }
         
         return ans;
