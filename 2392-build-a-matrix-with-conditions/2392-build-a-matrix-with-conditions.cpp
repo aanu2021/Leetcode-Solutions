5b1612c0b589c0c4fcefadcb4ea2b83633b4c0cf
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-28 11:22:41 +0530

    Time: 178 ms (100.00%), Space: 56.4 MB (100.00%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,55 +1,97 @@
 class Solution {
 public:
     
-    
-    
-    // return false if the graph formed is cyclic in nature. 
-    //normal graph building and topological sort code
-    bool buildGraph(vector<vector<int> >& arr, int n, vector<int>& data) {
-        vector<int> adj[n + 1];
-        vector<int> indeg(n + 1, 0);
-        for(auto& curr: arr) {
-            adj[curr[0]].push_back(curr[1]);
-            indeg[curr[1]]++;
-        }
-        queue<int> q;
-        for(int i = 1; i <= n; i++) {
-            if(indeg[i] == 0) {
-                q.push(i);
-            }
-        }
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            data.push_back(node);
-            for(auto& x: adj[node]) {
-                if(--indeg[x] == 0) {
-                    q.push(x);
-                }
-            }
-        }
-        return data.size() == n;
-    }
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        vector<int> topoRow, topoCol;
+    vector<int>topo_sort(int k,vector<vector<int>>&edges){
         
-        // if the graph formed by the two arrays is cyclic, then answer is not possible.
-        if(!buildGraph(rowConditions, k, topoRow) || !buildGraph(colConditions, k, topoCol)) {
+        vector<vector<int>>graph(k+1);
+        
+        vector<int>indegree(k+1,0);
+        
+        vector<int>path;
+        
+        for(int i=0;i<edges.size();++i){
+            
+            graph[edges[i][0]].push_back(edges[i][1]);
+            
+            indegree[edges[i][1]]++;
+            
+        }
+        
+        queue<int>q;
+        
+        for(int i=1;i<=k;++i){
+            
+            if(indegree[i]==0){
+                
+                q.push(i);
+                
+            }
+            
+        }
+        
+        while(!q.empty()){
+            
+            int sz=q.size();
+            
+            while(sz--){
+                
+                int node=q.front();
+                q.pop();
+                
+                path.push_back(node);
+                
+                for(int v:graph[node]){
+                    
+                    indegree[v]--;
+                    
+                    if(indegree[v]==0){
+                        q.push(v);
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        if(path.size()!=k){
             return {};
         }
         
-        int row[k + 1], col[k + 1];
+        else{
+            return path;
+        }
+        
+    }
     
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
         
-        for(int i = 0; i < k; i++) {
-            row[topoRow[i]] = i;
-            col[topoCol[i]] = i;
+        vector<int>row=topo_sort(k,rowConditions);
+        
+        vector<int>col=topo_sort(k,colConditions);
+        
+        if(row.size()==0 || col.size()==0){
+            return {};
         }
         
-        vector<vector<int> > arr(k, vector<int>(k, 0));
-        for(int i = 1; i <= k; i++) {
-            arr[row[i]][col[i]] = i;
+        vector<int>index_row(k+1,-1);
+        vector<int>index_col(k+1,-1);
+        
+        for(int i=0;i<k;++i){
+            
+            index_row[row[i]]=i;
+            index_col[col[i]]=i;
+            
         }
+        
+        vector<vector<int>>arr(k,vector<int>(k,0));
+        
+        for(int i=1;i<=k;++i){
+            
+            arr[index_row[i]][index_col[i]]=i;
+            
+        }
+        
         return arr;
         
     }
