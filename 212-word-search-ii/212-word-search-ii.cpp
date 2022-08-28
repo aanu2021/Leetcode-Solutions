2cd805eefaa7b8d7064043e8f8a090e9723f0044
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-28 19:20:01 +0530

    Time: 663 ms (77.74%), Space: 13.2 MB (40.48%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     
     struct Node{
-      
-        Node*links[26];
-        int endword;
-        string eword;
-    
-        bool containsKey(char ch){
-            return (links[ch-'a']!=NULL);
-        }
         
-        Node*get(char ch){
-            return links[ch-'a'];
-        }
+       Node*links[26];
+       int endword;
+       string eword; 
         
-        void put(char ch,Node*node){
-            links[ch-'a']=node;
-        }
+       bool containsKey(char ch){
+           
+           return (links[ch-'a']!=NULL);
+           
+       } 
         
+       Node*get(char ch){
+           
+           return links[ch-'a'];
+           
+       } 
+        
+       void put(char ch,Node*node){
+           
+           links[ch-'a']=node;
+           
+       } 
         
     };
     
@@ -28,25 +33,27 @@ public:
     
     void insert(string &word){
         
-        Node*node=root;
+        Node*curr=root;
         
         for(int i=0;i<word.length();++i){
             
-            if(!node->containsKey(word[i])){
-                node->put(word[i],new Node());
+            if(!curr->containsKey(word[i])){
+                curr->put(word[i],new Node());
             }
             
-            node=node->get(word[i]);
+            curr=curr->get(word[i]);
             
         }
         
-        node->endword++;
-        
-        node->eword=word;
+        curr->endword++;
+        curr->eword=word;
         
     }
     
-    void func(vector<vector<char>>&board,int i,int j,int m,int n,Node*curr){
+    void func(vector<vector<char>>&board,int i,int j,Node*curr){
+        
+        int m=board.size();
+        int n=board[0].size();
         
         if(board[i][j]=='*'){
             return;
@@ -67,19 +74,20 @@ public:
         }
         
         char temp=board[i][j];
+        
         board[i][j]='*';
         
         if(i>0)
-            func(board,i-1,j,m,n,curr);
+            func(board,i-1,j,curr);
         
         if(i<m-1)
-            func(board,i+1,j,m,n,curr);
+            func(board,i+1,j,curr);
         
         if(j>0)
-            func(board,i,j-1,m,n,curr);
+            func(board,i,j-1,curr);
         
         if(j<n-1)
-            func(board,i,j+1,m,n,curr);
+            func(board,i,j+1,curr);
         
         board[i][j]=temp;
         
@@ -87,18 +95,15 @@ public:
     
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         
-        int m=board.size();
-        int n=board[0].size();
-        
         for(string str:words){
             insert(str);
         }
         
         Node*curr=root;
         
-        for(int i=0;i<m;++i){
-            for(int j=0;j<n;++j){
-                func(board,i,j,m,n,curr);
+        for(int i=0;i<board.size();++i){
+            for(int j=0;j<board[0].size();++j){
+                func(board,i,j,curr);
             }
         }
         
