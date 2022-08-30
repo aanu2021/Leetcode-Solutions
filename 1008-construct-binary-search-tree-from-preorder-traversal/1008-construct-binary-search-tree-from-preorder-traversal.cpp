6225af756307b0bc8891c2b8dfdd7c25bae48bf1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-30 19:46:00 +0530

    Time: 4 ms (83.53%), Space: 13.7 MB (48.47%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,23 +10,19 @@ public:
     
     int idx=0;
     
-    unordered_map<int,int>index;
-    
-    TreeNode*BuildBST(vector<int>&preorder,vector<int>&inorder,int low,int high){
+    TreeNode*BuildBST(vector<int>&preorder,int left,int right){
         
-        if(low>high){
+        if(idx==preorder.size() || preorder[idx]<left || preorder[idx]>right || left>right){
             return NULL;
         }
         
         TreeNode*root=new TreeNode(preorder[idx]);
         
-        int mid=index[preorder[idx]];
-        
         idx++;
         
-        root->left=BuildBST(preorder,inorder,low,mid-1);
+        root->left=BuildBST(preorder,left,root->val-1);
         
-        root->right=BuildBST(preorder,inorder,mid+1,high);
+        root->right=BuildBST(preorder,root->val+1,right);
         
         return root;
         
@@ -36,15 +32,7 @@ public:
         
         int n=preorder.size();
         
-        vector<int>inorder=preorder;
-        
-        sort(inorder.begin(),inorder.end());
-        
-        for(int i=0;i<n;++i){
-            index[inorder[i]]=i;
-        }
-        
-        TreeNode*root=BuildBST(preorder,inorder,0,n-1);
+        TreeNode*root=BuildBST(preorder,-1e9,1e9);
         
         return root;
         
