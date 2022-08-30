0498b4904ab9cb1e8f5e28f96bee401da682d16d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-30 20:43:26 +0530

    Time: 117 ms (6.05%), Space: 41.9 MB (7.36%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    unordered_map<int,int>mapp;
+    
+    bool flag=false;
+    
+    void inorder(TreeNode*&root,int target){
+        
+        if(!root || flag){
+            return;
+        }
+        
+        inorder(root->left,target);
+        
+        mapp[root->val]++;
+        
+        int required=target-root->val;
+        
+        if(required!=root->val && mapp[required]>0){
+            
+            flag=true;
+            return;
+            
+        }
+        
+        inorder(root->right,target);
+        
+    }
+    
+    bool findTarget(TreeNode* root, int target){
+        
+        inorder(root,target);
+        
+        return flag;
+        
+    }
+};
