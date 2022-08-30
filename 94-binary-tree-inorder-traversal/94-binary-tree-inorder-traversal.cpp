fc06a532cd632375db60fa8b8000278a0b00117f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-30 11:58:18 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (39.80%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,41 +1,67 @@
 class Solution {
 public:
+    
+    /*
+    
+    Morris Traversal : Rightmost node of the left subtree is linked with the current node , with an imaginary thread.
+    
+    Time Complexity  : O(N)
+    Space Complexity : O(1)
+    
+    */
+    
     vector<int> inorderTraversal(TreeNode* root) {
-        
+     
         if(!root){
             return {};
         }
         
+        vector<int>inorder;
+        
         TreeNode*curr=root;
         
-        stack<TreeNode*>S;
-        
-        vector<int>ans;
-        
-        
-        while(curr!=NULL || !S.empty()){
+        while(curr!=NULL){
             
-            if(curr!=NULL){
+            if(curr->left==NULL){
                 
-                S.push(curr);
-                curr=curr->left;
+                inorder.push_back(curr->val);
+                curr=curr->right;
                 
             }
             
             else{
                 
-                curr=S.top();
-                S.pop();
-                        
-                ans.push_back(curr->val);
+                TreeNode*prev=curr->left;
                 
-                curr=curr->right;
+              while(prev && prev->right && prev->right!=curr){
+                    
+                    prev=prev->right;
+                    
+                }
+                
+                if(prev->right==NULL){
+                    
+                    prev->right=curr;
+                    
+                    curr=curr->left;
+                    
+                }
+                
+                else{
+                    
+                    prev->right=NULL;
+                    
+                    inorder.push_back(curr->val);
+                    
+                    curr=curr->right;
+                    
+                }
                 
             }
             
         }
         
-        return ans;
+        return inorder;
         
     }
 };
