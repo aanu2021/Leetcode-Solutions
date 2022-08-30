6f203e23537552a26fb0369d19f3b8432f8d7c06
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-30 13:21:22 +0530

    Time: 17 ms (64.44%), Space: 21.7 MB (70.21%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,62 +1,33 @@
 class Solution {
 public:
     
-    /*
-    
-    Validate a BST --> Validate every Node --> validate every node means for a particular node all the nodes in its left subtree should be smaller than the node->val and all the nodes in its right subtree should be greater then the node->val.
-    
-    For these , there are three nodes 1 , 2 , 3.
-    
-         1
-          \
-           3
-          /
-         2
-         
-     For node (2) , we have its value should be less than node (3) , and node(2) always be in (1)'s right subtree. That means (2)'s value should be strictly greater then (1)'s value , all in all every node's value should lie in a particular range.
-     
-     
-     Node's value [left , right]
-    
-    */
-    
     typedef long long ll;
     
-    bool isvalid(TreeNode*root,ll minval,ll maxval){
+    bool isValid(TreeNode*&root,ll lval,ll rval){
         
-        if(root==NULL){
+        if(!root){
             return true;
         }
         
-        if(root->val<minval || root->val>maxval){
+        if(root->val<lval || root->val>rval){
             return false;
         }
         
-        bool left=true,right=true;
+        bool lsubtree=isValid(root->left,(ll)lval,(ll)root->val-1);
         
-        if(root->left){
-            
-            left=isvalid(root->left,minval,(ll)root->val-1);
-            
-        }
+        bool rsubtree=isValid(root->right,(ll)root->val+1,(ll)rval);
         
-        if(root->right){
-            
-            right=isvalid(root->right,(ll)root->val+1,maxval);
-            
-        }
-        
-        return left && right;
+        return lsubtree && rsubtree; 
         
     }
     
     bool isValidBST(TreeNode* root) {
         
-        if(root==NULL){
+        if(!root){
             return true;
         }
         
-        return isvalid(root,(ll)-1e14,(ll)1e14);
+        return isValid(root,(ll)-1e14,(ll)1e14);
         
     }
 };
