451a542e6ddc51e2160f7d87b4ff8066f87b98b6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-30 10:12:07 +0530

    Time: 4 ms (89.83%), Space: 12.7 MB (78.38%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-
-    /*
-    
-    Observation : Last Node of the preorder traversal of left
-    subtree for the current node is connected with the First 
-    node of the preorder traversal of right subtree. 
-    
-    */
-    
     void flatten(TreeNode* root) {
         
         if(!root){
             return;
         }
         
+        TreeNode*tempLeft =root->left;
+        TreeNode*tempRight=root->right;
+        
+        flatten(tempLeft);
+        flatten(tempRight);
+        
+        root->right=tempLeft;
+        
+        root->left=NULL;
+        
         TreeNode*curr=root;
         
-        while(curr!=NULL){
-            
-            if(curr->left!=NULL){
-                
-                TreeNode*prev=curr->left;
-                
-                while(prev!=NULL && prev->right!=NULL){
-                    
-                    prev=prev->right;
-                    
-                }
-                
-                prev->right=curr->right;
-                
-                curr->right=curr->left;
-                
-                curr->left=NULL;
-                
-            }
-            
+        while(curr->right!=NULL){
             curr=curr->right;
-            
         }
         
+        curr->right=tempRight;
         
     }
 };
