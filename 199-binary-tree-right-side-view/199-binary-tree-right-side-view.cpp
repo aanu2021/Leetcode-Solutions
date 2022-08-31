c831c067f925c0cd74b36c0274ff49079402a04d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-08-31 12:37:14 +0530

    Time: 4 ms (67.69%), Space: 11.9 MB (88.71%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,56 +1,45 @@
 class Solution {
 public:
+    
+    vector<int>ans;
+    
+    void dfs(TreeNode*&root,int lvl,int &maxlevel){
+        
+        if(!root){
+            return;
+        }
+        
+        if(lvl>maxlevel){
+            
+            maxlevel=lvl;
+            
+            ans.push_back(root->val);
+            
+        }
+        
+        if(root->right){
+            
+            dfs(root->right,lvl+1,maxlevel);
+            
+        }
+        
+        if(root->left){
+            
+            dfs(root->left,lvl+1,maxlevel);
+            
+        }
+        
+    }
+    
     vector<int> rightSideView(TreeNode* root) {
         
         if(!root){
             return {};
         }
         
-        queue<TreeNode*>q;
-        
-        vector<int>ans;
-        
-        q.push(root);
-        
         int maxlevel=-1;
         
-        int lvl=0;
-        
-        
-        while(!q.empty()){
-            
-            int sz=q.size();
-            
-            while(sz--){
-                
-                auto curr=q.front();
-                q.pop();
-                
-                if(lvl>maxlevel){
-                    
-                    maxlevel=lvl;
-                    
-                    ans.push_back(curr->val);
-                    
-                }
-                
-                if(curr->right){
-                    
-                    q.push(curr->right);
-                    
-                }
-                
-                if(curr->left){
-                    
-                    q.push(curr->left);
-                    
-                }
-                
-            }
-            
-            lvl++;
-            
-        }
+        dfs(root,0,maxlevel);
         
         return ans;
         
