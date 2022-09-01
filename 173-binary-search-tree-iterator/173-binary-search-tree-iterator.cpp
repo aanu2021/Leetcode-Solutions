2b276afa73cad105a89344d35f51fe7939087db6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-01 12:07:50 +0530

    Time: 29 ms (93.20%), Space: 24 MB (98.46%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -1,95 +1,47 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class BSTIterator {
 public:
     
-    TreeNode*curr;
+    stack<TreeNode*>S;
     
     BSTIterator(TreeNode* root) {
         
-        curr=root;
+        pushAll(root);
         
     }
     
-    int morrisInorder(){
+    void pushAll(TreeNode*root){
         
-        int ans=-1;
-        
-        while(curr!=NULL){
-            
-            if(curr->left==NULL){
-                
-                ans=curr->val;
-                
-                curr=curr->right;
-                
-                break;
-                
-            }
-            
-            else{
-                
-                TreeNode*prev=curr->left;
-                
-                while(prev && prev->right && prev->right!=curr){
-                    
-                    prev=prev->right;
-                    
-                }
-                
-                if(prev->right==NULL){
-                    
-                    prev->right=curr;
-                    
-                    curr=curr->left;
-                    
-                }
-                
-                else{
-                    
-                    ans=curr->val;
-                    
-                    prev->right=NULL;
-                    
-                    curr=curr->right;
-                    
-                    break;
-                    
-                }
-                
-            }
-            
+        if(!root){
+            return;
         }
         
-        return ans;
+        while(root){
+            
+            S.push(root);
+            
+            root=root->left;
+            
+        }
         
     }
     
     int next() {
         
-        return morrisInorder();
+        auto node=S.top();
+        S.pop();
+        
+        int value=node->val;
+        
+        pushAll(node->right);
+        
+        return value;
         
     }
     
     bool hasNext() {
         
-        return (curr!=NULL);
+        return !S.empty();
         
     }
 };
 
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
