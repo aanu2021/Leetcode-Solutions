9bc1da02a6d692dfe7f24a35f01a714df213dbec
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-01 11:25:54 +0530

    Time: 47 ms (70.98%), Space: 32.7 MB (81.69%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -17,15 +17,18 @@ public:
         
         TreeNode*rightchild=root->right;
         
-        TreeNode*lastRightchild=root->left;
         
-        while(lastRightchild && lastRightchild->right){
+        TreeNode*lastchild=root->left;
+        
+        while(lastchild && lastchild->right){
             
-            lastRightchild=lastRightchild->right;
+            lastchild=lastchild->right;
             
         }
         
-        lastRightchild->right=rightchild;
+        lastchild->right=rightchild;
+        
+        root->right=NULL;
         
         return root->left;
         
@@ -33,55 +36,85 @@ public:
     
     TreeNode* deleteNode(TreeNode* root, int key) {
         
-        if(!root){
-            return NULL;
-        }
+        /*
         
-        if(root->val==key){
-            return helper(root);
-        }
+           5
+          / \
+         3   6
+        / \   \
+       2  4    7  
         
-        TreeNode*curr=root;
+               .Case 1 : Node is a leaf node
+               .Case 2 : Node has exactly one child(left/right)
+               Case 3 : Node has exactly two children
+               .Case 4 : Node does not exist in the given BST    
+               
+               3    
+              / \     
+             2   4
+                  \
+                   6
+                    \
+                     7
+                     
+                     */
         
-        while(curr!=NULL){
-            
-            if(curr->val>key){ // Explore the Left subtree
-                
-                if(curr->left && curr->left->val==key){
-                    
-                    curr->left=helper(curr->left);
-                    break;
-                    
-                }
-                
-                else{
-                    
-                    curr=curr->left;
-                    
-                }
-                
-            }
-            
-            else{ // Explore the Right subtree 
-                
-                if(curr->right && curr->right->val==key){
-                    
-                    curr->right=helper(curr->right);
-                    break;
-                    
-                }
-                
-                else{
-                    
-                    curr=curr->right;
-                    
-                }
-                
-            }
-            
-        }
+          
+          /* Code Begins */
+        
+         if(!root){
+             
+             return NULL;
+             
+         }
+        
+         if(root->val==key){
+             
+             return helper(root);
+             
+         }
+        
+         TreeNode*curr=root;
+        
+         while(curr){
+             
+             if(curr->val>key){
+                 
+                 if(curr->left && curr->left->val==key){
+                     
+                     curr->left=helper(curr->left);
+                     break;
+                     
+                 }
+                 
+                 else{
+                     
+                     curr=curr->left;
+                     
+                 }
+                 
+             }
+             
+             else{
+                 
+                 if(curr->right && curr->right->val==key){
+                     
+                     curr->right=helper(curr->right);
+                     break;
+                     
+                 }
+                 
+                 else{
+                     
+                     curr=curr->right;
+                     
+                 }
+                 
+             }
+             
+         }
         
         return root;
         
-    }
+    } 
 };
