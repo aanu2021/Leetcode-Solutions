8c567b706c5d8f6d207cf88e80617258cef19350
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-01 20:48:19 +0530

    Time: 47 ms (11.50%), Space: 23.7 MB (37.47%) - LeetHub

---
+++ 563-binary-tree-tilt/563-binary-tree-tilt.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int func(TreeNode*&root,int &sum){
+        
+        if(!root){
+            return 0;
+        }
+        
+        int left_sum=func(root->left,sum);
+        int right_sum=func(root->right,sum);
+        
+        int prev_root_val=root->val;
+        
+        root->val=abs(left_sum-right_sum);
+        
+        sum+=root->val;
+        
+        return prev_root_val + left_sum + right_sum;
+        
+    }
+    
+    int findTilt(TreeNode* root) {
+        
+        if(!root){
+            return 0;
+        }
+        
+        int sum=0;
+        
+        func(root,sum);
+        
+        return sum;
+        
+    }
+};
