8adec9b8e66390eb6801c324490e079a4721292a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-07 19:27:19 +0530

    Time: 43 ms (40.42%), Space: 24.6 MB (60.91%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -2,25 +2,57 @@ class Solution {
 public:
     string tree2str(TreeNode* root) {
         
-        if(!root){
+        stack<TreeNode*>S;
+        
+        string s="";
+        
+        S.push(root);
+        
+        set<TreeNode*>visited;
+        
+        while(!S.empty()){
             
-            return "";
+            auto curr=S.top();
+            
+            if(visited.find(curr)!=visited.end()){
+                
+                S.pop();
+                s+=")";
+                
+            }
+            
+            else{
+                
+                visited.insert(curr);
+                
+                s+="(";
+                s+=to_string(curr->val);
+                
+                if(curr->left==NULL && curr->right!=NULL){
+                    
+                    s+="()";
+                    
+                }
+                
+                if(curr->right!=NULL){
+                    
+                    S.push(curr->right);
+                    
+                }
+                
+                if(curr->left!=NULL){
+                    
+                    S.push(curr->left);
+                    
+                }
+                
+                
+                
+            }
             
         }
         
-        if(root->left==NULL && root->right==NULL){
-            
-            return to_string(root->val);
-            
-        }
-        
-        if(root->right==NULL){
-            
-            return to_string(root->val) + "(" + tree2str(root->left) + ")";
-            
-        }
-        
-        return to_string(root->val) + "(" + tree2str(root->left) + ")" + "(" + tree2str(root->right) + ")";
+        return s.substr(1,s.length()-2);
         
     }
 };
