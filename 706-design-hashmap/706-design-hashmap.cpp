e41a2647fa7ef281e3a0ab53b95cb0b15b61447d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-08 19:56:09 +0530

    Time: 139 ms (92.37%), Space: 52.3 MB (86.72%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,31 +1,113 @@
 class MyHashMap {
 public:
     
-    vector<int>arr;
-    int MAX = 1e6 + 5;
+    vector<list<pair<int,int>>>m;
+    
+    int size;
     
     MyHashMap() {
         
-        arr.clear();
-        arr.resize(MAX,-1);
+        size=109;
+        
+        m.resize(size);
+        
+    }
+    
+    int hash(int key){
+        
+        return key%size;
+        
+    }
+    
+    list<pair<int,int>> :: iterator search(int key){
+        
+        int i=hash(key);
+        
+        list<pair<int,int>> :: iterator it=m[i].begin();
+        
+        while(it!=m[i].end()){
+            
+            if(it->first==key){
+                return it;
+            }
+            
+            it++;
+            
+        }
+        
+        return it;
+        
+    }
+    
+    bool contains(int key){
+        
+        int i=hash(key);
+        
+        if(search(key)!=m[i].end()){
+            return true;
+        }else{
+            return false;
+        }
         
     }
     
     void put(int key, int value) {
         
-        arr[key]=value;
+        int i=hash(key);
+        
+        if(contains(key)){
+            
+            list<pair<int,int>> :: iterator it=search(key);
+            
+            it->second=value;
+            
+        }
+        
+        else{
+            
+            m[i].push_back({key,value});
+            
+        }
         
     }
     
     int get(int key) {
         
-        return arr[key];
+        int i=hash(key);
+        
+        if(contains(key)){
+            
+            list<pair<int,int>> :: iterator it=search(key);
+            
+            return it->second;
+            
+        }
+        
+        else{
+            
+            return -1;
+            
+        }
         
     }
     
     void remove(int key) {
         
-        arr[key]=-1;
+        int i=hash(key);
+        
+        if(contains(key)){
+            
+            list<pair<int,int>> :: iterator it=search(key);
+            
+            m[i].erase(it);
+            
+        }
+        
+        else{
+            
+            return;
+            
+        }
         
     }
 };
