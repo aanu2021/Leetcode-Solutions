b4950140ba2fa183722bc10b94b7043d7bdf647e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-08 14:04:25 +0530

    Time: 10 ms (5.11%), Space: 8.5 MB (39.59%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,54 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        
-        vector<int>inorder;
         
         if(!root){
             return {};
         }
         
+        vector<int>inorder;
+        
+        stack<TreeNode*>S;
+        
         TreeNode*curr=root;
         
-        while(curr){
+        while(curr || !S.empty()){
             
-            if(curr->left==NULL){
+            while(curr){
                 
-                inorder.push_back(curr->val);
-                
-                curr=curr->right;
+                S.push(curr);
+                curr=curr->left;
                 
             }
             
-            else{
-                
-                TreeNode*prev=curr->left;
-                
-                while(prev && prev->right && prev->right!=curr){
-                    
-                    prev=prev->right;
-                    
-                }
-                
-                if(prev->right==NULL){
-                    
-                    prev->right=curr;
-                    
-                    curr=curr->left;
-                    
-                }
-                
-                else{
-                    
-                    prev->right=NULL;
-                    
-                    inorder.push_back(curr->val);
-                    
-                    curr=curr->right;
-                    
-                }
-                
-            }
+            curr=S.top();
+            S.pop();
+            
+            inorder.push_back(curr->val);
+            
+            curr=curr->right;
             
         }
         
