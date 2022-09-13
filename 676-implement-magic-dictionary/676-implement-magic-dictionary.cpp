d1f18eb00a15074500a61b9190f316923e38aea3
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-13 10:24:28 +0530

    Time: 230 ms (55.95%), Space: 18.6 MB (81.97%) - LeetHub

---
+++ 676-implement-magic-dictionary/676-implement-magic-dictionary.cpp
@@ -0,0 +1,153 @@
+class Node{
+    
+  private:   
+    
+  Node*links[26];
+  bool flag=false;
+    
+  public:  
+    
+  bool containsKey(char ch){
+      
+      return links[ch-'a']!=NULL;
+      
+  } 
+    
+  Node*get(char ch){
+      
+      return links[ch-'a'];
+      
+  }  
+    
+  void put(char ch,Node*node){
+      
+      links[ch-'a']=node;
+      
+  }
+    
+  bool isEnd(){
+      
+      return flag;
+      
+  }  
+    
+  void setEnd(){
+      
+      flag=true;
+      
+  }  
+    
+};
+
+class Trie{
+    
+  private:  
+    
+    Node*root; 
+    
+  public:
+    
+    Trie(){
+        
+        root=new Node();
+        
+    }
+    
+    void insert(string &word){
+        
+        Node*node=root;
+        
+        for(int i=0;i<word.length();++i){
+            
+            if(!node->containsKey(word[i])){
+                
+                node->put(word[i],new Node());
+                
+            }
+            
+            node=node->get(word[i]);
+            
+        }
+        
+        node->setEnd();
+        
+    }
+    
+    bool Search_Word(string &word){
+        
+        Node*node=root;
+        
+        for(int i=0;i<word.length();++i){
+            
+            if(!node->containsKey(word[i])){
+                return false;
+            }
+            
+            node=node->get(word[i]);
+            
+        }
+        
+        return node->isEnd();
+        
+    }
+    
+};
+
+class MagicDictionary {
+public:
+    
+    unordered_set<string>S;
+    
+    MagicDictionary() {
+        
+    }
+    
+    void buildDict(vector<string> dictionary) {
+        
+        for(string str:dictionary){
+            
+            S.insert(str);
+            
+        }
+        
+    }
+    
+    bool search(string word) {
+        
+        int n=word.size();
+        
+        for(int i=0;i<n;++i){
+            
+            char temp=word[i];
+            
+            for(char ch='a';ch<='z';ch++){
+                
+                if(ch==temp){
+                    continue;
+                }
+                
+                word[i]=ch;
+                
+                if(S.count(word)>0){
+                    
+                    return true;
+                    
+                }
+                
+            }
+            
+            word[i]=temp;
+            
+        }
+        
+        return false;
+        
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
