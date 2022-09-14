7cb1a92c787d230b171a7f0d53ef8959bff88e7e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-14 12:33:55 +0530

    Time: 6 ms (47.03%), Space: 8 MB (65.37%) - LeetHub

--- 677-map-sum-pairs/677-map-sum-pairs.cpp
+++ 677-map-sum-pairs/677-map-sum-pairs.cpp
@@ -1,40 +1,189 @@
+struct Node{
+  
+    public:
+
+    Node*links[26];
+    int score;
+    bool flag=false;
+    int wscore;
+    
+    bool containsKey(char ch){
+        
+        return links[ch-'a']!=NULL;
+        
+    }
+    
+    Node*get(char ch){
+        
+        return links[ch-'a'];
+        
+    }
+    
+    void put(char ch,Node*node){
+        
+        links[ch-'a']=node;
+        
+    }
+    
+    void setScore(int sc){
+        
+        score+=sc;
+        
+    }
+    
+    void setEnd(){
+        
+        flag=true;
+        
+    }
+    
+    void removeScore(int sc){
+        
+        score-=sc;
+        
+    }
+    
+    int getScore(){
+        
+        return score;
+        
+    }
+    
+    bool isEnd(){
+        
+        return flag;
+        
+    }
+    
+};
+
+class Trie{
+    
+  public:
+    
+    Node*root;
+    
+    Trie(){
+    
+        root=new Node();
+        
+    }
+    
+    void insert(string &word,int score){
+        
+        Node*node=root;
+        
+        for(int i=0;i<word.length();++i){
+            
+            if(!node->containsKey(word[i])){
+                
+                node->put(word[i],new Node());
+                
+            }
+            
+            node=node->get(word[i]);
+            
+            node->setScore(score);
+            
+        }
+        
+        node->setEnd();
+        
+        node->wscore+=score;
+        
+    }
+    
+    void remove(string &word,int score){
+        
+        Node*node=root;
+        
+        for(int i=0;i<word.length();++i){
+            
+            if(!node->containsKey(word[i])){
+                
+                return;
+                
+            }
+            
+            node=node->get(word[i]);
+            
+            node->removeScore(score);
+            
+        }
+        
+        node->wscore-=score;
+        
+    }
+    
+    int isExist(string &word){
+        
+        Node*node=root;
+        
+        for(int i=0;i<word.length();++i){
+            
+            if(!node->containsKey(word[i])){
+                
+                return 0;
+                
+            }
+            
+            node=node->get(word[i]);
+            
+        }
+        
+        return node->wscore;
+        
+    }
+    
+    int sum_score(string &word){
+        
+        Node*node=root;
+        
+        for(int i=0;i<word.length();++i){
+            
+            if(!node->containsKey(word[i])){
+                
+                return 0;
+                
+            }
+            
+            node=node->get(word[i]);
+            
+        }
+        
+        return node->getScore();
+        
+    }
+    
+};
+
 
 class MapSum {
 public:
     
-    map<string,int>mapp;
+    Trie obj;
     
     MapSum() {
-        mapp.clear();
+        
     }
     
     void insert(string key, int val) {
         
-        mapp[key]=val;
+        int already_exist=obj.isExist(key);
+        
+        if(already_exist){
+            
+            obj.remove(key,already_exist);
+            
+        }
+        
+        obj.insert(key,val);
         
     }
     
     int sum(string prefix) {
         
-        int cnt=0;
-        
-        for(auto itr:mapp){
-            
-            string str=itr.first;
-            
-            if(str.length()<prefix.length()){
-                continue;
-            }
-            
-            if(str.substr(0,prefix.length())==prefix){
-                
-                cnt+=itr.second;
-                
-            }
-            
-        }
-        
-        return cnt;
+        return obj.sum_score(prefix);
         
     }
 };
