4b3f879b1db7ff77890ba4cc4c211411055e45c0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-22 23:37:23 +0530

    Time: 166 ms (43.66%), Space: 13.8 MB (73.88%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,86 @@
+struct Pair{
+    
+  int i;
+  int j;
+  int wt;
+    
+};
+
+class Solution {
+public:
+
+    struct comp{
+      
+        bool operator()(const Pair&x,const Pair&y){
+            
+            return x.wt > y.wt;
+            
+        }
+        
+    };
+    
+    int minCost(vector<vector<int>>& grid) {
+        
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
+        
+        priority_queue<Pair,vector<Pair>,comp>pq;
+        
+        pq.push({0,0,0});
+        
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        int val[4]={1,2,3,4};
+        
+        while(!pq.empty()){
+            
+            auto curr=pq.top();
+            pq.pop();
+            
+            int i=curr.i;
+            int j=curr.j;
+            int wt=curr.wt;
+            
+            if(visited[i][j]){
+                continue;
+            }
+            
+            visited[i][j]=true;
+            
+            if(i==m-1 && j==n-1){
+                
+                return wt;
+                
+            }
+            
+            for(int dir=0;dir<4;dir++){
+                
+                int ni = i + dx[dir];
+                int nj = j + dy[dir];
+                int nwt = wt + (val[dir]!=grid[i][j]);
+                
+                if(ni>=0 && nj>=0 && ni<m && nj<n){
+                    
+                    pq.push({ni,nj,nwt});
+                    
+                }
+                
+            }
+            
+        }
+        
+        return -1;
+        
+    }
+};
+
+// (-1,0) -> North 
+// ( 1,0) -> South
+// (0,-1) -> Left
+// (0, 1) -> Right
+
+// 0 , 0  , 1 , -1
+// 1 , -1 , 0 ,  0
+// 1    2   3    4    
