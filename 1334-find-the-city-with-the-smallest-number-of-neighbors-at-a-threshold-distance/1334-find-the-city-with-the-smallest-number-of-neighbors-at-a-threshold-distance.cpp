8f177845dc72ab23e39331d73f9fe3e702414d39
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-23 00:45:15 +0530

    Time: 168 ms (37.73%), Space: 13 MB (45.98%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    
-    int dijkstra(vector<vector<pair<int,int>>>&graph,int src,int n,int target){
+    int findTheCity(int n, vector<vector<int>>& edges, int target) {
         
-        int cnt_nodes=0;
+        vector<vector<pair<int,int>>>graph(n);
         
-        vector<int>dist(n,1e9);
+        vector<vector<int>>dist(n,vector<int>(n,10000000));
         
-        dist[src]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        
-        pq.push({0,src});
-        
-        
-        while(!pq.empty()){
+        for(int i=0;i<n;++i){
             
-            auto curr=pq.top();
-            pq.pop();
+            dist[i][i]=0;
             
-            int node=curr.second;
-            int wt=curr.first;
+        }
+        
+        for(int i=0;i<edges.size();++i){
             
-            if(dist[node] > wt){
-                
-                continue;
-                
-            }
-
+            dist[edges[i][0]][edges[i][1]]=edges[i][2];
             
-            for(auto nbr:graph[node]){
+            dist[edges[i][1]][edges[i][0]]=edges[i][2];
+            
+            graph[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+            graph[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+            
+        }
+        
+        
+        for(int k=0;k<n;++k){
+            
+            for(int i=0;i<n;++i){
                 
-                int v=nbr.first;
-                int edge_wt=nbr.second;
-                
-                if(dist[v] > dist[node] + edge_wt){
+                for(int j=0;j<n;++j){
                     
-                    dist[v] = dist[node] + edge_wt;
-                    
-                    pq.push({dist[v],v});
+                    if(dist[i][k] + dist[k][j] < dist[i][j]){
+                        
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        
+                    }
                     
                 }
                 
@@ -45,50 +42,36 @@ public:
             
         }
         
-
+        
+        int min_val = 1e9;
+        
+        int min_node = 0;
         
         for(int i=0;i<n;++i){
             
-            if(dist[i] <= target){
+            int cnt=0;
+            
+            for(int j=0;j<n;++j){
                 
-                cnt_nodes++;
+                if(dist[i][j] <= target){
+                    
+                    cnt++;
+                    
+                }
+                
+            }
+            
+            if(cnt<=min_val){
+                
+                min_val = cnt;
+                
+                min_node = i;
                 
             }
             
         }
         
-        return cnt_nodes;
-        
-    }
-    
-    int findTheCity(int n, vector<vector<int>>& edges, int target) {
-        
-        int min_nodes=1e9;
-        int node_no=-1;
-        
-        vector<vector<pair<int,int>>>graph(n);
-        
-        for(int i=0;i<edges.size();++i){
-            
-            graph[edges[i][0]].push_back({edges[i][1],edges[i][2]});
-            graph[edges[i][1]].push_back({edges[i][0],edges[i][2]});
-            
-        }
-        
-        for(int i=0;i<n;++i){
-            
-            int currval=dijkstra(graph,i,n,target);
-            
-            if(currval <= min_nodes){
-                
-                min_nodes = currval;
-                node_no=i;
-                
-            }
-            
-        }
-        
-        return node_no;
+        return min_node;
         
     }
 };
