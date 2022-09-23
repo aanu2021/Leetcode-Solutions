c8712ed5f52afb8ba2ef3ad1f0fe20ff823da1b3
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-23 12:25:50 +0530

    Time: 53 ms (14.27%), Space: 14.5 MB (90.83%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,72 +1,40 @@
 class Solution {
 public:
     
-    vector<int> bfs_red_blue(int n,vector<vector<pair<int,int>>>&graph){
-        
-        queue<pair<int,int>>q;
-        
-        vector<vector<bool>>visited(n,vector<bool>(2,false));
-        
-        q.push({0,0});   // 0 --> Red Color
-        q.push({0,1});  //  1 --> Blue Color
-        
-        int lvl=0;
+    vector<int>bfs_red_blue(int n,vector<vector<int>>&red,vector<vector<int>>&blue){
         
         vector<int>red_dist(n,1e9);
         
         vector<int>blue_dist(n,1e9);
         
-        red_dist[0] = 0;
+        red_dist[0]=0;
         
-        blue_dist[0] = 0;
+        blue_dist[0]=0;
+        
+        queue<pair<int,int>>q;
+        
+        q.push({0,0});
+        
+        q.push({0,1});
         
         
         while(!q.empty()){
             
-            int sz=q.size();
+            auto curr=q.front();
+            q.pop();
             
-            while(sz--){
+            int node=curr.first;
+            int col=curr.second;
+            
+            if(col==0){
                 
-                auto curr=q.front();
-                q.pop();
-                
-                int node=curr.first;
-                int color=curr.second;
-                
-                if(visited[node][color]){
+                for(int nbr:blue[node]){
                     
-                    continue;
-                    
-                }
-                
-                visited[node][color] = true;
-                
-                for(auto nbr:graph[node]){
-                    
-                    int v=nbr.first;
-                    int edge_col=nbr.second;
-                    
-                    if(color==0){
+                    if(blue_dist[nbr] > red_dist[node] + 1){
                         
-                        if(edge_col==1 && blue_dist[v] > red_dist[node] + 1){
-                            
-                            blue_dist[v] = red_dist[node] + 1;
-                            
-                            q.push({v,1});
-                            
-                        }
+                        q.push({nbr,1});
                         
-                    }
-                    
-                    else{
-                        
-                        if(edge_col==0 && red_dist[v] > blue_dist[node] + 1){
-                            
-                            red_dist[v] = blue_dist[node] + 1;
-                            
-                            q.push({v,0});
-                            
-                        }
+                        blue_dist[nbr] = red_dist[node] + 1;
                         
                     }
                     
@@ -74,7 +42,21 @@ public:
                 
             }
             
-            lvl++;
+            else{
+                
+                 for(int nbr:red[node]){
+                    
+                    if(red_dist[nbr] > blue_dist[node] + 1){
+                        
+                        q.push({nbr,0});
+                        
+                        red_dist[nbr] = blue_dist[node] + 1;
+                        
+                    }
+                    
+                }
+                
+            }
             
         }
         
@@ -82,13 +64,9 @@ public:
         
         for(int i=0;i<n;++i){
             
-            dist[i] = min(red_dist[i],blue_dist[i]);
+            dist[i]=min(red_dist[i],blue_dist[i]);
             
-            if(dist[i]>=1e9){
-                
-                dist[i] = -1;
-                
-            }
+            if(dist[i]>=1e9) dist[i]=-1;
             
         }
         
@@ -98,21 +76,23 @@ public:
     
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
         
-        vector<vector<pair<int,int>>>graph(n);
+        vector<vector<int>>rgraph(n);
+        
+        vector<vector<int>>bgraph(n);
         
         for(int i=0;i<redEdges.size();++i){
             
-            graph[redEdges[i][0]].push_back({redEdges[i][1],0});
+            rgraph[redEdges[i][0]].push_back(redEdges[i][1]);
             
         }
         
         for(int i=0;i<blueEdges.size();++i){
             
-            graph[blueEdges[i][0]].push_back({blueEdges[i][1],1});
+            bgraph[blueEdges[i][0]].push_back(blueEdges[i][1]);
             
         }
         
-        return bfs_red_blue(n,graph);
+        return bfs_red_blue(n,rgraph,bgraph);
         
     }
 };
