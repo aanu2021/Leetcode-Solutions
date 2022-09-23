dbb139682953ca05c2e883e0ae0694e1b5abea7f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-23 13:39:02 +0530

    Time: 4 ms (42.57%), Space: 7.3 MB (81.79%) - LeetHub

--- 765-couples-holding-hands/765-couples-holding-hands.cpp
+++ 765-couples-holding-hands/765-couples-holding-hands.cpp
@@ -2,35 +2,32 @@ class Solution {
 public:
     int minSwapsCouples(vector<int>& row) {
         
-        int swaps=0;
-        
         int n=row.size();
         
-        vector<int>ptn(n);
-        vector<int>pos(n);
+        vector<int>pos(n),ptn(n);
         
         for(int i=0;i<n;++i){
-            
             pos[row[i]]=i;
-            ptn[i]=(i%2 ? i-1 : i+1);
-            
+            ptn[i]=(i%2==0 ? i+1 : i-1);
         }
+        
+        int ans=0;
         
         for(int i=0;i<n;++i){
             
             for(int j=ptn[pos[ptn[row[i]]]];j!=i;j=ptn[pos[ptn[row[i]]]]){
                 
-                swaps++;
-                
                 swap(row[i],row[j]);
                 
                 swap(pos[row[i]],pos[row[j]]);
+                
+                ans++;
                 
             }
             
         }
         
-        return swaps;
+        return ans;
         
     }
 };
