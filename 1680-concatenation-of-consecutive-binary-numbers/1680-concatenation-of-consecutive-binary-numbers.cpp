10f01600cc5afccbd86e8df2acef205ab0161bf0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-23 11:16:16 +0530

    Time: 2414 ms (6.52%), Space: 6.1 MB (22.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -5,6 +5,44 @@ public:
     
     const ll M = 1e9 + 7;
     
+    ll mod(ll a){
+        
+        return ((a%M)+M)%M;
+        
+    }
+    
+    ll add(ll a,ll b){
+        
+        return mod(mod(a)+mod(b));
+        
+    }
+    
+    ll mul(ll a,ll b){
+        
+        return mod(mod(a)*mod(b));
+        
+    }
+    
+    ll modPow(ll a,ll b){
+        
+        if(b==0) return 1LL;
+        if(b==1) return a;
+        
+        ll res=1LL;
+        
+        while(b){
+            
+            if(b%2) res=mul(res,a);
+            
+            b/=2;
+            a=mul(a,a);
+            
+        }
+        
+        return res;
+        
+    }
+    
     int concatenatedBinary(int n) {
         
         string str="";
@@ -15,7 +53,11 @@ public:
         
         while(i<=n){
             
-            sum = ((sum << (1 + (int)log2(i)))%M + i) % M;
+            int curr_power = 1 + (int) log2(i);
+            
+            ll curr_value = mul(sum,modPow(2LL,(ll)curr_power));
+            
+            sum = add((ll)i,curr_value);
             
             i++;
             
@@ -26,7 +68,7 @@ public:
     }
 };
 
-// 11 100   
+//     11 100   
     
 //     product = 8
     
