e2b9a642f20935dc80b2509082ac5ddd966540ee
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-24 01:57:02 +0530

    Time: 500 ms (18.79%), Space: 53.9 MB (39.23%) - LeetHub

--- 882-reachable-nodes-in-subdivided-graph/882-reachable-nodes-in-subdivided-graph.cpp
+++ 882-reachable-nodes-in-subdivided-graph/882-reachable-nodes-in-subdivided-graph.cpp
@@ -9,59 +9,55 @@ public:
     
     /*
     
-    Time Complexity  : O(N*N*log(N))
-    Space Complexity : O(N)
+    Time Complexity  : O(E*log(V))
+    Space Complexity : O(V)
     
     */
     
     int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) {
         
-        // vector<vector<int>>graph(n,vector<int>(n,-1));
-        
         unordered_map<int,unordered_map<int,int>>graph;
         
         for(int i=0;i<edges.size();++i){
+            
             graph[edges[i][0]][edges[i][1]]=edges[i][2];
             graph[edges[i][1]][edges[i][0]]=edges[i][2];
+            
         }
         
-        // (Remaining moves , Current Node) //
+        vector<bool>visited(n,false);
+        
+        int total_vis_nodes = 0;
         
         priority_queue<pair<int,int>>pq;
         
         pq.push({maxMoves,0});
         
-        vector<bool>visited(n,false);  // Mark the visited nodes 
-        
-        int total_vis_nodes = 0;
-        
-        
         while(!pq.empty()){
             
-            auto curr=pq.top();
-            pq.pop();
+            auto curr=pq.top();pq.pop();
             
-            int node = curr.second;
-            int moves = curr.first;
-
+            int node=curr.second;
+            int moves=curr.first;
             
             if(visited[node]) continue;
             
             visited[node] = true;
             
-            total_vis_nodes++;  // As we visit the current node
+            total_vis_nodes++;
             
-            for(auto nbrs:graph[node]){
+            
+            for(auto nbrs : graph[node]){
                 
-                int nbr=nbrs.first;
+                int nbr = nbrs.first;
                 
-                if(moves>=graph[node][nbr]+1){
+                if(!visited[nbr] && moves >= 1+graph[node][nbr]){
                     
                     pq.push({moves-graph[node][nbr]-1,nbr});
                     
                 }
                 
-                int minCost = min(moves,graph[node][nbr]);
+                int minCost=min(moves,graph[node][nbr]);
                 
                 graph[node][nbr] -= minCost;
                 graph[nbr][node] -= minCost;
