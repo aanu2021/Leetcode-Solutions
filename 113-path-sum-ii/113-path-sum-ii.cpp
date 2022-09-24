c95a89872322249a3eb58bc1bdb92768dddb7fdb
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-24 10:32:00 +0530

    Time: 18 ms (61.85%), Space: 20 MB (49.33%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    vector<vector<int>>ans;
+    
+    void func(TreeNode*&root,int targetSum,vector<int>&solution){
+        
+        if(!root) return;
+        
+        targetSum-=root->val;
+        
+        if(!root->left && !root->right){
+            
+            if(targetSum==0){
+                
+                solution.push_back(root->val);
+                
+                ans.push_back(solution);
+                
+                solution.pop_back();
+                
+            }
+            
+        }
+        
+        solution.push_back(root->val);
+        
+        func(root->left,targetSum,solution);
+        
+        func(root->right,targetSum,solution);
+        
+        solution.pop_back();
+        
+    }
+    
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        
+        if(!root) return {};
+        
+        vector<int>solution;
+        
+        func(root,targetSum,solution);
+        
+        return ans;
+        
+    }
+};
