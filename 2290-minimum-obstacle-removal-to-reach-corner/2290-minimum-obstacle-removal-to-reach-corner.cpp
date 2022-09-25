220f1896c16bd8d5be9bce61ba1ff6e831d46ba1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-26 01:09:56 +0530

    Time: 2193 ms (11.51%), Space: 103.4 MB (87.40%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,69 @@
+struct Pair{
+  
+    int i;
+    int j;
+    int wt;
+    
+};
+
+class Solution {
+public:
+    
+    struct comp{
+      
+        bool operator()(const Pair&x,const Pair&y){
+            
+            return x.wt > y.wt;
+            
+        }
+        
+    };
+    
+    int minimumObstacles(vector<vector<int>>& grid) {
+        
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
+        
+        priority_queue<Pair,vector<Pair>,comp>pq;
+        
+        pq.push({0,0,0});
+        
+        int dx[4] = {0,0,1,-1};
+        int dy[4] = {1,-1,0,0};
+        
+        while(!pq.empty()){
+            
+            auto curr=pq.top();
+            pq.pop();
+            
+            int i=curr.i;
+            int j=curr.j;
+            int wt=curr.wt;
+            
+            if(visited[i][j]) continue;
+            
+            if(i==m-1 && j==n-1) return wt;
+            
+            visited[i][j] = true;
+            
+            for(int dir=0;dir<4;dir++){
+                
+                int x1=i+dx[dir];
+                int y1=j+dy[dir];
+                
+                if(x1>=0 && y1>=0 && x1<m && y1<n && visited[x1][y1]==false){
+                    
+                    pq.push({x1,y1,wt+grid[x1][y1]});
+                    
+                }
+                
+            }
+            
+        }
+        
+        return -1;
+        
+    }
+};
