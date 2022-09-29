795e50f5dfe8c098517258ff51f7b736befdc74e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-30 01:46:16 +0530

    Time: 137 ms (23.59%), Space: 27 MB (47.63%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -17,11 +17,32 @@ public:
        
         int n=s.length();
         
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
-        int ans = func(s,0,n-1,dp);
+        for(int i=0;i<n;++i){
+            dp[i][i]=0;
+        }
         
-        return ans;
+        for(int i=0;i<n-1;++i){
+            if(s[i]==s[i+1]){
+                dp[i][i+1]=0;
+            }else{
+                dp[i][i+1]=1;
+            }
+        }
+        
+        for(int L=3;L<=n;L++){
+            for(int i=0;i<n-L+1;++i){
+                int j=i+L-1;
+                if(s[i]==s[j]){
+                    dp[i][j]=dp[i+1][j-1];
+                }else{
+                    dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[0][n-1];
         
     }
 };
