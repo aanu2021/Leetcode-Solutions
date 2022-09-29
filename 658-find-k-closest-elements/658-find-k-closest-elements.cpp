a2d6f501173949b77a76cb278568681f409eb514
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-29 10:46:33 +0530

    Time: 101 ms (55.53%), Space: 31 MB (98.27%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,99 +1,23 @@
 class Solution {
 public:
+    
     vector<int> findClosestElements(vector<int>& nums, int k, int x) {
         
         int n=nums.size();
         
-        int idx=lower_bound(nums.begin(),nums.end(),x)-nums.begin();
+        int low=0,high=n-1;
         
-        int mid = idx;
-        
-        int i,j;
-        
-        if(mid==0){
+        while(high-low>=k){
             
-            i=mid;
-            j=mid+1;
-            
-        }
-        
-        else{
-            
-            if(mid+1<n){
-                
-                if(abs(nums[mid-1]-x)<=abs(nums[mid+1]-x)){
-                    
-                    i=mid-1;
-                    j=mid;
-                    
-                }
-                
-                else{
-                    
-                    i=mid;
-                    j=mid+1;
-                    
-                }
-                
-            }
-            
-            else{
-                
-                i=mid-1;
-                j=mid;
-                
+            if(abs(nums[low]-x) <= abs(nums[high]-x)){
+                high--;
+            }else{
+                low++;
             }
             
         }
         
-        
-        vector<int>result;
-        
-        while(i>=0 && j<n && k>0){
-            
-            if(abs(nums[i]-x)<=abs(nums[j]-x)){
-                
-                result.push_back(nums[i]);
-                
-                i--;
-                
-            }
-            
-            else{
-                
-                result.push_back(nums[j]);
-                
-                j++;
-                
-            }
-            
-            k--;
-            
-        }
-        
-        while(i>=0 && k>0){
-            
-            result.push_back(nums[i]);
-            
-            i--;
-            
-            k--;
-            
-        }
-        
-        while(j<n && k>0){
-            
-            result.push_back(nums[j]);
-            
-            j++;
-            
-            k--;
-            
-        }
-        
-        sort(result.begin(),result.end());
-        
-        return result;
+        return vector<int>(nums.begin()+low,nums.begin()+high+1);
         
     }
 };
