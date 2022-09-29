d808531c6e7810a9e7a1abddaa2b870ba2fa32e6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-29 23:55:26 +0530

    Time: 423 ms (19.68%), Space: 46.4 MB (12.03%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,43 +1,91 @@
 class Solution {
 public:
+    
+    /*
+    
+         Time Complexity  : O(M*logN) 
+         Space Complexity : O(1)
+    
+    */
+    
+    /*
+    
+       First try to fix either the row or the column , after
+       fixing the column , here we fix the column (do a binary 
+       search on it) and then find the maximum element out of the
+       column . Finally it is required to check the maximum 
+       element of that column (cell) with its adjacent neighbours
+       , if it is greater than both of them , then return that 
+       particular cell , otherwise check recursively for the 
+       right / left half .
+    
+    */
+    
+    /*
+    
+       Algorithm :
+       
+       low = 0  high = n-1
+       
+       mid_col = (low + high) / 2
+       
+       (max_row,mid_col) is the cell containing maximum element out of that column.
+       
+       if(grid[max_row][mid_col] > grid[max_row][mid_col-1] && grid[max_row][mid_col] > grid[max_row][mid_col+1]){
+       
+           print (max_row , mid_col);
+          
+       }
+       
+       else if(grid[max_row][mid_col] < grid[max_row][mid_col-1])       {
+           
+           high = mid_col - 1;
+       
+       } 
+       
+       else{
+       
+           low = mid_col + 1;
+       
+       }
+      
+    
+    */
+    
     vector<int> findPeakGrid(vector<vector<int>>& grid) {
         
-        int m=grid.size();
-        int n=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         
-        int low_col = 0 , high_col = n-1;
+        int low = 0,high = n-1;
         
-        while(low_col <= high_col){
+        while(low<=high){
             
-            int mid_col = (low_col + high_col)/2;
+            int mid=(low+high)/2;
             
-            int max_row = 0;
+            int row=0;
             
             for(int i=0;i<m;++i){
-                
-                if(grid[max_row][mid_col] < grid[i][mid_col]){
-                    
-                    max_row = i;
-                    
+                if(grid[row][mid]<grid[i][mid]){
+                    row=i;
                 }
+            }
+            
+            if((mid==0 || grid[row][mid] > grid[row][mid-1]) && (mid==n-1 || grid[row][mid] > grid[row][mid+1])){
+                
+                return {row,mid};
                 
             }
             
-            if((mid_col==0 || grid[max_row][mid_col] > grid[max_row][mid_col-1]) && (mid_col==n-1 || grid[max_row][mid_col]>grid[max_row][mid_col+1])){
+            else if(mid>0 && grid[row][mid] < grid[row][mid-1]){
                 
-                return {max_row,mid_col};
-                
-            }
-            
-            else if(mid_col > 0 && grid[max_row][mid_col-1] > grid[max_row][mid_col]){
-                
-                high_col = mid_col - 1;
+                high = mid - 1;
                 
             }
             
             else{
                 
-                low_col = mid_col + 1;
+                low = mid + 1;
                 
             }
             
