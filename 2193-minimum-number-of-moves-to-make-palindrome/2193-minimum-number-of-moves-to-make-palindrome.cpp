41806b9948f21aa7be8a37bee24641d246527470
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-09-30 01:55:16 +0530

    Time: 27 ms (67.11%), Space: 6.8 MB (41.51%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -4,36 +4,38 @@ public:
         
         int n=s.length();
         
-        int ans=0;
+        int left = 0 , right = n-1;
         
-        int left=0,right=n-1;
+        int ans = 0;
         
-        while(left<right){
+        while(left < right){
             
-            if(s[left]==s[right]){
+            if(s[left] == s[right]){
+                
                 left++;
                 right--;
                 continue;
+                
             }
             
-            int l=left;
-            int r=right;
+            int l = left;
+            int r = right;
             
             while(s[l]!=s[r]){
                 r--;
             }
             
             if(l==r){
-                swap(s[r],s[r+1]);
+                
                 ans++;
+                swap(s[r],s[r+1]);
                 continue;
+                
             }
             
-            for(int j=r;j<right;j++){
-                
-                swap(s[j],s[j+1]);
+            for(int i=r;i<right;++i){
+                swap(s[i],s[i+1]);
                 ans++;
-                
             }
             
             left++;
@@ -45,3 +47,10 @@ public:
         
     }
 };
+
+
+/*
+l e t t e l
+0 1 2 3 4 5
+    i j 
+    */
