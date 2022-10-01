71a5052214cb01b2befe1a3e345e3dd53e59c47b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-01 13:31:36 +0530

    Time: 825 ms (42.33%), Space: 181.8 MB (21.47%) - LeetHub

--- 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.cpp
+++ 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.cpp
@@ -7,30 +7,36 @@ public:
     
     ll add(ll a,ll b){
         
-        ll c = (a+b);
-        c += M;
-        c %= M;
+        ll c=(a+b);
+        c+=M;
+        c%=M;
         return c;
         
     }
     
-    bool Common(string &s,int i,int j,int len,vector<vector<int>>&same){
-        
-        int n = s.length();
+    bool isCommon(string &s,int i,int j,int len,vector<vector<int>>&same){
         
         int common = same[i][j];
         
-        if(common >= len) return true;
+        if(common >= len){
+            
+            return true;
+            
+        }
         
-        if(j+common < n && s[i+common] < s[j+common]) return true;
+        if(j+common < s.length() && s[i+common] < s[j+common]){
+            
+            return true;
+            
+        }
         
         return false;
         
     }
     
     int numberOfCombinations(string s) {
-        
-        int n = s.length();
+     
+        int n=s.length();
         
         if(s[0]=='0') return 0;
         
@@ -39,38 +45,28 @@ public:
         for(int i=n-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(s[i]==s[j]){
-                    same[i][j] = same[i+1][j+1] + 1;
+                    same[i][j]=same[i+1][j+1]+1;
                 }
             }
         }
         
-        // for(int i=0;i<n;++i){
-        //     for(int j=0;j<n;++j){
-        //         cout<<same[i][j]<<" ";
-        //     }cout<<"\n";
-        // }
-        
         vector<vector<ll>>pref(n,vector<ll>(n,0LL));
         
         for(int j=0;j<n;++j){
-            pref[0][j] = 1LL;
+            pref[0][j]=1LL;
         }
         
         for(int i=1;i<n;++i){
             
             if(s[i]=='0'){
-                
-                pref[i] = pref[i-1];
+                pref[i]=pref[i-1];
                 continue;
-                
             }
             
             for(int j=i;j<n;++j){
                 
                 int len = (j-i+1);
-                
-                int prevstart = i-len;
-                
+                int prevstart = (i-len);
                 ll currsum = 0LL;
                 
                 if(prevstart < 0){
@@ -81,27 +77,28 @@ public:
                 
                 else{
                     
-                    currsum = add(pref[i-1][i-1] , -pref[prevstart][i-1]);
+                    currsum = add(pref[i-1][i-1],-pref[prevstart][i-1]);
                     
-                    if(Common(s,prevstart,i,len,same)){
+                    if(isCommon(s,prevstart,i,len,same)){
                         
                         if(prevstart==0){
-                            
-                            currsum = add(currsum , pref[prevstart][i-1]);
-                            
+
+                            currsum = add(currsum,pref[prevstart][i-1]);
+
                         }
-                        
+
                         else{
-                            
+
                             currsum = add(currsum , add(pref[prevstart][i-1],-pref[prevstart-1][i-1]));
-                            
-                        }
+
+                        }   
                         
                     }
                     
                 }
                 
-                pref[i][j] = add(pref[i-1][j] , currsum);
+                pref[i][j] = add(pref[i-1][j],currsum);
+                
             }
             
         }
