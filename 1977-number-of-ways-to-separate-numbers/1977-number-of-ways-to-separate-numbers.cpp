623528cae3122696f249925a2bae522d86eae4b6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-01 12:36:12 +0530

    Time: 550 ms (60.74%), Space: 124 MB (50.92%) - LeetHub

--- 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.cpp
+++ 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.cpp
@@ -1,11 +1,28 @@
-#define ll long long
-ll mod = 1e9+7;
-ll ad(ll a, ll b){
-    return (a+b+mod)%mod;
-}
 class Solution {
 public:
-    int compare(int i, int j, int len, vector<vector<int>>&same, string &s){
+    
+    typedef long long ll;
+    
+    const ll M = 1e9 + 7;
+    
+    ll mod(ll a){
+        return ((a%M)+M)%M;
+    }
+    
+    ll mul(ll a,ll b){
+        return mod(mod(a)*mod(b));
+    }
+    
+    ll add(ll a,ll b){
+        ll c=(a+b);
+        c+=M;
+        c%=M;
+        return c;
+    }
+    
+    // Whether the last string is greater than or equal to second last string //
+    
+     int compare(int i, int j, int len, vector<vector<int>>&same, string &s){
         //returns true if s[i: i+len-1] <= s[i+len,i+2*len-1] 
         
         int common = same[i][j];   
@@ -14,22 +31,42 @@ public:
         else return 0;
     }
     
+//     int compare(string &s,int i,int j,vector<vector<int>>&same){
+        
+//         int len = (j-i+1);
+        
+//         int common = same[i][j];
+        
+//         if(common >= len) return 1;
+        
+//         if(s[i+common] < s[j+common]) return 1;
+        
+//         return 0;
+        
+//     }
+    
     int numberOfCombinations(string num) {
-        int n = num.length();
+        
+        int n=num.length();
+        
         if(num[0]=='0') return 0;
-
-        vector<vector<int>> same(n+1, vector<int>(n+1)); 
-        //same[i][j] = largest common prefix of num.substr(i) and num.substr(j)
-        for(int i=n-1; i>=0; --i){
-            for(int j=n-1; j>=0; --j){
+        
+        // Store the longest common prefix b/w substring starting at index i and index j //
+        
+        vector<vector<int>>same(n+1,vector<int>(n+1,0));
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=i;j--){
                 if(num[i]==num[j]){
-                    same[i][j] = same[i+1][j+1]+1;
+                    same[i][j]=same[i+1][j+1]+1;
                 }
             }
         }
         
+        // dp[i][j] --> stores the number of ways to form the list , with the last string ending at index j and starting at index i //
+
         
-        vector<vector<int>> pref(n, vector<int>(n)); 
+         vector<vector<int>> pref(n, vector<int>(n)); 
 
         //base case
         for(int j=0; j<n; ++j) pref[0][j] = 1;
@@ -48,23 +85,28 @@ public:
                 else {
                     
                     //when length of second last number < len
-                    count = ad(pref[i-1][i-1], -pref[prevStart][i-1]); //= dp[prevStart+1][i-1] ... dp[i-1][i-1]
+                    count = add(pref[i-1][i-1], -pref[prevStart][i-1]); //= dp[prevStart+1][i-1] ... dp[i-1][i-1]
                     
                     //length of second last number == len
                     if(compare(prevStart,i,len,same,num)){
                         int cnt = (prevStart==0 ? pref[prevStart][i-1] : 
-                                   ad(pref[prevStart][i-1], -pref[prevStart-1][i-1]));
-                        count = ad(count, cnt);
+                                   add(pref[prevStart][i-1], -pref[prevStart-1][i-1]));
+                        count = add(count, cnt);
                     }
                 }
                 
                 
-                pref[i][j] = ad(pref[i-1][j], count);   //updating prefix sum
+                pref[i][j] = add(pref[i-1][j], count);   //updating prefix sum
                 
             }
         }
 
         
         return pref[n-1][n-1];
+        
     }
 };
+
+
+    
+  
