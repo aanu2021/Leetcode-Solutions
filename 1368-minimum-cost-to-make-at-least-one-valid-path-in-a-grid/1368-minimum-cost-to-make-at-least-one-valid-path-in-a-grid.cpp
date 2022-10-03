1e55fcf6de32df2251b1fbbfa9ba9937b4424f5d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-04 00:16:10 +0530

    Time: 187 ms (37.43%), Space: 13.1 MB (94.79%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -7,68 +7,83 @@ Space Complexity : O(M*N)
 
 */
 
+/*
+
+0-1 BFS Algorithm : O(E + V)  [Applicable for 0/1 Edge Weights]
+
+Time Complexity  : O(M*N*4 + M*N)
+Space Complexity : O(M*N)
+
+*/
 
 struct Pair{
-    
-  int i;
-  int j;
-  int wt;
+  
+    int x;
+    int y;
+    int wt;
     
 };
 
 class Solution {
 public:
 
+    struct comp{
+      
+        bool operator()(const Pair&x,const Pair&y){
+            
+            return x.wt > y.wt;
+            
+        }
+        
+    };
     
     int minCost(vector<vector<int>>& grid) {
         
-        int m=grid.size();
-        int n=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         
         vector<vector<bool>>visited(m,vector<bool>(n,false));
         
-        deque<Pair>q;
+        priority_queue<Pair,vector<Pair>,comp>pq;
         
-        q.push_back({0,0,0});
+        pq.push({0,0,0});
         
-        int dx[4]={0,0,1,-1};
-        int dy[4]={1,-1,0,0};
-        int val[4]={1,2,3,4};
+        int dx[4]  = {0,0,1,-1};
+        int dy[4]  = {1,-1,0,0};
+        int val[4] = {1,2, 3,4};
         
-        while(!q.empty()){
+        while(!pq.empty()){
             
-            auto curr=q.front();
-            q.pop_front();
+            auto curr = pq.top();
+            pq.pop();
             
-            int i=curr.i;
-            int j=curr.j;
-            int wt=curr.wt;
+            int x = curr.x;
+            int y = curr.y;
+            int wt = curr.wt;
             
-            if(visited[i][j]){
-                continue;
-            }
-            
-            visited[i][j] = true;
-            
-            if(i==m-1 && j==n-1){
+            if(x==m-1 && y==n-1){
+                
                 return wt;
+                
             }
+            
+            if(visited[x][y]) continue;
+            
+            visited[x][y] = true;
             
             for(int dir=0;dir<4;dir++){
                 
-                int ni = i + dx[dir];
-                int nj = j + dy[dir];
-                int nwt = wt + (grid[i][j]!=val[dir]);
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
                 
-                int factor=(grid[i][j]!=val[dir]);
-                
-                if(ni>=0 && nj>=0 && ni<m && nj<n && visited[ni][nj]==false){
+                if(nx>=0 && nx<m && ny>=0 && ny<n){
                     
-                    if(factor==0){
-                        q.push_front({ni,nj,nwt});
-                    }
-                    else{
-                        q.push_back({ni,nj,nwt});
+                    if(!visited[nx][ny]){
+                        
+                        int nwt = wt + (grid[x][y]!=val[dir]);
+                        
+                        pq.push({nx,ny,nwt});
+                        
                     }
                     
                 }
