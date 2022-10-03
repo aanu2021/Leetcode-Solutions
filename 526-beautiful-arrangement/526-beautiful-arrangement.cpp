a781dbc50e66ae5ce9b22a499694406399d738a9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-04 00:41:45 +0530

    Time: 5 ms (97.09%), Space: 6.9 MB (26.52%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     
-    int func(int i,int n,vector<bool>&visited){
+    int func(int i,int n,int mask,vector<int>&dp){
         
         if(i>n){
             return 1;
         }
         
+        if(dp[mask]!=-1){
+            return dp[mask];
+        }
+        
         int ans = 0;
         
-        for(int val=1;val<=n;val++){
-            if(!visited[val] && (val%i==0 || i%val==0)){
-                visited[val] = true;
-                ans += func(i+1,n,visited);
-                visited[val] = false;
+        for(int j=1;j<=n;j++){
+            if((mask&(1<<j)) && (j%i==0 || i%j==0)){
+                ans += func(i+1,n,(mask^(1<<j)),dp);
             }
         }
         
-        return ans;
+        return dp[mask] = ans;
         
     }
     
     int countArrangement(int n) {
         
-        vector<bool>visited(n+1,false);
+        int mask = (1<<(n+1));
         
-        int ans = func(1,n,visited);
+        vector<int>dp(mask,-1);
         
-        return ans;
+        return func(1,n,mask-1,dp);
         
     }
 };
