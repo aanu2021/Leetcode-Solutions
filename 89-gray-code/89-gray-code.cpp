c35251eac5134129cf72dcb1c2c9034beee89b0d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-04 23:28:23 +0530

    Time: 5 ms (96.27%), Space: 11.9 MB (62.16%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,12 +1,44 @@
+/*
+
+If we can generate the gray code for (n-1) numbers , then it is super simple to generate for (n) numbers.
+
+Algorithm :-> 
+
+Simply generate the sequence for (n-1)-th number and we can make sure that using that sequence we can easily form the n-th number sequence as per our given requirements.
+
+e.g :-
+
+n=1 -> [0,1]
+n=2 -> [0,1,3,2]
+n=3 -> [0,1,3,2,6,7,5,4]
+n=4 -> [0,1,3,2,6,7,5,4,12,13,15,14,10,11,9,8]
+
+*/
+
 class Solution {
 public:
     vector<int> grayCode(int n) {
         
+        if(n==1){
+            return {0,1};
+        }
+        
         vector<int>ans;
         
-        for(int i=0;i<(1<<n);i++){
+        ans.push_back(0);
+        ans.push_back(1);
+        
+        for(int i=2;i<=n;i++){
             
-            ans.push_back(i^(i>>1));
+            int num_add=pow(2,i-1);
+            
+            int sz = ans.size();
+            
+            for(int j=sz-1;j>=0;j--){
+                
+                ans.push_back(num_add+ans[j]);
+                
+            }
             
         }
         
