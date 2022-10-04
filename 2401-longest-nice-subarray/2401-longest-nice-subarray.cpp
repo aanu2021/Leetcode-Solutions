ab745ed000037835384acad4b824ba5c5f31a4a7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-05 01:38:18 +0530

    Time: 448 ms (31.73%), Space: 57 MB (87.20%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,42 +1,12 @@
+/*
+
+Time Complexity  : O(N*32)
+Space Complexity : O(32)
+
+*/
+
 class Solution {
 public:
-    
-    void Mark_Bit(vector<int>&prefix,int num,int &cnt){
-        
-        for(int i=0;i<32;i++){
-            
-            if((num&(1<<i))){
-                
-                prefix[i]++;
-                
-                if(prefix[i] == 2){
-                    cnt++;
-                }
-                
-            }
-            
-        }
-        
-    }
-    
-    
-    void Unmark_Bit(vector<int>&prefix,int num,int &cnt){
-        
-        for(int i=0;i<32;i++){
-            
-            if((num&(1<<i))){
-                
-                prefix[i]--;
-                
-                if(prefix[i] == 1){
-                    cnt--;
-                }
-                
-            }
-            
-        }
-        
-    }
     
     int longestNiceSubarray(vector<int>& nums) {
         
@@ -44,26 +14,22 @@ public:
         
         int maxLen = 0;
         
+        int bit = 0;
+        
         int l = 0, r = 0;
-        
-        int count = 0;
-        
-        vector<int>prefix(32,0);
         
         while(l<n && r<n){
             
-            Mark_Bit(prefix,nums[r],count);
-            
-            while(l<=r && count){
+            while((bit&nums[r])!=0){
                 
-                Unmark_Bit(prefix,nums[l],count);
-                
-                l++;
+                bit^=nums[l++];
                 
             }
             
-            maxLen = max(maxLen,r-l+1);    
-                
+            bit|=nums[r];
+            
+            maxLen = max(maxLen,r-l+1);
+            
             r++;
             
         }
