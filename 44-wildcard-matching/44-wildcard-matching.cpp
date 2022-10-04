c5706a67b83678b80f1c4b82c2883b152b308997
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-05 02:19:44 +0530

    Time: 112 ms (41.33%), Space: 27.9 MB (15.41%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,57 +1,66 @@
 class Solution {
 public:
+    
+    int func(string &s,string &p,int i,int j,vector<vector<int>>&dp){
+        
+        if(i<0 && j<0){
+            return 1;
+        }
+        
+        if(i<0 && j>=0){
+            
+            for(int k=0;k<=j;k++){
+                if(p[k]!='*'){
+                    return 0;
+                }
+            }
+            
+            return 1;
+            
+        }
+        
+        if(i>=0 && j<0){
+            return 0;
+        }
+        
+        if(dp[i][j]!=-1){
+            
+            return dp[i][j];
+            
+        }
+        
+        dp[i][j] = 0;
+        
+        if(s[i]==p[j] || p[j]=='?'){
+            
+            return dp[i][j] = func(s,p,i-1,j-1,dp);
+            
+        }
+        
+        else if(p[j]=='*'){
+            
+            return dp[i][j] = func(s,p,i-1,j,dp) || func(s,p,i,j-1,dp);
+            
+        }
+            
+        return dp[i][j];
+        
+    }
+    
     bool isMatch(string s, string p) {
         
-        int m=s.length();
-        int n=p.length();
+        int m = s.length();
+        int n = p.length();
         
-        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         
-        dp[0][0]=true;
+        int ans = func(s,p,m-1,n-1,dp);
         
-        s="*"+s;
-        p="*"+p;
-        
-        
-        for(int i=1;i<=n;i++){
-         
-            if(p[i]=='*'){
-                dp[0][i]=dp[0][i-1];
-            }
-            else{
-                dp[0][i]=false;
-            }
-            
+        if(ans==1){
+            return true;
+        }else{
+            return false;
         }
-        
-        
-        for(int i=1;i<=m;++i){
-            
-            for(int j=1;j<=n;++j){
-                
-                if(s[i]==p[j] || p[j]=='?'){
-                    
-                    dp[i][j]=dp[i-1][j-1];
-                    
-                }
-                
-                else if(p[j]=='*'){
-                    
-                    dp[i][j]=dp[i][j-1];
-                    
-                    for(int k=i-1;k>=0;k--){
-                        
-                        dp[i][j]=dp[i][j]||dp[k][j-1];
-                        
-                    }
-                    
-                }
-                
-            }
-            
-        }
-        
-        return dp[m][n];
         
     }
 };
