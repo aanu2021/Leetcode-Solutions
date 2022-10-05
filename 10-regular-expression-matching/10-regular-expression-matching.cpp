5eff2604b07570fd9d1486c5ec6880ff7f1a371d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-05 12:26:24 +0530

    Time: 21 ms (34.22%), Space: 7 MB (63.77%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -2,42 +2,38 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         
-       int m=s.length();
-       int n=p.length();
-        
-        s="*"+s;
-        p="*"+p;
+        int m = s.length();
+        int n = p.length();
         
         vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
         
-        dp[0][0]=true;
+        s="#"+s;
+        p="#"+p;
+        
+        dp[0][0] = true;
         
         for(int i=1;i<=n;i++){
+            
             if(p[i]=='*'){
                 dp[0][i]=dp[0][i-2];
             }
+            
         }
+
         
         for(int i=1;i<=m;i++){
             
             for(int j=1;j<=n;j++){
                 
                 if(s[i]==p[j] || p[j]=='.'){
-                    
-                    dp[i][j]=dp[i-1][j-1];
-                    
+                    dp[i][j] = dp[i-1][j-1];
                 }
                 
                 else if(p[j]=='*'){
-                    
-                    dp[i][j]=dp[i][j-2];
-                    
-                    if(s[i]==p[j-1] || p[j-1]=='.'){
-                        
-                        dp[i][j]=dp[i][j] || dp[i-1][j];
-                        
+                    dp[i][j] = dp[i][j-2];
+                    if(p[j-1]=='.' || p[j-1]==s[i]){
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
                     }
-                    
                 }
                 
             }
