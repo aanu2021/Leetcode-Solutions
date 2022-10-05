4cb1e81e8f460ff6b72dfb4a91cd04bba61a77ad
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-05 12:37:03 +0530

    Time: 12 ms (47.72%), Space: 9.3 MB (25.98%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,46 +1,69 @@
 class Solution {
 public:
+    
+    int func(string &s,string &p,int i,int j,vector<vector<int>>&dp){
+        
+        if(i<0 && j<0){
+            return 1;
+        }
+        
+        if(i<0 && j>=0){
+            bool curr = false;
+            vector<bool>vec;
+            for(int k=0;k<=j;k++){
+                curr = false;
+                if(p[k]=='*'){
+                    curr = (k-2>=0 ? vec[k-2] : true);
+                }
+                vec.push_back(curr);
+            }
+            return vec.back();
+        }
+        
+        if(i>=0 && j<0){
+            return 0;
+        }
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+        dp[i][j] = 0;
+        
+        if(s[i]==p[j] || p[j]=='.'){
+            
+            return dp[i][j] = func(s,p,i-1,j-1,dp);
+            
+        }
+        
+        else if(p[j]=='*'){
+            
+            dp[i][j] = func(s,p,i,j-2,dp);
+            
+            if(s[i]==p[j-1] || p[j-1]=='.'){
+                dp[i][j] = max(dp[i][j],func(s,p,i-1,j,dp));
+            }
+            
+        }
+        
+        return dp[i][j];
+        
+    }
+    
     bool isMatch(string s, string p) {
         
         int m = s.length();
         int n = p.length();
         
-        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         
-        s="#"+s;
-        p="#"+p;
+        int ans = func(s,p,m-1,n-1,dp);
         
-        dp[0][0] = true;
-        
-        for(int i=1;i<=n;i++){
-            
-            if(p[i]=='*'){
-                dp[0][i]=dp[0][i-2];
-            }
-            
+        if(ans==1){
+            return true;
+        }else{
+            return false;
         }
-
-        
-        for(int i=1;i<=m;i++){
-            
-            for(int j=1;j<=n;j++){
-                
-                if(s[i]==p[j] || p[j]=='.'){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                
-                else if(p[j]=='*'){
-                    dp[i][j] = dp[i][j-2];
-                    if(p[j-1]=='.' || p[j-1]==s[i]){
-                        dp[i][j] = dp[i][j] || dp[i-1][j];
-                    }
-                }
-                
-            }
-            
-        }
-        
-        return dp[m][n];
         
     }
 };
