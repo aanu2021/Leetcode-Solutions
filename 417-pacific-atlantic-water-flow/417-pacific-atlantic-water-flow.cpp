2a6470cdd5e0364fcb2cb25cd305c534f4bdcb64
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-06 20:07:43 +0530

    Time: 107 ms (27.92%), Space: 17.4 MB (85.53%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,104 +1,70 @@
 class Solution {
 public:
     
-    int dx[4]={0,0,1,-1};
-    int dy[4]={1,-1,0,0};
-    
-    void dfs(vector<vector<int>>&grid,int i,int j,vector<vector<bool>>&visited){
+    void dfs(vector<vector<int>>&grid,int i,int j,int prev,vector<vector<bool>>&visited){
         
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || visited[i][j]){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] < prev || visited[i][j]){
+            
             return;
-        }
-        
-        visited[i][j]=true;
-        
-        for(int dir=0;dir<4;dir++){
-            
-            int ni=i+dx[dir];
-            int nj=j+dy[dir];
-            
-            if(ni>=0 && ni<grid.size() && nj>=0 && nj<grid[0].size() && grid[ni][nj]>=grid[i][j] && visited[ni][nj]==false){
-                
-                dfs(grid,ni,nj,visited);
-                
-            }
             
         }
+        
+        visited[i][j] = true;
+        
+        dfs(grid,i-1,j,grid[i][j],visited);
+        dfs(grid,i+1,j,grid[i][j],visited);
+        dfs(grid,i,j-1,grid[i][j],visited);
+        dfs(grid,i,j+1,grid[i][j],visited);
         
     }
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& grid) {
         
-        int m=grid.size();
-        int n=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         
-        vector<vector<bool>>pacific(m,vector<bool>(n,false));
+        vector<vector<bool>>vis1(m,vector<bool>(n,false));
+        vector<vector<bool>>vis2(m,vector<bool>(n,false));
         
-        vector<vector<bool>>atlantic(m,vector<bool>(n,false));
+        // Water flow from the pacific ocean //
         
-        
-        // For Pacific Ocean //
-        
-        for(int i=0;i<n;++i){
+        for(int i=0;i<n;i++){
             
-            if(pacific[0][i]==false){
-                
-                dfs(grid,0,i,pacific);
-                
-            }
+            dfs(grid,0,i,-1,vis1);
             
         }
         
-        for(int i=1;i<m;++i){
+        for(int i=1;i<m;i++){
             
-            if(pacific[i][0]==false){
-                
-                dfs(grid,i,0,pacific);
-                
-            }
+            dfs(grid,i,0,-1,vis1);
             
         }
         
+        // Water flow from the atlantic ocean //
         
-        // For Atlantic Ocean //
-        
-        
-        for(int i=0;i<m;++i){
+        for(int i=0;i<n;i++){
             
-            if(atlantic[i][n-1]==false){
-                
-                dfs(grid,i,n-1,atlantic);
-                
-            }
+            dfs(grid,m-1,i,-1,vis2);
             
         }
         
-        
-        for(int i=0;i<n-1;++i){
+        for(int i=0;i<m-1;i++){
             
-            if(atlantic[m-1][i]==false){
-                
-                dfs(grid,m-1,i,atlantic);
-                
-            }
+            dfs(grid,i,n-1,-1,vis2);
             
         }
-        
         
         vector<vector<int>>ans;
         
-        for(int i=0;i<m;++i){
-            
-            for(int j=0;j<n;++j){
-                
-                if(pacific[i][j] && atlantic[i][j]){
-                    
+        int cnt = 0;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis1[i][j] && vis2[i][j]){
+                    cnt++;
                     ans.push_back({i,j});
-                    
                 }
-                
             }
-            
         }
         
         return ans;
