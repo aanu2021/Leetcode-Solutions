99b96559fbe72c3677158591b6727822231c68b0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-07 11:41:43 +0530

    Time: 335 ms (14.60%), Space: 37.7 MB (86.96%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,24 +1,33 @@
+/*
+
+Approach : Brute Force Approach
+
+Compare the current interval with all other present intervals.
+
+T.C : O(N^2)
+S.C : O(N)
+
+*/
+
 class MyCalendar {
 public:
+    
+    // Container that will store the starting point and ending point of the intervals //
     
     vector<pair<int,int>>v;
     
     MyCalendar() {
-        
         v.clear();
-        
     }
     
     bool book(int start, int end) {
         
+        // Compare with other intervals //
+        
         for(int i=0;i<v.size();i++){
-            
             if(start < v[i].second && end > v[i].first){
-                
                 return false;
-                
             }
-            
         }
         
         v.push_back({start,end});
