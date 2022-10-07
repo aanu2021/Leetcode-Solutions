106158b4a15bc921fd0d32a91ed97ea9e3252266
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-07 19:35:01 +0530

    Time: 291 ms (22.72%), Space: 26.4 MB (37.06%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,8 +1,22 @@
 /*
 
-Goal : Maximum value of overlapped intervals at some points each time after adding a new interval.How many intervals cover a single point ???
+Goal : Maximum value of overlapped intervals at some points each
+time after adding a new interval.How many intervals cover a 
+single point ???
 
 Approach : Sweep Line Algorithm [Differential Array]
+
+Algorithm : We should take care of maximum number of intervals 
+which are intersected at a particular point.
+
+Now , the problem boils down to find out the maximum number of 
+intervals which are met with each other at a particular point.
+
+For , these we will use a map to store the contribution of each
+and every interval , i.e Give it a (+1) for the starting point
+and (-1) for the ending point. Now , we will calculate the prefix
+sum of the points from the very first to the end , figure out 
+their overall impact.
 
 T.c : O(N^2)
 S.C : O(N)
@@ -13,20 +27,20 @@ class MyCalendarThree {
 public:
     
     map<int,int>diff;
-    
+   
     MyCalendarThree() {
-        
+
         diff.clear();
         
     }
     
     int book(int start, int end) {
         
-        diff[start]++;
-        diff[end]--;
-        
         int curr = 0;
         int maxi = 0;
+        
+        diff[start]++;
+        diff[end]--;
         
         for(auto itr:diff){
             
@@ -40,10 +54,3 @@ public:
     }
 };
 
-    
-
-/**
- * Your MyCalendarThree object will be instantiated and called as such:
- * MyCalendarThree* obj = new MyCalendarThree();
- * int param_1 = obj->book(start,end);
- */
