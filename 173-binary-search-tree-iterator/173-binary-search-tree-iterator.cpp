79cad6af157a7bb38e7739633c013e375ff50435
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-09 20:25:09 +0530

    Time: 76 ms (15.60%), Space: 24.1 MB (87.40%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -1,9 +1,34 @@
-class BSTIterator {
+/*
+
+Approach : Iterative Inorder Traversal (Using a Stack)
+
+Algorithm :
+
+1. At any point of time , we need to find out the minimum valued node among the present ones.
+
+2. Again , we know inorder traversal of a BST gives a sorted array .
+
+3. Using point (2) , one approach would be initialize an vector and store the node values inside the BST by traversing it in an inorder fashion , and make a pointer to iterate through the values from the smallest to the largest.
+
+4. But for these approach , T.C : O(N) , S.C : O(N)
+
+5. Now , the question arises , can we reduce the space complexity , indeed we can.
+
+6. If we maintain a stack and store the Tree Nodes in an inorder fashion then in the worst case 'H' number of nodes will be present inside the stack.
+
+7. Push all the left most nodes from the current node , then start popping them , after popping a node , if we find that some nodes are present in ots right subtree , then starts exploring them , by pushing All left most nodes of that right subtree.
+
+8. For these approach , T.C : O(N) , S.C : O(H)
+
+*/
+
+
+class BSTIterator2 {
 public:
     
     stack<TreeNode*>S;
     
-    BSTIterator(TreeNode* root) {
+    BSTIterator2(TreeNode* root) {
         
         pushAll(root);
         
@@ -46,9 +71,73 @@ public:
     }
 };
 
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
+class BSTIterator {
+public:
+    
+    TreeNode*curr;
+    
+    BSTIterator(TreeNode* root) {
+        
+        curr = root;
+        
+    }
+   
+    
+    int next() {
+        
+        while(curr){
+            
+            if(curr->left==NULL){
+                
+                int value = curr->val;
+                
+                curr = curr->right;
+                
+                return value;
+                
+            }
+            
+            else{
+                
+                TreeNode*prev = curr->left;
+                
+                while(prev && prev->right && prev->right!=curr){
+                    
+                    prev = prev->right;
+                    
+                }
+                
+                if(prev->right==NULL){
+                    
+                    prev->right = curr;
+                    curr = curr->left;
+                    
+                }
+                
+                else{
+                    
+                    int value = curr->val;
+                    
+                    prev->right = NULL;
+                    
+                    curr = curr->right;
+                    
+                    return value;
+                    
+                }
+                
+            }
+            
+        }
+        
+        return -1;
+        
+    }
+    
+    bool hasNext() {
+        
+        return curr;
+        
+    }
+};
+
