a5c4b3561122e26696ed26333decb936727818be
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-11 00:15:22 +0530

    Time: 797 ms (29.44%), Space: 70.8 MB (67.70%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -4,7 +4,9 @@ public:
     bool static comp(const vector<int>&x,const vector<int>&y){
         
         if(x[0]==y[0]){
+            
             return x[1] < y[1];
+            
         }
         
         return x[0] < y[0];
@@ -15,34 +17,34 @@ public:
         
         sort(events.begin(),events.end(),comp);
         
+        int n = events.size();
+        
         priority_queue<int,vector<int>,greater<int>>pq;
         
-        int ans=0;
+        int i = 0;
         
-        int n=events.size();
+        int ans = 0;
         
-        int i=0;
-        
-        for(int d=1;d<=100000;++d){
+        for(int d=1;d<=100002;d++){
             
-           while(!pq.empty() && pq.top()<d){
-               
-               pq.pop();
-               
-           }
+            while(!pq.empty() && pq.top()<d){
+                
+                pq.pop();
+                
+            }
             
-           while(i<n && events[i][0]==d){
-               
-               pq.push(events[i++][1]);
-               
-           }
+            while(i<n && events[i][0]==d){
+                
+                pq.push(events[i++][1]);
+                
+            }
             
-           if(!pq.empty()){
-               
-               pq.pop();
-               ans++;
-               
-           } 
+            if(!pq.empty()){
+                
+                ans++;
+                pq.pop();
+                
+            }
             
         }
         
