8e4aa2ccfc0c33aa2fb9e9170e02f459fbe15bdc
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-11 01:09:51 +0530

    Time: 731 ms (22.92%), Space: 113.4 MB (14.06%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     
-    bool static comp(const vector<int>&x,const vector<int>&y){
-        if(x[0]==y[0]){
-            
-            return x[1] < y[1];
-            
-        }
-        
-        return x[0] < y[0];
-        
-    }
-    
     int func(vector<vector<int>>&events,int idx,int n,int k,vector<vector<int>>&dp){
         
         if(idx>=n){
@@ -22,31 +11,31 @@ public:
             return dp[idx][k];
         }
         
-        int currval=func(events,idx+1,n,k,dp);
+        int currval = func(events,idx+1,n,k,dp);
         
         if(k>0){
             
-            vector<int>temp={events[idx][1],INT_MAX,INT_MAX};
+            vector<int>temp = {events[idx][1],INT_MAX,INT_MAX};
             
-            int j=upper_bound(events.begin(),events.end(),temp)-events.begin();
+            int j = upper_bound(events.begin(),events.end(),temp)-events.begin();
             
-            currval=max(currval,events[idx][2] + func(events,j,n,k-1,dp));
+            currval = max(currval , events[idx][2] + func(events,j,n,k-1,dp));
             
         }
         
-        return dp[idx][k]=currval;
+        return dp[idx][k] = currval;
         
     }
     
     int maxValue(vector<vector<int>>& events, int k) {
         
-        int n=events.size();
+        sort(events.begin(),events.end());
         
-        sort(events.begin(),events.end(),comp);
+        int n = events.size();
         
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
         
-        int ans=func(events,0,n,k,dp);
+        int ans = func(events,0,n,k,dp);
         
         return ans;
         
