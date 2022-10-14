b670235d007f68bde93921aaa83dc415a90cd438
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 12:26:06 +0530

    Time: 836 ms (48.80%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -5,4 +5,17 @@
 # Instead of these we can use combination of CONCAT , SUBSTR , LOWER , UPPER . 
 
 
-SELECT user_id,CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,LENGTH(name)-1))) AS name FROM Users ORDER BY user_id;
+# SELECT user_id,CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,LENGTH(name)-1))) AS name FROM Users ORDER BY user_id;
+
+
+SELECT 
+
+user_id,
+
+CONCAT(UPPER(LEFT(name,1)),LOWER(RIGHT(name,LENGTH(name)-1))) 
+
+AS name
+
+FROM Users
+
+ORDER BY user_id;
