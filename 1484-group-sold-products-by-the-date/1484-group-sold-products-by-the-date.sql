8cd8be9150ee8022e6b8a4f8df6420f085ba3efd
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 13:12:49 +0530

    Time: 570 ms (50.20%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -2,14 +2,16 @@
 
 # Count(Distinct) Group By
 
+# GROUP_CONCAT is used to concatenate all the group of elements 
+
 SELECT sell_date,
 
-COUNT(DISTINCT product) AS num_sold,
-
-GROUP_CONCAT(DISTINCT product ORDER BY product ASC separator ',') AS products 
-
-FROM Activities
-
-GROUP BY sell_date
-
-ORDER BY sell_date ASC;
+       COUNT(DISTINCT product) AS num_sold,
+       
+       GROUP_CONCAT(DISTINCT product ORDER BY product separator ',') As products
+       
+       FROM Activities
+       
+       GROUP BY sell_date
+       
+       ORDER BY sell_date;
