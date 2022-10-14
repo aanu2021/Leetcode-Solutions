fe501ad976dd4a044bd27a6de4e209bb7e9fab0b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 12:20:35 +0530

    Time: 1234 ms (13.03%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,3 +1,5 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 
-SELECT user_id , INITCAP(name) AS name FROM Users ORDER BY user_id;
+# INITCAP is the utility that is used in SQL(Oracle) to do these , but there is no such in-built utility over MySQL
+
+Select user_id, CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,LENGTH(name)))) AS name FROM Users ORDER BY user_id;
