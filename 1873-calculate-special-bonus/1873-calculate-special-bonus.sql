3dc9c1c123a0ea33a4383fadd2de2163aea9713a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 11:26:16 +0530

    Time: 809 ms (41.55%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -14,18 +14,31 @@
 
 
 
-SELECT employee_id,
+SELECT 
 
-       CASE
+       employee_id,
        
-       WHEN employee_id%2=1 AND name NOT LIKE 'M%' THEN salary
-       
-       ELSE 0
-       
-       END 
+       If(employee_id%2 AND name NOT LIKE 'M%' , salary , 0) 
        
        AS bonus
        
-       FROM Employees
+       FROM Employees 
        
-       ORDER BY employee_id;
+       ORDER BY employee_id
+       
+
+# SELECT employee_id,
+
+#        CASE
+       
+#        WHEN employee_id%2=1 AND name NOT LIKE 'M%' THEN salary
+       
+#        ELSE 0
+       
+#        END 
+       
+#        AS bonus
+       
+#        FROM Employees
+       
+#        ORDER BY employee_id;
