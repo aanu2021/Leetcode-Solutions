68757ab48052afad04b34a93adc2f0d3b609cee4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 20:40:25 +0530

    Time: 384 ms (25.90%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -15,11 +15,7 @@
 
 SELECT 
 
-     IFNULL(
-     
      (SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET 1) 
-         
-         , NULL)
      
      AS SecondHighestSalary;
       
