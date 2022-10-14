028fc96572746ee1943e3f1b90588992c56f321b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 12:24:36 +0530

    Time: 903 ms (38.50%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -2,4 +2,7 @@
 
 # INITCAP is the utility that is used in SQL(Oracle) to do these , but there is no such in-built utility over MySQL
 
-Select user_id, CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,LENGTH(name)))) AS name FROM Users ORDER BY user_id;
+# Instead of these we can use combination of CONCAT , SUBSTR , LOWER , UPPER . 
+
+
+SELECT user_id,CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,LENGTH(name)-1))) AS name FROM Users ORDER BY user_id;
