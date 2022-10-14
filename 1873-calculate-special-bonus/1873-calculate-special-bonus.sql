ed31f9a149358510ca83f0548472eacac8e06b42
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 11:23:48 +0530

    Time: 609 ms (83.36%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -2,4 +2,30 @@
 
 # If - Else  /  Case  Statement
 
-SELECT employee_id , IF(employee_id%2 AND name NOT LIKE 'M%' , salary,0) AS bonus FROM Employees ORDER BY employee_id;
+# Syntax --> IF (Condition , if yes , if no)
+
+# Syntax -->  Case
+
+#             WHEN Condition THEN if yes
+             
+#             ELSE if no
+             
+#             END
+
+
+
+SELECT employee_id,
+
+       CASE
+       
+       WHEN employee_id%2=1 AND name NOT LIKE 'M%' THEN salary
+       
+       ELSE 0
+       
+       END 
+       
+       AS bonus
+       
+       FROM Employees
+       
+       ORDER BY employee_id;
