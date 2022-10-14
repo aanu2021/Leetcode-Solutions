ced3d40429b3999bbcf5542cb609ef8787837ca0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 10:44:43 +0530

    Time: 1425 ms (77.19%), Space: 294.8 MB (64.88%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,38 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
         
-        if(!head || !head->next){
-            return NULL;
-        }
+        if(!head) return NULL;
         
-        ListNode*slow=head;
-        ListNode*fast=head;
-        ListNode*follow_slow=head;
+        if(!head->next) return NULL;
+        
+        ListNode*slow = head;
+        ListNode*fast = head;
+        
+        ListNode*follow = NULL;
         
         while(fast && fast->next){
             
-            follow_slow=slow;
-            slow=slow->next;
-            fast=fast->next->next;
+            follow = slow;
+            slow = slow->next;
+            fast = fast->next->next;
             
         }
         
-        ListNode*ptr=follow_slow->next;
-        
-        follow_slow->next=follow_slow->next->next;
-        
-        delete(ptr);
+        if(follow){
+            
+            follow->next = follow->next->next;
+            
+        }
         
         return head;
         
