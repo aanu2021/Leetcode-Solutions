39d6163b714f55df3aee0d672b55c326f472bd11
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 19:55:43 +0530

    Time: 524 ms (52.47%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,26 +1,44 @@
 # Write your MySQL query statement below
 
-SELECT id , 'Root' AS type 
+# SELECT id , 'Root' AS type 
 
-       FROM Tree
+#        FROM Tree
        
-       WHERE p_id IS NULL
+#        WHERE p_id IS NULL
        
-UNION 
+# UNION 
 
-SELECT id , 'Leaf' AS type
+# SELECT id , 'Leaf' AS type
 
-        FROM Tree
+#         FROM Tree
         
-        WHERE id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
+#         WHERE id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
         
-UNION 
+# UNION 
 
-SELECT id , 'Inner' AS type
+# SELECT id , 'Inner' AS type
 
+#          FROM Tree
+         
+#           WHERE id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
+          
+#           ORDER BY id;
+
+
+SELECT id ,
+
+         CASE 
+         
+         WHEN p_id IS NULL THEN 'Root'
+         
+         WHEN id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOt NULL) AND p_id IS NOT NULL THEN 'Leaf'
+         
+         ELSE 'Inner'
+         
+         END 
+         
+         AS type
+         
          FROM Tree
          
-          WHERE id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
-          
-          ORDER BY id;
-
+         ORDER BY id;
