9824f6d3e582bfd63746d025afb27800bc41f801
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-14 19:21:17 +0530

    Time: 548 ms (72.63%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 
+# Union is used to join two set / column . If both the sets contain same entity , then it will be counted only once.
+
+
 SELECT employee_id FROM Employees WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
 
 UNION
@@ -7,3 +10,5 @@ UNION
 SELECT employee_id FROM Salaries WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
 
 ORDER BY employee_id;
+
+
