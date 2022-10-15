793fcd44b619a4ff83380b814d60ade7262cc044
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-16 01:50:45 +0530

    Time: 236 ms (19.87%), Space: 35.5 MB (81.14%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,3 +1,15 @@
+    /*
+
+    sum(speed) * minimum(efficiency)
+    
+    atmost k
+    
+    eff[i] < eff[i+1] < eff[i+2] < eff[i+3] < ....... < eff[n-1]
+    
+    Pick atmost k Top most Speeds 
+    
+    */
+
 class Solution {
 public:
     
@@ -21,54 +33,52 @@ public:
         
         vector<pair<int,int>>vec(n);
         
-        for(int i=0;i<n;++i){
-            
+        for(int i=0;i<n;i++){
             vec[i]={efficiency[i],speed[i]};
-            
         }
         
         sort(vec.begin(),vec.end());
         
-        ll sum_speed=0LL;
+        ll max_perf = 0LL;
         
-        ll min_eff=0LL;
+        ll sum_speed = 0LL;
         
-        ll maxi_per=0LL;
-        
+        ll min_effi = 0LL;
         
         priority_queue<int,vector<int>,greater<int>>pq;
         
+        
         for(int i=n-1;i>=0;i--){
             
-            int curr_speed=vec[i].second;
+            min_effi = vec[i].first;
             
-            min_eff=(ll)vec[i].first;
+            ll curr_speed = vec[i].second;
             
             if(pq.size()<k){
                 
                 pq.push(curr_speed);
                 
-                sum_speed+=(ll)curr_speed;
+                sum_speed += curr_speed;
                 
-                maxi_per=max(maxi_per,(sum_speed*min_eff));
+                max_perf = max(max_perf,(sum_speed*min_effi));
                 
             }
             
-            else if(!pq.empty() && pq.top()<(ll)curr_speed){
+            else if(!pq.empty() && pq.top() < curr_speed){
                 
-                sum_speed-=(ll)pq.top();
+                sum_speed -= pq.top();
                 pq.pop();
                 
+                sum_speed += curr_speed;
                 pq.push(curr_speed);
-                sum_speed+=(ll)curr_speed;
                 
-                maxi_per=max(maxi_per,(sum_speed*min_eff));
+                max_perf = max(max_perf,(sum_speed*min_effi));
                 
             }
             
         }
         
-        return mod(maxi_per);
+        return mod(max_perf);
         
     }
 };
