f4e8148d58a85642bf0c865d423f58f1f64b2c8d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-16 11:07:27 +0530

    Time: 56 ms (72.72%), Space: 7.9 MB (30.04%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -45,13 +45,51 @@ public:
         
         int n = nums.size();
         
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(k+1,1e9));
         
-        int ans = func(nums,0,n,k,dp);
+        int currmax = 0;
         
-        if(ans>=1e9) return -1;
+        for(int i=0;i<n;i++){
+            
+            currmax = max(currmax,nums[i]);
+            
+            dp[i][1] = currmax;
+            
+        }
         
-        return ans;
+        for(int i=0;i<n;i++){
+            
+            for(int day=2;day<=k;day++){
+                
+                int ans = 1e9;
+                
+                int currmax = nums[i];
+                
+                for(int j=i;j>=0;j--){
+                    
+                    currmax = max(currmax,nums[j]);
+                    
+                int prev = (j-1>=0 ? dp[j-1][day-1] : 1e9);
+                    
+                    ans = min(ans,currmax + prev);
+                    
+                }
+                
+                dp[i][day] = ans;
+                
+            }
+            
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<=k;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }cout<<"\n";
+        // }cout<<"\n";
+        
+        if(dp[n-1][k]>=1e9) return -1;
+        
+        return dp[n-1][k];
         
     }
 };
