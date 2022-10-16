ca7f18ca5d08da000058e145b9e7eddf49615c22
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-16 21:55:36 +0530

    Time: 11 ms (57.11%), Space: 6.2 MB (26.51%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     
-    int dp[12][1<<11][2][2];
+    int dp[12][1<<10][2][2];
     
-    int func(string &num,int n,int mask,bool tight,bool leading){
+    int func(string &num,int n,int mask,int leading,int tight){
         
-        if(n==0){
-            return 1;
-        }
+        if(n==0) return 1;
         
-        if(dp[n][mask][tight][leading]!=-1){
-            return dp[n][mask][tight][leading];
-        }
+        if(dp[n][mask][leading][tight]!=-1) return dp[n][mask][leading][tight];
         
-        int ans=0;
+        int ans = 0;
         
-        int ub=tight ? num[num.length()-n]-'0' : 9;
+        int ub = (tight ? num[num.length()-n]-'0' : 9);
         
         if(leading){
             
-            ans+=func(num,n-1,mask,false,true);
+            ans += func(num,n-1,mask,true,false);
             
             for(int dig=1;dig<=ub;dig++){
                 
-                if((mask&(1<<dig))==0)
-                
-                ans+=func(num,n-1,(mask|(1<<dig)),(tight&(ub==dig)),false);
+                ans += func(num,n-1,mask|(1<<dig),false,(tight&(ub==dig)));
                 
             }
             
@@ -35,35 +29,31 @@ public:
             
             for(int dig=0;dig<=ub;dig++){
                 
-                if((mask&(1<<dig))==0)
+                if((mask&(1<<dig))) continue;
                 
-                ans+=func(num,n-1,(mask|(1<<dig)),(tight&(ub==dig)),false);
+                ans += func(num,n-1,mask|(1<<dig),false,(tight&(ub==dig)));
                 
             }
             
         }
         
-        return dp[n][mask][tight][leading]=ans;
+        return dp[n][mask][leading][tight] = ans;
         
     }
     
     int numDupDigitsAtMostN(int n) {
         
-        int m=n;
+        int m = n;
+        
+        string num = to_string(n);
         
         memset(dp,-1,sizeof(dp));
         
-        string s=to_string(n);
+        int ans = func(num,num.length(),0,true,true);
         
-        n=s.length();
+        ans--;
         
-        int non_repeated=func(s,n,0,true,true);
-        
-       // cout<<non_repeated<<"\n";
-        
-        non_repeated--;
-        
-        return (m-non_repeated);
+        return (m-ans);
         
     }
 };
