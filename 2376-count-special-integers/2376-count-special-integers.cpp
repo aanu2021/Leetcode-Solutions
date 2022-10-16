e5e3535cda4f1a7d7cb60833636e65ab9ab4ec61
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-16 21:11:13 +0530

    Time: 16 ms (38.70%), Space: 6.2 MB (32.88%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-   
-     int dp[12][1<<10][2][2];
     
-    int func(string &num,int n,int mask,bool tight,bool leading){
+    int dp[12][1<<10][2][2];
+    
+    int func(string &num,int n,int mask,bool leading,bool tight){
+        if(n==0) return 1;
         
-        if(n==0){
-            
-            return 1;
-            
-        }
+        if(dp[n][mask][leading][tight]!=-1) return dp[n][mask][leading][tight];
         
-        if(dp[n][mask][tight][leading]!=-1){
-            
-            return dp[n][mask][tight][leading];
-            
-        }
+        int ans = 0;
         
+        int ub = (tight ? num[num.length()-n]-'0' : 9);
         
-        int ans=0;
-        
-        int ub=tight ? (num[num.length()-n]-'0') : 9;
-        
-        if(leading==true){
-            
-            ans+=func(num,n-1,mask,false,leading);
+        if(leading){
+ 
+            ans += func(num,n-1,mask,true,false);
             
             for(int dig=1;dig<=ub;dig++){
                 
-                if((mask&(1<<dig))==0){
-                    
-                    ans+=func(num,n-1,(mask|(1<<dig)),(tight&(dig==ub)),false);
-                    
-                }
+                ans += func(num,n-1,mask|(1<<dig),false,(tight&(dig==ub)));
                 
             }
             
@@ -41,38 +27,26 @@ public:
         else{
             
             for(int dig=0;dig<=ub;dig++){
-            
-                if(((mask&(1<<dig))==0)){
-
-                    int nmask=mask;
-                    nmask|=(1<<dig);
-
-                    ans+=func(num,n-1,nmask,(tight&(dig==ub)),false);
-
-                }
-            
-           }
+                
+                if((mask&(1<<dig))) continue;
+                
+                ans += func(num,n-1,mask|(1<<dig),false,(tight&(dig==ub)));
+                
+            }
             
         }
-
         
-        return dp[n][mask][tight][leading] = ans;
+        return dp[n][mask][leading][tight] = ans;
         
     }
     
     int countSpecialNumbers(int n) {
         
-        if(n<10){
-            return n;
-        }
-        
-        string s=to_string(n);
-        
-        n=s.length();
+        string num = to_string(n);
         
         memset(dp,-1,sizeof(dp));
         
-        int ans=func(s,n,0,true,true);
+        int ans = func(num,num.length(),0,true,true);
         
         return (ans-1);
         
