ad3bca3f7e3755829ec55a9f8b949805a1b12b47
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-19 20:22:03 +0530

    Time: 35 ms (18.26%), Space: 12.5 MB (76.98%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,109 +1,58 @@
-struct Node{
-  
-    Node*links[26];
-    bool endWord;
-    string eW;
-    
-    bool containsKey(char ch){
-        
-        return links[ch-'a']!=NULL;
-        
-    }
-    
-    Node*get(char ch){
-        
-        return links[ch-'a'];
-        
-    }
-    
-    void put(char ch,Node*node){
-        
-        links[ch-'a'] = node;
-        
-    }
-    
-};
-
-void addWords(Node*root,string word){
-    
-    Node*node = root;
-    
-    for(int i=0;i<word.length();i++){
-        
-        if(!node->containsKey(word[i])){
-            node->put(word[i],new Node());
-        }
-        
-        node = node->get(word[i]);
-    
-    }
-    
-    node->endWord = true;
-    node->eW = word;
-    
-}
-
-void search(Node*root,int k,vector<string>&list){
-    
-    if(root==NULL) return;
-    
-    if(root->endWord==true){
-        
-        root->endWord = false;
-        list.push_back(root->eW);
-        
-    }
-    
-    if(list.size()==k) return;
-    
-    for(char ch='a';ch<='z';ch++){
-        
-        if(root->containsKey(ch)){
-            
-            search(root->links[ch-'a'],k,list);
-            
-        }
-        
-    }
-    
-}
-
 class Solution {
 public:
+    
+    struct comp{
+        
+      bool operator()(const pair<int,string>&x,const pair<int,string>&y){
+          
+          if(x.first==y.first){
+              
+              return x.second < y.second;
+              
+          }
+          
+          return x.first > y.first;
+          
+      }  
+        
+    };
     
     vector<string> topKFrequent(vector<string>& words, int k) {
         
         int n = words.size();
         
-        unordered_map<string,int>freq;
+        unordered_map<string,int>mp;
         
         for(int i=0;i<n;i++){
-            freq[words[i]]++;
+            mp[words[i]]++;
         }
         
-        vector<Node*>arr(n+1);
+        vector<string>ans;
         
-        for(auto itr:freq){
+        priority_queue<pair<int,string>,vector<pair<int,string>>,comp>pq;
+        
+        for(auto itr:mp){
             
-            if(arr[itr.second]==NULL){
+            pq.push({itr.second,itr.first});
+            
+            if(pq.size() > k){
                 
-                arr[itr.second] = new Node();
+                pq.pop();
                 
             }
             
-            addWords(arr[itr.second],itr.first);
+        }
+        
+        while(!pq.empty()){
+            
+            ans.push_back(pq.top().second);
+            pq.pop();
             
         }
         
-        vector<string>list;
+        reverse(ans.begin(),ans.end());
         
-        for(int i=n;i>=1;i--){
-            
-            search(arr[i],k,list);
-            
-        }
-        
-        return list;
+        return ans;
         
     }
 };
