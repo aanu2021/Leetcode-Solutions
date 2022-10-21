8013ecf81c317a9ddee2008901b6931cfa1b8ea0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-21 20:26:39 +0530

    Time: 3950 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -2,6 +2,6 @@
 
 WITH CTE AS
 
-(SELECT sales_id FROM Orders o LEFT JOIN Company c ON o.com_id = c.com_id WHERE c.name = 'RED')
+(SELECT sales_id FROM Orders o LEFT JOIN Company c ON c.com_id = o.com_id WHERE c.name = 'RED')
 
-SELECT name FROM SalesPerson WHERE sales_id NOT IN (SELECT DISTINCT sales_id FROM CTE);
+SELECT name FROM SalesPerson WHERE sales_id NOT IN(SELECT DISTINCT sales_id FROM CTE);
