ba44ac3e90c682d432ae05b7a61f857c05eb8e27
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-21 12:16:05 +0530

    Time: 355 ms (33.96%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -24,10 +24,15 @@
 #      AS SecondHighestSalary;
       
     
+SELECT
 
-SELECT MAX(DISTINCT salary) AS SecondHighestSalary 
+     IFNULL(
+     
+       (SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET 1) , NULL 
+         
+     ) 
+     
+     AS SecondHighestSalary;
 
-       FROM Employee
-       
-       WHERE salary <> (SELECT MAX(salary) FROM Employee);
+
       
