b4c9190fa14589457d167a8769498f726ca8188b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-22 19:02:12 +0530

    Time: 1583 ms (46.20%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,21 +1,19 @@
 # Write your MySQL query statement below
 
-SELECT 
-
-user_id AS buyer_id, 
+SELECT user_id AS buyer_id,
 
 join_date,
 
 IFNULL(COUNT(order_date),0) AS orders_in_2019
 
-FROM Users 
+FROM Users
 
 LEFT JOIN
 
-Orders 
+Orders
 
 ON user_id = buyer_id
 
-AND YEAR(order_date)='2019'
+AND YEAR(order_date) = '2019'
 
 GROUP BY user_id;
