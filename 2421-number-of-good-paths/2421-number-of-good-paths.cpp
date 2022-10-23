f675b537ae93547d8e3f23adbb88035da9d5ec6a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-24 01:59:33 +0530

    Time: 912 ms (78.43%), Space: 202.5 MB (28.74%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.cpp
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -1,79 +1,63 @@
 class Solution {
 public:
     
-    vector<int>Parent,Rank;
+    vector<int>Parent;
+    vector<int>Rank;
     
     int find(int x){
-        
         if(Parent[x]==x) return x;
-        
         return Parent[x]=find(Parent[x]);
-        
     }
     
     void Union(int x,int y){
-        
         int lx=find(x);
         int ly=find(y);
-        
         if(lx!=ly){
-            
             if(Rank[lx]<Rank[ly]){
-                
                 Parent[lx]=ly;
-                
-            }
-            
-            else{
-                
+            }else{
                 Parent[ly]=lx;
-                
                 if(Rank[lx]==Rank[ly]){
                     Rank[lx]++;
                 }
-                
             }
-            
         }
-        
     }
     
     int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
         
-        int n=vals.size();
-        
-        Parent.resize(n);
-        Rank.resize(n);
-        
-        for(int i=0;i<n;++i){
-            Parent[i]=i;
-            Rank[i]=1;
-        }
-        
+        map<int,vector<int>>values;
         
         map<int,vector<pair<int,int>>>adj;
         
-        map<int,vector<int>>values;
+        int n = vals.size();
         
-        for(int i=0;i<edges.size();++i){
+        for(int i=0;i<n;i++){
+            values[vals[i]].push_back(i);
+        }
+        
+        for(int i=0;i<edges.size();i++){
             
-           int u=edges[i][0],v=edges[i][1];
+            int u = edges[i][0];
+            int v = edges[i][1];
             
             adj[max(vals[u],vals[v])].push_back({u,v});
             
         }
         
-        for(int i=0;i<n;++i){
-            
-            values[vals[i]].push_back(i);
-            
+        Parent.resize(n);
+        Rank.resize(n);
+        
+        for(int i=0;i<n;i++){
+            Parent[i]=i;
+            Rank[i]=1;
         }
         
-        int ans=0;
+        int ans = 0;
         
         for(auto x:adj){
             
-            int curr_val=x.first;
+            int currval = x.first;
             
             for(auto y:x.second){
                 
@@ -83,17 +67,17 @@ public:
             
             map<int,int>cnt;
             
-            for(auto y:values[curr_val]){
+            for(auto y:values[currval]){
                 
                 cnt[find(y)]++;
                 
             }
             
-            for(auto z1:cnt){
+            for(auto z:cnt){
                 
-                int z=z1.second;
+                int curr = z.second;
                 
-                ans+=((z*(z-1))/2);
+                ans+=((curr*(curr-1))/2);
                 
             }
             
