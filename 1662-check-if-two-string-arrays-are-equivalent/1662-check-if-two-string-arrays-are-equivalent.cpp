46c79c7c4350426b6c76bbc7907d52009bfaa496
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-25 10:34:04 +0530

    Time: 12 ms (31.04%), Space: 11.4 MB (89.94%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -8,39 +8,25 @@ public:
         int w1 = 0,w2 = 0;
         int p1 = 0,p2 = 0;
         
-        while(w1<m && w2<n){
+        while(w1 < m && w2 < n){
             
             if(word1[w1][p1]!=word2[w2][p2]) return false;
             
             if(p1 < word1[w1].length()-1){
-                
                 p1++;
-                
             }else{
-                
-                p1 = 0;
-                w1++;
-                
+                p1=0; w1++;
             }
             
-            if(p2 < word2[w2].length()-1){
-                
+            if(p2<word2[w2].length()-1){
                 p2++;
-                
             }else{
-                
-                p2 = 0;
-                w2++;
-                
+                p2=0; w2++;
             }
             
         }
         
-        if(w1==m && w2==n){
-            return true;
-        }else{
-            return false;
-        }
+        return w1==m && w2==n;
         
     }
 };
