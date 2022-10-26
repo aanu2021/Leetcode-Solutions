d1265a794511d97de3565a001d249788030047b2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-26 11:32:22 +0530

    Time: 634 ms (21.93%), Space: 104.8 MB (76.35%) - LeetHub

---
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    typedef long long ll;
+    
+    int shortestSubarray(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        
+        vector<ll>prefix(n,0LL);
+        
+        prefix[0] = (ll)nums[0];
+        
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + (ll)nums[i];
+        }
+        
+        deque<int>q;
+        
+        int minLen = n+1;
+        
+        for(int i=0;i<n;i++){
+            
+            if(prefix[i]>=k){
+                
+                minLen = min(minLen,i+1);
+                
+            }
+            
+            while(!q.empty() && prefix[q.back()]>=prefix[i]){
+                q.pop_back();
+            }
+            
+            while(!q.empty() && prefix[i]-prefix[q.front()]>=k){
+                minLen = min(minLen,i-q.front());
+                q.pop_front();
+            }
+            
+            q.push_back(i);
+            
+        }
+        
+        return minLen==n+1 ? -1 : minLen;
+        
+    }
+};
