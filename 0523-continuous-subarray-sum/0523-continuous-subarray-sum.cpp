a1c14a63a16746d3f79c0dc8abf0cf2cd0500ad5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-26 10:46:46 +0530

    Time: 711 ms (5.02%), Space: 117.3 MB (8.90%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        
+        int sum = 0;
+        int maxLen = 0;
+        
+        unordered_map<int,int>mp;
+        
+        mp[0] = -1;
+        
+        for(int i=0;i<n;i++){
+            
+            sum += nums[i];
+            
+            int rem = (sum%k);
+            
+            if(mp.find(rem)!=mp.end()){
+                
+                maxLen = max(maxLen,i-mp[rem]);
+                
+            }
+            
+            else{
+                
+                mp[rem] = i;
+                
+            }
+            
+        }
+        
+        if(maxLen>=2) return true;
+        else return false;
+        
+    }
+};
