2e38a70eeeaacd6b94e791f736ad881e091ffada
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-28 11:12:19 +0530

    Time: 210 ms (10.41%), Space: 29.9 MB (7.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
+    
+    string str_sort(string &str){
+        
+        int n = str.length();
+        
+        vector<int>count(26,0);
+        
+        for(char ch:str){
+            count[ch-'a']++;
+        }
+        
+        string res="";
+        
+        for(int i=0;i<26;i++){
+            res+=to_string(count[i]);
+            res+=(char)(i+'a');
+        }
+        
+        return res;
+        
+    }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
         unordered_map<string,vector<string>>mapp;
         
-        for(int i=0;i<strs.size();i++){
-            string str=strs[i];
-            string sort_str=str;
-            sort(sort_str.begin(),sort_str.end());
-            mapp[sort_str].push_back(str);
+        // for(int i=0;i<strs.size();i++){
+        //     string str=strs[i];
+        //     string sort_str=str;
+        //     sort(sort_str.begin(),sort_str.end());
+        //     mapp[sort_str].push_back(str);
+        // }
+        
+        for(string &str:strs){
+            mapp[str_sort(str)].push_back(str);
         }
         
         vector<vector<string>>ans;
