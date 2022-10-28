1edbc920a507404971d65c43251dbaec4fab64ec
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-29 01:28:57 +0530

    Time: 10 ms (95.57%), Space: 7.2 MB (80.71%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,49 +1,21 @@
 class Solution {
 public:
     
-    int dp[302][5002];
-    
-    int func(vector<int>&coins,int i,int amount,int n){
-        
-        if(amount==0){
-            return 1;
-        }
-        
-        if(i>=n){
-            if(amount==0){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
-        
-        if(dp[i][amount]!=-1){
-            return dp[i][amount];
-        }
-        
-        int ans = 0;
-        
-        ans = func(coins,i+1,amount,n);
-        
-        if(coins[i]<=amount){
-            
-            ans += func(coins,i,amount-coins[i],n);
-            
-        }
-        
-        return dp[i][amount] = ans;
-        
-    }
-    
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
         
-        memset(dp,-1,sizeof(dp));
+        vector<int>dp(amount+1,0);
         
-        int ans = func(coins,0,amount,n);
+        dp[0] = 1;
         
-        return ans;
+        for(int i=0;i<coins.size();i++){
+            for(int j=coins[i];j<=amount;j++){
+                dp[j]+=dp[j-coins[i]];
+            }
+        }
+        
+        return dp[amount];
         
     }
 };
