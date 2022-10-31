a7f466bcfb81a2cb1191358936acb7ec4e0f4593
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-10-31 10:58:55 +0530

    Time: 4 ms (99.84%), Space: 17.6 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -5,20 +5,29 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<int>rows(m+n,-1);
-        
         for(int i=0;i<m;i++){
+            
             for(int j=0;j<n;j++){
-                int idx=j-i+m;
-                if(rows[idx]==-1){
-                    rows[idx]=grid[i][j];
-                    continue;
-                }else{
-                    if(rows[idx]!=grid[i][j]){
+                
+                // Already Visited Till now.
+                
+                if(grid[i][j]==-1) continue;
+                
+                int x = i, y = j;
+                
+                int value = grid[x][y];
+                
+                while(x<m && y<n){
+                    if(grid[x][y]!=value){
                         return false;
                     }
+                    grid[x][y]=-1;
+                    x++;
+                    y++;
                 }
+                
             }
+            
         }
         
         return true;
