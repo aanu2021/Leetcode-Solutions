b407ebb7a5250af569894896b6eb3797f1ceb781
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-05 11:14:28 +0530

    Time: 1248 ms (51.12%), Space: 15.5 MB (24.04%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,106 @@
+struct Node{
+  
+    Node*links[26];
+    string word;
+    int endWord;
+    
+    bool containsKey(char ch){
+        return links[ch-'a']!=NULL;
+    }
+    
+    Node*get(char ch){
+        return links[ch-'a'];
+    }
+    
+    void put(char ch,Node*node){
+        links[ch-'a']=node;
+    }
+    
+};
+
+class Solution {
+public:
+    
+    vector<string>ans;
+    
+    Node*root = new Node();
+    
+    void insert(string &word){
+        
+        Node*node=root;
+        
+        for(int i=0;i<word.length();i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i],new Node());
+            }
+            node=node->get(word[i]);
+        }
+        
+        node->endWord++;
+        node->word=word;
+        
+    }
+    
+    void func(vector<vector<char>>&board,int i,int j,Node*curr){
+        
+        int m = board.size();
+        int n = board[0].size();
+        
+        if(board[i][j]=='*'){
+            return;
+        }
+        
+        if(!curr->containsKey(board[i][j])){
+            return;
+        }
+        
+        curr = curr->get(board[i][j]);
+        
+        if(curr->endWord>0){
+            
+            curr->endWord--;
+            ans.push_back(curr->word);
+            
+        }
+        
+        char temp = board[i][j];
+        board[i][j] = '*';
+        
+        if(i>0){
+            func(board,i-1,j,curr);
+        }
+        if(j>0){
+            func(board,i,j-1,curr);
+        }
+        if(i<m-1){
+            func(board,i+1,j,curr);
+        }
+        if(j<n-1){
+            func(board,i,j+1,curr);
+        }
+        
+        board[i][j] = temp;
+        
+    }
+    
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        
+        int m=board.size();
+        int n=board[0].size();
+        
+        for(string &str:words){
+            insert(str);
+        }
+        
+        Node*curr = root;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                func(board,i,j,curr);
+            }
+        }
+        
+        return ans;
+        
+    }
+};
