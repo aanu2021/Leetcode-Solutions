29ee67cd74bb716a91b74d2ee3f653b236fc67a1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-06 21:14:47 +0530

    Time: 168 ms (34.96%), Space: 25.8 MB (18.05%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     bool canReach(string s, int minJump, int maxJump) {
         int n = s.length();
-        if(s[n-1]=='1') return false;
         vector<int>dp(n,0);
-        dp[0]=1;
+        dp[0] = 1;
         int prev = 0;
         for(int i=1;i<n;i++){
             if(i>=minJump){
-                prev+=dp[i-minJump];
+                prev += dp[i-minJump];
             }
             if(i>maxJump){
-                prev-=dp[i-maxJump-1];
+                prev -= dp[i-maxJump-1];
             }
-            if(s[i]=='0' && prev>0){
+            if(s[i]=='0' && prev){
                 dp[i] = 1;
             }
         }
