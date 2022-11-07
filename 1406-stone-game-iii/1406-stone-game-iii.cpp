4b05bb1f08fe851e69fd3d2cd448358c8838429e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 01:48:29 +0530

    Time: 240 ms (97.46%), Space: 137.7 MB (40.82%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    string stoneGameIII(vector<int>& arr) {
-        
-        int n=arr.size();
-        
-        vector<int>dp(n+1,INT_MIN);
-        
-        dp[n-1]=arr[n-1];
-        dp[n]=0;
-        
-        for(int i=n-2;i>=0;i--){
-            
-            int currsum=0;
-            
-            int maxsum=INT_MIN;
-            
-            for(int j=i;j<=min(i+2,n-1);j++){
-                
-                currsum+=arr[j];
-                
-                maxsum=max(maxsum,currsum-dp[j+1]);
-                
-            }
-            
-            dp[i]=maxsum;
-            
+    
+    int func(vector<int>&nums,int i,int n,vector<int>&dp){
+        if(i==n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int sum = 0;
+        int ans = -1e9;
+        for(int j=i;j<min(i+3,n);j++){
+            sum+=nums[j];
+            ans = max(ans,sum - func(nums,j+1,n,dp));
         }
-        
-        if(dp[0]>0){
+        return dp[i] = ans;
+    }
+    
+    string stoneGameIII(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,-1);
+        int ans = func(nums,0,n,dp);
+        if(ans>0){
             return "Alice";
-        }
-        
-        else if(dp[0]<0){
+        }else if(ans<0){
             return "Bob";
-        }
-        
-        else{
+        }else{
             return "Tie";
         }
-        
     }
 };
