7993e6124df758e5d08cda408db47824c36d4473
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 01:55:39 +0530

    Time: 86 ms (71.80%), Space: 7.8 MB (57.52%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-        
-        vector<bool>dp(n+1,false);
-        
-        dp[1]=true;
-        
-        for(int i=2;i<=n;i++){
-            
-            dp[i]=false;
-            
+        vector<int>dp(n+1,false);
+        for(int i=1;i<=n;i++){
             for(int j=1;j*j<=i;j++){
-                
-                if(dp[i-(j*j)]==false){
-                    dp[i]=true;
+                if(dp[i-j*j]==false){
+                    dp[i] = true;
                     break;
                 }
-                
             }
-            
         }
-        
         return dp[n];
-        
     }
 };
