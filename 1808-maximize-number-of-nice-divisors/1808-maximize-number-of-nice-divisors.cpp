5b79501d8a3c152fe0b8418782791a08601709b2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 20:44:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.23%) - LeetHub

--- 1808-maximize-number-of-nice-divisors/1808-maximize-number-of-nice-divisors.cpp
+++ 1808-maximize-number-of-nice-divisors/1808-maximize-number-of-nice-divisors.cpp
@@ -1,59 +1,14 @@
 class Solution {
 public:
-    
-    /*
-    
-    A number can be divided into multiple chunks of prime numbers.
-    
-    N = (a*a*a...) * (b*b*b...) * (c*c*c...) ......
-    
-    Total number of occurences of a = x
-    Total number of occurences of b = y
-    Total number of occurences of c = z
-    
-    Total number of nice divisors , which are divisible by all the
-    prime factors (x*y*z)
-    
-    Now p = Maximum number of prime factors given to us.
-    
-    We need to distribute them into multiple chunks of prime factors
-    as per our requirement.
-    
-    */
-    
-    typedef long long ll;
-    
-    const ll M = 1e9 + 7;
-    
-    ll mod(ll a){
-        return ((a%M)+M)%M;
-    }
-    
-    ll mul(ll a,ll b){
-        return mod(mod(a)*mod(b));
-    }
-    
-    ll add(ll a,ll b){
-        return mod(mod(a)+mod(b));
-    }
-    
-    ll modPow(ll a,ll b){
-        if(b==0) return 1LL;
-        if(b==1) return a;
-        ll res = 1LL;
-        while(b){
-            if(b%2) res=mul(res,a);
-            b/=2;
-            a=mul(a,a);
-        }
-        return res;
-    }
-    
-    int maxNiceDivisors(int n) {
-        if(n==0) return 0;
-        if(n<=3) return n;
-        if(n%3==0) return modPow(3LL,n/3);
-        else if(n%3==1) return mul(4LL,modPow(3LL,(n/3)-1));
-        else return mul(2LL,modPow(3LL,n/3));
+    int integerBreak(int n) {
+        
+        if(n==2) return 1;
+        if(n==3) return 2;
+        if(n==4) return 4;
+        
+        if(n%3==0) return pow(3,n/3);
+        else if(n%3==1) return (4*pow(3,(n/3)-1));
+        else return (2*pow(3,n/3));
+        
     }
 };
