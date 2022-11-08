72b6b553d66c36e940cdc80995900b1194080dba
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 19:02:27 +0530

    Time: 3 ms (93.75%), Space: 11.2 MB (75.44%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -29,7 +29,7 @@ public:
         return ans;
     }
     
-    int maxProfit(vector<int>& prices) {
+    int maxProfit3(vector<int>& prices) {
         int n=prices.size();
         vector<vector<int>>dp(n+2,vector<int>(2,0));
         for(int i=n-1;i>=0;i--){
@@ -37,6 +37,32 @@ public:
             dp[i][0] = max(dp[i+1][0],prices[i]+dp[i+2][1]);
         }
         return dp[0][1];
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        
+        int nextbuy = 0,nextsold = 0;
+        int next_nextbuy = 0,next_nextsold = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            
+            int currbuy = 0;
+            int currsold = 0;
+            
+            currbuy  = max(nextbuy,-prices[i]+nextsold);
+            currsold = max(nextsold,prices[i]+next_nextbuy);
+            
+            next_nextbuy = nextbuy;
+            next_nextsold = nextsold;
+            
+            nextbuy = currbuy;
+            nextsold = currsold;
+
+        }
+        
+        return nextbuy;
+        
     }
     
 };
