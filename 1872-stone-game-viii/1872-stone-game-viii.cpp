21bd199ce004d636175e94f0a1c367e8be18ed5a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 12:24:50 +0530

    Time: 575 ms (5.83%), Space: 91 MB (31.55%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int stoneGameVIII(vector<int>& arr) {
+    int stoneGameVIII(vector<int>& nums) {
         
-        int n=arr.size();
+        int n = nums.size();
         
         vector<int>prefix(n,0);
-        
-        prefix[0]=arr[0];
+        prefix[0] = nums[0];
         
         for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+arr[i];
+            prefix[i] = prefix[i-1] + nums[i];
         }
         
         vector<int>dp(n);
         
-        dp[n-2]=prefix[n-1];
+        dp[n-2] = prefix[n-1];
         
         for(int i=n-3;i>=0;i--){
-            
-            dp[i]=max(dp[i+1],prefix[i+1]-dp[i+1]);
-            
+            dp[i] = max(dp[i+1],prefix[i+1]-dp[i+1]);
         }
         
         return dp[0];
         
     }
 };
+  
+       
+    
