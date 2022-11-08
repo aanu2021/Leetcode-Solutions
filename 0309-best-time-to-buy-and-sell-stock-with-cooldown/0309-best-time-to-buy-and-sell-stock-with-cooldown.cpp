edac12e7dbc6e85f4afa566a8c73c12fb343a810
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 18:58:27 +0530

    Time: 14 ms (25.30%), Space: 11.9 MB (48.70%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -22,10 +22,21 @@ public:
         
     }
     
-    int maxProfit(vector<int>& prices) {
+    int maxProfit2(vector<int>& prices) {
         int n=prices.size();
         vector<vector<int>>dp(n,vector<int>(2,-1));
         int ans = func(prices,0,1,n,dp);
         return ans;
     }
+    
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            dp[i][1] = max(dp[i+1][1],-prices[i]+dp[i+1][0]);
+            dp[i][0] = max(dp[i+1][0],prices[i]+dp[i+2][1]);
+        }
+        return dp[0][1];
+    }
+    
 };
