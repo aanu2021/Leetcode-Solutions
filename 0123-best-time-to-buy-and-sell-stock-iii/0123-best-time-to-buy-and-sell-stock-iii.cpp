92c6ebce27b4e843d265fdcdc1db8ccca10671c1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 18:40:36 +0530

    Time: 409 ms (66.11%), Space: 77.8 MB (69.90%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -24,11 +24,16 @@ public:
         int n = prices.size();
         for(int i=0;i<=n;i++){
             for(int j=2;j>=0;j--){
-                dp[i][j][0] = -1;
-                dp[i][j][1] = -1;
+                dp[i][j][0] = 0;
+                dp[i][j][1] = 0;
             }
         }
-        int ans = func(prices,0,2,1);
-        return ans;
+        for(int i=n-1;i>=0;i--){
+            for(int k=2;k>=1;k--){
+                dp[i][k][1] = max(dp[i+1][k][1],-prices[i]+dp[i+1][k][0]);
+                dp[i][k][0] = max(dp[i+1][k][0],prices[i]+dp[i+1][k-1][1]);
+            }
+        }
+        return dp[0][2][1];
     }
 };
