8fca24f6397038b5d628b24764a7d4be9d136302
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 11:46:37 +0530

    Time: 548 ms (58.95%), Space: 113.9 MB (54.74%) - LeetHub

--- 1686-stone-game-vi/1686-stone-game-vi.cpp
+++ 1686-stone-game-vi/1686-stone-game-vi.cpp
@@ -2,51 +2,32 @@ class Solution {
 public:
     int stoneGameVI(vector<int>& a, vector<int>& b) {
         
-        int n=a.size();
+        int n = a.size();
         
-        vector<pair<int,int>>vec(n);
+        vector<pair<int,int>>vec;
         
         for(int i=0;i<n;i++){
-            vec[i]={a[i]+b[i],i};
+            vec.push_back({a[i]+b[i],i});
         }
         
         sort(vec.rbegin(),vec.rend());
         
-        bool flag=true;
-        
-        int alice=0,bob=0;
+        int alice = 0,bob = 0;
+        int k = 0;
         
         for(int i=0;i<n;i++){
-            
-            if(flag==true){
-                
-                alice+=a[vec[i].second];
-                
-                flag=false;
-                
+            if(i%2==0){
+                alice+=a[vec[k].second];
+                k++;
+            }else{
+                bob+=b[vec[k].second];
+                k++;
             }
-            
-            else{
-                
-                bob+=b[vec[i].second];
-                
-                flag=true;
-                
-            }
-            
         }
         
-        if(alice>bob){
-            return 1;
-        }
-        
-        else if(alice<bob){
-            return -1;
-        }
-        
-        else{
-            return 0;
-        }
+        if(alice>bob) return 1;
+        else if(alice<bob) return -1;
+        else return 0;
         
     }
 };
