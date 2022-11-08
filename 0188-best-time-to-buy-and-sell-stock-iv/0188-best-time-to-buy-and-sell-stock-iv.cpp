414552a3a701f1bf78fce6142c49c0fd9e9a3f80
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 18:49:27 +0530

    Time: 163 ms (14.26%), Space: 28.6 MB (5.03%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -17,8 +17,13 @@ public:
     
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(k+1,vector<int>(2,-1)));
-        int ans = func(prices,0,k,1,n,dp);
-        return ans;
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(k+1,vector<int>(2,0)));
+        for(int i=n-1;i>=0;i--){
+            for(int j=k;j>=1;j--){
+                dp[i][j][1] = max(dp[i+1][j][1],-prices[i]+dp[i+1][j][0]);
+                dp[i][j][0] = max(dp[i+1][j][0],prices[i]+dp[i+1][j-1][1]);
+            }
+        }
+        return dp[0][k][1];
     }
 };
