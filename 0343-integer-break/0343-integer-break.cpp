9aee1941a0c468f03c511453203f1cc4339184c4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 20:05:18 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (50.92%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        if(n==4) return 4;
+        if(n==5) return 6;
+        if(n==6) return 9;
         vector<int>dp(n+1,0);
-        dp[1] = 1;
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<i;j++){
-                dp[i]=max(dp[i],j*dp[i-j]);
-                dp[i]=max(dp[i],j*(i-j));
-            }
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 4;
+        dp[5] = 6;
+        dp[6] = 9;
+        for(int i=7;i<=n;i++){
+            dp[i]=3*dp[i-3];
         }
-        // for(int i=1;i<=n;i++){
-        //     cout<<dp[i]<<" ";
-        // }cout<<"\n";
         return dp[n];
     }
 };
