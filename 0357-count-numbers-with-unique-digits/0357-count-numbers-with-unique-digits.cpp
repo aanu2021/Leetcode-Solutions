cee85aebfbbf18945e907745945d96f1d6943462
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 21:50:51 +0530

    Time: 6 ms (9.92%), Space: 5.8 MB (96.85%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -5,25 +5,18 @@ public:
         if(n==0) return 1;
         if(n==1) return 10;
         
-        vector<int>dp(n+1,0);
-        
-        dp[0] = 1;
-        dp[1] = 9;
-        
         int prod = 1;
         int cnt = 9;
+        int sum = 10;
         
         for(int i=2;i<=n;i++){
             
             prod*=cnt;
             cnt--;
             
-            dp[i] = 9*prod;
+            sum += 9*prod;
             
         }
-        
-        int sum = accumulate(dp.begin(),dp.end(),0);
-        
         return sum;
         
     }
