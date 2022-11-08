0a085d6d81a79a03f0b07af4b627de1f55679f0f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 22:06:39 +0530

    Time: 4 ms (63.85%), Space: 7.3 MB (28.78%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -4,27 +4,23 @@ public:
         
         int n = nums.size();
         
-        vector<int>inc(n,1);
-        vector<int>dec(n,1);
+        vector<int>up(n,1);
+        vector<int>down(n,1);
         
         for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    inc[i]=max(inc[i],1+dec[j]);
-                }else if(nums[i]<nums[j]){
-                    dec[i]=max(dec[i],1+inc[j]);
-                }
+            if(nums[i]>nums[i-1]){
+                up[i]=down[i-1]+1;
+                down[i]=down[i-1];
+            }else if(nums[i]<nums[i-1]){
+                down[i]=up[i-1]+1;
+                up[i]=up[i-1];
+            }else{
+                up[i]=up[i-1];
+                down[i]=down[i-1];
             }
         }
         
-        int maxLen = 0;
-        
-        for(int i=0;i<n;i++){
-            maxLen = max(maxLen,inc[i]);
-            maxLen = max(maxLen,dec[i]);
-        }
-        
-        return maxLen;
+        return max(up[n-1],down[n-1]);
         
     }
 };
