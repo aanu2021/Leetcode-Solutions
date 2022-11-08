71ecebb08d1e1f1a705df879ec13360ba14fdf38
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-08 12:07:38 +0530

    Time: 359 ms (75.65%), Space: 85.5 MB (7.33%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     
-    int func(vector<int>&nums,int i,int j,vector<int>&prefix,vector<vector<int>>&dp){
-        
-        if(i>=j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j] = max(prefix[j+1]-prefix[i+1]-func(nums,i+1,j,prefix,dp) , prefix[j]-prefix[i]-func(nums,i,j-1,prefix,dp));
-        
-    }
-    
     int stoneGameVII(vector<int>& nums) {
         int n = nums.size();
         vector<int>prefix(n+1,0);
         for(int i=0;i<n;i++){
             prefix[i+1] = prefix[i] + nums[i];
         }
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int ans = func(nums,0,n-1,prefix,dp);
-        return ans;
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int i=0;i<n-1;i++){
+            dp[i][i+1]=max(nums[i],nums[i+1]);
+        }
+        for(int L=3;L<=n;L++){
+            for(int i=0;i<n-L+1;i++){
+                int j=i+L-1;
+                dp[i][j] = max(prefix[j+1]-prefix[i+1]-dp[i+1][j] , prefix[j]-prefix[i]-dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
     }
 };
