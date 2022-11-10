7d8e0348a29a1206ccf024c4bd30ed05468bd5c3
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-10 11:02:59 +0530

    Time: 378 ms (57.86%), Space: 9.7 MB (92.35%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -26,7 +26,7 @@ public:
         return func(nums,0,target,n,dp);
     }
     
-    bool canPartition(vector<int>& nums) {
+    bool canPartition3(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum%2) return false;
@@ -47,6 +47,21 @@ public:
             }
         }
         return dp[n-1][target];
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2) return false;
+        int target = (sum/2);
+        vector<bool>dp(target+1,false);
+        dp[0] = true;
+        for(int i=0;i<n;i++){
+            for(int j=target;j>=nums[i];j--){
+                dp[j] = dp[j] || dp[j-nums[i]];
+            }
+        }
+        return dp[target];
     }
     
 };
