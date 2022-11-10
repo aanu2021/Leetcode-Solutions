71589cbff6879366587ee218ec63ff6ed2517907
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-10 10:50:46 +0530

    Time: 12 ms (98.62%), Space: 10.4 MB (84.64%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    string removeDuplicates(string s) {
+    
+    string removeDuplicates2(string s) {
         int n = s.length();
         string res="";
         for(int i=0;i<n;i++){
@@ -12,4 +13,21 @@ public:
         }
         return res;
     }
+    
+    string removeDuplicates(string s) {
+        int n = s.length();
+        int left = 0,right = 0;
+        for(right=0;right<n;right++){
+            if(left>0 && s[left-1]==s[right]){
+                left--;
+            }else{
+                s[left] = s[right];
+                left++;
+            }
+        }
+        return s.substr(0,left);
+    }
+    
 };
+
+   
