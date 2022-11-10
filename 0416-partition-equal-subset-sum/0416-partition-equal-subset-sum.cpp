d69b44cd820341d1237c4e27fd6f64548f986dea
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-10 10:57:14 +0530

    Time: 738 ms (23.88%), Space: 84.9 MB (37.28%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    int func(vector<int>&nums,int i,int target,int n,vector<vector<int>>&dp){
+        
+        if(target==0) return 1;
+        
+        if(i==n){
+            if(target==0){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        
+        if(dp[i][target]!=-1) return dp[i][target];
+        
+        int ans = 0;
+        
+        ans = func(nums,i+1,target,n,dp);
+        
+        if(target>=nums[i]){
+            ans = max(ans,func(nums,i+1,target-nums[i],n,dp));
+        }
+        
+        return dp[i][target] = ans;
+        
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2) return false;
+        int target = (sum/2);
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return func(nums,0,target,n,dp);
+    }
+};
