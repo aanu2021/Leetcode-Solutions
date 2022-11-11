dc1eb02dff5c613f8bc0ca72d5e1df47d99a4c3d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-11 19:41:53 +0530

    Time: 31 ms (75.53%), Space: 12.4 MB (5.05%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
-    int subarrayGCD(vector<int>& nums, int k) {
+    
+    /*
+    
+    T.C : O(N*N)
+    S.C : O(1)
+    
+    */
+    
+    int subarrayGCD2(vector<int>& nums, int k) {
         int n = nums.size();
         int ans = 0;
         for(int i=0;i<n;i++){
@@ -15,4 +23,31 @@ public:
         }
         return ans;
     }
+    
+    int subarrayGCD(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = 0;
+        unordered_map<int,int>gcd_prev;
+        if(nums[0]==k) ans++;
+        gcd_prev[nums[0]]++;
+        for(int i=1;i<n;i++){
+            int curr = nums[i];
+            unordered_map<int,int>gcd_new;
+            gcd_prev[nums[i]]++;
+            for(auto &itr:gcd_prev){
+                int key = itr.first;
+                int freq = itr.second;
+                int currgcd = __gcd(key,curr);
+                if(currgcd==k) ans+=freq;
+                gcd_new[currgcd]+=freq;
+            }
+            gcd_prev = gcd_new;
+        }
+        return ans;
+    }
+    
 };
+
+
+      
+      
