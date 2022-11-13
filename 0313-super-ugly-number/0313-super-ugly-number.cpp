b348a0db0658c0f3c1611b46a96631a03f45c9ad
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-13 21:28:56 +0530

    Time: 392 ms (72.78%), Space: 10.6 MB (51.22%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.cpp
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -3,7 +3,7 @@ public:
     
     typedef long long ll;
     
-    int nthSuperUglyNumber(int n, vector<int>& primes) {
+    int nthSuperUglyNumber2(int n, vector<int>& primes) {
         
         int m = primes.size();
         vector<ll>pointer(m,0LL);
@@ -27,4 +27,45 @@ public:
         return dp[n-1];
         
     }
+    
+    struct Pair{
+        ll value;
+        ll prime;
+        ll pointer;
+    };
+    
+    struct comp{
+       bool operator()(const Pair&x,const Pair&y){
+           return x.value > y.value;
+       }  
+    };
+    
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        
+        int m = primes.size();
+        
+        vector<ll>dp(n,0LL);
+        dp[0] = 1LL;
+        
+        int i = 1;
+        priority_queue<Pair,vector<Pair>,comp>pq;
+        
+        for(int i=0;i<m;i++){
+            pq.push({primes[i],primes[i],0LL});
+        }
+        
+        while(i<n){
+            auto curr = pq.top();
+            pq.pop();
+            ll curr_value = curr.value;
+            if(curr_value!=dp[i-1]){
+                dp[i++] = curr_value;
+            }
+            pq.push({curr.prime*dp[curr.pointer+1],curr.prime,curr.pointer+1});
+        }
+        
+        return dp[n-1];
+        
+    }
+    
 };
