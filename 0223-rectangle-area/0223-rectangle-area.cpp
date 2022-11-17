e48fbdac3a01b26766d8dd54a312d6b4f427e085
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-17 11:29:30 +0530

    Time: 27 ms (11.31%), Space: 5.9 MB (32.00%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
         
-        int leftx = max(ax1,bx1);
-        int lefty = max(ay1,by1);
-        int rightx = min(ax2,bx2);
-        int righty = min(ay2,by2);
+        int x1 = max(ax1,bx1);
+        int x2 = min(ax2,bx2);
+        int y1 = max(ay1,by1);
+        int y2 = min(ay2,by2);
         
-        // No Intersection //
+        if((x2-x1)<=0 || (y2-y1)<=0){
         
-        if(rightx-leftx<=0 || righty-lefty<=0){
-            
-            return ((ay2-ay1)*(ax2-ax1)) + ((by2-by1)*(bx2-bx1));
+           return ((ax2-ax1)*(ay2-ay1)) + ((bx2-bx1)*(by2-by1));
             
         }
         
-        // Have some Intersecting Region //
-        
         else{
             
-            return ((ay2-ay1)*(ax2-ax1)) + ((by2-by1)*(bx2-bx1)) - ((rightx-leftx)*(righty-lefty));
+           return ((ax2-ax1)*(ay2-ay1)) + ((bx2-bx1)*(by2-by1)) - ((x2-x1)*(y2-y1));
             
         }
         
