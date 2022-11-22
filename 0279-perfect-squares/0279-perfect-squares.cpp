24bf428d37d91462922e03a8108026892df9fd9a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-22 10:53:54 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.73%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numSquares(int n) {
+    int numSquares2(int n) {
        vector<int>dp(n+1,1e9);
        dp[0] = 0;
        dp[1] = 1;
@@ -9,7 +9,41 @@ public:
                dp[i] = min(dp[i],1 + dp[i-j*j]);
            }
        } 
-        
        return dp[n]; 
+    }
+    
+    bool isSquare(int n){
+        int ele = sqrt(n);
+        if(ele*ele==n) return true;
+        else return false;
+    }
+    
+    int numSquares(int n) {
+        
+       // Lagrange's Four Square Theorem 
+        
+       // Every number can be represented as the summation of atmost 4 integers 
+        
+        // NUM = 4^k * (8*m + 7)
+        
+        // When the Given number is itself a square.
+        
+        if(isSquare(n)) return 1;
+        
+        while(n && n%4==0){
+            n/=4;
+        }
+        
+        if(n%8==7) return 4;
+        
+        // Check for 2 , 3
+        
+        for(int i=1;i*i<=n;i++){
+            int rem = n - (i*i);
+            if(isSquare(rem)) return 2;
+        }
+        
+        return 3;
+        
     }
 };
