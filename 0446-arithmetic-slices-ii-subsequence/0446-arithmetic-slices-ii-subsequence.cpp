429580c439d346643fa7c8b8b715ac19d73fe3cd
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-27 11:16:55 +0530

    Time: 1344 ms (56.74%), Space: 154.8 MB (38.93%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    
     typedef long long ll;
-    
     int numberOfArithmeticSlices(vector<int>& nums) {
-        
-        int n=nums.size();
-        
+        int n = nums.size();
+        ll ans = 0LL;
         vector<unordered_map<ll,ll>>dp(n);
-        
-        ll ans=0;
-        
-        for(int i=1;i<n;++i){
-            
-            for(int j=0;j<i;++j){
-                
-                ll diff=(ll)nums[i]-nums[j];
-                
-                ll cnt=0;
-                
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                ll diff = (ll) ((ll)nums[i] - (ll)nums[j]);
+                ll cnt = 0LL;
                 if(dp[j].find(diff)!=dp[j].end()){
-                    
-                    cnt=dp[j][diff];
-                    
+                    cnt = dp[j][diff];
                 }
-                
                 ans+=cnt;
-                
                 dp[i][diff]+=(cnt+1);
-                
             }
-            
         }
-        
         return ans;
-        
     }
 };
