8931080882f5d7d6d10c4f34b7274daf3b2a5d7e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-28 12:49:21 +0530

    Time: 8 ms (53.56%), Space: 8.1 MB (35.24%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
     
-    int func(string str){
+    int solve(string s){
         
-        string temp = str;
-        
-        temp += '?';
-        
-        reverse(str.begin(),str.end());
-        
-        temp+=str;
-        
-        int n = temp.length();
-        
+        int n = s.length();
+        string temp = s;
+        reverse(temp.begin(),temp.end());
+        s+="*";
+        s+=temp;
+        n = s.length();
         vector<int>lps(n,0);
-        
         for(int i=1;i<n;i++){
-            
             int len = lps[i-1];
-            
-            while(len>0 && temp[i]!=temp[len]){
-                
+            while(len>0 && s[len]!=s[i]){
                 len = lps[len-1];
-                
             }
-            
-            if(temp[i]==temp[len]){
-                
+            if(s[i]==s[len]){
                 len++;
-                
             }
-            
             lps[i] = len;
-            
         }
-        
         return lps[n-1];
         
     }
@@ -42,23 +27,15 @@ public:
     string shortestPalindrome(string s) {
         
         int n = s.length();
-        
-        int longest_pref_pal = func(s);
-        
-        int req = (n-longest_pref_pal);
-        
-        string res="";
-        
-        for(int i=n-1;i>=0 && req>0;i--){
-            
-            res+=s[i];
-            
+        int ans = solve(s);
+        int req = (n-ans);
+        int i = n-1;
+        string res = "";
+        while(req>0){
+            res+=s[i--];
             req--;
-            
         }
-        
         res+=s;
-        
         return res;
         
     }
