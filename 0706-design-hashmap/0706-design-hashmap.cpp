5720ed1ccacbd265fa48cfb917bd540eeab18c69
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-29 12:30:31 +0530

    Time: 129 ms (95.84%), Space: 52.2 MB (86.66%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,69 @@
+class MyHashMap {
+public:
+    
+    vector<list<pair<int,int>>>m;
+    int size;
+    
+    MyHashMap() {
+        size = 97;
+        m.resize(size);
+    }
+    
+    int hash(int key){
+        return key%size;
+    }
+    
+    list<pair<int,int>>::iterator search(int key){
+        int i = hash(key);
+        auto it = m[i].begin();
+        while(it!=m[i].end()){
+            if(it->first==key) return it;
+            it++;
+        }
+        return m[i].end();
+    }
+    
+    bool containsKey(int key){
+        auto it=search(key);
+        int i = hash(key);
+        if(it==m[i].end()) return false;
+        else return true;
+    }
+    
+    void put(int key, int value) {
+        int i = hash(key);
+        if(containsKey(key)){
+            auto it = search(key);
+            it->second = value;
+        }else{
+            m[i].push_back({key,value});
+        }
+    }
+    
+    int get(int key) {
+        int i = hash(key);
+        if(containsKey(key)){
+            auto it = search(key);
+            return it->second;
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    void remove(int key) {
+        int i =  hash(key);
+        if(containsKey(key)){
+            auto it = search(key);
+            m[i].erase(it);
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
