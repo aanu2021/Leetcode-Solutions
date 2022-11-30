6b12a15e434ecf0dcdd1adce7dec3949cde9bb34
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2022-11-30 11:25:27 +0530

    Time: 3 ms (86.20%), Space: 8.1 MB (63.54%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         int n = arr.size();
-        int mini=arr[0];
-        for(int i=1;i<n;i++){
-            mini=min(mini,arr[i]);
-        }
-        for(int i=0;i<n;i++){
-            arr[i]-=mini;
-        }
         unordered_map<int,int>freq;
         for(int i=0;i<n;i++){
             freq[arr[i]]++;
