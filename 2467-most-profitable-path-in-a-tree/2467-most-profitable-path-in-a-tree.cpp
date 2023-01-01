ee9e6f850e742b3c64d4d861b678eb12c056f9fc
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-02 00:28:10 +0530

    Time: 460 ms (96.23%), Space: 155.3 MB (71.95%) - LeetHub

--- 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
@@ -3,11 +3,8 @@ public:
     
     vector<vector<int>>graph;
     vector<int>parent;
-    vector<int>subtree;
     vector<int>dist;
     vector<int>value;
-    
-    // first DFS function will calculate the smallest distance of the current node from the root node.
     
     void dfs1(int u,int p,int lvl){
         parent[u] = p;
@@ -18,11 +15,9 @@ public:
         }
     }
     
-    // Second DFS function calculates the path with maximum amount from the root node to any of the possible leaf nodes.
-    
     int dfs2(int u,int p){
-        int maxi = -1e9;
         int curr = value[u];
+        int maxi = -1e9;
         for(int v:graph[u]){
             if(v==p) continue;
             maxi = max(maxi,dfs2(v,u));
@@ -37,31 +32,23 @@ public:
         
         graph.resize(n);
         parent.resize(n,-1);
-        subtree.resize(n,0);
-        value.resize(n,0);
         dist.resize(n,0);
-        
+        value.resize(n);
         
         for(int i=0;i<edges.size();i++){
             graph[edges[i][0]].push_back(edges[i][1]);
             graph[edges[i][1]].push_back(edges[i][0]);
-        }
+        }   
         
         dfs1(0,-1,0);
         
-        int node = bob;
         int bob_dist = 0;
         
-        // Now we need to update the path from the root node to any of the leaf node optimally by cosidering the effect of bob's movement .
-        
-        while(node!=0){
-            
-            if(dist[node] > bob_dist) amount[node] = 0;
-            else if(dist[node] == bob_dist) amount[node]/=2;
-            
-            node = parent[node];
+        while(bob!=0){
+            if(bob_dist < dist[bob]) amount[bob] = 0;
+            else if(bob_dist == dist[bob]) amount[bob]/=2;
+            bob = parent[bob];
             bob_dist++;
-            
         }
         
         for(int i=0;i<n;i++){
