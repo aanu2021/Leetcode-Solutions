8aeb24746860fd692e0d82e2a076b23a823eaf28
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-01 23:41:36 +0530

    Time: 44 ms (78.47%), Space: 20.2 MB (18.98%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     string longestPrefix(string s) {
-        
-        int n=s.length();
-        
+        int n = s.length();
         vector<int>lps(n,0);
-        
-        int j=0;
-        
-        lps[0]=0;
-        
         for(int i=1;i<n;i++){
-            
-            int len=lps[i-1];
-            
-            while(len>0 && s[len]!=s[i]){
-                len=lps[len-1];
+            int len = lps[i-1];
+            while(len>0 && s[i]!=s[len]){
+                len = lps[len-1];
             }
-            
-            if(s[i]==s[len]){
-                len++;
-            }
-            
-            lps[i]=len;
-            
+            if(s[i]==s[len]) len++;
+            lps[i] = len;
         }
-        
         return s.substr(0,lps[n-1]);
-        
     }
 };
