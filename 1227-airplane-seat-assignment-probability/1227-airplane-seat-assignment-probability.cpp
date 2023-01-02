aa556d608d0bf580901ab9129f2510b23e7a83a4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-02 11:32:32 +0530

    Time: 0 ms (100.00%), Space: 6 MB (9.85%) - LeetHub

--- 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
+++ 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
@@ -6,43 +6,35 @@ public:
     }
 };
 
+
 /*
 
-1 2 3 4 5
-    
-1 -> 1.0
-2 -> 0.5
-3 -> 0.5
-4 -> 0.5    
-    
-1 2 3  *  
-2 1 3  *
-2 3 1   
-3 2 1   
-    
-1 2 3 4 *
-2 1 3 4 *
-2 3 1 4 *
-2 3 4 1
-2 4 3 1
-3 2 1 4 *
-3 2 4 1
-4 2 3 1
- 
+Case 1 : P1 takes S1
+Case 2 : P1 takes Sn
+Case 3 : P1 takes S2/S3/S4.../Sn-1
 
-There are two possible scenarios :
+Case 1 : probability = 1/n  (Taking exactly one seat out of n seats)
 
-Case 1 : When first person takes the first seat.
-         Then there is only one valid seat arrangement , and it is good seating arrangement as well.
-    
-Case 2 : When first person seat gets swapped with any of the person from person 2 to person n.
-         
-    2 1/3/4
-    
-    2 1 3 4
-    2 3 4 1
-    2 3 1 4
-    2 4 3 1
-    
-    
+Case 2 : Probability = 0 (try to observe carefully if P1 takes the required seat Sn of Pn , then it is impossible for Pn in future to acquire his corresponding seat Sn)
+
+Case 3 : Probability = (n-2)/n * 1/(n-2) * [f(n-1) + f(n-2) + ... + f(2)]
+
+Sum of probabilities = f(n) = 1/n + 1/n * [f(2) + f(3) + ... + f(n-1)]
+
+f(n) = 1/n * [f(1) + f(2) + f(3) + ... + f(n-1)]
+
+n*f(n) = f(1) + f(2) + f(3) + ... + f(n-2) + f(n-1) .... (1)
+
+f(n-1) = 1/(n-1) * [f(1) + f(2) + f(3) + ... f(n-2)]
+
+(n-1)*f(n-1) = f(1) + f(2) + f(3) + ... + f(n-2) ..... (2)
+
+Doing eq..n (1) - eq..n (2) ,
+
+n*f(n) - n*f(n-1) + f(n-1) = f(n-1)
+
+n*f(n) = n*f(n-1)
+
+f(n) = f(n-1)
+
 */   
