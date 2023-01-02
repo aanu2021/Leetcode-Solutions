3d23d8ec9253fb64df39207ef60cb3a5fd3db043
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-02 11:43:54 +0530

    Time: 3 ms (37.91%), Space: 6 MB (9.85%) - LeetHub

--- 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
+++ 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
@@ -6,35 +6,33 @@ public:
     }
 };
 
-
 /*
+
+P1 P2 P3 P4 P5 .... Pn-1 Pn
+S1 S2 S3 S4 S5 .... Sn-1 Sn
 
 Case 1 : P1 takes S1
 Case 2 : P1 takes Sn
-Case 3 : P1 takes S2/S3/S4.../Sn-1
+Case 3 : P1 takes either of S2/S3/S4/.../Sn-1
 
-Case 1 : probability = 1/n  (Taking exactly one seat out of n seats)
+Probability of Case 1 : Taking exactly one seat from n number of seats == 1/n
 
-Case 2 : Probability = 0 (try to observe carefully if P1 takes the required seat Sn of Pn , then it is impossible for Pn in future to acquire his corresponding seat Sn)
+Probability of Case 2 : Taking Sn seat will leave no way for Pn to acquire his  desired seat in future , so probability = 0
 
-Case 3 : Probability = (n-2)/n * 1/(n-2) * [f(n-1) + f(n-2) + ... + f(2)]
+Probability of Case 3 : Taking (n-2) seats in cosideration from total n seats having probability = (n-2)/n . And to get the probability of good seating arrangement 1/(n-2) * [f(n-1) + f(n-2) + f(n-3) + ... + f(2)]
 
-Sum of probabilities = f(n) = 1/n + 1/n * [f(2) + f(3) + ... + f(n-1)]
+f(n) = 1/n + 1/n * [f(2) + f(3) + ... + f(n-1)]
+f(n) = 1/n * [1 + f(2) + f(3) + ... + f(n-1)]
+f(n) = 1/n * [f(1) + f(2) + f(3) + ... + f(n-1)] {as f(1) = 1}
+n*f(n) = f(1) + f(2) + f(3) + .... + f(n-2) + f(n-1)
 
-f(n) = 1/n * [f(1) + f(2) + f(3) + ... + f(n-1)]
+similarly for n-1 number of seats,
 
-n*f(n) = f(1) + f(2) + f(3) + ... + f(n-2) + f(n-1) .... (1)
+(n-1)*f(n-1) = f(1) + f(2) + f(3) + ... + f(n-2)
 
-f(n-1) = 1/(n-1) * [f(1) + f(2) + f(3) + ... f(n-2)]
-
-(n-1)*f(n-1) = f(1) + f(2) + f(3) + ... + f(n-2) ..... (2)
-
-Doing eq..n (1) - eq..n (2) ,
-
+Now , 
 n*f(n) - n*f(n-1) + f(n-1) = f(n-1)
-
 n*f(n) = n*f(n-1)
-
 f(n) = f(n-1)
 
-*/   
+*/
