483f73a13de86beec6667dcf68b3e81073293d5e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-03 11:03:23 +0530

    Time: 92 ms (50.52%), Space: 12.1 MB (72.46%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].length();
+        int ans = 0;
+        for(int i=0;i<m;i++){
+            char ch='a';
+            bool flag = true;
+            for(int j=0;j<n;j++){
+                if(strs[j][i]>=ch){
+                    ch = strs[j][i];
+                }
+                else{
+                    // cout<<ch<<" "<<strs[i][j]<<"\n";
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag) ans++;
+        }
+        return ans;
+    }
+};
