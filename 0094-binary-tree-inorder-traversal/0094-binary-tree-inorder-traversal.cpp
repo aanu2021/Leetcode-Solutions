7adf9663c6067c5ad694c47d0617fd42609045c5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-09 20:29:00 +0530

    Time: 3 ms (59.17%), Space: 8.6 MB (15.59%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,13 +9,40 @@ public:
         inorder_arr.push_back(root->val);
         inorder_recursive(root->right,inorder_arr);
         
+    }
+    
+    void inorder_iterative(TreeNode*&root,vector<int>&inorder_arr)
+    {
+        
+        if(!root) return;
+        stack<TreeNode*>S;
+        TreeNode*curr = root;
+        while(curr){
+            S.push(curr);
+            curr = curr->left;
+        }
+        
+        while(!S.empty()){
+            auto node = S.top();
+            S.pop();
+            inorder_arr.push_back(node->val);
+            if(node->right){
+                curr = node->right;
+                while(curr){
+                    S.push(curr);
+                    curr = curr->left;
+                }
+            }
+        }
+        
     }    
     
     vector<int> inorderTraversal(TreeNode* root) {
         
         if(!root) return {};
         vector<int>inorder_arr;
-        inorder_recursive(root,inorder_arr);
+        // inorder_recursive(root,inorder_arr);
+        inorder_iterative(root,inorder_arr);
         return inorder_arr;
         
     }
