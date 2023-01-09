b15974e8231c446acbc373b8b333b091ebaf83e5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-09 20:05:25 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (44.83%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -7,12 +7,39 @@ public:
         preorder_arr.push_back(root->val);
         preorder_recursive(root->left,preorder_arr);
         preorder_recursive(root->right,preorder_arr);
+    }  
+    
+    void preorder_iterative(TreeNode*&root,vector<int>&preorder_arr)
+    {
+        
+        if(!root) return ;
+        
+        stack<TreeNode*>S;
+        S.push(root);
+        
+        while(!S.empty()){
+            
+            auto node = S.top();
+            S.pop();
+            
+            preorder_arr.push_back(node->val);
+            
+            if(node->right){
+                S.push(node->right);
+            }
+            if(node->left){
+                S.push(node->left);
+            }
+            
+        }
+        
     }    
     
     vector<int> preorderTraversal(TreeNode* root) {
         if(!root) return {};
         vector<int>preorder_arr;
-        preorder_recursive(root,preorder_arr);
+        // preorder_recursive(root,preorder_arr);
+        preorder_iterative(root,preorder_arr);
         return preorder_arr;
     }
 };
