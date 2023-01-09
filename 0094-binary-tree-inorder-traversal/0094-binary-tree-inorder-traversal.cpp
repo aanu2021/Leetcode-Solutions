ddc38b32836ffd1c671b46d22c98d3d8011b84d5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-09 20:36:03 +0530

    Time: 3 ms (59.17%), Space: 8.4 MB (41.96%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -34,12 +34,44 @@ public:
         
     }    
     
+    void inorder_Morris_Traversal(TreeNode*&root,vector<int>&inorder_arr)
+    {
+        
+        TreeNode*curr = root;
+        
+        while(curr){
+            
+            if(curr->left == NULL){
+                inorder_arr.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode*prev = curr->left;
+                while(prev && prev->right && prev->right!=curr){
+                    prev = prev->right;
+                }
+                if(prev->right == curr){
+                    inorder_arr.push_back(curr->val);
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+                else{
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+            }
+            
+        }
+        
+    }    
+    
     vector<int> inorderTraversal(TreeNode* root) {
         
         if(!root) return {};
         vector<int>inorder_arr;
         // inorder_recursive(root,inorder_arr);
-        inorder_iterative(root,inorder_arr);
+        // inorder_iterative(root,inorder_arr);
+        inorder_Morris_Traversal(root,inorder_arr);
         return inorder_arr;
         
     }
