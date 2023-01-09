25bd084cfc993f6c0c539cf908b179b0d2080350
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-09 20:49:27 +0530

    Time: 6 ms (21.18%), Space: 8.4 MB (88.55%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -7,13 +7,32 @@ public:
         postorder_recursive(root->left,postorder_arr);
         postorder_recursive(root->right,postorder_arr);
         postorder_arr.push_back(root->val);
+    }
+    
+    void postorder_iterative(TreeNode*&root,vector<int>&postorder_arr)
+    {
+        if(!root) return;
+        stack<TreeNode*>S;
+        S.push(root);
+        
+        while(!S.empty()){
+            auto node = S.top();
+            S.pop();
+            postorder_arr.push_back(node->val);
+            if(node->left) S.push(node->left);
+            if(node->right) S.push(node->right);
+        }
+        
+        reverse(postorder_arr.begin(),postorder_arr.end());
+        
     }    
     
     vector<int> postorderTraversal(TreeNode* root) {
         
         if(!root) return {};
         vector<int>postorder_arr;
-        postorder_recursive(root,postorder_arr);
+        // postorder_recursive(root,postorder_arr);
+        postorder_iterative(root,postorder_arr);
         return postorder_arr;
         
     }
