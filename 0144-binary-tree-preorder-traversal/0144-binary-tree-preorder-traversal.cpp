fca44e8e94f19f7a6527e3bd8ba997d049f673d9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-09 20:17:17 +0530

    Time: 3 ms (58.98%), Space: 8.4 MB (44.83%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -3,10 +3,12 @@ public:
     
     void preorder_recursive(TreeNode*&root,vector<int>&preorder_arr)
     {
+        
         if(!root) return;
         preorder_arr.push_back(root->val);
         preorder_recursive(root->left,preorder_arr);
         preorder_recursive(root->right,preorder_arr);
+        
     }  
     
     void preorder_iterative(TreeNode*&root,vector<int>&preorder_arr)
@@ -35,11 +37,44 @@ public:
         
     }    
     
+    void preorder_Morris_Traversal(TreeNode*&root,vector<int>&preorder_arr)
+    {
+        
+        TreeNode*curr = root;
+        
+        while(curr){
+            
+            if(curr->left == NULL){
+                preorder_arr.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode * prev = curr->left;
+                while(prev && prev->right!=NULL && prev->right!=curr)
+                {
+                    prev = prev->right;
+                }    
+                if(prev->right == NULL){
+                    preorder_arr.push_back(curr->val);
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+            
+        }
+        
+    }    
+    
     vector<int> preorderTraversal(TreeNode* root) {
         if(!root) return {};
         vector<int>preorder_arr;
         // preorder_recursive(root,preorder_arr);
-        preorder_iterative(root,preorder_arr);
+        // preorder_iterative(root,preorder_arr);
+        preorder_Morris_Traversal(root,preorder_arr);
         return preorder_arr;
     }
 };
