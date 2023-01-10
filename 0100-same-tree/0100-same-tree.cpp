ddeb9da4dbfe2556af52ac1dec41f549dfe1b1d5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-10 10:52:28 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (8.85%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL) return true;
+        if(p==NULL || q==NULL) return false;
+        queue<TreeNode*>q1,q2;
+        q1.push(p); q2.push(q);
+        while(!q1.empty() || !q2.empty()){
+            auto node1 = q1.front(); q1.pop();
+            auto node2 = q2.front(); q2.pop();
+            if(node1==NULL && node2==NULL) continue;
+            if(node1==NULL || node2==NULL) return false;
+            if(node1->val != node2->val) return false;
+            q1.push(node1->left);
+            q1.push(node1->right);
+            q2.push(node2->left);
+            q2.push(node2->right);
+        }
+        return true;
+    }
+};
