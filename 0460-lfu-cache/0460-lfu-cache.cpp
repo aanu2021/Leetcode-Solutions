abda189a2923528889fd421a6d9df7fa79f78931
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-10 13:11:15 +0530

    Time: 579 ms (72.44%), Space: 203.1 MB (13.78%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -19,8 +19,8 @@ class Node{
 };
 
 class List{
-  
-    public:
+    
+  public:
     
     Node*head;
     Node*tail;
@@ -56,49 +56,43 @@ class List{
 class LFUCache {
 public:
     
-    unordered_map<int,List*>freqListMap;  // freq , [n1 , n2 , n3 , .....]
-    unordered_map<int,Node*>keyNode;      // key , node address
-    int capacity;                         // Maximum Capacity
-    int currSize;                         // currsize of the Cache
-    int minFreq;                          // minimum freq among all the currently present nodes
+    unordered_map<int,List*>freqListMap;
+    unordered_map<int,Node*>keyNode;
+    int currSize;
+    int minFreq;
+    int capacity;
     
     LFUCache(int cap) {
+        capacity = cap;
         currSize = 0;
         minFreq = 0;
-        capacity = cap;
         freqListMap.clear();
         keyNode.clear();
     }
     
-    // Below function is used to update the cnt of the current node , which are used how many number of times . 
-    
     void UpdateFreqListMap(Node*node){
-        
         keyNode.erase(node->key);
         freqListMap[node->cnt]->deleteNode(node);
-        
-        if(node->cnt == minFreq && freqListMap[node->cnt]->size==0){
+        if(node->cnt == minFreq && freqListMap[node->cnt]->size == 0)
+        {
             minFreq++;
-        }
-        
+        }    
         node->cnt++;
         List*list = new List();
-        if(freqListMap.find(node->cnt)!=freqListMap.end()){
+        if(freqListMap.find(node->cnt) != freqListMap.end()){
             list = freqListMap[node->cnt];
         }
-        
         list->addNode(node);
-        freqListMap[node->cnt] = list;
         keyNode[node->key] = node;
-        
+        freqListMap[node->cnt] = list;
     }
     
     int get(int key) {
         if(keyNode.find(key)!=keyNode.end()){
             Node*node = keyNode[key];
-            int currvalue = node->val;
+            int nodeValue = node->val;
             UpdateFreqListMap(node);
-            return currvalue;
+            return nodeValue;
         }
         else{
             return -1;
@@ -107,45 +101,32 @@ public:
     
     void put(int key, int value) {
         
-        // Corner Case
         if(capacity == 0) return;
         
-        // We already have a node with the desired key. 
-        
         if(keyNode.find(key)!=keyNode.end()){
-            
             Node*node = keyNode[key];
             node->val = value;
             UpdateFreqListMap(node);
-            
         }
-        
-        // There is no node with that particular value.
-        
         else{
-            
             if(currSize == capacity){
                 List*list = freqListMap[minFreq];
                 keyNode.erase(list->tail->prev->key);
-                list->deleteNode(list->tail->prev);
-               // freqListMap[minFreq] = list;
+                freqListMap[minFreq]->deleteNode(list->tail->prev);
                 currSize--;
             }
-            
             currSize++;
-            Node*node = new Node(key,value);
             int currFreq = 1;
             minFreq = 1;
+            Node*node = new Node(key,value);
             List*list = new List();
-            if(freqListMap.find(currFreq)!=freqListMap.end()){
+            if(freqListMap.find(currFreq) != freqListMap.end()){
                 list = freqListMap[currFreq];
             }
             list->addNode(node);
+            keyNode[key] = node;
             freqListMap[currFreq] = list;
-            keyNode[node->key] = node;
-            
         }
-        
     }
 };
 
