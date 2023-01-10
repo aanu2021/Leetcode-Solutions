231c35521c91176b5e29ca3c12e5c39e062c8014
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-10 10:55:38 +0530

    Time: 3 ms (65.62%), Space: 10 MB (79.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -31,32 +31,15 @@ public:
         
         if(p==NULL && q==NULL) return true;
         if(p==NULL || q==NULL) return false;
+        if(p->val != q->val) return false;
         
-        queue<TreeNode*>q1,q2;
-        q1.push(p); q2.push(q);
+        return isSameTreeDFS(p->left,q->left) && isSameTreeDFS(p->right,q->right);
         
-        while(!q1.empty() || !q2.empty()){
-            
-            auto node1 = q1.front(); q1.pop();
-            auto node2 = q2.front(); q2.pop();
-            
-            if(node1==NULL && node2==NULL) continue;
-            if(node1==NULL || node2==NULL) return false;
-            if(node1->val != node2->val) return false;
-            
-            q1.push(node1->left);
-            q1.push(node1->right);
-            
-            q2.push(node2->left);
-            q2.push(node2->right);
-            
-        }
-        return true;
     }
     
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
-        return isSameTreeBFS(p,q);
+        return isSameTreeDFS(p,q);
         
     }
     
