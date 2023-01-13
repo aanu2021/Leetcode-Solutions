e94fdfff3e8aea713a87630b8fe776bab2292fb1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-13 20:50:10 +0530

    Time: 17 ms (81.59%), Space: 14.4 MB (18.68%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -2,10 +2,18 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL || root == p || root == q) return root;
-        TreeNode * left = lowestCommonAncestor(root->left,p,q);
-        TreeNode * right = lowestCommonAncestor(root->right,p,q);
-        if(left == NULL) return right;
-        if(right == NULL) return left;
+        TreeNode * leftNode = lowestCommonAncestor(root->left,p,q);
+        TreeNode * rightNode = lowestCommonAncestor(root->right,p,q);
+        if(leftNode == NULL) return rightNode;
+        if(rightNode == NULL) return leftNode;
         return root;
     }
 };
+
+/*
+
+Check whether we are able to find any of the p/q node in the left subtree of the current node.
+
+And then the same for the right subtree of the current node.
+
+*/
