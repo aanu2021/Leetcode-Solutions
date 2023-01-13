ac0fff7d7f405de2e8f8d941a1119f218c5c11c2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-13 19:43:43 +0530

    Time: 112 ms (58.04%), Space: 57 MB (53.57%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -1,12 +1,33 @@
 class Solution {
     public:
+    
+    bool isLeaf(TreeNode* root){
+        if(!root->left && !root->right) return true;
+        else return false;
+    }
+    
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         if(!root) return new TreeNode(val);
-        if(root->val > val){
-            root->left = insertIntoBST(root->left,val);
-        }
-        else{
-            root->right = insertIntoBST(root->right,val);
+        TreeNode*curr = root;
+        while(curr){
+            if(curr->val > val){
+                if(!curr->left){
+                    curr->left = new TreeNode(val);
+                    break;
+                }
+                else{
+                    curr = curr->left;
+                }
+            }
+            else{
+                if(!curr->right){
+                    curr->right = new TreeNode(val);
+                    break;
+                }
+                else{
+                    curr = curr->right;
+                }
+            }
         }
         return root;
     }
