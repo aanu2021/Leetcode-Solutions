80a45c7263da1b614dc2881374f2ef1eed40fb81
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-14 22:45:44 +0530

    Time: 144 ms (94.32%), Space: 32.6 MB (58.02%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int dp[602][101][101];
+   /* int dp[602][101][101];
     
     int func(vector<int>&countOne,vector<int>&countZero,int i,int n,int M,int N){
         if(i == n) return 0;
@@ -11,23 +11,35 @@ public:
             ans = max(ans,1 + func(countOne,countZero,i+1,n,M-countZero[i],N-countOne[i]));
         }
         return dp[i][M][N] = ans;
-    }
+    } */
     
     int findMaxForm(vector<string>& strs, int M, int N) {
         int n = strs.size();
-        vector<int>countOne(n,0);
-        vector<int>countZero(n,0);
+        vector<int>countOne(n+1,0);
+        vector<int>countZero(n+1,0);
         for(int i=0;i<n;i++){
             int zeroCnt = 0, oneCnt = 0;
             for(char ch:strs[i]){
                 if(ch == '0') zeroCnt++;
                 else          oneCnt++;
             }
-            countOne[i]  = oneCnt;
-            countZero[i] = zeroCnt;
+            countOne[i+1]  = oneCnt;
+            countZero[i+1] = zeroCnt;
         }
-        memset(dp,-1,sizeof(dp));
-        int ans = func(countOne,countZero,0,n,M,N);
-        return ans;
+        int dp[n+1][M+1][N+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++){
+            int cntZero = countZero[i];
+            int cntOne  = countOne[i];
+            for(int zero = 0;zero <= M;zero++){
+                for(int one = 0;one <= N;one++){
+                    dp[i][zero][one] = dp[i-1][zero][one];
+                    if(zero >= cntZero && one >= cntOne){
+                        dp[i][zero][one] = max(dp[i][zero][one],1 + dp[i-1][zero-cntZero][one-cntOne]);
+                    }
+                }
+            }
+        }
+        return dp[n][M][N];
     }
 };
