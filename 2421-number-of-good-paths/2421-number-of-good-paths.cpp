cf07f61689db9ac915f5332aa48cbde5dbc00b49
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-15 10:43:12 +0530

    Time: 736 ms (85.15%), Space: 195.1 MB (82.27%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.cpp
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -5,97 +5,66 @@ public:
     vector<int>Rank;
     
     int find(int x){
-        if(Parent[x]==x) return x;
-        return Parent[x]=find(Parent[x]);
+        if(Parent[x] == x) return x;
+        return Parent[x] = find(Parent[x]);
     }
     
     void Union(int x,int y){
-        int lx=find(x);
-        int ly=find(y);
-        if(lx!=ly){
-            if(Rank[lx]<Rank[ly]){
-                Parent[lx]=ly;
-            }else{
-                Parent[ly]=lx;
-                if(Rank[lx]==Rank[ly]){
-                    Rank[lx]++;
-                }
+        int lx = find(x);
+        int ly = find(y);
+        if(lx != ly){
+            if(Rank[lx] < Rank[ly]){
+                Parent[lx] = ly;
+            }
+            else{
+                Parent[ly] = lx;
+                if(Rank[lx] == Rank[ly]) Rank[lx]++;
             }
         }
     }
     
-    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+    int numberOfGoodPaths(vector<int>& value, vector<vector<int>>& edges) {
         
-        int n = vals.size();
-        
-        Parent.resize(n,0);
-        Rank.resize(n,0);
-        
-        for(int i=0;i<n;i++){
-            Parent[i]=i;
-            Rank[i]=1;
-        }
-        
-        map<int,vector<int>>values;
-        
+        map<int,vector<int>>vals;
         map<int,vector<pair<int,int>>>adj;
         
+        int n = value.size();
+        int ans = n;
+        
+        Parent.resize(n);
+        Rank.resize(n);
+        
         for(int i=0;i<n;i++){
-            
-            values[vals[i]].push_back(i);
-            
+            Parent[i] = i;
+            Rank[i] = 1;
+        }
+        
+        for(int i=0;i<n;i++){
+            vals[value[i]].push_back(i);
         }
         
         for(int i=0;i<edges.size();i++){
-            
             int u = edges[i][0];
             int v = edges[i][1];
-            
-            adj[max(vals[u],vals[v])].push_back({u,v});
-            
+            adj[max(value[u],value[v])].push_back({u,v});
         }
         
-        int ans = 0;
-        
-        for(auto x:adj){
-            
+        for(auto &x : adj){
             int currval = x.first;
-            
-            for(auto y:x.second){
-                
+            for(auto &y : x.second){
                 Union(y.first,y.second);
-                
             }
-            
             map<int,int>cnt;
-            
-            for(auto y:values[currval]){
-                
-                cnt[find(y)]++;
-                
+            for(auto &ele : vals[currval]){
+                cnt[find(ele)]++;
             }
-            
-            for(auto y:cnt){
-                
-                int z = y.second;
-                
-                ans += (z*(z-1))/2;
-                
+            for(auto &itr : cnt){
+                int z = itr.second;
+                ans += ((z*(z-1))/2);
             }
-            
         }
         
-        return ans+n;
+        return ans;
         
     }
 };
-
-/*
-
-
-SIMILAR GFG PROBLEM LINK :
-
-https://practice.geeksforgeeks.org/contest/weekly-interview-series-73/problems/
-
-
-*/
