45463d75b1aa0f8e9c4966275547b54863c44296
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-17 00:16:38 +0530

    Time: 10 ms (27.01%), Space: 9.6 MB (77.84%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n = days.back();
-        vector<int>dp(n+1,0);
-        int j = 0;
-        for(int i=1;i<=n;i++){
-            if(i == days[j]){
-                int cost1 = costs[0];
-                int cost2 = costs[1];
-                int cost3 = costs[2];
-                if(i-1 >= 0) cost1 += dp[i-1];
-                if(i-7 >= 0) cost2 += dp[i-7];
-                if(i-30 >= 0) cost3 += dp[i-30];
-                dp[i] = min({cost1,cost2,cost3});
-                j++;
-            }
-            else{
-                dp[i] = dp[i-1];
-            }
+        int n = days.size();
+        vector<int>dp(n,0);
+        dp[n-1] = min({costs[0],costs[1],costs[2]});
+        for(int i=n-2;i>=0;i--){
+            int c1 = costs[0];
+            int c7 = costs[1];
+            int c30 = costs[2];
+            int j = lower_bound(days.begin(),days.end(),days[i] + 1) - days.begin();
+            if(j < n) c1 += dp[j];
+            j = lower_bound(days.begin(),days.end(),days[i] + 7) - days.begin();
+            if(j < n) c7 += dp[j];
+            j = lower_bound(days.begin(),days.end(),days[i] + 30) - days.begin();
+            if(j < n) c30 += dp[j];
+            dp[i] = min({c1,c7,c30});
         }
-        return dp[n];
+        return dp[0];
     }
 };
