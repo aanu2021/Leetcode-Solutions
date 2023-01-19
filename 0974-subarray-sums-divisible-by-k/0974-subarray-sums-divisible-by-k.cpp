08e878ac3495a31f221bac110ec1450c01b86d14
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-19 11:17:21 +0530

    Time: 73 ms (57.69%), Space: 31.5 MB (70.50%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
         int n = nums.size();
+        unordered_map<int,int>mp;
         mp[0] = 1;
-        int ans = 0;
-        int currsum = 0;
+        int currSum = 0 , ans = 0;
         for(int i=0;i<n;i++){
-            currsum+=nums[i];
-            currsum = (((currsum+k)%k)+k)%k;
-            ans+=mp[currsum];
-            mp[currsum]++;
+            currSum += nums[i];
+            currSum = ( ( (currSum + k) % k) + k ) % k;
+            ans += mp[currSum];
+            mp[currSum]++;
         }
         return ans;
     }
 };
+
+   /* 
+
+
+    a0 a1 a2 a3 a4 a5 a6
+
+    p0 p1 p2 p3 p4 p5 p6
+    
+    subarray_sum[2...5] = (a0 + a1 + a2 + a3 + a4 + a5) - (a0 + a1)
+        
+    subarray_sum[2...5] = (p5) - (p1)
+        
+    subarray_sum[l...r] = (p[r]) - (p[l-1])
+        
+    Let say the subarray starting at index l , and ending at index 
+    r , is divisible by k.
+        
+    subarray_sum[l...r] % k == 0
+        
+    (p[r] - p[l-1]) % k     == 0
+        
+    ((p[r] % k) - (p[l-1] % k)) % k == 0  
+        
+    p[r] = (q1*k) + r1
+    p[l-1] = q2*k + r2
+    
+    (r1 - r2) % k == 0
+        
+    r1 == r2  
+    
+    
+    */
+
