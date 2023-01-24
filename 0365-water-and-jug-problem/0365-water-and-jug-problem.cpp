80d3036493bf174a8573a94732cda5e7567bad7a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-24 11:45:14 +0530

    Time: 703 ms (13.42%), Space: 67.9 MB (15.65%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -26,7 +26,6 @@ public:
     int gcd(int x,int y){
         if(x==0) return y;
         if(y==0) return x;
-        if(x==y) return x;
         if(x>y) return gcd(x%y,y);
         else return gcd(x,y%x);
     }
