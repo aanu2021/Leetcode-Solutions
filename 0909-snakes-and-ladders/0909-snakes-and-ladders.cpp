39b12f05068133f2bf6ae5f590c772e932b58120
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-01-24 11:31:02 +0530

    Time: 43 ms (40.34%), Space: 13.1 MB (57.30%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -3,12 +3,10 @@ public:
     int snakesAndLadders(vector<vector<int>>& board) {
         
         int n = board.size();
+        int target = (n*n);
         
-        queue<int>q;
-        q.push(1);
-        
-        vector<int>vis(n*n + 1 , 0);
-        vector<int>jump(n*n + 1 , -1);
+        vector<int>Jump(n*n+1 , -1);
+        vector<int>Vis(n*n+1 , 0);
         
         int currCell = 1;
         reverse(board.begin(),board.end());
@@ -18,36 +16,42 @@ public:
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                jump[currCell++] = board[i][j];
+                Jump[currCell++] = board[i][j];
             }
         }
         
-        // for(int i=1;i<=n*n;i++){
-        //     cout<<jump[i]<<" ";
-        // }cout<<"\n";
-        
+        queue<int>q;
+        q.push(1);
+        Vis[1] = 1;
         int steps = 0;
-        int target = n*n;
         
         while(!q.empty()){
+            
             int sz = q.size();
             while(sz--){
+                
                 int node = q.front();
                 q.pop();
-               // cout<<node<<"\n";
-                if(vis[node]) continue;
-                vis[node] = 1;
                 if(node == target) return steps;
-                   
-                         for(int currJump = 1;currJump <= 6;currJump++){
-                        int nextNode = node + currJump;
-                        if(nextNode < 0 || nextNode > target) continue;
-                        if(jump[nextNode] != -1) q.push(jump[nextNode]);
-                             else q.push(nextNode);
+                for(int currJump = 1; currJump <= 6 ; currJump++){
+                    int nextNode = node + currJump;
+                    if(nextNode < 0 || nextNode > target) continue;
+                    if(Jump[nextNode]!=-1){
+                        if(Vis[Jump[nextNode]]==0){
+                            Vis[Jump[nextNode]] = 1;
+                            q.push(Jump[nextNode]);
+                        }
                     }
+                    else{
+                        if(Vis[nextNode]==0){
+                            Vis[nextNode] = 1;
+                            q.push(nextNode);
+                        }
+                    }
+                }
             }
-            // cout<<"------------------\n";
             steps++;
+            
         }
         return -1;
     }
