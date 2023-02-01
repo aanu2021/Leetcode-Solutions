56a81cae7ca50633ff0fbe227f342fe672d76b40
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-02 00:41:45 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,37 +8,63 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
+  
+     vector<int>topoSorting(int n,vector<vector<pair<int,int>>>&graph){
+         vector<int>indegree(n,0);
+         for(int i=0;i<n;i++){
+             for(auto nbrs:graph[i]){
+                 indegree[nbrs.first]++;
+             }
+         }
+         queue<int>q;
+         for(int i=0;i<n;i++){
+             if(indegree[i]==0){
+                 q.push(i);
+             }
+         }
+         vector<int>topo;
+         while(!q.empty()){
+             int node = q.front(); q.pop();
+             topo.push_back(node);
+             for(auto nbrs:graph[node]){
+                 int nbr = nbrs.first;
+                 indegree[nbr]--;
+                 if(indegree[nbr]==0){
+                     q.push(nbr);
+                 }
+             }
+         }
+         return topo;
+     }
+  
      vector<int> shortestPath(int n,int m, vector<vector<int>>& edges){
+        
         vector<vector<pair<int,int>>>graph(n);
-        vector<bool>visited(n,false);
+        
         for(int i=0;i<edges.size();i++){
             graph[edges[i][0]].push_back({edges[i][1],edges[i][2]});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,0});
+        
+        vector<int>vec = topoSorting(n,graph);
+        
         vector<int>dist(n,1e9);
         dist[0] = 0;
-        while(!pq.empty()){
-            auto curr = pq.top(); pq.pop();
-            int wt = curr.first;
-            int node = curr.second;
-            if(visited[node]) continue;
-            visited[node] = true;
-            for(auto nbrs:graph[node]){
-               int nbr = nbrs.first;
-               int edge_wt = nbrs.second;
-               if(visited[nbr]) continue;
-               if(dist[nbr] > dist[node] + edge_wt){
-                   dist[nbr] = dist[node] + edge_wt;
-                   pq.push({dist[nbr],nbr});
-               }
+        
+        for(int i=0;i<vec.size();i++){
+            int node = vec[i];
+            for(auto nbrs : graph[node]){
+                int nbr = nbrs.first;
+                int edgeWt = nbrs.second;
+                dist[nbr] = min(dist[nbr],dist[node] + edgeWt);
             }
         }
+        
         for(int i=0;i<n;i++){
-            if(dist[i]>=1e9) dist[i] = -1;
+            if(dist[i] >= 1e9) dist[i] = -1;
         }
         return dist;
-    }
+        
+     }
 };
 
 
