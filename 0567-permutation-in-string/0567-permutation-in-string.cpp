7974fe1e52f24d316b83b5e6edc403fa7b0263a0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-04 19:33:20 +0530

    Time: 3 ms (97.53%), Space: 7.4 MB (45.48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    
-    bool Match(vector<int>&freq1,vector<int>&freq2){
-        for(int i=0;i<26;i++){
-            if(freq1[i] != freq2[i]) return false;
-        }
-        return true;
-    }
-    
     bool checkInclusion(string s1, string s2) {
         int m = s1.length();
         int n = s2.length();
-        vector<int>freq_demand(26,0);
-        vector<int>freq_curr(26,0);
+        int dist_char = 0;
+        vector<int>freq(26,0);
         for(int i=0;i<m;i++){
-            freq_demand[s1[i]-'a']++;
+            if(freq[s1[i]-'a'] == 0) dist_char++;
+            freq[s1[i]-'a']++;
         }
         for(int i=0;i<n;i++){
-            freq_curr[s2[i]-'a']++;
+            if(freq[s2[i]-'a']==0) dist_char++;
+            freq[s2[i]-'a']--;
+            if(freq[s2[i]-'a']==0) dist_char--;
             if(i >= m){
-                freq_curr[s2[i-m]-'a']--;
+                if(freq[s2[i-m]-'a']==0) dist_char++;
+                freq[s2[i-m]-'a']++;
+                if(freq[s2[i-m]-'a']==0) dist_char--;
             }
-            if(Match(freq_demand,freq_curr)){
-                return true;
-            }
+            if(dist_char == 0) return true;
         }
         return false;
     }
