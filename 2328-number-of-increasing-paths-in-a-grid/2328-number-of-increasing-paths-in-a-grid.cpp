aa5f78f92177b08a516c2155c9ac0829b08a04e3
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-06 00:11:15 +0530

    Time: 317 ms (51.77%), Space: 61.5 MB (17.32%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -2,83 +2,53 @@ class Solution {
 public:
     
     typedef long long ll;
-    
     const ll M = 1e9 + 7;
     
     ll mod(ll a){
         return ((a%M)+M)%M;
     }
-    
     ll mul(ll a,ll b){
         return mod(mod(a)*mod(b));
     }
-    
     ll add(ll a,ll b){
         return mod(mod(a)+mod(b));
     }
     
     ll dp[1002][1002];
     
+    int dx[4] = {0,0,1,-1};
+    int dy[4] = {1,-1,0,0};
+    
     ll func(vector<vector<int>>&grid,int i,int j,int m,int n,int prev){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j] <= prev) return 0LL;
+        if(dp[i][j] != -1LL) return dp[i][j];
         
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]<=prev){
-            
-            return 0LL;
-            
+        ll ways = 1LL;
+        for(int dir = 0;dir<4;dir++){
+            int ni = i + dx[dir];
+            int nj = j + dy[dir];
+            ways += func(grid,ni,nj,m,n,grid[i][j]);
+            ways %= M;
         }
         
-        if(dp[i][j]!=-1LL){
-            
-            return dp[i][j];
-            
-        }
-        
-        ll curr_paths=1LL;
-        
-        ll op1=func(grid,i+1,j,m,n,grid[i][j]);
-        ll op2=func(grid,i-1,j,m,n,grid[i][j]);
-        ll op3=func(grid,i,j+1,m,n,grid[i][j]);
-        ll op4=func(grid,i,j-1,m,n,grid[i][j]);
-        
-        curr_paths=add(curr_paths,op1);
-        curr_paths=add(curr_paths,op2);
-        curr_paths=add(curr_paths,op3);
-        curr_paths=add(curr_paths,op4);
-        
-        return dp[i][j] = curr_paths;
+        return dp[i][j] = ways;
         
     }
     
     int countPaths(vector<vector<int>>& grid) {
-        
-        ll m=grid.size();
-        ll n=grid[0].size();
-        
+        int m = grid.size();
+        int n = grid[0].size();
         memset(dp,-1,sizeof(dp));
-        
-        ll total_paths=0LL;
-        
-        for(int i=0;i<m;++i){
-            
-            for(int j=0;j<n;++j){
-                
+        ll answer = 0LL;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(dp[i][j]==-1){
-                    
-                    ll curr_paths=func(grid,i,j,m,n,-1);
-                    
-                    dp[i][j]=curr_paths;
-                    
+                    func(grid,i,j,m,n,-1);
                 }
-                
-                total_paths=add(total_paths,dp[i][j]);
-                
+                answer += dp[i][j];
+                answer %= M;
             }
-            
         }
-        
-        return total_paths;
-        
+        return answer;
     }
 };
-
-
