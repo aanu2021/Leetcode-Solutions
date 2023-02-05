26000dd037b4fc57aa819d375e7c5122cd8fee31
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-05 20:13:33 +0530

    Time: 27 ms (62.41%), Space: 10.5 MB (83.92%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    
-    string removeDuplicates2(string s) {
-        int n = s.length();
-        string res="";
-        for(int i=0;i<n;i++){
-            if(res.length() && res.back()==s[i]){
-                res.pop_back();
-            }else{
-                res+=s[i];
-            }
-        }
-        return res;
-    }
-    
     string removeDuplicates(string s) {
         int n = s.length();
-        int left = 0,right = 0;
-        for(right=0;right<n;right++){
-            if(left>0 && s[left-1]==s[right]){
-                left--;
-            }else{
-                s[left] = s[right];
-                left++;
+        stack<char>S;
+        for(int i=0;i<n;i++){
+            if(!S.empty() && S.top()==s[i]){
+                S.pop();
+            }
+            else{
+                S.push(s[i]);
             }
         }
-        return s.substr(0,left);
+        s.clear();
+        while(!S.empty()){
+            s += S.top(); S.pop();
+        }
+        reverse(s.begin(),s.end());
+        return s;
     }
-    
 };
-
-   
