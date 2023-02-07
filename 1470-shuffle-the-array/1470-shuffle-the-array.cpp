c3bd2720962a88ac714f9a181f9e256b84937849
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-07 09:27:19 +0530

    Time: 7 ms (61.84%), Space: 9.8 MB (61.99%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>ans(2*n,-1);
-        int l = 0, r = n;
-        int idx = 0;
-        while(r < 2*n){
-            ans[idx++] = nums[l++];
-            ans[idx++] = nums[r++];
+        for(int i=0;i<2*n;i++){
+            int currNum = nums[i];
+            int currPos = i;
+            while(currNum > 0){
+                int nextNum , nextPos;
+                if(currPos < n) nextPos = 2 * currPos;
+                else            nextPos = 2 * (currPos - n) + 1;
+                nextNum = nums[nextPos];
+                nums[nextPos] = -currNum;
+                currNum = nextNum;
+                currPos = nextPos;
+            }
         }
-        return ans;
+        for(int i=0;i<2*n;i++){
+            if(nums[i] < 0) nums[i] = -nums[i];
+        }
+        return nums;
     }
 };
+
+/*
+
+2 5 1 3 4 7
+0 1 2 3 4 5
+    
+currNum =  5     nextNum =   
+currPos =  1     nextPos =      
+    
+if(currPos < n) nextPos = 2 * currPos
+else            nextPos = 2 * (currPos - n) + 1
+
+-2 -3 -5 -4 -1 -7    
+    
+2 3 5 4 1 7    
+0 1 2 3 4 5    
+    
+*/    
