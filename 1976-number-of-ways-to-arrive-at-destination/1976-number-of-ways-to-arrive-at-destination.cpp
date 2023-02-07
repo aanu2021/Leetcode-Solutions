55d9e6da52bbdc306a1c24a1594e39063e51fb19
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-07 10:32:05 +0530

    Time: 90 ms (73.78%), Space: 30.4 MB (83.35%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -2,70 +2,52 @@ class Solution {
 public:
     
     typedef long long ll;
-    
     const ll M = 1e9 + 7;
     
     ll mod(ll a){
         return ((a%M)+M)%M;
     }
-    
     ll mul(ll a,ll b){
         return mod(mod(a)*mod(b));
     }
-    
     ll add(ll a,ll b){
         return mod(mod(a)+mod(b));
     }
     
-    int countPaths(int n, vector<vector<int>>& road) {
+    int countPaths(int n, vector<vector<int>>& roads) {
         
         vector<vector<pair<int,int>>>graph(n);
-        
-        for(int i=0;i<road.size();i++){
-            graph[road[i][0]].push_back({road[i][1],road[i][2]});
-            graph[road[i][1]].push_back({road[i][0],road[i][2]});
+        for(int i=0;i<roads.size();i++){
+            graph[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+            graph[roads[i][1]].push_back({roads[i][0],roads[i][2]});
         }
         
-        vector<ll>dist(n,1e15);
-        vector<ll>roads(n,0LL);
-        
+        vector<ll>dist(n+1,1e15);
+        vector<ll>ways(n+1,0LL);
         dist[0] = 0LL;
-        roads[0] = 1LL;
-        
+        ways[0] = 1LL;
         priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
-        
         pq.push({0LL,0LL});
         
         while(!pq.empty()){
-            
             auto curr = pq.top();
             pq.pop();
-            
             ll node = curr.second;
-            ll wt = curr.first;
-            
-            if(dist[node] < wt) continue;
-            
-            for(auto nbrs:graph[node]){
-                
-                int nbr = nbrs.first;
-                int edge_wt = nbrs.second;
-                
-                if(dist[nbr] > dist[node] + (ll)edge_wt){
-                    dist[nbr] = dist[node] + (ll)edge_wt;
-                    roads[nbr] = roads[node];
-                    pq.push({dist[nbr],(ll)nbr});
+            ll weight = curr.first;
+            if(dist[node] < weight) continue;
+            for(auto &nbrs : graph[node]){
+                ll nbr = nbrs.first;
+                ll edgeWt = nbrs.second;
+                if(dist[nbr] > dist[node] + edgeWt){
+                    dist[nbr] = dist[node] + edgeWt;
+                    ways[nbr] = ways[node];
+                    pq.push({dist[nbr],nbr});
                 }
-                
-                else if(dist[nbr] == dist[node] + (ll)edge_wt){
-                    roads[nbr] = add(roads[nbr],roads[node]);
+                else if(dist[nbr] == dist[node] + edgeWt){
+                    ways[nbr] = add(ways[nbr],ways[node]);
                 }
-                
             }
-            
         }
-        
-        return mod(roads[n-1]);
-        
+        return ways[n-1];
     }
 };
