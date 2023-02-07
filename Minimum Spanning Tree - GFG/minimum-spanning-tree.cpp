2604f044573f2f7215d0ef4050b2fe20228f6833
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-07 20:22:52 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,32 +3,83 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DSU{
+  
+  public:
+  
+  vector<int>Parent;
+  vector<int>Rank;
+  
+  DSU(int n){
+      Parent.resize(n);
+      Rank.resize(n);
+      for(int i=0;i<n;i++){
+          Parent[i] = i;
+          Rank[i] = 1;
+      }
+  }
+  
+  int find(int x){
+      if(Parent[x]==x) return x;
+      return Parent[x] = find(Parent[x]);
+  }
+  
+  void Union(int x,int y){
+      int lx = find(x);
+      int ly = find(y);
+      if(lx != ly){
+          if(Rank[lx] < Rank[ly]){
+              Parent[lx] = ly;
+          }
+          else{
+              Parent[ly] = lx;
+              if(Rank[lx] == Rank[ly]){
+                  Rank[lx]++;
+              }
+          }
+      }
+  }
+    
+};
+
+
 class Solution
 {
 	public:
+	
+	bool static comp(const vector<int>&x,const vector<int>&y){
+	    return x[2] < y[2];
+	}
+	
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int n, vector<vector<int>> adj[])
     {
-        vector<bool>visited(n,false);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,0});
-        int sum = 0;
-        while(!pq.empty()){
-            auto curr = pq.top();
-            pq.pop();
-            int node = curr.second;
-            int wt = curr.first;
-            if(visited[node]) continue;
-            visited[node] = true;
-            sum += wt;
-            for(auto &nbrs:adj[node]){
-                int nbr = nbrs[0];
+        vector<vector<int>>edges;
+        for(int i=0;i<n;i++){
+            for(auto &nbrs : adj[i]){
+                int j = nbrs[0];
                 int edgeWt = nbrs[1];
-                if(visited[nbr]) continue;
-                pq.push({edgeWt,nbr});
+                if(i < j) edges.push_back({i,j,edgeWt});
             }
         }
-        return sum;
+        
+        DSU obj(n);
+        sort(edges.begin(),edges.end(),comp);
+        
+        int sumEdgeWt = 0;
+        
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+            if(obj.find(u) == obj.find(v)) continue;
+            obj.Union(u,v);
+            sumEdgeWt += wt;
+        }
+        
+        return sumEdgeWt;
+        
     }
 };
 
