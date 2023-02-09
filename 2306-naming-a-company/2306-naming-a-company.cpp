98aab3e380ec67b8cdde6ae84c8f1b1eff5628f6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-09 11:03:58 +0530

    Time: 438 ms (92.68%), Space: 83.4 MB (43.90%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -4,7 +4,6 @@ public:
     typedef long long ll;
     
     long long distinctNames(vector<string>& ideas) {
-        int n = ideas.size();
         ll ans = 0LL;
         vector<unordered_set<string>>initialGrp(26);
         for(string &str : ideas){
@@ -12,18 +11,17 @@ public:
         }
         for(int i=0;i<26;i++){
             for(int j=i+1;j<26;j++){
-                ll mutualGroups = 0LL;
+                ll mutualGrp = 0LL;
                 for(auto &str : initialGrp[i]){
-                    if(initialGrp[j].find(str) != initialGrp[j].end())
-                    {
-                        mutualGroups++;
+                    if(initialGrp[j].count(str)){
+                        mutualGrp++;
                     }
                 }
                 ll distinct_first_set = (ll)initialGrp[i].size();
                 ll distinct_second_set = (ll)initialGrp[j].size();
-                distinct_first_set -= mutualGroups;
-                distinct_second_set -= mutualGroups;
-                ans += 2LL*distinct_first_set*distinct_second_set;
+                distinct_first_set -= mutualGrp;
+                distinct_second_set -= mutualGrp;
+                ans += (2LL * distinct_first_set * distinct_second_set);
             }
         }
         return ans;
