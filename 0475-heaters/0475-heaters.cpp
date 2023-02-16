940de706f47057137f3816752d07adee5bc65713
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-16 19:49:06 +0530

    Time: 69 ms (71.62%), Space: 25.7 MB (37.08%) - LeetHub

--- 0475-heaters/0475-heaters.cpp
+++ 0475-heaters/0475-heaters.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     int findRadius(vector<int>& houses, vector<int>& heaters) {
-        int n = houses.size();
-        int m = heaters.size();
+        int m = houses.size();
+        int n = heaters.size();
         sort(houses.begin(),houses.end());
         sort(heaters.begin(),heaters.end());
-        vector<int>left(n,INT_MAX);
-        vector<int>right(n,INT_MAX);
         int i=0, j=0;
-        while(i<n && j<m){
+        vector<int>answer(m,INT_MAX);
+        while(i<m && j<n){
             if(houses[i] <= heaters[j]){
-                right[i] = min(right[i],heaters[j]-houses[i]);
+                answer[i] = min(answer[i],heaters[j]-houses[i]);
                 i++;
             }
             else{
                 j++;
             }
         }
-        i=n-1, j=m-1;
+        i=m-1, j=n-1;
         while(i>=0 && j>=0){
             if(houses[i] >= heaters[j]){
-                left[i] = min(left[i],houses[i]-heaters[j]);
+                answer[i] = min(answer[i],houses[i]-heaters[j]);
                 i--;
             }
             else{
                 j--;
             }
         }
-        vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            ans[i] = min(left[i],right[i]);
-        }
-        return *max_element(ans.begin(),ans.end());
+        return *max_element(answer.begin(),answer.end());
     }
 };
