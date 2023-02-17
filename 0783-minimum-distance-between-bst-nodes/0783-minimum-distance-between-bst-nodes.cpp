6601a6ab046762d1772c86c7b2fa0ce7b816d69e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-17 10:20:46 +0530

    Time: 4 ms (56.01%), Space: 9.6 MB (99.47%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -11,11 +11,47 @@ public:
         inorderTraversal(root->right,prev,minDiff);
     }
     
-    int minDiffInBST(TreeNode* root) {
+    int minDiffInBST2(TreeNode* root) {
         if(!root) return 0;
         int minDiff = INT_MAX;
         int prev = -1;
         inorderTraversal(root,prev,minDiff);
         return minDiff;
     }
+    
+    int minDiffInBST(TreeNode* root){
+        if(!root) return 0;
+        int minDiff = INT_MAX;
+        int prev = -1;
+        TreeNode * curr = root;
+        while(curr){
+            if(curr->left == NULL){
+                if(prev != -1){
+                    minDiff = min(minDiff,curr->val - prev);
+                }
+                prev = curr->val;
+                curr = curr->right;
+            }
+            else{
+                TreeNode * prevNode = curr->left;
+                while(prevNode && prevNode->right && prevNode->right != curr){
+                    prevNode = prevNode->right;
+                }
+                if(prevNode->right == NULL){
+                    prevNode->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    prevNode->right = NULL;
+                    if(prev != -1){
+                        minDiff = min(minDiff,curr->val - prev);
+                    }
+                    prev = curr->val;
+                    curr = curr->right;
+                }
+            }
+        }
+        return minDiff;
+    }
+    
 };
