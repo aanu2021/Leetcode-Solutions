4d3c37750544ff6b8ee38a2bbb671ce59051ddf9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-18 19:29:39 +0530

    Time: 4 ms (33.95%), Space: 9.8 MB (7.06%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -2,19 +2,35 @@
 class Solution {
 public:
     
-    void func(TreeNode*&root){
+    void DFS(TreeNode*&root){
         if(!root) return;
         TreeNode * tempLeft = root->left;
         TreeNode * tempRight = root->right;
         root->left = tempRight;
         root->right = tempLeft;
-        func(root->left);
-        func(root->right);
+        DFS(root->left);
+        DFS(root->right);
+    }
+    
+    TreeNode * BFS(TreeNode *& root){
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            TreeNode * tempLeft = node->left;
+            TreeNode * tempRight = node->right;
+            node->left = tempRight;
+            node->right = tempLeft;
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+        }
+        return root;
     }
     
     TreeNode* invertTree(TreeNode* root) {
         if(!root) return root;
-        func(root);
-        return root;
+        // DFS(root);
+        return BFS(root);
     }
 };
