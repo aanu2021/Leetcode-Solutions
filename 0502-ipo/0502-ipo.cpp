543ade5474f093e5a5e55852fbbd859e831330e3
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-23 13:37:12 +0530

    Time: 221 ms (84.29%), Space: 78.2 MB (87.14%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        vector<pair<int,int>>vec;
-        int n = profits.size();
+        int n = capital.size();
+        vector<pair<int,int>>vec(n);
         for(int i=0;i<n;i++){
-            vec.push_back({capital[i],profits[i]});
+            vec[i] = {capital[i],profits[i]};
         }
         sort(vec.begin(),vec.end());
         priority_queue<int>pq;
         int ptr = 0;
         for(int i=0;i<k;i++){
-            while(ptr < n && vec[ptr].first <= w){
+            while(ptr<n && vec[ptr].first <= w){
                 pq.push(vec[ptr++].second);
             }
             if(!pq.empty()){
@@ -21,4 +21,3 @@ public:
         return w;
     }
 };
-   
