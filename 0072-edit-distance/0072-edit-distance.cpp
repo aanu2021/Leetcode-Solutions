b6f7e002e373c277fb42ea17cb1d585042d6f395
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-26 11:05:58 +0530

    Time: 18 ms (48.15%), Space: 9.2 MB (20.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    
+    int func(string &s1,string &s2,int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i == m && j == n) return 0;
+        if(i == m) return (n-j);
+        if(j == n) return (m-i);
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = 0;
+        if(s1[i] == s2[j]){
+            return dp[i][j] = func(s1,s2,i+1,j+1,m,n,dp);
+        }
+        else{
+            int insOps = func(s1,s2,i,j+1,m,n,dp);
+            int delOps = func(s1,s2,i+1,j,m,n,dp);
+            int repOps = func(s1,s2,i+1,j+1,m,n,dp);
+            return dp[i][j] = 1 + min({insOps,delOps,repOps});
+        }
+    }
+    
     int minDistance(string s1, string s2) {
         int m = s1.length();
         int n = s2.length();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        s1 = "#" + s1;
-        s2 = "#" + s2;
-        dp[0][0] = 0;
-        for(int i=1;i<=n;i++){
-            dp[0][i] = i;
-        }
-        for(int i=1;i<=m;i++){
-            dp[i][0] = i;
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s1[i] == s2[j]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
-                }
-            }
-        }
-        return dp[m][n];
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        int ans = func(s1,s2,0,0,m,n,dp);
+        return ans;
     }
 };
