3c77c8eeaf3e8f49c8a672e7b12bb5cd53bfd444
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-02-27 19:33:18 +0530

    Time: 34 ms (45.32%), Space: 17 MB (42.59%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
     bool canMeasureWater(int x, int y, int z) {
-        if(x+y < z) return false;
-        if(x+y == z) return true;
-        set<int>visited;
+        if(z > x+y) return false;
+        vector<int>visited(x+y+1,0);
         queue<int>q;
         q.push(0);
-        visited.insert(0);
-        int direction[4] = {x,-x,y,-y};
+        visited[0] = true;
+        int dir[4] = {x,-x,y,-y};
         while(!q.empty()){
             int node = q.front();
             q.pop();
             if(node == z) return true;
-            for(int dir=0;dir<4;dir++){
-                int nextNode = node + direction[dir];
-                if(nextNode < 0 || nextNode > x+y) continue;
-                if(visited.find(nextNode) != visited.end()) continue;
-                visited.insert(nextNode);
+            for(int d=0;d<4;d++){
+                int nextNode = node + dir[d];
+                if(nextNode < 0 || nextNode > x + y) continue;
+                if(visited[nextNode]) continue;
+                visited[nextNode] = 1;
                 q.push(nextNode);
             }
         }
         return false;
-    }
-    
-    int gcd(int x,int y){
-        if(x==0) return y;
-        if(y==0) return x;
-        if(x>y) return gcd(x%y,y);
-        else return gcd(x,y%x);
-    }
-    
-    bool canMeasureWater2(int x, int y, int z) {
-        if(x+y < z) return false;
-        if(x+y == z || x==z || y==z) return true;
-        int g = gcd(x,y);
-        return (z%g==0);
     }
 };
