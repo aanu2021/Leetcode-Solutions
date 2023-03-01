b58205840bf0543cde5c8ca1c81284cdea391ed0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-01 23:49:21 +0530

    Added solution - LeetHub

---
+++ Radix Sort - GFG/radix-sort.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include  <bits/stdc++.h> 
+using namespace std; 
+  
+
+// } Driver Code Ends
+//User function Template for C++
+
+void do_sort(int arr[],int n,int exp){
+    int output[n] = {0};
+    int count[10] = {0};
+    for(int i=0;i<n;i++){
+        count[(arr[i]/exp)%10]++;
+    }
+    for(int i=1;i<10;i++){
+        count[i] += count[i-1];
+    }
+    for(int i=n-1;i>=0;i--){
+        output[count[((arr[i]/exp)%10)]-1] = arr[i];
+        count[(arr[i]/exp)%10]--; 
+    }
+    for(int i=0;i<n;i++){
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(int arr[], int n) 
+{ 
+  
+    int mx = *max_element(arr,arr+n);
+    for(int exp=1;mx/exp > 0;exp*=10){
+        do_sort(arr,n,exp);
+    }
+  
+} 
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+  
+        radixSort(arr, n); 
+        
+        for(int i=0;i<n;i++)
+            cout<<arr[i]<<" ";
+        
+        cout<<endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
