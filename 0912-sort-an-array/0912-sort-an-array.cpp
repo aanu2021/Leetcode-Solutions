c1d891a5049a8400f31f3e2ac4b5be8633086edc
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-01 19:37:25 +0530

    Time: 254 ms (43.61%), Space: 61.2 MB (65.89%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -39,7 +39,67 @@ public:
         return nums;
     }    
     
+    int findPartition(vector<int>&nums,int low,int high){
+        int i = low + 1, j = high;
+        int pivot = nums[low];
+        do{
+            
+            while(i <= j && nums[i] <= pivot) i++;
+            while(i <= j && nums[j] > pivot) j--;
+            if(i < j){
+                swap(nums[i],nums[j]);
+            }
+            
+        }while(i <= j);
+        swap(nums[low],nums[j]);
+        return j;
+    }
+    
+    void quicksort(vector<int>&nums,int low,int high){
+        if(low < high){
+            int partition = findPartition(nums,low,high);
+            quicksort(nums,low,partition-1);
+            quicksort(nums,partition+1,high);
+        }
+    }
+    
+    vector<int>Quick_Sort(vector<int>&nums){
+        int n = nums.size();
+        quicksort(nums,0,n-1);
+        return nums;
+    }
+    
+    void heapify(vector<int>&nums,int i,int n){
+        int l = (2*i+1);
+        int r = (2*i+2);
+        int greatest = i;
+        if(l < n && nums[l] > nums[greatest]){
+            greatest = l;
+        }
+        if(r < n && nums[r] > nums[greatest]){
+            greatest = r;
+        }
+        if(greatest != i){
+            swap(nums[i],nums[greatest]);
+            heapify(nums,greatest,n);
+        }
+    }
+    
+    vector<int>Heap_Sort(vector<int>&nums){
+        int n = nums.size();
+        for(int i=n/2-1;i>=0;i--){
+            heapify(nums,i,n);
+        }
+        for(int i=n-1;i>0;i--){
+            swap(nums[0],nums[i]);
+            heapify(nums,0,i);
+        }
+        return nums;
+    }
+    
     vector<int> sortArray(vector<int>& nums) {
-        return Merge_Sort(nums);
+        // return Merge_Sort(nums);
+        // return Quick_Sort(nums);
+        return Heap_Sort(nums);
     }
 };
