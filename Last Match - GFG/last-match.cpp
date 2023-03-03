1b689809a5446f455ae35ecdb8c75b5482e7c00c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-03 19:06:08 +0530

    Added solution - LeetHub

--- Last Match - GFG/last-match.cpp
+++ Last Match - GFG/last-match.cpp
@@ -8,33 +8,38 @@ public:
     int findLastOccurence(string s,string p){
         int n = s.length();
         int m = p.length();
+        if(n < m) return -1;
         vector<int>lps(m,0);
         for(int i=1;i<m;i++){
             int len = lps[i-1];
-            while(len>0 && p[len]!=p[i]){
+            while(len > 0 && p[len] != p[i]){
                 len = lps[len-1];
             }
-            if(p[len]==p[i]) len++;
+            if(p[len] == p[i]){
+                len++;
+            }
             lps[i] = len;
         }
         int idx = -2;
         int j = 0;
         for(int i=0;i<n;i++){
-            if(s[i]==p[j]){
+            if(s[i] == p[j]){
                 j++;
-            }
-            else{
-                while(j>0 && s[i]!=p[j]){
+                if(j == m){
+                    idx = i-m+1;
                     j = lps[j-1];
                 }
-                if(s[i]==p[j]) j++;
             }
-            if(j==m){
-                idx = i-j+1;
-                j = lps[j-1];
+            else{
+                while(j > 0 && s[i] != p[j]){
+                    j = lps[j-1];
+                }
+                if(s[i] == p[j]){
+                    j++;
+                }
             }
         }
-        return idx + 1;
+        return idx+1;
     }
 };
 
