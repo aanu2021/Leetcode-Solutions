0b600f69528af6d822830ab5a1c181446397db7e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-03 19:08:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (29.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // current prefix na hole ager prefix ta dekh lo vmro ...
+    
     int strStr(string s, string p) {
         int n = s.length();
         int m = p.length();
@@ -7,21 +10,16 @@ public:
         vector<int>lps(m,0);
         for(int i=1;i<m;i++){
             int len = lps[i-1];
-            while(len > 0 && p[i] != p[len]){
-                len = lps[len-1];
-            }
-            if(p[len] == p[i]){
-                len++;
-            }
+            while(len > 0 && p[i] != p[len]) len = lps[len-1];
+            if(p[i] == p[len]) len++;
             lps[i] = len;
         }
-        vector<int>indices;
         int j = 0;
         for(int i=0;i<n;i++){
-            if(s[i]==p[j]){
+            if(s[i] == p[j]){
                 j++;
                 if(j == m){
-                    indices.push_back(i-m+1);
+                    return i-m+1;
                     j = lps[j-1];
                 }
             }
@@ -29,15 +27,9 @@ public:
                 while(j > 0 && s[i] != p[j]){
                     j = lps[j-1];
                 }
-                if(s[i] == p[j]){
-                    j++;
-                }
+                if(s[i] == p[j]) j++;
             }
         }
-        // for(int i=0;i<indices.size();i++){
-        //     cout<<indices[i]<<" ";
-        // }cout<<"\n";
-        if(indices.size()) return indices[0];
-        else return -1;
+        return -1;
     }
 };
