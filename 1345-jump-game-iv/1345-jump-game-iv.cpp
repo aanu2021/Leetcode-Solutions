9bc9e8f259ca4a59f6aac6352b17ba0414f9cc56
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-05 08:56:36 +0530

    Time: 204 ms (85.23%), Space: 73 MB (76.87%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -2,46 +2,38 @@ class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int n = arr.size();
-        unordered_map<int,vector<int>>adj;
-        for(int i=0;i<n;i++){
-            adj[arr[i]].push_back(i);
-        }
-        vector<bool>visited(n,false);
+        if(n==1) return 0;
         queue<int>q;
+        vector<int>visited(n,0);
         q.push(0);
-        visited[0] = true;
+        visited[0] = 1;
+        unordered_map<int,vector<int>>indices;
+        for(int i=0;i<n;i++){
+            indices[arr[i]].push_back(i);
+        }
         int lvl = 0;
         while(!q.empty()){
             int sz = q.size();
             while(sz--){
-                
                 int node = q.front();
                 q.pop();
-                
-                if(node==n-1) return lvl;
-                
-                if(node-1>=0 && visited[node-1]==false){
+                if(node == n-1){
+                    return lvl;
+                }
+                if(node - 1 >= 0 && visited[node-1] == 0){
                     q.push(node-1);
-                    visited[node-1] = true;
+                    visited[node-1] = 1;
                 }
-                
-                if(node+1<n && visited[node+1]==false){
+                if(node + 1 < n && visited[node+1] == 0){
                     q.push(node+1);
-                    visited[node+1] = true;
+                    visited[node+1] = 1;
                 }
-                
-                while(adj[arr[node]].size()){
-                    
-                    int j = adj[arr[node]].back();
-                    adj[arr[node]].pop_back();
-                    
-                    if(visited[j]==false){
-                        q.push(j);
-                        visited[j] = true;
-                    }
-                    
+                for(int j : indices[arr[node]]){
+                    if(visited[j]) continue;
+                    visited[j] = 1;
+                    q.push(j);
                 }
-                
+                indices[arr[node]].clear();
             }
             lvl++;
         }
