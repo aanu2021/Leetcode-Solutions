227ad2fd498a3f185279771db0ec002619c792c8
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-07 12:34:37 +0530

    Added solution - LeetHub

--- Best Node - GFG/best-node.cpp
+++ Best Node - GFG/best-node.cpp
@@ -32,51 +32,47 @@ class Solution {
   public:
   
     typedef long long ll;
-    vector<vector<int>>graph; 
-    vector<int>cost;
   
+    vector<vector<int>>graph;
+    vector<int>cost;
+    
     pair<ll,ll> dfs(int u,ll &maxScore){
         ll currScore = (ll)cost[u];
-        // Leaf Node
-        if(graph[u].size()==0){
+        if(graph[u].size() == 0){
             maxScore = max(maxScore,currScore);
             return {currScore,currScore};
         }
-        // Non-Leaf Node
         else{
             ll minNbr = 1e18;
             ll maxNbr = -1e18;
-            for(int nbr:graph[u]){
-                auto nbrScore = dfs(nbr,maxScore);
-                minNbr = min({minNbr,nbrScore.first,nbrScore.second});
-                maxNbr = max({maxNbr,nbrScore.first,nbrScore.second});
+            for(int v:graph[u]){
+                auto nbr = dfs(v,maxScore);
+                minNbr = min({minNbr,nbr.first,nbr.second});
+                maxNbr = max({maxNbr,nbr.first,nbr.second});
             }
-            ll currMax = currScore - minNbr;
-            ll currMin = currScore - maxNbr;
+            ll currMax = currScore;
+            ll currMin = currScore;
+            currMax -= minNbr;
+            currMin -= maxNbr;
             maxScore = max(maxScore,currMax);
-            return {currMax,currMin};
+            return {currMin,currMax};
         }
     }
   
     long long bestNode(int N, vector<int> &A, vector<int> &P) {
-        
-        graph.clear();
-        graph.resize(N+1);
-        cost.clear();
-        cost.resize(N+1);
-        
-        for(int i=1;i<N;i++){
-            graph[P[i]].push_back(i+1);
-        }
-        
-        for(int i=0;i<N;i++){
-            cost[i+1] = A[i];
-        }
-        
-        ll maxScore = -1e18;
-        dfs(1,maxScore);
-        return maxScore;
-        
+         graph.clear();
+         cost.clear();
+         graph.resize(N+1);
+         cost.resize(N+1);
+         for(int i=1;i<N;i++){
+             graph[P[i]].push_back(i+1);
+         }
+         for(int i=0;i<N;i++){
+             cost[i+1] = A[i];
+         }
+         ll maxScore = -1e18;
+         dfs(1,maxScore);
+         return maxScore;
     }
 };
 
