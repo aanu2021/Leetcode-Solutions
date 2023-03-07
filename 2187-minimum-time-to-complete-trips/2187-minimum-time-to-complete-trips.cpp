2c09524d207dfb57495207634122c28ffffcfaa2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-07 11:06:34 +0530

    Time: 330 ms (75.80%), Space: 94.6 MB (33.40%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -3,22 +3,22 @@ public:
     
     typedef long long ll;
     
-    bool canMake(vector<int>&nums,ll currTime,int k){
-        ll trips = 0LL;
+    bool canMake(vector<int>&nums,ll mid,int k){
+        ll curr = 0LL;
         for(int i=0;i<nums.size();i++){
-            // if(trips >= k) return true;
-            trips += (ll)(currTime/nums[i]);
+            curr += (ll)(mid/nums[i]);
+            if(curr >= k) return true;
         }
-        return trips >= k;
+        return false;
     }
     
     long long minimumTime(vector<int>& nums, int k) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        ll low = 0LL , high = (ll)((ll)nums[0]*(ll)k);
+        int mini = *min_element(nums.begin(),nums.end());
+        ll low = 0LL , high = (ll)((ll)mini*(ll)k);
         ll ans = -1LL;
         while(low <= high){
-            ll mid = low + (high-low)/2;
+            ll mid = (low + high)/2;
             if(canMake(nums,mid,k)){
                 ans = mid;
                 high = mid - 1;
