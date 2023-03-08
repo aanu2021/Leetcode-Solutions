a219e8154e101c48cf1309598f3411f646422e5e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-08 20:41:37 +0530

    Added solution - LeetHub

--- Maximum Bipartite Matching - GFG/maximum-bipartite-matching.cpp
+++ Maximum Bipartite Matching - GFG/maximum-bipartite-matching.cpp
@@ -3,46 +3,39 @@
 using namespace std;
 
 // } Driver Code Ends
-
 class Solution {
 public:
-	vector<int>mt;
-	vector<bool>used;
-	bool try_kunh(int u, vector<vector<int>>&G){
-	    for (int v = 0; v < G[0].size(); v++){
-	        if (G[u][v] && !used[v])
-	        {
-	            used[v] = true;
-	            if (mt[v] < 0 || try_kunh(mt[v], G))
-	            {
-	                mt[v] = u;
-	                return true;
-	            }
-	        }
-	    }
-    return false;
-	}
+    vector<bool>vis;
+    vector<int>appName;
+    
+    bool func(int u,vector<vector<int>>&G){
+        for(int v=0;v<G[u].size();v++){
+            if(G[u][v] && !vis[v]){
+                vis[v] = true;
+                if(appName[v]==-1 || func(appName[v],G)){
+                    appName[v] = u;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
 	int maximumMatch(vector<vector<int>>&G){
-		int m = G.size();
-		int n = G[0].size();
-		int N = max(n, m);
-		int ans = 0;
-		mt.assign(N, -1);
-		for(int i = 0; i < m; i++){
-			used.assign(N, false);
-			if(try_kunh(i, G))
-				ans++;
-		}
-		return ans;
+	    int m = G.size();
+	    int n = G[0].size();
+	    int N = max(m,n);
+	    appName.clear();
+	    int ans = 0;
+	    appName.assign(N,-1);
+	    for(int i=0;i<m;i++){
+	        vis.assign(N,false);
+	        if(func(i,G)) ans++;
+	    }
+	    return ans;
 	}
 
 };
-
-
-// 0 -> 0 1 3 4
-// 1 -> 1 4
-// 2 -> 0 1 3 4
-
 
 //{ Driver Code Starts.
 int main(){
