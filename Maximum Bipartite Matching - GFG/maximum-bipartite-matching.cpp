bf57b06270e2ebf30b7125c7a5867e7fc4ca730d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-08 20:52:34 +0530

    Added solution - LeetHub

--- Maximum Bipartite Matching - GFG/maximum-bipartite-matching.cpp
+++ Maximum Bipartite Matching - GFG/maximum-bipartite-matching.cpp
@@ -5,14 +5,12 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    vector<bool>vis;
-    vector<int>appName;
-    
-    bool func(int u,vector<vector<int>>&G){
-        for(int v=0;v<G[u].size();v++){
+
+    bool func(int u,vector<vector<int>>&G,vector<int>&appName,vector<int>&vis){
+        for(int v=0;v<G[0].size();v++){
             if(G[u][v] && !vis[v]){
-                vis[v] = true;
-                if(appName[v]==-1 || func(appName[v],G)){
+                vis[v] = 1;
+                if(appName[v] < 0 || func(appName[v],G,appName,vis)){
                     appName[v] = u;
                     return true;
                 }
@@ -20,17 +18,15 @@ public:
         }
         return false;
     }
-    
+
 	int maximumMatch(vector<vector<int>>&G){
 	    int m = G.size();
 	    int n = G[0].size();
-	    int N = max(m,n);
-	    appName.clear();
+	    vector<int>appName(n,-1);
 	    int ans = 0;
-	    appName.assign(N,-1);
 	    for(int i=0;i<m;i++){
-	        vis.assign(N,false);
-	        if(func(i,G)) ans++;
+	        vector<int>vis(n,0);
+	        if(func(i,G,appName,vis)) ans++;
 	    }
 	    return ans;
 	}
