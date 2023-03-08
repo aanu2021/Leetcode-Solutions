583152deb667dc3835ecbf0afa464829bd794860
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-08 23:11:08 +0530

    Time: 34 ms (90.54%), Space: 11.9 MB (75.70%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -40,9 +40,9 @@ public:
     }
     
      
-     vector<int>tree;
+    vector<int>tree;
     
-     void BuildSeg(int idx,int low,int high){
+    void BuildSeg(int idx,int low,int high){
          if(low > high) return;
          if(low == high){
              tree[idx] = 0;
@@ -86,44 +86,30 @@ public:
         }
     }
     
-     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-         sort(people.begin(),people.end(),comp2);
-         int n = people.size();
-         // for(int i=0;i<people.size();i++){
-         //     cout<<people[i][0]<<" "<<people[i][1]<<" ,";
-         // }cout<<"\n";
-         vector<vector<int>>answer(n);
-         tree.resize(4*n,0);
-         BuildSeg(0,0,n-1);
-         for(int i=0;i<n;i++){
-             int reqd = people[i][1];
-             int low = 0, high = n-1;
-             int ans = -1;
-             while(low <= high){
-                 int mid = (low + high)/2;
-                 int bigger = (mid);
-                 bigger -= query(0,0,n-1,0,mid-1);
-                 // cout<<mid<<" "<<bigger<<"\n";
-                 if(bigger <= reqd){
-                     ans = mid;
-                     low = mid + 1;
-                 }
-                 else{
-                     high = mid - 1;
-                 }
-             }
-             // cout<<ans<<"\n";
-             answer[ans] = people[i];
-             Update(0,0,n-1,ans);
-             // for(int j=0;j<n;j++){
-             //     if(answer[j].size()){
-             //         cout<<answer[j][0]<<" "<<answer[j][1]<<", ";
-             //     }else{
-             //         cout<<"    ,";
-             //     }
-             // }cout<<"\n";
-         }
-         return answer;
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        int n = people.size();
+        sort(people.begin(),people.end(),comp2);
+        tree.resize(4*n);
+        BuildSeg(0,0,n-1);
+        vector<vector<int>>ans(n);
+        for(int i=0;i<n;i++){
+            int low = 0, high = n-1;
+            int idx = -1;
+            while(low <= high){
+                int mid = (low + high)/2;
+                int leftBigger = mid - query(0,0,n-1,0,mid-1);
+                if(leftBigger <= people[i][1]){
+                    idx = mid;
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+            ans[idx] = people[i];
+            Update(0,0,n-1,idx);
+        }
+        return ans;
     }
     
 };
@@ -138,9 +124,9 @@ public:
 
 /*
 
-[4,4] [5,0] [5,2] [6,1] [7,0] [7,1]
+[4,4] [5,2] [5,0] [6,1] [7,1] [7,0]
 
-                         4
+ 5     7     5     6     4     7
 ---   ---   ---   ---   ---   ---
  0     1     2     3     4     5   
  
