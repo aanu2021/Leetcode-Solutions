1249c99f79d3438aff52ff0bd0bbf1b93ddc7ec4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-08 10:22:24 +0530

    Time: 72 ms (25.97%), Space: 18.9 MB (82.50%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     
-    bool canTake(vector<int>&piles,int n,int banana_per_hour,int maxHour){
-        int currHour = 0;
+    bool canTake(vector<int>&piles,int n,int bph,int maxHour){
+        int totHour = 0;
         for(int i=0;i<n;i++){
-            currHour += (piles[i]/banana_per_hour);
-            if(piles[i]%banana_per_hour) currHour++;
-            if(currHour > maxHour) return false;
+            totHour += (piles[i]/bph);
+            if(piles[i]%bph) totHour++;
+            if(totHour > maxHour) return false;
         }
         return true;
     }
     
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        int low = 1;
-        int high = *max_element(piles.begin(),piles.end());
-        int ans = -1;
+        int low = 1,high = *max_element(piles.begin(),piles.end());
+        int ans = high;
         while(low <= high){
             int mid = low + (high-low)/2;
             if(canTake(piles,n,mid,h)){
@@ -29,17 +28,3 @@ public:
         return ans;
     }
 };
-
-/*
-
-4 11 20 23 30
-    
-1 + 1 + 1 + 1 + 2 = 6 <= 6
-    
-low  = 23
-high = 22
-mid = 23
-    
-ans = 23    
-
-*/
