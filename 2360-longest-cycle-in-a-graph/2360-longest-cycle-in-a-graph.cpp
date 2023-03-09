b1c97c730b559f25d2662e7b909b39758310f6f7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-09 11:32:50 +0530

    Time: 419 ms (50.06%), Space: 180.8 MB (33.14%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -3,46 +3,45 @@ public:
     
     vector<vector<int>>graph;
     vector<int>vis;
-    vector<int>tim;
+    vector<int>timer;
     
-    void dfs(int u,int p,int t,int &maxLen){
-        if(vis[u]) return;
+    void dfs(int u,int t,int &maxLen){
         vis[u] = 1;
-        tim[u] = t;
+        timer[u] = t;
         for(int v:graph[u]){
             if(vis[v]){
-                if(tim[v]){
-                    maxLen = max(maxLen,tim[u]-tim[v]+1);
+                if(timer[v]){
+                    maxLen = max(maxLen,timer[u]-timer[v]+1);
                 }
             }
             else{
-                dfs(v,u,t+1,maxLen);
+                dfs(v,t+1,maxLen);
             }
         }
-        tim[u] = 0;
+        timer[u] = 0;
     }
     
     int longestCycle(vector<int>& edges) {
-        
         int n = edges.size();
+        
         graph.clear();
         vis.clear();
-        tim.clear();
+        timer.clear();
         
         graph.resize(n);
         vis.resize(n,0);
-        tim.resize(n,0);
+        timer.resize(n,0);
         
-        for(int i=0;i<n;i++){
-            if(edges[i] == -1) continue;
+        for(int i=0;i<edges.size();i++){
+            if(edges[i]==-1) continue;
             graph[i].push_back(edges[i]);
         }
         
-        int maxLen = 0;
+        int maxLen = -1;
         for(int i=0;i<n;i++){
             if(vis[i]) continue;
-            dfs(i,-1,1,maxLen);
+            dfs(i,1,maxLen);
         }
-        return maxLen==0 ? -1 : maxLen;
+        return maxLen;
     }
 };
