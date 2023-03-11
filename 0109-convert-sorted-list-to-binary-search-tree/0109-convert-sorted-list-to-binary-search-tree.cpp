f68242229fff95122f3f1caaa0529140862a3390
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-11 10:45:00 +0530

    Time: 26 ms (78.13%), Space: 28.4 MB (47.40%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     
-    void printNodes(ListNode*head){
-        while(head){
-            cout<<head->val<<" ";
-            head = head->next;
-        }cout<<"\n";
-    }
+    /* Preorder Traversal (Root -> Left -> Right) */
     
-    TreeNode* sortedListToBST(ListNode* head) {
+    TreeNode* sortedListToBST2(ListNode* head) {
         
         if(!head) return NULL;
         if(!head->next) return new TreeNode(head->val);
@@ -32,19 +27,54 @@ public:
         return root;
         
     }
+    
+    /*
+
+    T(n) = 2*T(n/2) + n
+    T(n/2) = 2*T(n/4) + (n/2)
+    T(n/4) = 2*T(n/8) + (n/4)
+
+    T(n/2) = 2*2*T(n/8) + n
+    T(n) = 2^3 * T(n/2^3) + 3*n
+    T(n) = 2^k * T(n/2^k) + k*n
+
+    T(n) = n*O(1) + n*log2(n)
+    T(n) = nlogn  
+
+    Time Complexity  : O(N*logN)
+    Space Complexity : O(1) 
+
+    */
+
+    
+    /* Inorder Traversal (Left -> Root -> Right) */
+    
+    ListNode*head;
+    
+    TreeNode* recursion(int low,int high){
+        if(low > high) return NULL;
+        int mid = (low + high)/2;
+        TreeNode* root = new TreeNode(-1);
+        root->left = recursion(low,mid-1);
+        root->val = head->val;
+        head = head->next;
+        root->right = recursion(mid+1,high);
+        return root;
+    }
+    
+    TreeNode * sortedListToBST(ListNode* head){
+        
+        if(!head) return NULL;
+        if(!head->next) return new TreeNode(head->val);
+        int n = 0;
+        ListNode * ptr = head;
+        while(ptr){
+            n++; ptr = ptr->next;
+        }
+        this->head = head;
+        return recursion(0,n-1);
+        
+    }
+    
 };
 
-/*
-
-T(n) = 2*T(n/2) + n
-T(n/2) = 2*T(n/4) + (n/2)
-T(n/4) = 2*T(n/8) + (n/4)
-    
-T(n/2) = 2*2*T(n/8) + n
-T(n) = 2^3 * T(n/2^3) + 3*n
-T(n) = 2^k * T(n/2^k) + k*n
-
-T(n) = n*O(1) + n*log2(n)
-T(n) = nlogn  
-
-*/
