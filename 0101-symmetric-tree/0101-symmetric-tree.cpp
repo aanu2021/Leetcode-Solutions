c6611b4594bd77967b0440fa3825bdfa9da97a92
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-13 20:03:15 +0530

    Time: 0 ms (100.00%), Space: 16.8 MB (8.51%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     
-    bool isSym(TreeNode*&p , TreeNode*&q){
+    bool isSymRec(TreeNode*&p , TreeNode*&q){
         if(!p && !q) return true;
         if(!p || !q) return false;
         if(p->val != q->val) return false;
-        return isSym(p->left,q->right) && isSym(p->right,q->left);
+        return isSymRec(p->left,q->right) && isSymRec(p->right,q->left);
+    }
+    
+    bool isSymIter(TreeNode* &root){
+        queue<TreeNode*>p,q;
+        p.push(root->left);
+        q.push(root->right);
+        while(!p.empty() || !q.empty()){
+            auto r1 = p.front(); p.pop();
+            auto r2 = q.front(); q.pop();
+            if(!r1 && !r2) continue;
+            if(!r1 || !r2) return false;
+            if(r1->val != r2->val) return false;
+            p.push(r1->left);
+            p.push(r1->right);
+            q.push(r2->right);
+            q.push(r2->left);
+        }
+        return true;
     }
     
     bool isSymmetric(TreeNode* root) {
         if(!root) return true;
         if(!root->left && !root->right) return true;
-        return isSym(root->left,root->right);
+        // return isSymRec(root->left,root->right);
+        return isSymIter(root);
     }
 };
