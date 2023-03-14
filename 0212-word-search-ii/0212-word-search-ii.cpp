7d108c99d1a18fcee54e2bb96b55aaf5a70d1be5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-14 19:42:46 +0530

    Time: 738 ms (68.44%), Space: 15.6 MB (53.29%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,8 +1,15 @@
 struct Node{
-  
+    
     Node*links[26];
     int endWord;
     string eW;
+    
+    Node(){
+        for(int i=0;i<26;i++){
+            links[i] = NULL;
+        }
+        endWord = 0;
+    }
     
     bool containsKey(char ch){
         return (links[ch-'a'] != NULL);
@@ -13,68 +20,74 @@ struct Node{
     void put(char ch,Node*node){
         links[ch-'a'] = node;
     }
-    void setWord(string &str){
-        endWord++;
-        eW = str;
-    }
     
 };
 
 class Solution {
 public:
     
-    Node * root = new Node();
-    
-    vector<string>ans;
+    Node*root = new Node();
     
     void insert(string &word){
-        Node * node = root;
+        Node*node = root;
         for(int i=0;i<word.length();i++){
             if(!node->containsKey(word[i])){
                 node->put(word[i],new Node());
             }
             node = node->get(word[i]);
         }
-        node->setWord(word);
+        node->eW = word;
+        node->endWord++;
     }
     
-    void dfs(vector<vector<char>>&board,int i,int j,Node*curr){
+    vector<string>answer;
+    
+    void findWord(vector<vector<char>>&board,int i,int j,Node* curr){
+        
         int m = board.size();
         int n = board[0].size();
-        if(board[i][j]=='*') return;
+        
+        if(board[i][j] == '*') return;
         if(!curr->containsKey(board[i][j])) return;
-        char temp = board[i][j];
+        
         curr = curr->get(board[i][j]);
-        board[i][j] = '*';
         if(curr->endWord > 0){
             curr->endWord--;
-            ans.push_back(curr->eW);
+            answer.push_back(curr->eW);
         }
+        
+        char temp = board[i][j];
+        board[i][j] = '*';
+        
         if(i > 0){
-            dfs(board,i-1,j,curr);
+            findWord(board,i-1,j,curr);
+        }
+        if(i < m-1){
+            findWord(board,i+1,j,curr);
         }
         if(j > 0){
-            dfs(board,i,j-1,curr);
+            findWord(board,i,j-1,curr);
         }
-        if(i + 1 < m){
-            dfs(board,i+1,j,curr);
+        if(j < n-1){
+            findWord(board,i,j+1,curr);
         }
-        if(j + 1 < n){
-            dfs(board,i,j+1,curr);
-        }
+        
         board[i][j] = temp;
+        
     }
     
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Node * curr = root;
-        for(string &str:words){
+        int m = board.size();
+        int n = board[0].size();
+        for(string &str : words){
             insert(str);
         }
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                dfs(board,i,j,curr);
+        Node * curr =root;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                findWord(board,i,j,curr);
             }
         }
-        return ans;
+        return answer;
     }
 };
