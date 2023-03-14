85a03336a0dd9e4ca6a343a03189d4eb2b486bee
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-14 19:56:24 +0530

    Time: 4 ms (86.83%), Space: 7.9 MB (86.43%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -18,14 +18,34 @@ public:
         return dp[i] = currValue;
     }
     
+    int func2(string &s){
+        int n = s.length();
+        vector<bool>dp(n+1,false);
+        dp[n] = true;
+        
+        for(int i=n-1;i>=0;i--){
+            string word = "";
+            for(int j=i;j<n;j++){
+                word += s[j];
+                if(dict.find(word) != dict.end() && dp[j+1]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[0];
+    }
+    
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
-        vector<int>dp(n,-1);
         for(string &str : wordDict){
             dict.insert(str);
         }
-        int ans = func(s,0,n,dp);
-        if(ans > 0) return true;
-        else return false;
+        // vector<int>dp(n,-1);
+        // int ans = func(s,0,n,dp);
+        // if(ans > 0) return true;
+        // else return false;
+        return func2(s);
     }
 };
