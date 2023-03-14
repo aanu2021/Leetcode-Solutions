2dffb06c3137494d937292453846dcbe233309eb
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-14 11:56:29 +0530

    Time: 517 ms (55.49%), Space: 10.6 MB (26.76%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -4,19 +4,17 @@ public:
         int n = nums.size();
         vector<int>arr;
         arr.push_back(1);
-        for(int ele : nums){
-            arr.push_back(ele);
-        }
+        for(int &ele : nums) arr.push_back(ele);
         arr.push_back(1);
         vector<vector<int>>dp(n+5,vector<int>(n+5,0));
         for(int i=1;i<=n;i++){
-            dp[i][i] = (arr[i]*arr[i-1]*arr[i+1]);
+            dp[i][i] = (arr[i-1]*arr[i]*arr[i+1]);
         }
         for(int L=2;L<=n;L++){
             for(int i=1;i<=n-L+1;i++){
                 int j = i+L-1;
                 for(int k=i;k<=j;k++){
-                    dp[i][j] = max(dp[i][j],(arr[i-1]*arr[k]*arr[j+1]) + dp[i][k-1] + dp[k+1][j]);
+                    dp[i][j] = max(dp[i][j], (arr[i-1]*arr[k]*arr[j+1]) + dp[i][k-1] + dp[k+1][j]);
                 }
             }
         }
