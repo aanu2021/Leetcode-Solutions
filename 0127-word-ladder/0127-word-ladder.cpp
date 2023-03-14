7eb6f664909628fd178e9ecd488acd99e4928a2d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-14 20:17:54 +0530

    Time: 113 ms (61.60%), Space: 15.6 MB (35.16%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,38 +1,58 @@
 class Solution {
 public:
+    
+    /*
+    
+    Time Complexity  : O(wordListLen * beginWordLen * 26)
+    Space Complexity : O(wordListLen * beginWordLen * 26)
+    
+    */
+    
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>Good;
-        unordered_set<string>Visited;
+        
+        unordered_set<string>dict;
+        unordered_set<string>vis;
+        
         for(string &str : wordList){
-            Good.insert(str);
+            dict.insert(str);
         }
-        if(Good.find(endWord) == Good.end()) return 0;
+        if(dict.find(endWord) == dict.end()) return 0;
+        
         queue<string>q;
         q.push(beginWord);
-        Visited.insert(beginWord);
-        int lvl = 1;
+        vis.insert(beginWord);
+        
+        int lvl = 0;
         while(!q.empty()){
+            
             int sz = q.size();
             while(sz--){
-                auto str = q.front(); q.pop();
-                if(str == endWord) return lvl;
-                for(int i=0;i<str.length();i++){
-                    char prev = str[i];
+                
+                string word = q.front();
+                q.pop();
+                if(word == endWord) return lvl + 1;
+                
+                for(int i=0;i<word.length();i++){
+                    char pre = word[i];
                     for(char ch='a';ch<='z';ch++){
-                        if(prev == ch) continue;
-                        str[i] = ch;
-                        if(Good.find(str) != Good.end()){
-                            if(Visited.find(str) == Visited.end()){
-                                Visited.insert(str);
-                                q.push(str);
+                        if(ch==pre) continue;
+                        word[i] = ch;
+                        if(dict.find(word) != dict.end()){
+                            if(vis.find(word) == vis.end()){
+                                vis.insert(word);
+                                q.push(word);
                             }
                         }
+                        word[i] = pre;
                     }
-                    str[i] = prev;
                 }
+                
             }
+            
             lvl++;
         }
+        
         return 0;
+        
     }
 };
