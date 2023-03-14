47820cce3ffeb8686f048d750f4cf22757352004
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-14 20:54:32 +0530

    Time: 22 ms (52.74%), Space: 9.6 MB (36.45%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -7,25 +7,28 @@ public:
         for(int i=0;i<n;i++){
             if(s1[i] != s2[i]) diff++;
         }
-        return diff==1;
+        return diff == 1;
     }
     
-    void BFS(int src,int n,vector<vector<int>>&graph,vector<vector<int>>&parent){
+    void BFS(int src,int dest,int n,vector<vector<int>>&graph,vector<vector<int>>&parent){
+        
+        queue<int>q;
+        q.push(src);
         
         vector<int>dist(n,1e9);
         dist[src] = 0;
-        parent[src].push_back(-1);
-        queue<int>q;
-        q.push(src);
+        parent[src] = {-1};
+        
         while(!q.empty()){
-            int node = q.front(); q.pop();
-            for(int nbr:graph[node]){
-                if(dist[nbr] > 1 + dist[node]){
-                    dist[nbr] = 1 + dist[node];
+            int node = q.front();
+            q.pop();
+            for(int nbr : graph[node]){
+                if(dist[nbr] > dist[node] + 1){
+                    dist[nbr] = dist[node] + 1;
                     parent[nbr].push_back(node);
                     q.push(nbr);
                 }
-                else if(dist[nbr] == 1 + dist[node]){
+                else if(dist[nbr] == dist[node] + 1){
                     parent[nbr].push_back(node);
                 }
             }
@@ -35,43 +38,43 @@ public:
     
     void DFS(int node,int n,vector<vector<int>>&parent,vector<int>&path,vector<vector<int>>&allPath){
         
-        if(node == -1){
+        if(node==-1){
             allPath.push_back(path);
             return;
         }
-        
-        for(int par : parent[node]){
-            path.push_back(par);
-            DFS(par,n,parent,path,allPath);
+        for(int nbr : parent[node]){
+            path.push_back(nbr);
+            DFS(nbr,n,parent,path,allPath);
             path.pop_back();
         }
         
     }
     
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        int src = -1;
-        int dest = -1;
+        
         int n = wordList.size();
+        
+        int srcIdx = -1, destIdx = -1;
         for(int i=0;i<wordList.size();i++){
-            if(wordList[i]==endWord){
-                dest = i;
-                break;
+            if(wordList[i] == endWord){
+                destIdx = i;
+            }
+            if(wordList[i] == beginWord){
+                srcIdx = i;
             }
         }
-        if(dest == -1) return {};
-        for(int i=0;i<wordList.size();i++){
-            if(wordList[i]==beginWord){
-                src = i;
-                break;
-            }
-        }
-        if(src == -1){
-            wordList.push_back(beginWord);
-            src = n;
+        
+        if(destIdx == -1) return {};
+        if(srcIdx == -1){
+            srcIdx = n;
             n++;
+            wordList.push_back(beginWord);
         }
         
         vector<vector<int>>graph(n);
+        vector<vector<int>>parent(n);
+        vector<vector<string>>answer;
+        
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(isMatch(wordList[i],wordList[j])){
@@ -81,35 +84,63 @@ public:
             }
         }
         
-        vector<vector<int>>parent(n);
-        BFS(src,n,graph,parent);
+        BFS(srcIdx,destIdx,n,graph,parent);
         
-        vector<vector<int>>allPath;
-        vector<int>path;
-        DFS(dest,n,parent,path,allPath);
-        
-        // for(int i=0;i<allPath.size();i++){
-        //     for(int j=0;j<allPath[i].size();j++){
-        //         if(allPath[i][j]!=-1){
-        //             cout<<wordList[allPath[i][j]]<<" ";
-        //         }
+        // for(int i=0;i<n;i++){
+        //     cout<<wordList[i]<<"=>";
+        //     for(int j=0;j<parent[i].size();j++){
+        //         if(parent[i][j]==-1) continue;
+        //         cout<<wordList[parent[i][j]]<<" ";
         //     }cout<<"\n";
         // }
         
-        vector<vector<string>>ans;
+        vector<int>path;
+        vector<vector<int>>allPath;
+        
+        DFS(destIdx,n,parent,path,allPath);
         
         for(int i=0;i<allPath.size();i++){
             vector<string>currPath;
             allPath[i].pop_back();
-            currPath.push_back(wordList[dest]);
+            currPath.push_back(endWord);
             for(int j=0;j<allPath[i].size();j++){
                 currPath.push_back(wordList[allPath[i][j]]);
             }
             reverse(currPath.begin(),currPath.end());
-            ans.push_back(currPath);
+            answer.push_back(currPath);
         }
         
-        return ans;
+        return answer;
         
     }
 };
+
+/*
+
+src = hit
+des = cog
+
+hit hot dot dog lot log cog
+0   1    2   3   4   5   6
+    
+0 -> [1]
+1 -> [0,2,4]
+2 -> [1,3,4]
+3 -> [2,5,6]
+4 -> [1,2,5]
+5 -> [3,4,6]
+6 -> [3,5]
+
+0   
+1
+2 4
+3 5
+6
+    
+dist[i] = shortest distance of the i-th word from src.
+now we have to backtrack from the endWord 
+and create edges from the current word to immediately previous word , which can lead us to the src word.
+    
+ After that , we have to do a simple DFS traversal from the endWord using all the previously created edges during bfs traversal , and add them in our resultant container.   
+
+*/
