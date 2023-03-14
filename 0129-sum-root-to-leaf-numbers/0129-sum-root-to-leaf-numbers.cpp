b9fd971fe7c1710410eb8cc046b4080508211ba2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-14 11:13:45 +0530

    Time: 7 ms (19.15%), Space: 9.3 MB (52.62%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     
-    void dfs(TreeNode*& root,int currSum,int &totalSum){
+    void dfsRec(TreeNode*& root,int currSum,int &totalSum){
         if(!root) return;
         currSum = (currSum*10) + (root->val);
         if(!root->left && !root->right) totalSum += currSum;
-        dfs(root->left,currSum,totalSum);
-        dfs(root->right,currSum,totalSum);
+        dfsRec(root->left,currSum,totalSum);
+        dfsRec(root->right,currSum,totalSum);
+    }
+    
+    int dfsIter(TreeNode*& root){
+        stack<pair<TreeNode*,int>>S;
+        S.push({root,root->val});
+        int totalSum = 0;
+        while(!S.empty()){
+            
+            int currSum = S.top().second;
+            auto node = S.top().first;
+            S.pop();
+            
+            if(!node->left && !node->right) totalSum += currSum;
+            if(node->left) S.push({node->left,(currSum*10) + (node->left->val)});
+            if(node->right) S.push({node->right,(currSum*10) + (node->right->val)});
+            
+        }
+        
+        return totalSum;
     }
     
     int sumNumbers(TreeNode* root) {
         if(!root) return 0;
         int totalSum = 0;
-        dfs(root,0,totalSum);
-        return totalSum;
+        // dfsRec(root,0,totalSum);
+        // return totalSum;
+        return dfsIter(root);
     }
 };
