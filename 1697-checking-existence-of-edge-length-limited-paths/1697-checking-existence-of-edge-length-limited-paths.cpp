72d127b48d1beeacb3fbc5b703efe70895d14a7a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-21 22:12:18 +0530

    Time: 632 ms (66.29%), Space: 110.2 MB (74.72%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
@@ -1,77 +1,81 @@
+
+class DSU{
+  
+  public:
+  
+  vector<int>Parent;
+  vector<int>Size;
+  
+  DSU(int n){
+      Parent.resize(n);
+      Size.resize(n);
+      for(int i=0;i<n;i++){
+          Parent[i] = i;
+          Size[i] = 1;
+      }
+  }
+  
+  int find(int x){
+      if(Parent[x]==x) return x;
+      return Parent[x] = find(Parent[x]);
+  }
+  
+  bool Union(int x,int y){
+      int lx = find(x);
+      int ly = find(y);
+      if(lx != ly){
+          if(Size[lx] > Size[ly]){
+              Size[lx] += Size[ly];
+              Parent[ly] = lx;
+          }
+          else{
+              Size[ly] += Size[lx];
+              Parent[lx] = ly;
+          }
+          return true;
+      }
+      else{
+          return false;
+      }
+  }
+  
+  int getSize(int x){
+      int lx = find(x);
+      return Size[lx];
+  }
+    
+};
+
+
 class Solution {
 public:
-    
-    vector<int>Parent;
-    vector<int>Rank;
-    
-    int find(int x){
-        if(Parent[x]==x) return x;
-        return Parent[x]=find(Parent[x]);
-    }
-    
-    void Union(int x,int y){
-        int lx=find(x);
-        int ly=find(y);
-        if(lx!=ly){
-            if(Rank[lx]<Rank[ly]){
-                Parent[lx]=ly;
-            }else{
-                Parent[ly]=lx;
-                if(Rank[lx]==Rank[ly]){
-                    Rank[lx]++;
-                }
-            }
-        }
-    }
     
     bool static comp(const vector<int>&x,const vector<int>&y){
         return x[2] < y[2];
     }
     
     vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& queries) {
-        
+        DSU obj(n);
         int q = queries.size();
-        
-        Parent.resize(n);
-        Rank.resize(n);
-        
-        for(int i=0;i<n;i++){
-            Parent[i]=i;
-            Rank[i]=1;
-        }
-        
-        vector<bool>ans(q,false);
-        
         for(int i=0;i<q;i++){
             queries[i].push_back(i);
         }
-        
-        sort(queries.begin(),queries.end(),comp);
         sort(edgeList.begin(),edgeList.end(),comp);
-        
-        int j=0;
-        
+        sort(queries.begin(),queries.end(),comp);
+        vector<bool>answer(q,false);
+        int j = 0;
+        int sz = edgeList.size();
         for(int i=0;i<q;i++){
-            
-            while(j<edgeList.size() && edgeList[j][2] < queries[i][2]){
-                
-                Union(edgeList[j][0],edgeList[j++][1]);
-                
+            while(j<sz && edgeList[j][2] < queries[i][2]){
+                obj.Union(edgeList[j][0],edgeList[j][1]);
+                j++;
             }
-            
-            int u = queries[i][0];
-            int v = queries[i][1];
-            
-            int pu = find(u);
-            int pv = find(v);
-            
-            if(pu==pv){
-                ans[queries[i][3]] = true;
+            if(obj.find(queries[i][0]) == obj.find(queries[i][1])){
+                answer[queries[i][3]] = true;
+            }else{
+                answer[queries[i][3]] = false;
             }
-            
         }
-        
-        return ans;
-        
+        return answer;
     }
 };
