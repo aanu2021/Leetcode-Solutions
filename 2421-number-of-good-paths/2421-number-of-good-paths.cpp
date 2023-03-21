5b0070fe8caa849a67c14e926ba294e3dfb6b672
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-21 22:02:35 +0530

    Time: 697 ms (81.41%), Space: 195 MB (70.26%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.cpp
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -1,59 +1,82 @@
+
+class DSU{
+  
+  public:
+  
+  vector<int>Parent;
+  vector<int>Size;
+  
+  DSU(int n){
+      Parent.resize(n);
+      Size.resize(n);
+      for(int i=0;i<n;i++){
+          Parent[i] = i;
+          Size[i] = 1;
+      }
+  }
+  
+  int find(int x){
+      if(Parent[x]==x) return x;
+      return Parent[x] = find(Parent[x]);
+  }
+  
+  bool Union(int x,int y){
+      int lx = find(x);
+      int ly = find(y);
+      if(lx != ly){
+          if(Size[lx] > Size[ly]){
+              Size[lx] += Size[ly];
+              Parent[ly] = lx;
+          }
+          else{
+              Size[ly] += Size[lx];
+              Parent[lx] = ly;
+          }
+          return true;
+      }
+      else{
+          return false;
+      }
+  }
+  
+  int getSize(int x){
+      int lx = find(x);
+      return Size[lx];
+  }
+    
+};
+
+
 class Solution {
 public:
-    
-    vector<int>Parent;
-    vector<int>Rank;
-    
-    int find(int x){
-        if(Parent[x] == x) return x;
-        return Parent[x] = find(Parent[x]);
-    }
-    
-    void Union(int x,int y){
-        int lx = find(x);
-        int ly = find(y);
-        if(lx != ly){
-            if(Rank[lx] < Rank[ly]){
-                Parent[lx] = ly;
-            }
-            else{
-                Parent[ly] = lx;
-                if(Rank[lx] == Rank[ly]) Rank[lx]++;
-            }
-        }
-    }
-    
-    int numberOfGoodPaths(vector<int>& value, vector<vector<int>>& edges) {
-        int n = value.size();
-        map<int,vector<int>>vals;
+    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+        int n = vals.size();
+        map<int,vector<int>>values;
         map<int,vector<pair<int,int>>>adj;
         for(int i=0;i<n;i++){
-            vals[value[i]].push_back(i);
+            values[vals[i]].push_back(i);
         }
-        for(int i=0;i<edges.size();i++){
+        for(int i=0;i<edges.size();++i){
             int u = edges[i][0];
             int v = edges[i][1];
-            adj[max(value[u],value[v])].push_back({u,v});
+            adj[max(vals[u],vals[v])].push_back({u,v});
         }
         int ans = n;
-        Parent.resize(n);
-        Rank.resize(n);
-        for(int i=0;i<n;i++){
-            Parent[i] = i;
-            Rank[i] = 1;
-        }
-        for(auto &x : adj){
-            int currval = x.first;
-            for(auto &y : x.second){
-                Union(y.first,y.second);
+        DSU obj(n);
+        for(auto &itr : adj){
+            int currValue = itr.first;
+            for(auto &it : itr.second){
+                int u = it.first;
+                int v = it.second;
+                obj.Union(u,v);
             }
             map<int,int>cnt;
-            for(auto &ele : vals[currval]){
-                cnt[find(ele)]++;
+            for(int &ele : values[currValue]){
+                cnt[obj.find(ele)]++;
             }
-            for(auto &itr : cnt){
-                int z = itr.second;
-                ans += ((z*(z-1))/2);
+            for(auto &it : cnt){
+                int curr = it.second;
+                ans += ((curr*(curr-1))/2);
             }
         }
         return ans;
