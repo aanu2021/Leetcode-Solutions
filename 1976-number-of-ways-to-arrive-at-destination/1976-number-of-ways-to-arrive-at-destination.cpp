810bef76309b7f1e0874b091665a61cd7d21340a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-22 19:36:27 +0530

    Time: 76 ms (94.92%), Space: 30.3 MB (91.32%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -4,50 +4,37 @@ public:
     typedef long long ll;
     const ll M = 1e9 + 7;
     
-    ll mod(ll a){
-        return ((a%M)+M)%M;
-    }
-    ll mul(ll a,ll b){
-        return mod(mod(a)*mod(b));
-    }
-    ll add(ll a,ll b){
-        return mod(mod(a)+mod(b));
-    }
-    
     int countPaths(int n, vector<vector<int>>& roads) {
-        
         vector<vector<pair<int,int>>>graph(n);
         for(int i=0;i<roads.size();i++){
             graph[roads[i][0]].push_back({roads[i][1],roads[i][2]});
             graph[roads[i][1]].push_back({roads[i][0],roads[i][2]});
         }
-        
-        vector<ll>dist(n+1,1e15);
-        vector<ll>ways(n+1,0LL);
+        vector<ll>dist(n,1e15);
+        vector<ll>path(n,0LL);
         dist[0] = 0LL;
-        ways[0] = 1LL;
+        path[0] = 1LL;
         priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
         pq.push({0LL,0LL});
-        
         while(!pq.empty()){
-            auto curr = pq.top();
-            pq.pop();
-            ll node = curr.second;
+            auto curr = pq.top(); pq.pop();
             ll weight = curr.first;
+            ll node = curr.second;
             if(dist[node] < weight) continue;
             for(auto &nbrs : graph[node]){
-                ll nbr = nbrs.first;
-                ll edgeWt = nbrs.second;
-                if(dist[nbr] > dist[node] + edgeWt){
-                    dist[nbr] = dist[node] + edgeWt;
-                    ways[nbr] = ways[node];
+                int nbr = nbrs.first;
+                int edgeWt = nbrs.second;
+                if(dist[nbr] > weight + (ll)edgeWt){
+                    dist[nbr] = weight + (ll)edgeWt;
+                    path[nbr] = path[node];
                     pq.push({dist[nbr],nbr});
                 }
-                else if(dist[nbr] == dist[node] + edgeWt){
-                    ways[nbr] = add(ways[nbr],ways[node]);
+                else if(dist[nbr] == weight + (ll)edgeWt){
+                    path[nbr] += path[node];
+                    path[nbr] %= M;
                 }
             }
         }
-        return ways[n-1];
+        return path[n-1];
     }
 };
