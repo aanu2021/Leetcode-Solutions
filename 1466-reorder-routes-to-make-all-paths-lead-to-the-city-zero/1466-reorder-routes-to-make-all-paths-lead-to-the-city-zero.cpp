6889bcd7fc799d53b3514295ceeac855652e2b11
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-24 20:18:52 +0530

    Time: 325 ms (98.54%), Space: 110.7 MB (63.12%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -2,26 +2,40 @@ class Solution {
 public:
     
     vector<vector<pair<int,int>>>graph;
+    vector<int>subtree;
+    vector<int>ans;
     
-    int dfs(int u,int p){
+    void precompute(int u,int p){
         int sum = 0;
         for(auto &nbrs:graph[u]){
             int v = nbrs.first;
             int wt = nbrs.second;
             if(v==p) continue;
             sum += wt;
-            sum += dfs(v,u);
+            precompute(v,u);
+            sum += subtree[v];
         }
-        return sum;
+        subtree[u] = sum;
     }
     
     int minReorder(int n, vector<vector<int>>& connections) {
+        
+        graph.clear();
+        subtree.clear();
+        ans.clear();
+        
         graph.resize(n);
+        subtree.resize(n,0);
+        ans.resize(n,0);
+        
         for(int i=0;i<connections.size();i++){
-            graph[connections[i][0]].push_back({connections[i][1],0});
-            graph[connections[i][1]].push_back({connections[i][0],1});
+            int u = connections[i][0];
+            int v = connections[i][1];
+            graph[u].push_back({v,1});
+            graph[v].push_back({u,0});
         }
-        int change = dfs(0,-1);
-        return n-1-change;
+        
+        precompute(0,-1);
+        return subtree[0];
     }
 };
