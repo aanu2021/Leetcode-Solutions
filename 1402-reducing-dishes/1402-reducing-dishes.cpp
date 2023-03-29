1d7ad34e4ab3143e68b993b8c35f5a9211c3a884
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-29 10:31:30 +0530

    Time: 46 ms (39.05%), Space: 8.3 MB (69.31%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -4,30 +4,45 @@ class Solution {
 public:
     
     int maxSatisfaction(vector<int>& nums) {
+        
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1,MIN));
+        vector<int>prev(n+1,MIN);
+        vector<int>curr(n+1,MIN);
         
-        for(int i=0;i<n;i++) dp[i][0] = 0;
-        dp[0][1] = nums[0];
+        prev[0] = 0;
+        prev[1] = nums[0];
         
         for(int i=1;i<n;i++){
+            curr[0] = 0;
             for(int j=1;j<=n;j++){
-                dp[i][j] = max(dp[i-1][j],(j*nums[i]) + dp[i-1][j-1]);
+                curr[j] = max(prev[j],(j*nums[i]) + prev[j-1]);
+            }
+            for(int j=0;j<=n;j++){
+                prev[j] = curr[j];
             }
         }
-
+        
         int ans = 0;
         for(int i=0;i<=n;i++){
-            ans = max(ans,dp[n-1][i]);
+            ans = max(ans,prev[i]);
         }
         return ans;
+        
     }
 };
 
-// -9 -8 0 1 5
- 
-    // dp[idx][turn] = dp[idx-1][turn] , (turn*nums[idx]) + dp[idx-1][turn-1]
+/*
+
+base case : curr[0] = 0 [for all 0 <= i < n]
+            prev[0] = 0  ,  prev[1] = nums[0] 
+            
+
+prev[turn]
+curr[turn] = max(prev[turn],(turn*nums[idx]) + prev[turn-1])
+swap(prev,curr)    
     
+    
+*/
     
 
