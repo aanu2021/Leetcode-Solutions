ef8ebe82461f1c1b7eec98a06e6d76bd040bcd9a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-30 11:15:26 +0530

    Time: 45 ms (73.19%), Space: 14.4 MB (81.05%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -4,34 +4,29 @@ public:
     int dp[31][31][31];
     
     int func(string &s1,string &s2,int l1,int r1,int l2,int r2){
-       
-        int len = r1-l1+1;
-        if(len == 1) return s1[l1]==s2[l2];
-        if(dp[l1][r1][l2] != -1) return dp[l1][r1][l2];
+        
+        int len = (r1-l1+1);
+        if(l1==r1) return dp[l1][r1][l2] = (s1[l1] == s2[l2]);
         if(s1.substr(l1,len) == s2.substr(l2,len)) return dp[l1][r1][l2] = 1;
-         
-        int ans = 0;
+        if(dp[l1][r1][l2] != -1) return dp[l1][r1][l2];
         
+
         for(int i=l1;i<r1;i++){
-   
             int len1 = (i-l1+1);
-            int len2 = (len-len1);
-            
-            if(func(s1,s2,l1,i,l2,l2+len1-1) && func(s1,s2,i+1,r1,r2-len2+1,r2)) return dp[l1][r1][l2] = 1;
-            
-            if(func(s1,s2,l1,i,r2-len1+1,r2) && func(s1,s2,i+1,r1,l2,l2+len2-1)) return dp[l1][r1][l2] = 1;
-            
+            int len2 = len-len1;
+            if(func(s1,s2,l1,l1+len1-1,l2,l2+len1-1) && func(s1,s2,r1-len2+1,r1,r2-len2+1,r2)) return dp[l1][r1][l2] = 1;
+            if(func(s1,s2,l1,l1+len1-1,r2-len1+1,r2) && func(s1,s2,r1-len2+1,r1,l2,l2+len2-1)) return dp[l1][r1][l2] = 1;
         }
-        
         return dp[l1][r1][l2] = 0;
         
     }
     
     bool isScramble(string s1, string s2) {
+        
         int n = s1.length();
         memset(dp,-1,sizeof(dp));
         int ans = func(s1,s2,0,n-1,0,n-1);
         return ans > 0;
+        
     }
 };
-
