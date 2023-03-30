6ce7bc48591965f5b2b498193379cd25759ba786
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-03-30 11:25:24 +0530

    Time: 45 ms (73.19%), Space: 9.5 MB (93.30%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,32 +1,69 @@
 class Solution {
 public:
     
-    int dp[31][31][31];
+    /*
     
-    int func(string &s1,string &s2,int l1,int r1,int l2,int r2){
+    Approach : Recursion + Memoization
+    Time Complexity  : O(N^4)
+    Space Complexity : O(N^3)
+    
+    */
+    
+//     int dp[31][31][31];
+    
+//     int func(string &s1,string &s2,int l1,int r1,int l2,int r2){
         
-        int len = (r1-l1+1);
-        if(l1==r1) return dp[l1][r1][l2] = (s1[l1] == s2[l2]);
-        if(s1.substr(l1,len) == s2.substr(l2,len)) return dp[l1][r1][l2] = 1;
-        if(dp[l1][r1][l2] != -1) return dp[l1][r1][l2];
+//         int len = (r1-l1+1);
+//         if(l1==r1) return dp[l1][r1][l2] = (s1[l1] == s2[l2]);
+//         if(s1.substr(l1,len) == s2.substr(l2,len)) return dp[l1][r1][l2] = 1;
+//         if(dp[l1][r1][l2] != -1) return dp[l1][r1][l2];
         
 
-        for(int i=l1;i<r1;i++){
-            int len1 = (i-l1+1);
-            int len2 = len-len1;
-            if(func(s1,s2,l1,l1+len1-1,l2,l2+len1-1) && func(s1,s2,r1-len2+1,r1,r2-len2+1,r2)) return dp[l1][r1][l2] = 1;
-            if(func(s1,s2,l1,l1+len1-1,r2-len1+1,r2) && func(s1,s2,r1-len2+1,r1,l2,l2+len2-1)) return dp[l1][r1][l2] = 1;
-        }
-        return dp[l1][r1][l2] = 0;
+//         for(int i=l1;i<r1;i++){
+//             int len1 = (i-l1+1);
+//             int len2 = len-len1;
+//             if(func(s1,s2,l1,l1+len1-1,l2,l2+len1-1) && func(s1,s2,r1-len2+1,r1,r2-len2+1,r2)) return dp[l1][r1][l2] = 1;
+//             if(func(s1,s2,l1,l1+len1-1,r2-len1+1,r2) && func(s1,s2,r1-len2+1,r1,l2,l2+len2-1)) return dp[l1][r1][l2] = 1;
+//         }
+//         return dp[l1][r1][l2] = 0;
         
-    }
+//     }
     
     bool isScramble(string s1, string s2) {
         
         int n = s1.length();
-        memset(dp,-1,sizeof(dp));
-        int ans = func(s1,s2,0,n-1,0,n-1);
-        return ans > 0;
+        
+        bool dp[n][n][n+1];
+        memset(dp,false,sizeof(dp));
+        
+        // Base case -> 1-length substrings from both side.
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j][1] = (s1[i]==s2[j]);
+            }
+        }
+        
+        // Recursive Transition.
+        
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<n-len+1;i++){
+                for(int j=0;j<n-len+1;j++){
+                    if(s1.substr(i,len) == s2.substr(j,len)){
+                        dp[i][j][len] = true;
+                    }
+                    else{
+                        // we have to check all possible partitions 
+                        
+                        for(int k=1;k<len && !dp[i][j][len];k++){
+                            dp[i][j][len] = (dp[i][j][k] && dp[i+k][j+k][len-k]) || (dp[i][j+len-k][k] && dp[i+k][j][len-k]);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return dp[0][0][n];
         
     }
 };
