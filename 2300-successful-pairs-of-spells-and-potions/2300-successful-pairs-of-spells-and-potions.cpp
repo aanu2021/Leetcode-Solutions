eb715ed2e728920e2eeea6a000c5b6fb47d44f51
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-02 11:53:07 +0530

    Time: 259 ms (52.76%), Space: 106 MB (11.26%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
+    
+    typedef long long ll;
+    
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        
         vector<int>answer;
-        sort(potions.begin(),potions.end());
         int n = spells.size();
         int m = potions.size();
+        answer.resize(n,0);
+        
+        vector<pair<int,int>>spellsArr;
         for(int i=0;i<n;i++){
-            long long req = (success/spells[i]);
-            if(success%spells[i]) req++;
-            int idx = lower_bound(potions.begin(),potions.end(),req) - potions.begin();
-            answer.push_back(m-idx);
+            spellsArr.push_back({spells[i],i});
+        }
+        
+        sort(spellsArr.begin(),spellsArr.end());
+        sort(potions.begin(),potions.end());
+        
+        int i = n-1;
+        int j = 0;
+        
+        for(i=n-1;i>=0;i--){
+            while(j < m && (ll)((ll)spellsArr[i].first*(ll)potions[j]) < success){
+                j++;
+            }
+            answer[spellsArr[i].second] = (m-j);
         }
         return answer;
+        
     }
 };
+
+// n = 3
+// m = 5
+// success = 7    
+
+// 1 3 5
+// 1 2 3 4 5    
 
 // 5 * x >= 7
 // x >= 7/5    
