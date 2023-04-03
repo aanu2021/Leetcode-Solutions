d65a5f3428c401c3ca5c0b3c6286c51b5bd48b78
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-03 11:26:35 +0530

    Time: 350 ms (35.28%), Space: 85.1 MB (49.17%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -6,41 +6,38 @@ public:
     
     int waysToSplit(vector<int>& nums) {
         int n = nums.size();
-        int sum = accumulate(nums.begin(),nums.end(),0);
         vector<int>prefix(n,0);
         prefix[0] = nums[0];
         for(int i=1;i<n;i++){
             prefix[i] = prefix[i-1];
             prefix[i] += nums[i];
         }
-        ll ways = 0LL;
+        ll answer = 0LL;
         for(int i=1;i<n-1;i++){
             int leftSum = prefix[i-1];
-            int lidx = -1, ridx = -1;
-            int low = i , high = n-2;
-            lidx = lower_bound(prefix.begin()+i,prefix.begin()+n-1,2*leftSum) - prefix.begin();
+            int lidx = lower_bound(prefix.begin()+i,prefix.end(),2*leftSum) - prefix.begin();
             if(lidx >= n-1) lidx = -1;
+            int ridx = -1;
             if(lidx != -1){
-                low = lidx , high = n-2;
+                int low = lidx , high = n-2;
                 while(low <= high){
                     int mid = (low + high)/2;
                     int midSum = prefix[mid] - prefix[i-1];
                     int rightSum = prefix[n-1] - prefix[mid];
-                    if(midSum >= leftSum && rightSum >= midSum){
+                    if(leftSum <= midSum && midSum <= rightSum){
                         ridx = mid;
                         low = mid + 1;
                     }
                     else{
                         high = mid - 1;
                     }
-                }    
+                }
             }
-            
-            // cout<<lidx<<" "<<ridx<<"\n";
-            if(lidx == -1 || ridx == -1) continue;
-            ways += (ll)(ridx-lidx+1);
-            ways %= M;
+            if(lidx != -1 && ridx != -1){
+                answer += (ll)(ridx-lidx+1);
+                answer %= M;
+            }
         }
-        return ways;
+        return answer;
     }
 };
