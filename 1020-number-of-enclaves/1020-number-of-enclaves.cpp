bae75716cdc1c78e94349cdb13c30bd4c5f3e16c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-07 10:26:32 +0530

    Time: 65 ms (96.02%), Space: 28.4 MB (68.59%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,72 +1,47 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>&grid,int i,int j){
-        
-        int m=grid.size();
-        int n=grid[0].size();
-        
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0){
+    int dx[4] = {0,0,1,-1};
+    int dy[4] = {1,-1,0,0};
+    
+    void dfs(vector<vector<int>>&grid,int i,int j,int m,int n)
+    {
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j]==0){
             return;
         }
-        
-        grid[i][j]=0;
-        
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
-        
+        grid[i][j] = 0;
+        for(int dir=0;dir<4;dir++){
+            int ni = i + dx[dir];
+            int nj = j + dy[dir];
+            dfs(grid,ni,nj,m,n);
+        }
     }
     
     int numEnclaves(vector<vector<int>>& grid) {
-        
-        int ans=0;
-        
-         int m=grid.size();
-         int n=grid[0].size();  
-        
+        int m = grid.size();
+        int n = grid[0].size();
         for(int i=0;i<m;i++){
-            
             if(grid[i][0]==1){
-                
-                dfs(grid,i,0);
-                
+                dfs(grid,i,0,m,n);
             }
-            
             if(grid[i][n-1]==1){
-                
-                dfs(grid,i,n-1);
-                
+                dfs(grid,i,n-1,m,n);
             }
-            
         }
-        
-        
         for(int i=0;i<n;i++){
-            
             if(grid[0][i]==1){
-                
-                dfs(grid,0,i);
-                
+                dfs(grid,0,i,m,n);
             }
-            
             if(grid[m-1][i]==1){
-                
-                dfs(grid,m-1,i);
-                
+                dfs(grid,m-1,i,m,n);
             }
-            
         }
-        
-        
+        int ans = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                ans+=grid[i][j];
+                ans += grid[i][j];
             }
         }
-        
         return ans;
-        
     }
 };
