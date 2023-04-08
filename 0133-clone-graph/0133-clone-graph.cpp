2ef4fadecd5c312471dd6155f3b3750e6cb11476
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-08 19:34:25 +0530

    Time: 5 ms (68.56%), Space: 8.4 MB (95.28%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    unordered_map<Node*,Node*>copies;
+    
+    Node* cloneGraph(Node* node) {
+        if(!node) return NULL;
+        Node*copy = new Node(node->val);
+        copies[node] = copy;
+        queue<Node*>q;
+        q.push(node);
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            for(auto &nbrs : curr->neighbors){
+                if(copies.find(nbrs) == copies.end()){
+                    copies[nbrs] = new Node(nbrs->val);
+                    q.push(nbrs);
+                }
+                copies[curr]->neighbors.push_back(copies[nbrs]);
+            }
+        }
+        return copy;
+    }
+};
