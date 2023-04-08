6ba7dc385a83d1ae484a330960e56b49bd8103d1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-08 19:48:57 +0530

    Time: 3 ms (95.03%), Space: 8.8 MB (65.38%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,23 +21,19 @@ public:
 
 class Solution {
 public:
+    
+    unordered_map<Node*,Node*>copies;
+    
     Node* cloneGraph(Node* node) {
         if(!node) return NULL;
-        unordered_map<Node*,Node*>copies;
-        Node*copy = new Node(node->val);
+        Node * copy = new Node(node->val);
         copies[node] = copy;
-        queue<Node*>q;
-        q.push(node);
-        while(!q.empty()){
-            auto curr = q.front();
-            q.pop();
-            for(Node*nbrs : curr->neighbors){
-                if(copies.find(nbrs) == copies.end()){
-                    copies[nbrs] = new Node(nbrs->val);
-                    q.push(nbrs);
-                }
-                copies[curr]->neighbors.push_back(copies[nbrs]);
+        for(auto &nbrs : node->neighbors){
+            if(copies.find(nbrs) == copies.end()){
+                copies[nbrs] = new Node(nbrs->val);
+                cloneGraph(nbrs);
             }
+            copies[node]->neighbors.push_back(copies[nbrs]);
         }
         return copy;
     }
