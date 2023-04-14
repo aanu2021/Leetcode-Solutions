16ebb910fc97c4c3565efafbaec52abef3ca0be7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-14 17:48:12 +0530

    Time: 202 ms (36.17%), Space: 6.5 MB (98.67%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -4,20 +4,23 @@ public:
         int n = s1.length();
         string s2 = s1;
         reverse(s2.begin(),s2.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>prev(n+1,0);
+        vector<int>curr(n+1,0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(s1[i-1]==s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    curr[j] = max(prev[j],curr[j-1]);
                 }
             }
+            for(int j=1;j<=n;j++){
+                prev[j] = curr[j];
+            }
         }
-        return dp[n][n];
+        return curr[n];
     }
 };
 
-// abcda
-// adcba
-// 01234
+  // dp[i][j] = dp[i-1][j-1] + 1
+  // dp[i][j] = max(dp[i-1][j],dp[i][j-1])  
