cd6639bfd2876d98692cb77740eedd0da2a52446
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-15 09:59:13 +0530

    Time: 1231 ms (7.49%), Space: 92.7 MB (14.54%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -1,56 +1,28 @@
 class Solution {
 public:
-    
-    struct Node{
-        int node;
-        int timer;
-        int cost;
-    };
-    
-    struct comp{
-       bool operator()(const Node&x,const Node&y){
-           return x.cost > y.cost; 
-       }  
-    };
-    
     int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
         
         int n = passingFees.size();
-        vector<int>minTime(n,maxTime+1);
+        int ans = 1e9;
         
-        vector<vector<pair<int,int>>>graph(n);
+        vector<vector<int>>dp(n,vector<int>(maxTime+1,1e9));
+        dp[0][0] = passingFees[0];
         
-        for(int i=0;i<edges.size();i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-            graph[u].push_back({v,w});
-            graph[v].push_back({u,w});
-        }
-        
-        priority_queue<Node,vector<Node>,comp>pq;
-        pq.push({0,0,passingFees[0]});
-        minTime[0] = 0;
-        
-        while(!pq.empty()){
-            auto curr = pq.top(); pq.pop();
-            int node = curr.node;
-            int timer = curr.timer;
-            int cost = curr.cost;
-            if(node==n-1) return cost;
-            for(auto &nbrs : graph[node]){
-                int nbr = nbrs.first;
-                int edgeTime = nbrs.second;
-                int nextTime = edgeTime + timer;
-                int nextCost = cost + passingFees[nbr];
-                if(nextTime < minTime[nbr]){
-                    minTime[nbr] = nextTime;
-                    pq.push({nbr,minTime[nbr],nextCost});
+        for(int i=1;i<=maxTime;i++){
+            for(int j=0;j<edges.size();j++){
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int tim = edges[j][2];
+                if(i >= tim){
+                    dp[u][i] = min(dp[u][i],dp[v][i-tim]+passingFees[u]);
+                    dp[v][i] = min(dp[v][i],dp[u][i-tim]+passingFees[v]);
                 }
             }
+            ans = min(ans,dp[n-1][i]);
         }
         
-        return -1;
+        if(ans >= 1e9) return -1;
+        else return ans;
         
     }
 };
