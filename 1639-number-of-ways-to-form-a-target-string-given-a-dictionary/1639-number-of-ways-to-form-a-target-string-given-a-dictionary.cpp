ca1a9d031b5dd5a6128344a58672eebe5d15cc66
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-17 20:19:43 +0530

    Time: 347 ms (46.25%), Space: 79 MB (30.00%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -4,42 +4,33 @@ public:
     typedef long long ll;
     const ll M = 1e9 + 7;
     
+    vector<vector<int>>freq;
+    vector<vector<ll>>dp;
+    
+    ll func(string &target,int i,int j,int m,int n){
+        if(j==n) return 1LL;
+        if(i==m) return 0LL;
+        if(dp[i][j] != -1LL) return dp[i][j];
+        ll ans = func(target,i+1,j,m,n);
+        int pos = target[j]-'a';
+        ans += freq[i][pos]*func(target,i+1,j+1,m,n);
+        ans %= M;
+        return dp[i][j] = ans;
+    }
+    
     int numWays(vector<string>& words, string target) {
         int m = words[0].length();
         int n = target.length();
-        
-        vector<vector<int>>freq(m,vector<int>(26,0));
-        for(int i=0;i<words[0].length();++i){
-            for(int j=0;j<words.size();++j){
+        freq.clear();
+        dp.clear();
+        freq = vector<vector<int>>(m,vector<int>(26,0));
+        dp = vector<vector<ll>>(m,vector<ll>(n,-1LL));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<words.size();j++){
                 freq[i][words[j][i]-'a']++;
             }
         }
-        
-        vector<vector<ll>>dp(m+1,vector<ll>(n+1,0LL));
-        for(int i=0;i<=m;i++){
-            dp[i][n] = 1LL;
-        }
-        
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                int pos = (target[j]-'a');
-                ll ways = dp[i+1][j];
-                ways %= M;
-                ways += (freq[i][pos]*dp[i+1][j+1]);
-                ways %= M;
-                dp[i][j] = ways;
-            }
-        }
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<2;j++){
-        //         cout<<freq[i][j]<<" ";
-        //     }cout<<"\n";
-        // }
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }cout<<"\n";
-        // }
-        return dp[0][0];
+        ll ans = func(target,0,0,m,n);
+        return ans;
     }
 };
