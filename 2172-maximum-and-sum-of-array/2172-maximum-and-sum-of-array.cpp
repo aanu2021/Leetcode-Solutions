ea8acd17b7a9111c30c67739a8de2fc0c6e1fd8f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-18 20:08:39 +0530

    Time: 21 ms (100.00%), Space: 9.3 MB (88.05%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -3,27 +3,23 @@ public:
    
     vector<int>dp;
     
-    int func(vector<int>&nums,int i,int n,int mask,int ns,int numSlots){
-        
+    int func(vector<int>&nums,int i,int n,int mask,int ns){
         if(i==n) return 0;
         if(dp[mask] != -1) return dp[mask];
         int currMax = 0;
-        for(int j=0;j<ns;j++){
-            if((mask&(1<<j))==0) continue;
-            int curr = j;
-            if(j >= numSlots) curr -= numSlots;
-            currMax = max(currMax,((curr+1)&nums[i]) + func(nums,i+1,n,(mask^(1<<j)),ns,numSlots));
+        for(int slot=1,bit=1;slot<=ns;slot++,bit*=3){
+            if((mask/bit)%3 > 0){
+                currMax = max(currMax,(nums[i]&slot) + func(nums,i+1,n,mask-bit,ns));
+            }
         }
         return dp[mask] = currMax;
-        
     }
     
     int maximumANDSum(vector<int>& nums, int numSlots) {
         int n = nums.size();
-        int ns = 2*numSlots;
-        int MASK = (1<<ns);
-        dp = vector<int>(MASK,-1);
-        int ans = func(nums,0,n,MASK-1,ns,numSlots);
+        int mask = pow(3,numSlots);
+        dp = vector<int>(mask,-1);
+        int ans = func(nums,0,n,mask-1,numSlots);
         return ans;
     }
 };
@@ -37,4 +33,18 @@ public:
     
 // dp[i][mask]    
     
-// 3^0 + 3^1 + 3^2 + .....     
+// 3^0 + 3^1 + 3^2 + ..... 
+
+// 3^3 = 27 - 1 = 26
+    
+// 2*3^2 + 2*3^1 + 2*3^0
+    
+    
+
+// 1 + 3 + 9 + 27 = 40
+    
+    
+
+// 3^1 + 3^2 + 3^3 + 3^4 + ......
+    
+    
