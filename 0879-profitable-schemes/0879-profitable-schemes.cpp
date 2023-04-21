3783475ad5deef3b3f248fe9f755956cfe14c6fd
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-21 10:52:31 +0530

    Time: 334 ms (24.53%), Space: 93.9 MB (9.91%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    typedef long long ll;
+    const ll M = 1e9 + 7;
+    
+    vector<int>group;
+    vector<int>profit;
+    vector<vector<vector<ll>>>dp;
+    
+    ll func(int i,int n,int G,int P){
+        if(i==n){
+            if(P <= 0) return 1LL;
+            else return 0LL;
+        }
+        if(P < 0) P = 0;
+        if(dp[i][G][P] != -1) return dp[i][G][P];
+        ll ans = func(i+1,n,G,P);
+        if(G >= group[i]){
+            ans += func(i+1,n,G-group[i],P-profit[i]);
+            ans %= M;
+        }
+        return dp[i][G][P] = ans;
+    }
+    
+    int profitableSchemes(int G, int P, vector<int>& group_, vector<int>& profit_) {
+        int n = group_.size();
+        group = group_;
+        profit = profit_;
+        dp = vector<vector<vector<ll>>>(n+1,vector<vector<ll>>(G+1,vector<ll>(P+1,-1LL)));
+        ll ans = func(0,n,G,P);
+        return ans;
+    }
+};
