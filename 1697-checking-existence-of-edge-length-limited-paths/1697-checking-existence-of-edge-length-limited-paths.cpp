a42a2e4b94da3c785b41c171dd7f34cb1ec48552
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-04-29 10:31:01 +0530

    Time: 731 ms (12.87%), Space: 110 MB (85.06%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
@@ -1,51 +1,60 @@
-
 class DSU{
   
-  public:
-  
-  vector<int>Parent;
-  vector<int>Size;
-  
-  DSU(int n){
-      Parent.resize(n);
-      Size.resize(n);
-      for(int i=0;i<n;i++){
-          Parent[i] = i;
-          Size[i] = 1;
-      }
-  }
-  
-  int find(int x){
-      if(Parent[x]==x) return x;
-      return Parent[x] = find(Parent[x]);
-  }
-  
-  bool Union(int x,int y){
-      int lx = find(x);
-      int ly = find(y);
-      if(lx != ly){
-          if(Size[lx] > Size[ly]){
-              Size[lx] += Size[ly];
-              Parent[ly] = lx;
-          }
-          else{
-              Size[ly] += Size[lx];
-              Parent[lx] = ly;
-          }
-          return true;
-      }
-      else{
-          return false;
-      }
-  }
-  
-  int getSize(int x){
-      int lx = find(x);
-      return Size[lx];
-  }
+    private:
+    
+    vector<int>Parent,Rank;
+    
+    public:
+    
+    DSU(int n){
+        Parent.resize(n);
+        Rank.resize(n);
+        for(int i=0;i<n;i++){
+            Parent[i] = i;
+            Rank[i] = 1;
+        }
+    }
+    
+    int find(int x){
+        if(Parent[x]==x) return x;
+        return Parent[x] = find(Parent[x]);
+    }
+    
+    bool isSameLeader(int x,int y){
+        int lx = find(x);
+        int ly = find(y);
+        if(lx != ly){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+    bool Union(int x,int y){
+        int lx = find(x);
+        int ly = find(y);
+        if(lx != ly){
+            if(Rank[lx] > Rank[lx]){
+                Parent[ly] = lx;
+            }
+            else{
+                Parent[lx] = ly;
+                if(Rank[lx]==Rank[ly]) Rank[lx]++;
+            }
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool isLeader(int x){
+        int lx = find(x);
+        return lx==x;
+    }
     
 };
-
 
 class Solution {
 public:
@@ -55,26 +64,28 @@ public:
     }
     
     vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& queries) {
-        DSU obj(n);
+        
         int q = queries.size();
-        for(int i=0;i<q;i++){
+        int m = edgeList.size();
+        
+        for(int i=0;i<queries.size();i++){
             queries[i].push_back(i);
         }
+        
         sort(edgeList.begin(),edgeList.end(),comp);
         sort(queries.begin(),queries.end(),comp);
+        
         vector<bool>answer(q,false);
-        int j = 0;
-        int sz = edgeList.size();
+        DSU obj(n);
+        
+        int ptr = 0;
+        
         for(int i=0;i<q;i++){
-            while(j<sz && edgeList[j][2] < queries[i][2]){
-                obj.Union(edgeList[j][0],edgeList[j][1]);
-                j++;
+            while(ptr < m && edgeList[ptr][2] < queries[i][2]){
+                obj.Union(edgeList[ptr][0],edgeList[ptr][1]);
+                ptr++;
             }
-            if(obj.find(queries[i][0]) == obj.find(queries[i][1])){
-                answer[queries[i][3]] = true;
-            }else{
-                answer[queries[i][3]] = false;
-            }
+            answer[queries[i][3]] = obj.isSameLeader(queries[i][0],queries[i][1]);
         }
         return answer;
     }
