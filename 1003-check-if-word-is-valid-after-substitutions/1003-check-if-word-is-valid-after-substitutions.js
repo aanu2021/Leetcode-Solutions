570a29a552d8841943d4f18840101c71ee558bd7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-05-05 10:57:59 +0530

    Time: 65 ms (76.19%), Space: 48.6 MB (19.05%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.js
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.js
@@ -1,28 +1,19 @@
-class Solution {
-public:
-    bool isValid(string s) {
-        int n = s.length();
-        stack<char>S;
-        for(int i=0;i<n;i++){
-            if(s[i]=='a' || s[i]=='b'){
-                S.push(s[i]);
-            }
-            else{
-                if(S.size() < 2) return false;
-                char ch2 = S.top(); S.pop();
-                char ch1 = S.top(); S.pop();
-                if(ch2=='b' && ch1=='a'){
-                    continue;
-                }
-                else{
-                    S.push(ch1);
-                    S.push(ch2);
-                    S.push(s[i]);
-                }
-            }
-        }
-        return S.size()==0;
-    }
-};
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 
-// abcababcc
+
+var isValid = function(s) {
+     const stack = [];
+
+    for (const char of s) {
+        if (char === 'c') {
+            if (stack.length < 2) return false;
+            if (stack.at(-1) !== 'b' || stack.at(-2) !== 'a') return false;
+            stack.splice(-2, 2);
+        }
+        else stack.push(char);
+    }
+    return stack.length === 0;
+};
