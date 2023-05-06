c8c25865e0fc5619c13341b92153f9b27a10af8b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-05-06 10:35:02 +0530

    Time: 303 ms (5.07%), Space: 47.7 MB (93.20%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,75 +1,76 @@
 class Solution {
 public:
     
-    /*
-    
-    Binary Search Approach
-    
-    Time Complexity  : O(N*logN)
-    Space Complexity : O(N)
-    
-    */
-    
     typedef long long ll;
-    
     const ll M = 1e9 + 7;
     
     ll mod(ll a){
         return ((a%M)+M)%M;
     }
-    
     ll mul(ll a,ll b){
         return mod(mod(a)*mod(b));
     }
-    
     ll add(ll a,ll b){
         return mod(mod(a)+mod(b));
     }
-    
-   
+    ll modPow(ll a,ll b){
+        if(b==0) return 1LL;
+        if(b==1) return a;
+        ll res = 1LL;
+        while(b){
+            if(b%2) res = mul(res,a);
+            a = mul(a,a);
+            b/=2;
+        }
+        return res;
+    }
     
     int numSubseq(vector<int>& nums, int target) {
+        int n = nums.size();
         
         sort(nums.begin(),nums.end());
         
-        ll n=nums.size();
-        
-        vector<ll>pow2(n+1,0LL);
-        
-        pow2[0]=1LL;
-        
-        for(ll i=1;i<=n;++i){
+        ll ans = 0LL;
+        for(int i=0;i<n;i++){
             
-            pow2[i]=mul(2LL,pow2[i-1]);
+            int ele = nums[i];
+            if(2*ele > target) break;
             
-        }
-        
-        
-        ll l=0,r=n-1,ans=0LL;
-        
-        while(l<=r){
+            int j = upper_bound(nums.begin()+i,nums.end(),target-ele) - nums.begin();
+            int cntEle = (j-i-1);
+            // cout<<cntEle<<"\n";
             
-            ll currsum=nums[l]+nums[r];
-            
-            if(currsum>target){
-                
-                r--;
-                
-            }
-            
-            else{
-                
-                ll currval=pow2[r-l];
-                
-                ans=add(ans,currval);
-                
-                l++;
-                
-            }
+            ans += modPow(2LL,(ll)cntEle);
+            ans %= M;
             
         }
-        
         return ans;
-        
     }
 };
+
+/*
+
+min max
+
+min + max <= target
+
+sort
+
+a0 a1 a2 a3 a4 a5
+
+a0 + a4 > target
+
+a0,a1,a2,a3
+
+a0 (2^3) = 8 + 
+    
+a0 _
+a0 a1
+a0 a2
+a0 a3
+a0 a1 a2
+a0 a1 a3
+a0 a2 a3
+a0 a1 a2 a3
+
+*/
