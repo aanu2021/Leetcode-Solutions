21baf391ca1968a9a73d52e7942a519f43f04fd7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-05-11 08:34:59 +0530

    Time: 20 ms (64.75%), Space: 13.2 MB (9.73%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -21,9 +21,25 @@ public:
         int n = nums2.size();
         
         dp.clear();
-        dp = vector<vector<int>>(m+1,vector<int>(n+1,-1));
+        dp = vector<vector<int>>(m+5,vector<int>(n+5,0));
         
-        int ans = func(nums1,nums2,0,0,m,n);
-        return ans;
+        // Top-Down Approach
+        // int ans = func(nums1,nums2,0,0,m,n);
+        // return ans;
+        
+        // Bottom-Up Approach
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(nums1[i]==nums2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        
+        return dp[0][0];
     }
 };
+
