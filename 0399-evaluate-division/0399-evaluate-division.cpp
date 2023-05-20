1f89de7334829d320d1f4e092cc26ddfee8735c9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-05-20 20:48:20 +0530

    Time: 3 ms (57.42%), Space: 8.3 MB (73.76%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -6,7 +6,7 @@ public:
     unordered_map<string,int>visited;
     
     void dfs(string start,string end,bool &found,double &val){
-       
+        
         if(visited[start]) return;
         visited[start] = 1;
         
@@ -52,48 +52,47 @@ public:
             string rel = left + "->" + right;
             string rev_rel = right + "->" + left;
             
-            graph[left].push_back(right);
-            graph[right].push_back(left);
-            
             MAP[rel] = values[i];
             MAP[rev_rel] = 1.0/values[i];
+            
+            graph[left].push_back(right);
+            graph[right].push_back(left);
             
         }
         
         for(int i=0;i<q;i++){
             
-            string left = queries[i][0];
-            string right = queries[i][1];
+            string start = queries[i][0];
+            string end = queries[i][1];
             
-            if(graph.find(left) == graph.end() || graph.find(right) == graph.end())
-            {
-                // cout<<"dada\n";
+            if(graph.find(start) == graph.end() || graph.find(end) == graph.end()){
                 answer[i] = -1.0;
                 continue;
-            }    
+            }
             else{
-                if(left == right){
+                if(start == end){
                     answer[i] = 1.0;
                     continue;
                 }
                 else{
                     
+                    visited.clear();
                     bool found = false;
                     double val = 1.0;
-                    visited.clear();
                     
-                    dfs(left,right,found,val);
+                    dfs(start,end,found,val);
                     
                     if(found){
-                        answer[i] = val;
+                       answer[i] = val;    
                     }
                     else{
                         answer[i] = -1.0;
                     }
-                    
                     continue;
+                    
                 }
             }
+            
         }
         
         return answer;
