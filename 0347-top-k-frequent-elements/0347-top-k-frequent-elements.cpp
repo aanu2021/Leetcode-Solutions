6ede82e7b1da97bcddcf1c5daa7128c9bfe30908
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-05-22 10:35:36 +0530

    Time: 16 ms (72.37%), Space: 13.6 MB (88.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,38 +4,23 @@ public:
         
         int n = nums.size();
         
-        unordered_map<int,int>mp;
+        unordered_map<int,int>freq;
         
-        for(int i:nums){
-            mp[i]++;
+        for(int &ele : nums) freq[ele]++;
+        
+        vector<pair<int,int>>freqArr;
+        
+        for(auto &itr : freq){
+            freqArr.push_back({itr.second,itr.first});
         }
         
-        unordered_map<int,vector<int>>freq;
+        sort(freqArr.rbegin(),freqArr.rend());
+        vector<int>answer;
         
-        vector<int>freq_arr(n+1,0);
-        
-        for(auto itr:mp){
-            freq[itr.second].push_back(itr.first);
-            freq_arr[itr.second]++;
+        for(int i=0;i<k;i++){
+            answer.push_back(freqArr[i].second);
         }
-        
-        vector<int>ans;
-        
-        for(int i=n;i>=1;i--){
-            
-            while(k>0 && freq_arr[i]>0){
-                
-                ans.push_back(freq[i].back());
-                freq[i].pop_back();
-                
-                freq_arr[i]--;
-                k--;
-                
-            }
-            
-        }
-        
-        return ans;
+        return answer;
         
     }
 };
