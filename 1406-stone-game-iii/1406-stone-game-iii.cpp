2ae8fbbb1d05868f43b0834647a9c48af59574e6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-05-27 10:04:03 +0530

    Time: 309 ms (30.33%), Space: 137.6 MB (40.99%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     
-    int func(vector<int>&nums,int i,int n,vector<int>&dp){
+    vector<int>dp;
+    
+    int func(vector<int>&nums,int i,int n){
         if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int sum = 0;
+        if(dp[i] != -1) return dp[i];
         int ans = -1e9;
-        for(int j=i;j<min(i+3,n);j++){
-            sum+=nums[j];
-            ans = max(ans,sum - func(nums,j+1,n,dp));
+        int currSum = 0;
+        for(int j=i;j<=min(i+2,n-1);j++){
+            currSum += nums[j];
+            ans = max(ans,currSum - func(nums,j+1,n));
         }
         return dp[i] = ans;
     }
     
     string stoneGameIII(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
-        int ans = func(nums,0,n,dp);
-        if(ans>0){
-            return "Alice";
-        }else if(ans<0){
-            return "Bob";
-        }else{
-            return "Tie";
-        }
+        dp = vector<int>(n,-1);
+        int maxDiff = func(nums,0,n);
+        int currSum = accumulate(nums.begin(),nums.end(),0);
+        int aliceSum = (currSum+maxDiff)/2;
+        int bobSum = (currSum-maxDiff)/2;
+        if(aliceSum > bobSum) return "Alice";
+        else if(aliceSum < bobSum) return "Bob";
+        else return "Tie";
     }
 };
