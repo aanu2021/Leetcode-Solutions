ad2314db6b0f5e323f689564add8c6d6bef64004
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-06-06 11:16:51 +0530

    Time: 3 ms (85.28%), Space: 9.1 MB (28.46%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-       
+        
         int n = arr.size();
         
-        int minValue = arr[0];
-        int maxValue = arr[0];
+        int minValue = *min_element(arr.begin(),arr.end());
+        int maxValue = *max_element(arr.begin(),arr.end());
         
-        for(int i=1;i<n;i++){
-            minValue = min(minValue,arr[i]);
-            maxValue = max(maxValue,arr[i]);
-        }
+        if((maxValue - minValue)%(n-1) != 0) return false;
         
-        if((maxValue-minValue)%(n-1) != 0) return false;
+        int d = (maxValue - minValue) / (n - 1);
         
-        int d = (maxValue - minValue)/(n - 1);
-        
-        int i=0;
+        int i = 0;
         
         for(;i<n;){
+            
             if(arr[i] == minValue + (i*d)){
                 i++;
                 continue;
             }
-            else if((arr[i]-minValue)%d != 0){
+            else if((arr[i]-minValue) % d != 0){
                 return false;
             }
             else{
@@ -31,14 +27,13 @@ public:
                 if(arr[i] == arr[j]){
                     return false;
                 }
-                swap(arr[i],arr[j]);
+                else{
+                    swap(arr[i],arr[j]);
+                }
             }
+            
         }
         
         return true;
     }
 };
-
-
-    
-    
