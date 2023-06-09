197262671ce8cab21f5d300e4f22f2629a0e304c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-06-09 11:04:42 +0530

    Time: 3 ms (69.03%), Space: 7.8 MB (50.88%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -24,14 +24,34 @@ public:
         
     }
     
+    void func2(vector<int>&nums,int index,int n){
+        
+        if(index == n){
+            answer.push_back(nums);
+            return;
+        }
+        
+        for(int i=index;i<n;i++){
+            
+            swap(nums[index],nums[i]);
+            
+            func2(nums,index+1,n);
+            
+            swap(nums[index],nums[i]);
+            
+        }
+        
+    }
+    
     vector<vector<int>> permute(vector<int>& nums) {
         
         int n = nums.size();
         
-        vector<int>vis(n,0);
-        vector<int>solution;
+        // vector<int>vis(n,0);
+        // vector<int>solution;
+        // func(nums,0,n,solution,vis);
         
-        func(nums,0,n,solution,vis);
+        func2(nums,0,n);
         
         return answer;
         
