727612cc473487c2afcd252446d6d72b1abc7059
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-06-09 12:49:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (84.30%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -4,16 +4,13 @@ public:
     void func(vector<int>&nums,vector<int>&fact,int n,int k,string &ans){
         
         if(n==1){
-            ans += (char)(nums[0]+'0');
+            ans += (char)(nums[0] + '0');
             return;
         }
         
         int idx = k/fact[n-1];
-        
         ans += (char)(nums[idx] + '0');
-        
         nums.erase(nums.begin()+idx);
-        
         k -= (idx*fact[n-1]);
         
         func(nums,fact,n-1,k,ans);
@@ -22,24 +19,21 @@ public:
     
     string getPermutation(int n, int k) {
         
-        k--;
-        
         vector<int>nums;
         vector<int>fact(n+1,0);
         
         fact[0] = 1;
+        k--;
         
         for(int i=1;i<=n;i++){
-            fact[i] = fact[i-1]*i;
             nums.push_back(i);
+            fact[i] = fact[i-1]*i;
         }
         
         string ans = "";
-        
         func(nums,fact,n,k,ans);
         
         return ans;
         
     }
 };
-
