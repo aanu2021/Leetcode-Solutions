1c43d0d9b7724a11d0d168a0c4c41eb2ade33bf4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-06-11 20:42:50 +0530

    Time: 23 ms (16.51%), Space: 27.4 MB (13.25%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -1,39 +1,37 @@
+
 class Solution {
 public:
     
-    unordered_set<TreeNode*>covered;
-    int cam;
+    // dp[u][0] -> down subtrees are covered excluding current node
+    // dp[u][1] -> down subtrees are covered along with current node, without placing a camera on the current node.
     
-    void dfs(TreeNode* &root,TreeNode *&parent){
+    vector<int>dfs(TreeNode* &root){
         
-        if(!root) return;
-        
-        dfs(root->left,root);
-        dfs(root->right,root);
-        
-        if((parent==NULL && covered.find(root) == covered.end()) || covered.find(root->left) == covered.end() || covered.find(root->right) == covered.end()){
-            
-            cam++;
-            
-            covered.insert(root);
-            covered.insert(parent);
-            covered.insert(root->left);
-            covered.insert(root->right);
-            
+        if(!root){
+            return {0,0,1000000};
         }
+        
+        vector<int>L = dfs(root->left);
+        vector<int>R = dfs(root->right);
+        
+        int mL12 = min(L[1],L[2]);
+        int mR12 = min(R[1],R[2]);
+        
+        int d0 = L[1] + R[1];
+        int d1 = min(L[2] + mR12, R[2] + mL12);
+        int d2 = 1 + min(L[0],mL12) + min(R[0],mR12);
+        
+        return {d0,d1,d2};
+        
     }
     
     int minCameraCover(TreeNode* root) {
         
         if(!root) return 0;
-        cam = 0;
         
-        covered.insert(NULL);
-        covered.insert(NULL);
+        vector<int>ans = dfs(root);
         
-        TreeNode * parent = NULL;
-        dfs(root,parent);
-        return cam;
+        return min(ans[1],ans[2]);
         
     }
 };
