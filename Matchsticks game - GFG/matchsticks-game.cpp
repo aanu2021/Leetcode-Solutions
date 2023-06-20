935dc658190a95ae6f3f45d52e678ea0dba6d874
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-06-20 10:34:32 +0530

    Added solution - LeetHub

--- Matchsticks game - GFG/matchsticks-game.cpp
+++ Matchsticks game - GFG/matchsticks-game.cpp
@@ -7,11 +7,35 @@ class Solution {
   public:
     int matchGame(long long N) {
         
-        if(N%5 == 0) return -1;
+        if(N%5==0) return -1;
         else return N%5;
         
     }
 };
+
+/*
+
+N % 5 == 0
+
+A = 1   B = 4
+A = 2   B = 3
+A = 3   B = 2
+A = 4   B = 1
+
+A loose in all possible situations.....
+
+
+N % 5 == 1
+
+A = 1  (N-1)%5 == 0
+
+B loose in all possible situations.......
+
+
+
+
+*/
+
 
 //{ Driver Code Starts.
 int main() {
