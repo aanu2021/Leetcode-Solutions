bbcb04114d3132967f6c51a1895fccb1ec353ec5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-06-22 09:03:29 +0530

    Time: 132 ms (71.02%), Space: 60.1 MB (63.58%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    int maxProfit2(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
-        for(int i=n-1;i>=0;i--){
-            dp[i][1] = max(dp[i+1][1],-prices[i] + dp[i+1][0]);
-            dp[i][0] = max(dp[i+1][0],prices[i]-fee+dp[i+1][1]);
+    
+    int dp[50005][2];
+    
+    int func(vector<int>&prices,int i,bool bought,int n,int fee)
+    {
+        if(i == n) return 0;
+        if(dp[i][bought] != -1) return dp[i][bought];
+        
+        // skip
+        
+        int answer = func(prices,i+1,bought,n,fee);
+        
+        // pick 
+        
+        if(bought){
+            answer = max(answer,-prices[i] + func(prices,i+1,false,n,fee));
         }
-        return dp[0][1];
+        else{
+            answer = max(answer,prices[i] - fee + func(prices,i+1,true,n,fee));
+        }
+        
+        return dp[i][bought] = answer;
+        
     }
     
     int maxProfit(vector<int>& prices, int fee) {
         
         int n = prices.size();
         
-        int oldbuy = 0, oldsold = 0;
-        
-        for(int i=n-1;i>=0;i--){
-            
-            int newbuy = 0,newsold = 0;
-            
-            newbuy = max(oldbuy,-prices[i]+oldsold);
-            newsold = max(oldsold,prices[i]-fee+oldbuy);
-            
-            oldbuy = newbuy;
-            oldsold = newsold;
-
+        for(int i=0;i<=n;i++){
+            dp[i][0] = -1;
+            dp[i][1] = -1;
         }
         
-        return oldbuy;
+        int ans = func(prices,0,true,n,fee);
+        return ans;
+        
     }
 };
