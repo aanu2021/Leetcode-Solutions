f61f0e5d57b4d5a5f10160e0963c69586f961780
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-01 10:21:12 +0530

    Time: 1585 ms (52.07%), Space: 6.8 MB (62.54%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     
-    void func(vector<int>&cookies,int i,int n,int k,vector<int>&container,int &minSum){
+    void func(vector<int>&cookies,vector<int>&container,int i,int n,int k,int &minOps){
         
         if(i==n){
-            int currMax = 0;
-            for(int j=0;j<k;j++) currMax = max(currMax,container[j]);
-            minSum = min(minSum,currMax);
+            int maxSum = 0;
+            for(int j=0;j<k;j++){
+                maxSum = max(maxSum,container[j]);
+            }
+            minOps = min(minOps,maxSum);
             return;
         }
         
         for(int j=0;j<k;j++){
             container[j] += cookies[i];
-            func(cookies,i+1,n,k,container,minSum);
+            func(cookies,container,i+1,n,k,minOps);
             container[j] -= cookies[i];
         }
         
     }
     
     int distributeCookies(vector<int>& cookies, int k) {
-        vector<int>container(k,0);
+        
         int n = cookies.size();
-        int minSum = INT_MAX;
-        func(cookies,0,n,k,container,minSum);
-        return minSum;
+        vector<int>container(k,0);
+        int minOps = INT_MAX;
+        func(cookies,container,0,n,k,minOps);
+        return minOps;
+        
     }
 };
