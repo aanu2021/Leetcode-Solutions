dd05befee66de08ff77bc73f0065d8cbed5c287a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-03 10:42:03 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.cpp
+++ Maximum Index - GFG/maximum-index.cpp
@@ -7,18 +7,22 @@ using namespace std;
 class Solution{
 public:
     int maxIndexDiff(int arr[], int n) {
+    
         vector<int>minL(n);
         vector<int>maxR(n);
+        
         minL[0] = arr[0];
         maxR[n-1] = arr[n-1];
+        
         for(int i=1;i<n;i++){
             minL[i] = min(minL[i-1],arr[i]);
         }
         for(int i=n-2;i>=0;i--){
             maxR[i] = max(maxR[i+1],arr[i]);
         }
+        
+        int i=0,j=0;
         int maxDiff = 0;
-        int i = 0,j = 0;
         
         while(i<n && j<n){
             if(minL[i] <= maxR[j]){
@@ -29,21 +33,12 @@ public:
                 i++;
             }
         }
-        
+    
+    
         return maxDiff;
     }
 
 };
-
-/*
-
-arr  : 34 8  10 3  2  80 30 33 1
-
-minL : 34 8  8  3  2  2  2  2  1
-
-maxR : 80 80 80 80 80 80 33 33 1
-
-*/
 
 
 //{ Driver Code Starts.
