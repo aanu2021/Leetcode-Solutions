ddca98f8565ef75fc82941c0b16a04918a8c7172
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-06 01:40:31 +0530

    Added solution - LeetHub

--- Quick Sort - GFG/quick-sort.cpp
+++ Quick Sort - GFG/quick-sort.cpp
@@ -20,41 +20,38 @@ class Solution
     void quickSort(int arr[], int low, int high)
     {
         if(low >= high) return;
-        int partitionIndex = partition(arr,low,high);
-        quickSort(arr,low,partitionIndex-1);
-        quickSort(arr,partitionIndex+1,high);
+        int pIdx = partition(arr,low,high);
+        quickSort(arr,low,pIdx-1);
+        quickSort(arr,pIdx+1,high);
     }
     
     public:
     int partition (int arr[], int low, int high)
     {
-        int pivot = arr[low];
-        int i = low + 1;
-        int j = high;
-        do{
+        
+       int pivot = arr[low];
+       int i = low + 1, j = high;
+       
+       do{
+         
+          while(i <= high && arr[i] <= pivot){
+              i++;
+          }
+          while(j > low && arr[j] > pivot){
+              j--;
+          }
           
-           while(i <= high && arr[i] <= pivot){
-               i++;
-           }
-           while(j > low && arr[j] > pivot){
-               j--;
-           }
+          if(i < j){
+              swap(arr[i],arr[j]);
+          }
            
-           if(i < j){
-               swap(arr[i],arr[j]);
-           }
-               
-        }while(i < j);
-        swap(arr[low],arr[j]);
-        return j;
+       }while(i < j);
+       
+       swap(arr[low],arr[j]);
+       return j;
+       
     }
 };
-
-// 4 1 3 9 7
-//     j i
-
-// 1 1 2 10 4 6 3 9 7
-//     j i         
 
 
 //{ Driver Code Starts.
