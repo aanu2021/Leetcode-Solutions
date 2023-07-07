ee6f97860ccc5f928312ea785a0999e89c3f07f1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-07 10:46:07 +0530

    Added solution - LeetHub

--- Arranging the array - GFG/arranging-the-array.cpp
+++ Arranging the array - GFG/arranging-the-array.cpp
@@ -8,43 +8,38 @@ class Solution
 {
     public:
     
-        void reverse(int arr[],int l,int r){
-            
+        void reverseArr(int arr[],int l,int r){
+            if(l > r) return;
             while(l < r){
                 swap(arr[l++],arr[r--]);
             }
-            
         }
     
-        void merge(int arr[],int l,int m,int r){
-            
-            int i = l, j = m+1;
-            
-            while(i<=m && arr[i] < 0) i++;
-            while(j<=r && arr[j] < 0) j++;
-            
-            reverse(arr,i,m);
-            reverse(arr,m+1,j-1);
-            reverse(arr,i,j-1);
-            
-        }
-    
-        void mergesort(int arr[],int l,int r){
-            
-            if(l < r){
-                int m = (l + r)/2;
-                mergesort(arr,l,m);
-                mergesort(arr,m+1,r);
-                merge(arr,l,m,r);
+        void merge(int arr[],int low,int mid,int high){
+            int i=low, j=mid+1;
+            while(i<=mid && arr[i] < 0){
+                i++;
             }
-            
+            while(j<=high && arr[j] < 0){
+                j++;
+            }
+            reverseArr(arr,i,mid);
+            reverseArr(arr,mid+1,j-1);
+            reverseArr(arr,i,j-1);
+        }
+    
+        void mergesort(int arr[],int low,int high){
+            if(low < high){
+                int mid = (low + high)/2;
+                mergesort(arr,low,mid);
+                mergesort(arr,mid+1,high);
+                merge(arr,low,mid,high);
+            }
         }
     
         void Rearrange(int arr[], int n)
         {
-            
-            mergesort(arr,0,n-1);
-            
+             mergesort(arr,0,n-1);
         }
 };
 
