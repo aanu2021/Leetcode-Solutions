07ad5c0128c06a6916766e8ed354c08f541ac1da
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-07 09:41:22 +0530

    Time: 26 ms (73.90%), Space: 10 MB (95.61%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,61 +1,29 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string s, int k) {
+    
+    int func(string &s,int n,int k,char target){
         
-        int n=s.length();
+        int maxLen = 0;
+        int l = 0,r = 0,cnt = 0;
         
-        int l=0,r=0,count=0;
-        
-        int maxLen=0;
-        
-        while(r<n){
-            
-            if(s[r]=='F'){
-                count++;
-            }
-            
-            while(l<=r && count>k){
-                
-                if(s[l]=='F'){
-                    count--;
-                }
-                
+        while(r < n){
+            if(s[r] != target) cnt++;
+            while(l <= r && cnt > k){
+                if(s[l] != target) cnt--;
                 l++;
-                
             }
-            
-            maxLen=max(maxLen,r-l+1);
-            
+            maxLen = max(maxLen,r-l+1);
             r++;
-            
-        }
-        
-        
-        l=0,r=0,count=0;
-        
-        while(r<n){
-            
-            if(s[r]=='T'){
-                count++;
-            }
-            
-            while(l<=r && count>k){
-                
-                if(s[l]=='T'){
-                    count--;
-                }
-                
-                l++;
-                
-            }
-            
-            maxLen=max(maxLen,r-l+1);
-            
-            r++;
-            
         }
         
         return maxLen;
         
+    }
+    
+    int maxConsecutiveAnswers(string s, int k) {
+        int n = s.length();
+        int maxLen = func(s,n,k,'T');
+        maxLen = max(maxLen, func(s,n,k,'F'));
+        return maxLen;
     }
 };
