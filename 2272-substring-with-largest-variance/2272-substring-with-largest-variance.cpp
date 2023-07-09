070ecedeabf47331ce72b464f42084ee67c00618
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-09 09:27:26 +0530

    Time: 395 ms (22.01%), Space: 6.8 MB (96.23%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -1,99 +1,57 @@
 class Solution {
 public:
     
-    int find_variance(string &s,char ch1,char ch2){
-        
-        int cnt=0,currsum=0;
-        
-        int maxsum=0;
-        
-        int n=s.length();
-        
-        for(int i=0;i<n;++i){
-            
+    int findVariance(string &s,int n,char ch1,char ch2){
+        int maxSum = 0;
+        int currSum = 0;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
             if(s[i]==ch1){
-                currsum++;
+                currSum++;
             }
-            
-            else if(s[i]==ch2){
-                currsum--;
+            if(s[i]==ch2){
                 cnt++;
+                currSum--;
             }
-            
-            
-            if(cnt!=0){
-                
-                maxsum=max(maxsum,currsum);
-                
+            if(cnt > 0){
+                maxSum = max(maxSum, currSum);
             }
-            
-            if(currsum<0){
-                
-                currsum=0;
-                cnt=0;
-                
+            if(currSum < 0){
+                cnt = 0;
+                currSum = 0;
             }
-            
         }
-        
-        return maxsum;
-        
+        return maxSum;
     }
     
     int largestVariance(string s) {
         
-        int n=s.length();
-        
-        int max_variance=0;
+        int n = s.length();
         
         vector<int>freq(26,0);
-        
-        for(int i=0;i<n;++i){
-            
-            freq[s[i]-'a']++;
-            
+        for(char ch:s){
+            freq[ch-'a']++;
         }
+        
+        int maxVariance = 0;
         
         for(char ch1='a';ch1<='z';ch1++){
-            
             for(char ch2='a';ch2<='z';ch2++){
-                
-                if(ch1==ch2 || freq[ch1-'a']==0 || freq[ch2-'a']==0){
-
-                        continue;
-
-                }
-                
-                int curr_variance=find_variance(s,ch1,ch2);
-                
-                max_variance=max(max_variance,curr_variance);
-                
+                if(ch1==ch2 || freq[ch1-'a']==0 || freq[ch2-'a']==0) continue;
+                maxVariance = max(maxVariance, findVariance(s,n,ch1,ch2));
             }
-            
         }
-        
         
         reverse(s.begin(),s.end());
         
         for(char ch1='a';ch1<='z';ch1++){
-            
             for(char ch2='a';ch2<='z';ch2++){
-                
-                if(ch1==ch2 || freq[ch1-'a']==0 || freq[ch2-'a']==0){
-
-                        continue;
-
-                }
-                
-                int curr_variance=find_variance(s,ch1,ch2);
-                
-                max_variance=max(max_variance,curr_variance);
-                
+                if(ch1==ch2 || freq[ch1-'a']==0 || freq[ch2-'a']==0) continue;
+                maxVariance = max(maxVariance, findVariance(s,n,ch1,ch2));
             }
-            
         }
         
-        return max_variance;
+        return maxVariance;
         
     }
 };
