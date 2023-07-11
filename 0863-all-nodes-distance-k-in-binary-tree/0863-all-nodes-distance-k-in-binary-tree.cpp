a0de486d113faa372a99e266e06a72a79bc8bd9d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-11 09:29:04 +0530

    Time: 8 ms (46.62%), Space: 12.4 MB (96.14%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -3,42 +3,41 @@ public:
     
     vector<int>answer;
     
-    void add_subtree(TreeNode* &root, TreeNode* &target,int dist){
+    void add_subtree(TreeNode* &root,int k){
         if(!root) return;
-        if(dist==0) { answer.push_back(root->val); return; }
-        add_subtree(root->left,target,dist-1);
-        add_subtree(root->right,target,dist-1);
+        if(k==0){
+            answer.push_back(root->val);
+            return;
+        }
+        add_subtree(root->left,k-1);
+        add_subtree(root->right,k-1);
     }
     
     int traverse(TreeNode* &root,TreeNode* &target,int k){
         if(!root) return -1;
         if(root->val == target->val){
-            add_subtree(root,target,k);
+            add_subtree(root,k);
             return k-1;
         }
-        
         int dist = traverse(root->left,target,k);
         if(dist > -1){
-            if(dist==0) answer.push_back(root->val);
-            add_subtree(root->right,target,dist-1);
+            if(dist == 0) answer.push_back(root->val);
+            add_subtree(root->right,dist-1);
             return dist - 1;
         }
-        
         dist = traverse(root->right,target,k);
         if(dist > -1){
-            if(dist==0) answer.push_back(root->val);
-            add_subtree(root->left,target,dist-1);
+            if(dist == 0) answer.push_back(root->val);
+            add_subtree(root->left,dist-1);
             return dist - 1;
         }
-        
         return -1;
     }
     
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-         
-        if(!root) return {};
+        answer.clear();
+        if(!root) return answer;
         traverse(root,target,k);
         return answer;
-        
     }
 };
