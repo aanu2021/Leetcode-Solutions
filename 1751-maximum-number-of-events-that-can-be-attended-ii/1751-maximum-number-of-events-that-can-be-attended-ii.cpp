ec22dd69e72d05a2091c95d1eb7c68c34cff8dc9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-15 09:42:58 +0530

    Time: 328 ms (71.72%), Space: 72.8 MB (69.51%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,59 +1,39 @@
 class Solution {
 public:
     
-    int func(vector<vector<int>>&events,int idx,int n,int k,vector<vector<int>>&dp){
+    vector<vector<int>>dp;
+    
+    int func(vector<vector<int>>&events,int i,int k,int n){
         
-        if(idx>=n){
-            return 0;
+        if(k==0) return 0;
+        if(i==n) return 0;
+        if(dp[i][k] != -1) return dp[i][k];
+        
+        int ans = func(events,i+1,k,n);
+        
+        int inc = n,low = i+1, high = n-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(events[mid][0] > events[i][1]){
+                inc = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
         }
         
-        if(dp[idx][k]!=-1){
-            return dp[idx][k];
-        }
-        
-        int currval = func(events,idx+1,n,k,dp);
-        
-        if(k>0){
-            
-            vector<int>temp = {events[idx][1],INT_MAX,INT_MAX};
-            
-            int j = upper_bound(events.begin(),events.end(),temp)-events.begin();
-            
-            currval = max(currval , events[idx][2] + func(events,j,n,k-1,dp));
-            
-        }
-        
-        return dp[idx][k] = currval;
-        
+        ans = max(ans, events[i][2] + func(events,inc,k-1,n));   
+        return dp[i][k] = ans;
     }
     
     int maxValue(vector<vector<int>>& events, int k) {
         
-        sort(events.begin(),events.end());
-        
         int n = events.size();
-        
-        vector<vector<int>>dp(n+1,vector<int>(k+1,0));
-        
-        for(int i=n-1;i>=0;i--){
-            
-            for(int j=1;j<=k;j++){
-                
-                dp[i][j] = dp[i+1][j];
-                
-                vector<int>temp = {events[i][1],INT_MAX,INT_MAX};
-                
-                int idx = upper_bound(events.begin(),events.end(),temp)-events.begin();
-                
-                dp[i][j] = max(dp[i][j] , events[i][2] + dp[idx][j-1]);
-                
-            }
-            
-        }
-        
-        
-        
-        return dp[0][k];
+        dp = vector<vector<int>>(n,vector<int>(k+1,-1));
+        sort(events.begin(),events.end());
+        int ans = func(events,0,k,n);
+        return ans;
         
     }
 };
