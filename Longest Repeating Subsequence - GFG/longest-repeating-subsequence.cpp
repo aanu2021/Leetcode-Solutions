08e5aa9467a456202602c6a77fa490bfa2ff3610
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-18 00:27:30 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -1,43 +1,31 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
 	public:
-		int LongestRepeatingSubsequence(string s){
-		    
-		    int n=s.length();
-		    
-		    s="*"+s;
-		    
-		    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-		    
-		    int maxLen=0;
-		    
+		int LongestRepeatingSubsequence(string str){
+		    int n = str.length();
+		    int dp[n+1][n+1];
+		    memset(dp,0,sizeof(dp));
+		    str = "#" + str;
 		    for(int i=1;i<=n;i++){
-		        
 		        for(int j=1;j<=n;j++){
-		            
-		            if(s[i]==s[j] && i!=j){
-		                dp[i][j]=1+dp[i-1][j-1];
-		            }else{
-		                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		            if(str[i] == str[j] && i!=j){
+		                dp[i][j] = 1 + dp[i-1][j-1];
 		            }
-		            
-		            maxLen=max(maxLen,dp[i][j]);
-		            
+		            else{
+		                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+		            }
 		        }
-		        
 		    }
-		    
-		    return maxLen;
-		    
+		    return dp[n][n];
 		}
 
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main(){
 	int tc;
 	cin >> tc;
@@ -49,4 +37,5 @@ int main(){
 		cout << ans << "\n";
 	}
 	return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
