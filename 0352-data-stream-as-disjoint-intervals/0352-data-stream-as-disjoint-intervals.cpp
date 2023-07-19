d8d791a78364846db7e895e3c1cec1866a84b7a6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-19 17:31:26 +0530

    Time: 3 ms (63.99%), Space: 7.6 MB (68.28%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,42 +1,41 @@
 class SummaryRanges {
 public:
     
-    map<int,int>intervals;
+    map<int,int>mp;
     
     SummaryRanges() {
-        intervals.clear();
+        mp.clear();
     }
     
     void addNum(int value) {
         int left  = value;
         int right = value;
-        auto itr = intervals.upper_bound(value);
-        if(itr != intervals.begin()){
-            auto prev_itr = itr;
-            prev_itr--;
+        auto itr = mp.upper_bound(value);
+        if(itr != mp.begin()){
+            auto prev_itr = prev(itr);
             if(prev_itr->second >= value){
                 left = prev_itr->first;
                 right = max(right,prev_itr->second);
             }
-            if(prev_itr->second == value - 1){
+            else if(prev_itr->second == value-1){
                 left = prev_itr->first;
             }
         }
-        if(itr != intervals.end()){
+        if(itr != mp.end()){
             if(itr->first == right + 1){
                 right = max(right,itr->second);
-                intervals.erase(itr);
+                mp.erase(itr->first);
             }
         }
-        intervals[left] = right;
+        mp[left] = right;
     }
     
     vector<vector<int>> getIntervals() {
-        vector<vector<int>>ans;
-        for(auto &itr : intervals){
-            ans.push_back({itr.first,itr.second});
+        vector<vector<int>>answer;
+        for(auto &itr : mp){
+            answer.push_back({itr.first,itr.second});
         }
-        return ans;
+        return answer;
     }
 };
 
