e58047f22921170ff0c2e255c3cfbefbe38465c2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-19 20:39:25 +0530

    Time: 225 ms (63.68%), Space: 70.5 MB (90.80%) - LeetHub

--- 0715-range-module/0715-range-module.cpp
+++ 0715-range-module/0715-range-module.cpp
@@ -22,7 +22,7 @@ public:
         }
         if(itr != mp.end()){
             for(;itr!=mp.end();){
-                if(right >= itr->first-1){
+                if(itr->first <= right + 1){
                     right = max(right,itr->second);
                     mp.erase(itr++);
                 }
@@ -30,24 +30,12 @@ public:
                     break;
                 }
             }
-            // if(right >= itr->first - 1){
-            //     right = max(right,itr->second);
-            //     mp.erase(itr->first);
-            // }
         }
         mp[left] = right;
-        // cout<<"Add Range\n";
-        // for(auto &itr : mp){
-        //     cout<<itr.first<<" "<<itr.second<<"\n";
-        // }
     }
     
     bool queryRange(int left, int right) {
         right--;
-        // cout<<"Query Range\n";
-        // for(auto &itr : mp){
-        //     cout<<itr.first<<" "<<itr.second<<"\n";
-        // }
         auto itr = mp.upper_bound(left);
         if(itr != mp.begin()){
             auto prev_itr = prev(itr);
@@ -66,84 +54,43 @@ public:
         if(itr != mp.begin()){
             auto prev_itr = prev(itr);
             if(prev_itr->second >= left){
-                int cl = prev_itr->first;
-                int l1 = prev_itr->first;
-                int r1 = left - 1;
-                int l2 = right + 1;
-                int r2 = prev_itr->second;
-                mp.erase(cl);
-                // cout<<"Remove Range\n";
-                // cout<<l1<<" "<<r1<<" "<<l2<<" "<<r2<<"\n";
+                int l = prev_itr->first;
+                int r = prev_itr->second;
+                int l1 = l;
+                int r1 = left-1;
+                int l2 = right+1;
+                int r2 = r;
+                mp.erase(l);
                 if(l1 <= r1){
                     mp[l1] = r1;
                 }
                 if(l2 <= r2){
                     mp[l2] = r2;
-                }
+                }   
             }
         }
         if(itr != mp.end()){
             while(itr != mp.end()){
-                int cl = itr->first;
-                int cr = itr->second;
-                if(cl > right){
+                int l = itr->first;
+                int r = itr->second;
+                if(l > right){
                     break;
                 }
-                if(cl >= left && cr <= right){
-                    mp.erase(cl);
+                if(l >= left && r <= right){
+                    mp.erase(l);
                 }
                 else{
-                    if(right + 1 <= cr){
-                        mp[right + 1] = cr;
+                    int nl = right + 1;
+                    int nr = r;
+                    if(nl <= nr){
+                        mp[nl] = nr;
                     }
-                    mp.erase(cl);
+                    mp.erase(l);
                     break;
                 }
                 itr = mp.upper_bound(left);
             }
-            // for(;itr!=mp.end();){
-            //     itr = upper_b
-            //     int cl = itr->first;
-            //     int cr = itr->second;
-            //     if(cl >= left && cr <= right){
-            //         mp.erase(itr++);
-            //     }
-            //     else{
-            //         if(right + 1 <= cr){
-            //             mp[right + 1] = cr;
-            //         }
-            //         mp.erase(itr++);
-            //         break;
-            //         // mp.erase(cl);
-            //         // mp[right+1] = cr;
-            //         // break;
-            //     }
-            // }
         }
-        // cout<<"Remove Range\n";
-        // for(auto &itr : mp){
-        //     cout<<itr.first<<" "<<itr.second<<"\n";
-        // }
     }
 };
 
-
-// [1,5] [20,25]
-// [1,3] [9,10]
-
-// [4,22]
-
-// [10,19]
-
-// [14,15]
-
-
-
-
-/**
- * Your RangeModule object will be instantiated and called as such:
- * RangeModule* obj = new RangeModule();
- * obj->addRange(left,right);
- * bool param_2 = obj->queryRange(left,right);
- * obj->removeRange(left,right);
- */
