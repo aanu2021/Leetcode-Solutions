cbf771c2a234c2f0ecb6a6c55be3ff0529bec39e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-21 17:59:30 +0530

    Time: 13 ms (89.00%), Space: 9 MB (90.21%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     
-    bool static comp(const vector<int>&x, const vector<int>&y){
+    bool static comp(const vector<int>&x,const vector<int>&y){
         return x[0] + x[1] + x[2] > y[0] + y[1] + y[2];
     }
     
     int maxHeight(vector<vector<int>>& c) {
+        
         int n = c.size();
+        
         for(int i=0;i<n;i++){
             sort(c[i].begin(),c[i].end());
         }
+        
         sort(c.begin(),c.end(),comp);
+        
         vector<int>dp(n,0);
+        
         dp[0] = c[0][2];
+        
         for(int i=1;i<n;i++){
             int maxHeight = 0;
             for(int j=0;j<i;j++){
-                if(c[j][0] >= c[i][0] && c[j][1] >= c[i][1] && c[j][2] >= c[i][2]){
+                if(c[i][0] <= c[j][0] && c[i][1] <= c[j][1] && c[i][2] <= c[j][2]){
                     maxHeight = max(maxHeight, dp[j]);
                 }
             }
             dp[i] = maxHeight + c[i][2];
         }
+        
         return *max_element(dp.begin(),dp.end());
+        
     }
 };
 
