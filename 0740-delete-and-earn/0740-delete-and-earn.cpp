55170ec79abf520fa3cb430cf4a32e09f3c5f2d3
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-21 16:55:30 +0530

    Time: 12 ms (81.13%), Space: 11.7 MB (91.22%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
-    
-    int func(vector<int>&nums,int i,int n,vector<int>&dp){
-        
-        // Base Case
-        
-        if(i==n) return 0;
-        
-        // Memoization
-        
-        if(dp[i]!=-1) return dp[i];
-        
-        int score_skip = 0 , score_pick = 0;
-        
-        // Skip 
-        
-        int j = i;
-        
-        while(j<n && nums[j]==nums[i]){
-            j++;
-        }
-        
-        score_skip = func(nums,j,n,dp);
-        
-        // Pick
-        
-        score_pick += (nums[i]*(j-i));
-        
-        while(j<n && nums[j]==nums[i] + 1){
-            j++;
-        }
-        
-        score_pick += func(nums,j,n,dp);
-        
-        return dp[i] = max(score_skip,score_pick);
-        
-    }
-    
     int deleteAndEarn(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        vector<int>dp(n,-1);
-        int ans = func(nums,0,n,dp);
-        return ans;
+        int MAX = *max_element(nums.begin(),nums.end());
+        vector<int>freq(MAX+1,0);
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        vector<int>dp(MAX+1,0);
+        dp[1] = freq[1];
+        for(int i=2;i<=MAX;i++){
+            dp[i] = max(dp[i-1], dp[i-2] + (freq[i]*i));
+        }
+        return dp[MAX];
     }
 };
