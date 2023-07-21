f4fe49c75408e1e5f51150698fa70f7459d3e378
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-21 17:08:57 +0530

    Time: 212 ms (39.91%), Space: 51.4 MB (32.97%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,92 +1,30 @@
-/* 
-
-The Question can be solved with the help of two or three approaches , including bfs , dp and so on.
-
-  But the most efficient and little bit tricky approach is greedy.
-  
-  
-   Now we have to sort the strings in the increasing order of 
-         their length , then we have to pick each and every string from 
-         the extreme left to extreme right . When we are at a string
-         (lets say str) , we have to traverse the string str and one by one 
-         delete every character and check whther it is found beforehand or not.
-         
-         e.g : str = " b d c a " [ See Sample Example 1 ]
-         
-         first , delete 0-th indexed 'b' --> new_str = " d c a " [new_str is not found previously].
-         
-         second , delete 1-st indexed 'd' --> new_str = " b c a " [new_str is found previously ].
-         
-         third , delete 2-nd indexed 'c' --> new_str = " b d a " [ new_str is found  previously ] .
-         
-         fourth , delete 3-rd indexed 'a' --> new_str = " b d c " [ new_str is not found previously ].
-         
-         
-         Here we can observe that we are able to form 4 kind of new strings , "dca" ,"bca" , "bda" , "bdc" respectively . Among all the strings "bca" and "bda" are already found in the [words] set. So , we can easily reach the string "bda" / "bca" from "bdca" , then recursively find the same thing for "bca" / "bda" .......
-         
-         
-         Now why we sort the strings in the increasing order of their length , 
-         
-  well , if you observe carefully we can only insert a single charcater in a string
-  
-  , and that is equivalent to delete a single charcater from a string to get the
-  
-  older one.
-  
-   e.g : "abcd" --> erase 'c' at index 2 --> "abd" . is equivalent to
-         "abd" --> insert 'c' at index 2 --> "abcd".
-         
-         
-         And if we are able to make some groups of string with respect to their length , like group1 ( all the strings of length 1 ) , group2 ( all the strings 
- of length 2 ) , and so on.....
- 
-     And a string of length L cab be reduced to only string of length (L-1) , not less than these , not even more than these , so sorting enables these feature to solve the problem efficiently.
-         
-  
-  */
-
 class Solution {
 public:
     
-    bool static comp(const string&s1 , const string&s2){
-        
-        return s1.length() < s2.length();
-        
+    bool static comp(const string&x, const string&y){
+        return x.length() < y.length();
     }
     
     int longestStrChain(vector<string>& words) {
-        
-        int n=words.size();
-        
-        map<string,int>mp;
-        
+        int n = words.size();
         sort(words.begin(),words.end(),comp);
-        
-        int maxLen=0;
-        
-        for(string str:words){
-            
-            string temp=str;
-            
-            for(int i=0;i<str.length();i++){
-                
-                temp=str;
-                
-                temp.erase(i,1);
-                
-                if(mp.find(temp)!=mp.end()){
-                    mp[str]=max(mp[str],mp[temp]+1);
-                }else{
-                    mp[str]=max(mp[str],1);
-                }
-                
-            }
-            
-            maxLen=max(maxLen,mp[str]);
-            
+        unordered_map<string,int>dp;
+        for(int i=0;i<n;i++){
+            dp[words[i]] = 1;
         }
-        
+        int maxLen = 1;
+        for(int i=1;i<n;i++){
+            string str = words[i];
+            int len = str.length();
+            for(int j=0;j<len;j++){
+                string pref = str.substr(0,j);
+                string suff = str.substr(j+1);
+                string curr = pref;
+                curr += suff;
+                dp[str] = max(dp[str], 1 + dp[curr]);
+                maxLen = max(maxLen, dp[str]);
+            }
+        }
         return maxLen;
-        
     }
 };
