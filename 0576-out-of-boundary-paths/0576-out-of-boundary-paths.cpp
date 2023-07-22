e9a74554a15e2e7ff7c3e4d03ef084613568390b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-22 10:34:05 +0530

    Time: 35 ms (16.70%), Space: 10.8 MB (18.35%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -2,33 +2,53 @@ class Solution {
 public:
     
     typedef long long ll;
-    
     const ll M = 1e9 + 7;
     
-    int dp[51][51][51];
-    
-    int dx[4] = {0,0,1,-1};
-    int dy[4] = {1,-1,0,0};
-    
-    int func(int row,int col,int move,int m,int n){
-        if(move < 0) return 0;
-        if(row < 0 || col < 0 || row >= m || col >= n) return 1;
-        if(dp[row][col][move] != -1) return dp[row][col][move];
-        ll ways = 0LL;
-        for(int dir=0;dir<4;dir++){
-            int nrow = row + dx[dir];
-            int ncol = col + dy[dir];
-            ways += (ll)func(nrow,ncol,move-1,m,n);
-            ways %= M;
-        }
-        return dp[row][col][move] = (int)ways;
-    }
-    
-    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+    int findPaths(int m, int n, int move, int row, int col) {
         
-        memset(dp,-1,sizeof(dp));
-        int ans = func(startRow,startColumn,maxMove,m,n);
-        return ans;
+        if(move == 0) return 0;
+        
+        vector<vector<ll>>dp(m,vector<ll>(n,0));
+        
+        int dx[4] = {0,0,1,-1};
+        int dy[4] = {1,-1,0,0};
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int dir=0;dir<4;dir++){
+                    int ni = i + dx[dir];
+                    int nj = j + dy[dir];
+                    if(ni < 0 || nj < 0 || ni >= m || nj >= n){
+                        dp[i][j]++;
+                    }
+                }
+            }
+        }
+        
+        ll totalSum = 0LL;
+        totalSum += dp[row][col];
+        totalSum %= M;
+        
+        for(int mo=2;mo<=move;mo++){
+            vector<vector<ll>>dp2(m,vector<ll>(n,0));
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    for(int dir=0;dir<4;dir++){
+                        int ni = i + dx[dir];
+                        int nj = j + dy[dir];
+                        if(ni >= 0 && ni < m && nj >= 0 && nj < n){
+                            dp2[i][j] += dp[ni][nj];
+                            dp2[i][j] %= M;
+                        }
+                    }
+                }
+            }
+            dp = dp2;
+            totalSum += dp[row][col];
+            totalSum %= M;
+        }
+        
+        return (int)totalSum;
         
     }
 };
