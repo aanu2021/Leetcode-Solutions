9a96000c15fe63cf7be6f4c25501da15a88cb3f6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-23 11:02:27 +0530

    Time: 131 ms (19.55%), Space: 38.2 MB (18.64%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     
-    vector<TreeNode*>recursion(int n){
-       
+    vector<TreeNode*>solve(int n){
         vector<TreeNode*>answer;
-        
         if(n==1){
             return {new TreeNode(0)};
         }
         for(int i=0;i<n;i++){
-            int x = i;
-            int y = n-i-1;
-            if(x%2 == 0 || y%2 == 0) continue;
-            for(auto l : recursion(x)){
-                for(auto r : recursion(y)){
+            int x = i,y = n-i-1;
+            if(x%2==0 || y%2==0) continue;
+            for(auto l : solve(x)){
+                for(auto r : solve(y)){
                     TreeNode*node = new TreeNode(0);
                     node->left = l;
                     node->right = r;
@@ -22,21 +19,10 @@ public:
             }
         }
         return answer;
-        
     }
     
     vector<TreeNode*> allPossibleFBT(int n) {
         if(n%2==0) return {};
-        return recursion(n);
+        return solve(n);
     }
 };
-
-// 7 - 1 = 6
-    
-// 1 5
-// 3 3 
-// 5 1    
-    
-//      1
-//     / \
-//    2   3
