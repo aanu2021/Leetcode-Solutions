1df8db477e9a1d7e6f028b4b018f3b918315874b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-24 09:52:10 +0530

    Time: 4 ms (40.18%), Space: 6.1 MB (28.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -5,20 +5,24 @@ public:
     
     double myPow(double x, int nn) {
         
-        if(nn==0) return 1.0;
-        if(nn==1) return x;
         ll n = (ll)nn;
-        double res = 1.0;
+        
+        if(n==0) return 1.0;
+        if(n==1) return x;
+        
+        double result = 1.0;
         if(n < 0){
-            n = -1*n;
+            n *= -1;
             x = 1.0/x;
         }
+        
         while(n){
-            if(n%2) res *= x;
+            if(n%2) result *= x;
             n /= 2;
             x *= x;
         }
         
-        return res;
+        return result;
+        
     }
 };
