9c789ce5d1a11fdd8ea31c0b8d793b4fc6ce0811
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-26 12:00:00 +0530

    Time: 402 ms (34.31%), Space: 54.3 MB (46.37%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -4,36 +4,33 @@ public:
     typedef long long ll;
     const ll M = 1e9 + 7;
     
-    vector<vector<int>>dp;
-    
-    int func(vector<vector<int>>&freq,string &target,int i,int j,int n,int m){
-        if(i==n) return 1;
-        if(j==m) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        ll ways = func(freq,target,i,j+1,n,m);
-        ways += (ll)(freq[j][target[i]-'a'])*(ll)func(freq,target,i+1,j+1,n,m);
-        ways %= M;
-        return dp[i][j] = (int)ways;
-    }
-    
     int numWays(vector<string>& words, string target) {
         
         int m = words[0].length();
         int n = target.length();
         
         vector<vector<int>>freq(m,vector<int>(26,0));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         
         for(int i=0;i<words.size();i++){
-            for(int j=0;j<words[i].length();j++){
+            for(int j=0;j<words[0].length();j++){
                 freq[j][words[i][j]-'a']++;
             }
         }
         
-        dp.clear();
-        dp = vector<vector<int>>(n+1,vector<int>(m+1,-1));
+        for(int i=0;i<=m;i++){
+            dp[n][i] = 1;
+        }
         
-        int ans = func(freq,target,0,0,n,m);
-        return ans;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                ll ways = dp[i][j+1];
+                ways += (ll)dp[i+1][j+1]*(ll)(freq[j][target[i]-'a']);
+                ways %= M;
+                dp[i][j] = (int)ways;
+            }
+        }
         
+        return dp[0][0];
     }
 };
