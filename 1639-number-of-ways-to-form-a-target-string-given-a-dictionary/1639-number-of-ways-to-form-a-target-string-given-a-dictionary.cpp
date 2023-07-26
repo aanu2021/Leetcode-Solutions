daaa3755006c1a40357932c7471b08f5f26e0aea
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-26 11:50:35 +0530

    Time: 345 ms (57.96%), Space: 54.6 MB (41.88%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -4,33 +4,36 @@ public:
     typedef long long ll;
     const ll M = 1e9 + 7;
     
-    vector<vector<int>>freq;
-    vector<vector<ll>>dp;
+    vector<vector<int>>dp;
     
-    ll func(string &target,int i,int j,int m,int n){
-        if(j==n) return 1LL;
-        if(i==m) return 0LL;
-        if(dp[i][j] != -1LL) return dp[i][j];
-        ll ans = func(target,i+1,j,m,n);
-        int pos = target[j]-'a';
-        ans += freq[i][pos]*func(target,i+1,j+1,m,n);
-        ans %= M;
-        return dp[i][j] = ans;
+    int func(vector<vector<int>>&freq,string &target,int i,int j,int n,int m){
+        if(i==n) return 1;
+        if(j==m) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        ll ways = func(freq,target,i,j+1,n,m);
+        ways += (ll)(freq[j][target[i]-'a'])*(ll)func(freq,target,i+1,j+1,n,m);
+        ways %= M;
+        return dp[i][j] = (int)ways;
     }
     
     int numWays(vector<string>& words, string target) {
+        
         int m = words[0].length();
         int n = target.length();
-        freq.clear();
-        dp.clear();
-        freq = vector<vector<int>>(m,vector<int>(26,0));
-        dp = vector<vector<ll>>(m,vector<ll>(n,-1LL));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<words.size();j++){
-                freq[i][words[j][i]-'a']++;
+        
+        vector<vector<int>>freq(m,vector<int>(26,0));
+        
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words[i].length();j++){
+                freq[j][words[i][j]-'a']++;
             }
         }
-        ll ans = func(target,0,0,m,n);
+        
+        dp.clear();
+        dp = vector<vector<int>>(n+1,vector<int>(m+1,-1));
+        
+        int ans = func(freq,target,0,0,n,m);
         return ans;
+        
     }
 };
