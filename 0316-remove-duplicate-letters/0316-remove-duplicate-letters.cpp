86451358d19714ec5a8a2a15f9c73b5d54f51d0e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-28 12:40:15 +0530

    Time: 6 ms (35.42%), Space: 7 MB (16.20%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        
         int n = s.length();
-        
         stack<char>S;
-        
         unordered_map<char,bool>visited;
         unordered_map<char,int>lastIdx;
-        
         for(int i=0;i<n;i++){
             visited[s[i]] = false;
             lastIdx[s[i]] = i;
         }
-        
         for(int i=0;i<n;i++){
             if(visited[s[i]]) continue;
-            while(!S.empty() && (S.top() > s[i]) && lastIdx[S.top()] > i){
+            while(!S.empty() && S.top() > s[i] && lastIdx[S.top()] > i){
                 visited[S.top()] = false;
                 S.pop();
             }
@@ -29,9 +24,7 @@ public:
             res += S.top();
             S.pop();
         }
-        
         reverse(res.begin(),res.end());
         return res;
-        
     }
 };
