01b2eb0bf341907534d94b50d720725ffb4fad54
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-07-30 09:04:39 +0530

    Time: 27 ms (86.62%), Space: 9.4 MB (43.21%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    
-    int func(string &s,int i,int j,vector<vector<int>>&dp){
-        
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int ans = 1 + func(s,i+1,j,dp);
-        
-        for(int k=i+1;k<=j;k++){
-            if(s[i] == s[k]){
-                ans = min(ans, func(s,i+1,k,dp) + func(s,k+1,j,dp));
+    vector<vector<int>>dp;
+    int func(string &s,int l,int r){
+        if(l > r) return 0;
+        if(l == r) return 1;
+        if(dp[l][r] != -1) return dp[l][r];
+        int ans = 1 + func(s,l+1,r);
+        for(int k=l+1;k<=r;k++){
+            if(s[l] == s[k]){
+                ans = min(ans,func(s,l+1,k) + func(s,k+1,r));
             }
         }
-        
-        return dp[i][j] = ans;
-        
+        return dp[l][r] = ans;
     }
-    
     int strangePrinter(string s) {
-        
         int n = s.length();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int ans = func(s,0,n-1,dp);
+        dp.clear();
+        dp = vector<vector<int>>(n,vector<int>(n,-1));
+        int ans = func(s,0,n-1);
         return ans;
-        
     }
 };
