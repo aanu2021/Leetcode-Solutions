9cbc59132b1b1e50dd4f7ff1ba476e5055c0a16b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-02 12:11:36 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -10,34 +10,23 @@ class Solution{
         void merge(long long arr1[], long long arr2[], int n, int m) 
         { 
             
-            int i = 0,j = 0,k = n-1;
+            int i = 0, j = 0, k = n-1;
             
-            while(i <= k && j < m){
+            while(i<=k && j<m){
                 if(arr1[i] <= arr2[j]){
                     i++;
                 }
                 else{
-                    swap(arr2[j++],arr1[k--]);
+                    swap(arr1[k--],arr2[j++]);
                 }
             }
             
-            sort(arr1,arr1 + n);
-            sort(arr2,arr2 + m);
+            sort(arr1,arr1+n);
+            sort(arr2,arr2+m);
             
         } 
 };
 
-/*
-
-This is more like a merge sort step.
-
-We are given with two sorted arrays.
-
-We have to merge them.
-
-[a1, a2, a3, a4]   [b1, b2, b3, b4, b5]
- 
-*/
 
 
 
