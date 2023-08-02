cc1cddb8adb061571aa076b19c4a7cef3d4d1a99
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-02 23:35:58 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -12,40 +12,25 @@ class Solution{
     //railway station such that no train waits.
     int findPlatform(int arr[], int dep[], int n)
     {
-    	
     	sort(arr,arr+n);
     	sort(dep,dep+n);
-    	
     	int i=0,j=0;
-    	int curr = 0, ans = 0;
-    	
+    	int currPlatform = 0;
+    	int maxPlatform = 0;
     	while(i<n && j<n){
     	    if(arr[i] <= dep[j]){
-    	        curr++;
+    	        currPlatform++;
     	        i++;
-    	    } 
+    	    }
     	    else{
-    	        curr--;
+    	        currPlatform--;
     	        j++;
     	    }
-    	    ans = max(ans,curr);
+    	    maxPlatform = max(maxPlatform, currPlatform);
     	}
-    	return ans;
-    	
+    	return maxPlatform;
     }
 };
-
-
-/*
-                              i
-900 940  950  1100 1500 1800
-910 1120 1130 1200 1900 2000
-                    j
-
-curr = 2
-ans = 3
-
-*/
 
 
 //{ Driver Code Starts.
