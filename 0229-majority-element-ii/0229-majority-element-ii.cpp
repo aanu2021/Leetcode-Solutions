5b72817ffb37d100b2a0d655c4c853a500a57137
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-02 20:26:59 +0530

    Time: 11 ms (86.40%), Space: 15.9 MB (63.19%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        vector<int>answer;
+        int first = nums[0], second = nums[0];
+        int cnt1 = 1, cnt2 = 0;
+        
+        for(int i=1;i<n;i++){
+            if(nums[i] == first){
+                cnt1++;
+            }
+            else if(nums[i] == second){
+                cnt2++;
+            }
+            else{
+                if(cnt1 == 0){
+                    first = nums[i];
+                    cnt1 = 1;
+                }
+                else if(cnt2 == 0){
+                    second = nums[i];
+                    cnt2 = 1;
+                }
+                else{
+                    cnt1--;
+                    cnt2--;
+                }
+            }
+        }
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == first){
+                cnt1++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i] == second){
+                cnt2++;
+            }
+        }
+        if(cnt1 > (n/3)){
+            answer.push_back(first);
+        }
+        if(cnt2 > (n/3) && (first != second)){
+            answer.push_back(second);
+        }
+        
+        return answer;
+    }
+};
+
+/*
+
+n%3 == 0 -->  atmost 2 majority elements [4,4,1]
+n%3 == 1 -->  atmost 2 majority elements [4,4,2]
+n%3 == 2 -->  atmost 2 majority elements [4,4,3]
+
+number of majority elements must be in the range [0,2]
+
+*/
