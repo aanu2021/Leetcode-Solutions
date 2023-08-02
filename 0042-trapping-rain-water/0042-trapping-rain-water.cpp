9c8477a41f3ebb381db79275625fd038673d38c4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-02 22:02:32 +0530

    Time: 11 ms (92.68%), Space: 19.6 MB (90.70%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
+        
         int n = nums.size();
-        vector<int>prefix(n);
-        vector<int>suffix(n);
-        prefix[0] = nums[0];
-        suffix[n-1] = nums[n-1];
-        for(int i=1;i<n;i++){
-            prefix[i] = max(prefix[i-1], nums[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = max(suffix[i+1], nums[i]);
-        }
+        int left = 0, right = n-1;
+        int leftMax = 0, rightMax = 0;
         int answer = 0;
-        for(int i=0;i<n;i++){
-            answer += min(prefix[i], suffix[i]) - nums[i];
+        
+        while(left <= right){
+           if(nums[left] <= nums[right]){
+               leftMax = max(leftMax, nums[left]);
+               answer += (leftMax - nums[left]);
+               left++;
+           }else{
+               rightMax = max(rightMax, nums[right]);
+               answer += (rightMax - nums[right]);
+               right--;
+           } 
         }
+        
         return answer;
     }
 };
