2053cd6fcd5c11623c8d845da93bfa67ab6b449c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-02 09:45:19 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (60.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,57 +3,21 @@ public:
     
     vector<vector<int>>answer;
     
-    void func(vector<int>&nums,int i,int n,vector<int>&solution,vector<int>&vis){
-        
-        if(i == n){
-            answer.push_back(solution);
-            return;
-        }
-        
-        for(int j=0;j<n;j++){
-            if(vis[j]) continue;
-            
-            vis[j] = 1;
-            solution.push_back(nums[j]);
-            
-            func(nums,i+1,n,solution,vis);
-            
-            solution.pop_back();
-            vis[j] = 0;
-        }
-        
-    }
-    
-    void func2(vector<int>&nums,int index,int n){
-        
+    void func(vector<int>&nums,int index,int n){
         if(index == n){
             answer.push_back(nums);
             return;
         }
-        
         for(int i=index;i<n;i++){
-            
-            swap(nums[index],nums[i]);
-            
-            func2(nums,index+1,n);
-            
-            swap(nums[index],nums[i]);
-            
+            swap(nums[i], nums[index]);
+            func(nums,index+1,n);
+            swap(nums[i], nums[index]);
         }
-        
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        
         int n = nums.size();
-        
-        // vector<int>vis(n,0);
-        // vector<int>solution;
-        // func(nums,0,n,solution,vis);
-        
-        func2(nums,0,n);
-        
+        func(nums,0,n);
         return answer;
-        
     }
 };
