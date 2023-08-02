68408239f9dc2d113457d791db041bb3c341bac1
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-03 00:56:16 +0530

    Time: 3 ms (55.51%), Space: 6 MB (42.95%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
     
-    void func(vector<int>&nums,vector<int>&fact,int n,int k,string &ans){
-        
+    void recursion(vector<int>&nums,vector<int>&fact,string &ans,int n,int k){
         if(n==1){
             ans += (char)(nums[0] + '0');
             return;
         }
-        
-        int idx = k/fact[n-1];
+        int idx = (k/fact[n-1]);
         ans += (char)(nums[idx] + '0');
-        nums.erase(nums.begin()+idx);
+        nums.erase(nums.begin() + idx);
         k -= (idx*fact[n-1]);
-        
-        func(nums,fact,n-1,k,ans);
-        
+        recursion(nums,fact,ans,n-1,k);
     }
     
     string getPermutation(int n, int k) {
         
-        vector<int>nums;
-        vector<int>fact(n+1,0);
-        
-        fact[0] = 1;
-        k--;
-        
-        for(int i=1;i<=n;i++){
-            nums.push_back(i);
-            fact[i] = fact[i-1]*i;
+        string ans = "";
+        vector<int>nums(n);
+        for(int i=0;i<n;i++){
+            nums[i] = i+1;
         }
         
-        string ans = "";
-        func(nums,fact,n,k,ans);
-        
+        vector<int>fact(n+1);
+        fact[0] = 1;
+        for(int i=1;i<=n;i++){
+            fact[i] = fact[i-1]*i;
+        }
+        k--;
+        recursion(nums,fact,ans,n,k);
         return ans;
-        
     }
 };
+
+// 1 2 3
+// 1 3 2
+// 2 1 3
+// 2 3 1
+// 3 1 2
+// 3 2 1
+  
+// 1 4
+    
+// k = 0
+    
+// 2 3 1 4     
+    
+    
