2adbcd6c2dacbac3b16d275732757bb462449431
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-02 12:55:03 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (53.38%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,43 +4,22 @@ public:
         
         int m = grid.size();
         int n = grid[0].size();
-        int lowRow = 0, highRow = m-1;
-        int idxRow = -1;
         
-        while(lowRow <= highRow){
-            
-            int midRow = (lowRow + highRow)/2;
-            
-            if(grid[midRow][0] <= target){
-                idxRow = midRow;
-                lowRow = midRow + 1;
+        int row = 0, col = n-1;
+        
+        while(row >= 0 && row < m && col >= 0 && col < n){
+            if(grid[row][col] == target){
+                return true;
+            }
+            if(grid[row][col] > target){
+                col--;
             }
             else{
-                highRow = midRow - 1;
+                row++;
             }
-            
-        }
-        
-        if(idxRow == -1) return false;
-        
-        int lowCol = 0, highCol = n-1;
-        int idxCol = -1;
-        
-        while(lowCol <= highCol){
-            
-            int midCol = (lowCol + highCol)/2;
-            
-            if(grid[idxRow][midCol] == target) return true;
-            
-            if(grid[idxRow][midCol] > target){
-                highCol = midCol - 1;
-            }
-            else{
-                lowCol = midCol + 1;
-            }
-            
         }
         
         return false;
+        
     }
 };
