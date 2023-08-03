98156997d553c578bba24b2023884ede66b075dd
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-04 02:28:05 +0530

    Time: 3 ms (91.38%), Space: 8 MB (81.09%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
-    
-    unordered_set<string>dict;
-    
-    int func(string &s,int i,int n,vector<int>&dp){
-        if(i==n) return 1;
-        if(dp[i] != -1) return dp[i];
-        string word = "";
-        int currValue = 0;
-        for(int j=i;j<n;j++){
-            word += s[j];
-            if(dict.find(word) != dict.end() && func(s,j+1,n,dp)){
-                currValue = 1;
-                break;
-            }
-        }
-        return dp[i] = currValue;
-    }
-    
-    int func2(string &s){
+    bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
+        unordered_set<string>S;
+        for(string &str : wordDict){
+            S.insert(str);
+        }
         vector<bool>dp(n+1,false);
         dp[n] = true;
-        
         for(int i=n-1;i>=0;i--){
-            string word = "";
+            string str = "";
             for(int j=i;j<n;j++){
-                word += s[j];
-                if(dict.find(word) != dict.end() && dp[j+1]){
-                    dp[i] = true;
-                    break;
+                str += s[j];
+                if(S.find(str) != S.end()){
+                    dp[i] = dp[i] || dp[j+1];
+                    if(dp[i]){
+                        break;
+                    }
                 }
             }
         }
-        
         return dp[0];
-    }
-    
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.length();
-        for(string &str : wordDict){
-            dict.insert(str);
-        }
-        // vector<int>dp(n,-1);
-        // int ans = func(s,0,n,dp);
-        // if(ans > 0) return true;
-        // else return false;
-        return func2(s);
     }
 };
