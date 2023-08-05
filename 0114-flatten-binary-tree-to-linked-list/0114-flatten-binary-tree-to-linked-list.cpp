631ca144ac5f640eec7a4530dc27d4b4c3e00127
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-05 22:01:05 +0530

    Time: 4 ms (72.88%), Space: 12.7 MB (51.88%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,87 +1,18 @@
-
 class Solution {
 public:
-    
-    /*
-    
-    T.C : O(N*N)
-    S.C : O(H)
-    
-    */
-    
-    void flatten3(TreeNode* root) {
-       
+    void flatten(TreeNode* root) {
         if(!root) return;
-        
         if(!root->left && !root->right) return;
-        
-        flatten3(root->left);
-        flatten3(root->right);
-        
+        flatten(root->left);
+        flatten(root->right);
         TreeNode*leftChild = root->left;
         TreeNode*rightChild = root->right;
-        
         root->left = NULL;
         root->right = leftChild;
-        TreeNode*curr = root;
-        while(curr && curr->right){
-            curr = curr->right;
+        TreeNode*tmp = root;
+        while(tmp && tmp->right){
+            tmp = tmp->right;
         }
-        curr->right = rightChild;
-        
+        tmp->right = rightChild;
     }
-    
-    
-    /*
-    
-    T.C : O(N)
-    S.C : O(H)
-    
-    */
-    
-    TreeNode * prevNode = NULL;
-    
-    void flatten2(TreeNode* root) {
-       
-        if(!root) return;
-        
-        flatten(root->right);
-        flatten(root->left);
-        
-        root->right = prevNode;
-        root->left = NULL;
-        prevNode = root;
-        
-    }
-    
-    /*
-    
-    T.C : O(N)
-    S.C : O(1)
-    
-    root , left_subtree , right_subtree
-    
-    */
-    
-    void flatten(TreeNode* root){
-        
-        if(!root) return;
-        
-        TreeNode*curr = root;
-       
-        while(curr){
-            if(curr->left){
-                TreeNode * prev = curr->left;
-                while(prev && prev->right){
-                    prev = prev->right;
-                }
-                prev->right = curr->right;
-                curr->right = curr->left;
-                curr->left = NULL;
-            }
-            curr = curr->right;
-        } 
-        
-    }
-    
 };
