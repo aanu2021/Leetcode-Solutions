3cf006d8031f48f9643a479609f8b31e62bd52ff
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-05 11:12:28 +0530

    Time: 4 ms (93.98%), Space: 7.4 MB (38.51%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.length()!=t.length()){
+            return false;
+        }
+        
+        vector<int>v(26,0);
+        
+        for(int i=0;i<s.length();++i){
+            v[s[i]-'a']++;
+        }
+        
+        for(int i=0;i<t.length();++i){
+            v[t[i]-'a']--;
+        }
+        
+        for(int i=0;i<26;++i){
+            if(v[i]!=0){
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+};
