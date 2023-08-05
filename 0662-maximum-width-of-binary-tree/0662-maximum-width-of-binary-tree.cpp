22d87f4f42b1575a151f8d464a64a679c3db81f2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-05 18:12:49 +0530

    Time: 10 ms (54.00%), Space: 17.3 MB (72.21%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -4,38 +4,31 @@ public:
     typedef long long ll;
     
     int widthOfBinaryTree(TreeNode* root) {
-       
         if(!root) return 0;
+        ll maxLevel = 0LL;
         queue<pair<TreeNode*,ll>>q;
         q.push({root,0LL});
-        
-        ll maxLevel = 0LL;
         while(!q.empty()){
-           
             int sz = q.size();
-            ll left = -1LL , right = -1LL;
+            ll left = -1LL, right = -1LL;
             while(sz--){
-                
                 auto curr = q.front(); q.pop();
                 auto node = curr.first;
                 ll lvl = curr.second;
-                
-                if(left==-1) left = lvl;
+                if(left == -1){
+                    left = lvl;
+                }
                 right = lvl;
                 ll currLevel = (right-left+1LL);
-                maxLevel = max(maxLevel,currLevel);
-                
+                maxLevel = max(maxLevel, currLevel);
                 if(node->left){
                     q.push({node->left,2*currLevel});
                 }
                 if(node->right){
-                    q.push({node->right,2*currLevel + 1LL});
+                    q.push({node->right,2*currLevel + 1});
                 }
-                
             }
-            
         }
-        
         return maxLevel;
     }
 };
