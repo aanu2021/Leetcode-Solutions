0c029c7116b10896e2f4b3a5c4fe2c9ed92d1883
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-05 12:55:41 +0530

    Time: 4 ms (28.76%), Space: 8.5 MB (21.01%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,74 +1,19 @@
 class Solution {
 public:
     
-    void postorder_recursive(TreeNode*&root,vector<int>&postorder_arr)
-    {
+    vector<int>answer;
+    
+    void postorder(TreeNode* &root){
         if(!root) return;
-        postorder_recursive(root->left,postorder_arr);
-        postorder_recursive(root->right,postorder_arr);
-        postorder_arr.push_back(root->val);
+        postorder(root->left);
+        postorder(root->right);
+        answer.push_back(root->val);
     }
     
-    void postorder_iterative(TreeNode*&root,vector<int>&postorder_arr)
-    {
-        if(!root) return;
-        stack<TreeNode*>S;
-        S.push(root);
-        
-        while(!S.empty()){
-            auto node = S.top();
-            S.pop();
-            postorder_arr.push_back(node->val);
-            if(node->left) S.push(node->left);
-            if(node->right) S.push(node->right);
-        }
-        
-        reverse(postorder_arr.begin(),postorder_arr.end());
-        
-    }   
-    
-    void postorder_Morris_Traversal(TreeNode*&root,vector<int>&postorder_arr)
-    {
-        
-        if(!root) return;
-        TreeNode*curr = root;
-        
-        while(curr){
-            
-            if(curr->right == NULL){
-                postorder_arr.push_back(curr->val);
-                curr = curr->left;
-            }
-            else{
-                TreeNode*prev = curr->right;
-                while(prev && prev->left && prev->left != curr){
-                    prev = prev->left;
-                }
-                if(prev->left == NULL){
-                    postorder_arr.push_back(curr->val);
-                    prev->left = curr;
-                    curr = curr->right;
-                }
-                else{
-                    prev->left = NULL;
-                    curr = curr->left;
-                }
-            }
-            
-        }
-        
-        reverse(postorder_arr.begin(),postorder_arr.end());
-        
-    }    
-    
     vector<int> postorderTraversal(TreeNode* root) {
-        
         if(!root) return {};
-        vector<int>postorder_arr;
-        // postorder_recursive(root,postorder_arr);
-        // postorder_iterative(root,postorder_arr);
-        postorder_Morris_Traversal(root,postorder_arr);
-        return postorder_arr;
-        
+        answer.clear();
+        postorder(root);
+        return answer;
     }
 };
