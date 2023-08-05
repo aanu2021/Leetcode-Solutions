6e9bd7cf5e2320384c8aee2babbbbd59992da0b9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-05 11:35:53 +0530

    Added solution - LeetHub

--- Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
@@ -1,73 +1,38 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
   public:
     string longestPalin (string s) {
-        
-        int n=s.length();
-        
-        int maxLen=0,startidx=-1;
-        
-        vector<vector<bool>>dp(n+1,vector<bool>(n+1,false));
-        
+        int n = s.length();
+        int maxLen = 0;
+        int startIdx = 0;
         for(int i=0;i<n;i++){
-            dp[i][i]=true;
-        }
-        
-        for(int i=0;i<n-1;i++){
-            
-            if(s[i]==s[i+1]){
-                dp[i][i+1]=true;
-            }else{
-                dp[i][i+1]=false;
+            int low = i-1, high = i+1;
+            while(low >= 0 && s[low]==s[i]){
+                low--;
             }
-            
-        }
-        
-        for(int L=3;L<=n;L++){
-            
-            for(int i=0;i<n-L+1;i++){
-                
-                int j=i+L-1;
-                
-                if(s[i]==s[j] && dp[i+1][j-1]==true){
-                    dp[i][j]=true;
-                }
-                
+            while(high <n && s[high]==s[i]){
+                high++;
             }
-            
-        }
-        
-        
-        for(int i=0;i<n;i++){
-            
-            for(int j=i;j<n;j++){
-                
-                if(dp[i][j]==true){
-                    
-                    int currlen=(j-i+1);
-                    
-                    if(currlen>maxLen){
-                        maxLen=currlen;
-                        startidx=i;
-                    }
-                    
-                }
-                
+            while(low >= 0 && high < n && s[low] == s[high]){
+                low--;
+                high++;
             }
-            
+            int currLen = high - low - 1;
+            if(currLen > maxLen){
+                maxLen = currLen;
+                startIdx = low+1;
+            }
         }
-        
-        return s.substr(startidx,maxLen);
-        
+        return s.substr(startIdx, maxLen);
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main()
 {
@@ -81,4 +46,5 @@ int main()
     }
 }
 // Contributed By: Pranay Bansal
-  // } Driver Code Ends
+
+// } Driver Code Ends
