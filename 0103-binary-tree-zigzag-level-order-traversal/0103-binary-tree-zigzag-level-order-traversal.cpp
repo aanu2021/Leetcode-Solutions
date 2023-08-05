f5cf36d5986cfb25ad27da589f5e43ab0badf4e7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-05 19:19:37 +0530

    Time: 0 ms (100.00%), Space: 12 MB (53.19%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
+    
+    vector<vector<int>>answer;
+    
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         if(!root) return {};
-        vector<vector<int>>levels;
-        int lvl = 0;
+        answer.clear();
         queue<TreeNode*>q;
         q.push(root);
+        int lvl = 0;
         while(!q.empty()){
             int sz = q.size();
-            vector<int>level(sz);
-            int i = 0;
-            if(lvl%2) i = sz-1;
+            vector<int>levelArr(sz);
+            int l = 0, r = sz-1;
             while(sz--){
-                auto node = q.front();
-                q.pop();
-                if(lvl%2 == 0){
-                    level[i++] = node->val;
+                auto node = q.front(); q.pop();
+                if(lvl%2==0){
+                    levelArr[l++] = node->val;
                 }
                 else{
-                    level[i--] = node->val;
+                    levelArr[r--] = node->val;
                 }
-                if(node->left)  q.push(node->left);
-                if(node->right) q.push(node->right);
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
             }
+            answer.push_back(levelArr);
             lvl++;
-            levels.push_back(level);
         }
-        return levels;
+        return answer;
     }
 };
