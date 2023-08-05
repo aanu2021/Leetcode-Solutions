40f17d7f93a3a66de648ce7986b22491d85e75e2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-05 21:45:46 +0530

    Time: 16 ms (80.25%), Space: 26.5 MB (19.84%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     
-    unordered_map<int,int>index;
     int idx = 0;
+    unordered_map<int,int>IndexMap;
     
-    TreeNode * BuildBT(vector<int>&preorder,vector<int>&inorder,int low,int high){
-        
+    TreeNode*BuildBT(vector<int>&preorder,vector<int>&inorder,int low,int high){
         if(low > high) return NULL;
-        if(low == high) return new TreeNode(preorder[idx++]);
-        
-        TreeNode * root = new TreeNode(preorder[idx]);
         int value = preorder[idx++];
-        int inorder_pos = index[value];
-        
-        root->left = BuildBT(preorder,inorder,low,inorder_pos-1);
-        root->right = BuildBT(preorder,inorder,inorder_pos+1,high);
+        TreeNode*root = new TreeNode(value);
+        int idx = IndexMap[value];
+        root->left = BuildBT(preorder,inorder,low,idx-1);
+        root->right = BuildBT(preorder,inorder,idx+1,high);
         return root;
-        
     }
     
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)     {
         int n = preorder.size();
-        
         for(int i=0;i<n;i++){
-            index[inorder[i]] = i;
+            IndexMap[inorder[i]] = i;
         }
-        
-        TreeNode * root = BuildBT(preorder,inorder,0,n-1);
+        idx = 0;
+        TreeNode*root = BuildBT(preorder,inorder,0,n-1);
         return root;
-        
     }
 };
