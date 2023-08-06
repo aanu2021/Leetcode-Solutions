69abe370d76aad55f5c2d017bd6d0c3bee4c44aa
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-06 13:14:36 +0530

    Time: 10 ms (87.68%), Space: 10.3 MB (85.02%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    /*
+    
+     Optimized Approach using Binary Search
+     
+     Time Complexity  : O(N*logN)
+     Space Complexity : O(N)
+    
+    */
+    
+    int lengthOfLIS(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        vector<int>tails;
+        
+        tails.push_back(nums[0]);
+        
+        for(int i=1;i<n;++i){
+            
+            if(tails.back()<nums[i]){
+                tails.push_back(nums[i]);
+            }
+            
+            else{
+                int index=lower_bound(tails.begin(),tails.end(),nums[i])-tails.begin();
+                tails[index]=nums[i];
+            }
+            
+        }
+        
+        return tails.size();
+        
+    }
+};
