b6c1762b86dda37ce3d53b8fb646a3405a1a787f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-06 12:30:03 +0530

    Time: 45 ms (94.87%), Space: 43.6 MB (83.69%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,28 +1,27 @@
 struct Node{
-  
-    Node*links[26];
-    bool endWord;
-    
-    Node(){
-        for(int i=0;i<26;i++){
-            links[i] = NULL;
-        }
-        endWord = false;
-    }
-    
-    bool containsKey(char ch){
-        return (links[ch-'a'] != NULL);
-    }
-    Node*get(char ch){
-        return links[ch-'a'];
-    }
-    void put(char ch,Node*node){
-        links[ch-'a'] = node;
-    }
-    void setWord(){
-        endWord = true;
-    }
-    
+   Node*links[26];
+   bool endWord;
+   Node(){
+       endWord = false;
+       for(int i=0;i<26;i++){
+           links[i] = NULL;
+       }
+   }  
+   bool containsKey(char ch){
+       return (links[ch-'a'] != NULL);
+   } 
+   Node*get(char ch){
+       return links[ch-'a'];
+   } 
+   void put(char ch,Node*node){
+       links[ch-'a'] = node;
+   } 
+   void setEnd(){
+       endWord = true;
+   } 
+   bool isEnd(){
+       return endWord;
+   } 
 };
 
 class Trie {
@@ -42,32 +41,29 @@ public:
             }
             node = node->get(word[i]);
         }
-        node->setWord();
+        node->setEnd();
     }
     
     bool search(string word) {
         Node*node = root;
         for(int i=0;i<word.length();i++){
-            if(!node->containsKey(word[i])) return false;
+            if(!node->containsKey(word[i])){
+                return false;
+            }
             node = node->get(word[i]);
         }
-        return node->endWord;
+        return node->isEnd();
     }
     
     bool startsWith(string word) {
         Node*node = root;
         for(int i=0;i<word.length();i++){
-            if(!node->containsKey(word[i])) return false;
+            if(!node->containsKey(word[i])){
+                return false;
+            }
             node = node->get(word[i]);
         }
         return true;
     }
 };
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
