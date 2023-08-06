879f21fbb6b953266cfab68b1364d8c8f3e8ca49
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-06 14:07:38 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -1,50 +1,34 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 // Initial Template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 // User function Template for C++
 
 class Solution{
 public:
-    int matrixMultiplication(int n, int arr[])
+    int matrixMultiplication(int N, int arr[])
     {
-        
-         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-         
-         for(int i=1;i<n-1;i++){
-             
-             dp[i][i+1]=(arr[i-1]*arr[i]*arr[i+1]);
-             
-         }
-         
-         
-         for(int L=3;L<n;L++){
-             
-             for(int i=1;i<=n-L;i++){
-                 
-                 int j=i+L-1;
-                 
-                 dp[i][j]=INT_MAX;
-                 
-                 for(int k=i;k<=j-1;k++){
-                     
-                     dp[i][j]=min(dp[i][j],dp[i][k]+dp[k+1][j]+(arr[i-1]*arr[k]*arr[j]));
-                     
-                 }
-                 
-             }
-             
-         }
-         
-         return dp[1][n-1];
-        
+        vector<vector<int>>dp(N+1,vector<int>(N+1,0));
+        for(int i=1;i<N-1;i++){
+            dp[i][i+1] = arr[i-1]*arr[i]*arr[i+1];
+        }
+        for(int L=3;L<N;L++){
+            for(int i=1;i<=N-L;i++){
+                int j = i+L-1;
+                dp[i][j] = INT_MAX;
+                for(int k=i;k<=j-1;k++){
+                    dp[i][j] = min(dp[i][j], (arr[i-1]*arr[k]*arr[j]) + dp[i][k] + dp[k+1][j]);
+                }
+            }
+        }
+        return dp[1][N-1];
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main(){
     int t;
@@ -60,4 +44,5 @@ int main(){
         cout<<ob.matrixMultiplication(N, arr)<<endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
