c99df921122e63abbeeae8399b3793411cd415f9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-06 20:13:29 +0530

    Time: 14 ms (98.86%), Space: 13.5 MB (48.47%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,73 +1,30 @@
 class Solution {
 public:
     
-    bool isCycle(int u,vector<vector<int>>&graph,vector<int>&color){
-        
+    bool check(int u,vector<vector<int>>&graph,vector<int>&color){
         for(int v:graph[u]){
-            if(color[v]==-1){
+            if(color[v] == -1){
                 color[v] = 1 - color[u];
-                bool flag = isCycle(v,graph,color);
-                if(flag) return true;
+                bool flag = check(v,graph,color);
+                if(!flag) return false;
             }
-            else{
-                if(color[v]==color[u]){
-                    return true;
-                }
+            else if(color[v]==color[u]){
+                return false;
             }
         }
-        
-        return false;
-        
-    }
-    
-    bool isCycle2(int src,vector<vector<int>>&graph,vector<int>&color){
-        
-        queue<pair<int,int>>q;
-        q.push({src,-1});
-        
-        while(!q.empty()){
-            
-            int sz = q.size();
-            
-            while(sz--){
-                
-                auto curr = q.front(); q.pop();
-                int u = curr.first;
-                int par = curr.second;
-                
-                for(int v:graph[u]){
-                    if(v==par) continue;
-                    if(color[v]==-1){
-                        color[v] = 1 - color[u];
-                        q.push({v,u});
-                    }
-                    else{
-                        if(color[v]==color[u]) return true;
-                    }
-                }
-                
-            }
-        }
-        
-        return false;        
+        return true;
     }
     
     bool isBipartite(vector<vector<int>>& graph) {
-        
         int n = graph.size();
-        
         vector<int>color(n,-1);
-        
         for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                color[i] = 1;
-                if(isCycle2(i,graph,color)){
-                    return false;
-                }
+            if(color[i] == -1){
+                color[i] = 0;
+                bool flag = check(i,graph,color);
+                if(!flag) return false;
             }
         }
-        
         return true;
-        
     }
 };
