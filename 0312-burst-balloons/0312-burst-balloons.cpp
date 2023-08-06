3377038a68128014f1f12dd3bada5c8fd654426a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-06 17:57:51 +0530

    Time: 390 ms (78.95%), Space: 10.3 MB (33.32%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    
-    int func(vector<int>&arr,int i,int j,int n,vector<vector<int>>&dp){
-        if(i > j) return 0;
-        if(i == j) return (arr[i]*arr[i-1]*arr[i+1]);
-        if(dp[i][j] != -1) return dp[i][j];
-        int currMax = 0;
-        for(int k=i;k<=j;k++){
-            currMax = max(currMax,(arr[i-1]*arr[k]*arr[j+1]) + func(arr,i,k-1,n,dp) + func(arr,k+1,j,n,dp));
-        }
-        return dp[i][j] = currMax;
-    }
-    
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         vector<int>arr;
         arr.push_back(1);
-        for(int &ele : nums) arr.push_back(ele);
+        for(int i=0;i<n;i++){
+            arr.push_back(nums[i]);
+        }
         arr.push_back(1);
-        vector<vector<int>>dp(n+5,vector<int>(n+5,-1));
-        
-        /*
-        
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
         for(int i=1;i<=n;i++){
             dp[i][i] = (arr[i-1]*arr[i]*arr[i+1]);
         }
@@ -34,10 +21,5 @@ public:
             }
         }
         return dp[1][n];
-        
-        */
-        
-        int ans = func(arr,1,n,n,dp);
-        return ans;
     }
 };
