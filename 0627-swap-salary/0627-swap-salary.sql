915c637444e7747a18994fc1bd0657eab0673327
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-11 12:52:09 +0530

    Time: 485 ms (72.66%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -11,17 +11,8 @@
 #                  END
 
 
-
-UPDATE Salary
-
-       SET sex = 
-       
-       CASE sex
-       
-       WHEN 'm' THEN 'f'
-       
-       ELSE 'm'
-       
-       END
-                 
-   
+update Salary
+set sex = case
+          when sex='m' then 'f'
+          else 'm'
+          end
