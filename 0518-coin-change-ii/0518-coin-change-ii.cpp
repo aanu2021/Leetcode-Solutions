8e503aa94f11d83ad71bcc1c11b22237e94f0ae7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-11 09:48:09 +0530

    Time: 46 ms (35.97%), Space: 18.5 MB (37.65%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    
     int change(int amount, vector<int>& coins) {
-        
         int n = coins.size();
-        
-        vector<int>dp(amount+1,0);
-        
-        dp[0] = 1;
-        
-        for(int i=0;i<coins.size();i++){
-            for(int j=coins[i];j<=amount;j++){
-                dp[j]+=dp[j-coins[i]];
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,0));
+        dp[0][0] = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                dp[i][j] = dp[i-1][j];
+                if(j >= coins[i-1]){
+                    dp[i][j] += dp[i][j-coins[i-1]];
+                }
             }
         }
-        
-        return dp[amount];
-        
+        return dp[n][amount];
     }
 };
