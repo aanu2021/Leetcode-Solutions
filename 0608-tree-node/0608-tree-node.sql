3a97101a74ffdf21fc96539bf982747f8d17c931
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-11 12:47:29 +0530

    Time: 1120 ms (44.19%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,70 +1,10 @@
 # Write your MySQL query statement below
 
-
-# Union And Select
-
-# SELECT id , 'Root' AS type
-
-#        FROM Tree
-       
-#        WHERE p_id IS NULL
-       
-# UNION
-
-# SELECT id , 'Leaf' AS type
-
-#        FROM Tree
-       
-#        WHERE id NOT IN(SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
-       
-# UNION
-
-# SELECT id , 'Inner' AS type
-
-#        FROM Tree
-       
-#        WHERE id IN (SELECT p_id FROM Tree WHERE p_id is NOT NULL) AND p_id IS NOT NULL
-       
-#        ORDER BY id;
-
-
-
-# Control Flow Statement - Case
-
-# SELECT id , 
-
-#         CASE
-        
-#         WHEN p_id IS NULL THEN 'Root'
-        
-#         WHEN id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL THEN 'Leaf'
-        
-#         ELSE 'Inner'
-        
-#         END
-        
-#         AS type
-        
-#         FROM Tree
-        
-#         ORDER BY id;
-
-
-
-SELECT id ,
-
-       CASE 
-       
-       WHEN p_id IS NULL THEN 'Root'
-       
-       WHEN id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL THEN 'Leaf'
-       
-       ELSE 'Inner'
-       
-       END 
-       
-       AS type
-       
-       FROM Tree
-       
-       ORDER BY id;
+select id,
+case
+when p_id is null then 'Root'
+when id not in (select p_id from Tree where p_id is not null) and p_id is not null then 'Leaf'
+else 'Inner'
+end
+as type
+from Tree
