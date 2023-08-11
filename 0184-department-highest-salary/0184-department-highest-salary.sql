7f5eff7298ea8870b2ae7a4cd0b0b466c9589c5a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-11 11:23:42 +0530

    Time: 1807 ms (21.13%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -2,16 +2,16 @@
 
 select 
 Department.name as Department,
-Employee.name as Employee, 
-Employee.salary as Salary 
+Employee.name as Employee,
+Employee.salary as Salary
 from Employee join Department
 on Employee.departmentId = Department.id
-where (Employee.departmentId, Employee.salary)
+where
+(Employee.departmentId,Employee.salary)
 in
 (
-select
-departmentId,MAX(salary)
-from Employee
-group by departmentId    
+  select departmentId, MAX(salary)
+  from Employee 
+  group by departmentId  
 )
 
