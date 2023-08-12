c660afddf5b539ab6df4fa8f8365be016b6fc68c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-13 01:17:22 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -8,54 +8,20 @@ using namespace std;
 class Solution {
   public:
   
-    typedef long long int ll;
+    typedef long long ll;
     const ll M = 1e9 + 7;
-    
-    ll mod(ll a){
-        return ((a%M)+M)%M;
-    }
-    ll mul(ll a,ll b){
-        return mod(mod(a)*mod(b));
-    }
-    ll add(ll a,ll b){
-        return mod(mod(a)+mod(b));
-    }
-    
-    ll fib(ll n){
-        if(n==0) return 1;
+  
+    int nthFibonacci(int n){
+        if(n==0) return 0;
         if(n==1) return 1;
         if(n==2) return 1;
-        ll F[2][2] = {{1,1},{1,0}};
-        power(F,n-1);
-        return F[0][0];
-    }
-    
-    void power(ll F[2][2],ll n){
-        if(n==0 || n==1) return;
-        ll M[2][2] = {{1,1},{1,0}};
-        power(F,n/2);
-        multiply(F,F);
-        if(n%2) multiply(F,M);
-    }
-    
-    void multiply(ll F[2][2],ll M[2][2]){
-        
-        ll x = add(mul(F[0][0],M[0][0]),mul(F[0][1],M[1][0]));
-        ll y = add(mul(F[0][0],M[0][1]),mul(F[0][1],M[1][1]));
-        ll z = add(mul(F[1][0],M[0][0]),mul(F[1][1],M[1][0]));
-        ll w = add(mul(F[1][0],M[0][1]),mul(F[1][1],M[1][1]));
-        
-        F[0][0] = x;
-        F[0][1] = y;
-        F[1][0] = z;
-        F[1][1] = w;
-        
-    }
-  
-    long long int nthFibonacci(long long int n){
-        
-        return fib(n);
-        
+        ll a = 1LL, b = 1LL;
+        for(int i=3;i<=n;i++){
+            ll c = (a+b)%M;
+            a = b;
+            b = c;
+        }
+        return b;
     }
 };
 
@@ -64,7 +30,7 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        long long int n;
+        int n;
         cin >> n;
         Solution ob;
         cout << ob.nthFibonacci(n) << endl;
