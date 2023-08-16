1ea38b41b943ee7b15cc1a1594c3ee49c21153b6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-17 00:03:26 +0530

    Time: 4302 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -7,8 +7,6 @@
 # weight
 # turn
 
-with Q as 
-(select * from Queue order by turn asc)
 
-select Q1.person_name from Q Q1
-where (select SUM(Q2.weight) from Q Q2 where Q1.turn >= Q2.turn) <= 1000 order by Q1.turn desc limit 1;
+select Q1.person_name from Queue Q1
+where (select SUM(Q2.weight) from Queue Q2 where Q1.turn >= Q2.turn) <= 1000 order by Q1.turn desc limit 1;
