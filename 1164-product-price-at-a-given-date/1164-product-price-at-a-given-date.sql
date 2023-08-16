4dc566c1882237d79cf8d96e9ab1aa64c7528ae4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-16 21:32:26 +0530

    Time: 1103 ms (38.73%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -3,16 +3,19 @@
 # on  2019-08-16
 # valid dates are -> datediff(change_date,'2019-08-16') >= 0
 
-(select product_id,new_price as price 
-from Products
-where (product_id,change_date) in 
-(select product_id,MAX(change_date)
-from Products
-where datediff(change_date,'2019-08-16') <= 0
-group by product_id))
-Union
-(select product_id,10 as price
-from Products
-group by product_id
-having datediff(MIN(change_date),'2019-08-16') > 0);
+select * from 
+(
+    (select product_id,new_price as price 
+    from Products
+    where (product_id,change_date) in 
+    (select product_id,MAX(change_date)
+    from Products
+    where datediff(change_date,'2019-08-16') <= 0
+    group by product_id))
+    Union
+    (select product_id,10 as price
+    from Products
+    group by product_id
+    having datediff(MIN(change_date),'2019-08-16') > 0)
+) as Tablet;    
 
