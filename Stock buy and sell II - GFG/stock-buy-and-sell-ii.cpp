9e67d2a6f9dd8e0bf4f1a4bd409d59865fdbf5c2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-19 01:02:52 +0530

    Added solution - LeetHub

--- Stock buy and sell II - GFG/stock-buy-and-sell-ii.cpp
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.cpp
@@ -34,9 +34,7 @@ class Solution {
     int stockBuyAndSell(int n, vector<int> &prices) {
         int sum = 0;
         for(int i=1;i<n;i++){
-            if(prices[i] > prices[i-1]){
-                sum += (prices[i] - prices[i-1]);
-            }
+            sum += max(prices[i]-prices[i-1],0);
         }
         return sum;
     }
