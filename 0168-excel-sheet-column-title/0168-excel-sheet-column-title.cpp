39720700401aa07808aa88aa69147f6e1d7f6a14
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-22 08:20:02 +0530

    Time: 2 ms (49.78%), Space: 5.9 MB (79.56%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -56,11 +56,10 @@ public:
     
     string convertToTitle(int num) {
         string res = "";
-        while(num>0){
+        while(num > 0){
             num--;
-            int rem = (num%26);
-            res+=(char)(rem+'A');
-            num/=26;
+            res += (char)((num%26)+'A');
+            num /= 26;
         }
         reverse(res.begin(),res.end());
         return res;
