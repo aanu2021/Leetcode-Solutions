ff9558459e362f0ca9f42e34deefacab57385668
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-22 14:53:26 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.59%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,48 +1,27 @@
 class Solution {
 public:
     
-    void recursion(vector<int>&nums,vector<int>&fact,string &ans,int n,int k){
+    void recursion(vector<int>&nums,vector<int>&fact,int n,int k,string &ans){
         if(n==1){
-            ans += (char)(nums[0] + '0');
+            ans += (char)(nums[0]+'0');
             return;
         }
         int idx = (k/fact[n-1]);
-        ans += (char)(nums[idx] + '0');
-        nums.erase(nums.begin() + idx);
+        ans += (char)(nums[idx]+'0');
+        nums.erase(nums.begin()+idx);
         k -= (idx*fact[n-1]);
-        recursion(nums,fact,ans,n-1,k);
+        recursion(nums,fact,n-1,k,ans);
     }
     
     string getPermutation(int n, int k) {
-        
         string ans = "";
         vector<int>nums(n);
-        for(int i=0;i<n;i++){
-            nums[i] = i+1;
-        }
-        
         vector<int>fact(n+1);
-        fact[0] = 1;
-        for(int i=1;i<=n;i++){
-            fact[i] = fact[i-1]*i;
-        }
         k--;
-        recursion(nums,fact,ans,n,k);
+        fact[0] = 1;
+        for(int i=1;i<=n;i++) fact[i] = fact[i-1]*i;
+        for(int i=0;i<n;i++) nums[i] = i+1;
+        recursion(nums,fact,n,k,ans);
         return ans;
     }
 };
-
-// 1 2 3
-// 1 3 2
-// 2 1 3
-// 2 3 1
-// 3 1 2
-// 3 2 1
-  
-// 1 4
-    
-// k = 0
-    
-// 2 3 1 4     
-    
-    
