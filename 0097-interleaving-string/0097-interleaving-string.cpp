36ce225f08bce106420b8a3bd657c9810b1c2ab5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-08-25 12:18:52 +0530

    Time: 5 ms (61.24%), Space: 6.7 MB (66.93%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -8,27 +8,12 @@ public:
         
         if((m+n) != len) return false;
         
-        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
         vector<int>prev(n+1,false);
-        
-        dp[0][0] = true;
         prev[0] = true;
         
         for(int i=1;i<=n;i++){
             if(s2[i-1]==s3[i-1]){
                 prev[i] = prev[i-1];
-            }
-        }
-        
-        for(int i=1;i<=n;i++){
-            if(s2[i-1]==s3[i-1]){
-                dp[0][i] = dp[0][i-1];
-            }
-        }
-        
-        for(int i=1;i<=m;i++){
-            if(s1[i-1]==s3[i-1]){
-                dp[i][0] = dp[i-1][0];
             }
         }
         
@@ -47,17 +32,6 @@ public:
             }
             for(int j=0;j<=n;j++){
                 prev[j] = curr[j];
-            }
-        }
-        
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s1[i-1] == s3[i+j-1]){
-                    dp[i][j] = dp[i-1][j];
-                }
-                if(s2[j-1] == s3[i+j-1]){
-                    dp[i][j] = dp[i][j] || dp[i][j-1];
-                }
             }
         }
         
