6b21a1091865f30e63463668d7c0d7bd48627077
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-09-06 10:48:25 +0530

    Time: 6 ms (63.09%), Space: 8.9 MB (45.79%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -1,67 +1,48 @@
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        
+        int n = 0;
         ListNode*ptr = head;
-        int n = 0; // Length of the LL
+        
         while(ptr){
             ptr = ptr->next;
             n++;
         }
-
-        vector<ListNode*>ans(k,NULL); 
-
-        // Case 1 --> n < k (some seg might be empty)
-
+        
+        vector<ListNode*>ans(k,NULL);
+        int index = 0;
+        
         if(n < k){
-
             ptr = head;
-
-            for(int i=0;i<n;i++){
-                ans[i] = new ListNode(ptr->val);
+            while(ptr){
+                ans[index++] = new ListNode(ptr->val);
                 ptr = ptr->next;
             }
-
             return ans;
-
-        }  
-
-        // Case 2 --> n >= k (every seg must have some nodes + no two parts should have a size differing by more than one)
-
-        //  Len = 13  k = 3
-
-        //  10 / 3 = 3   (3*3) = 9  10 - 9 = 1
-
-        //  5 4 4  
-
-        //  each segment will get (n/k) nodes for sure
-        //  additionally first (n%k) segments get one nodes extra
-
-         int each_have = (n/k);
-         int extra_some = (n%k);
-
-         for(int i=0;i<k;i++){
-
-             int currLen = each_have;
-             if(extra_some > 0) currLen++;
-             extra_some--;
-
-             ListNode * pointer = head;
-             ListNode * follow = pointer;
-             ListNode * temp = pointer;
-
-             while(currLen){
-                 follow = pointer;
-                 pointer = pointer->next;
-                 currLen--;
-             }
-             
-             follow->next = NULL;
-             head = pointer;
-             ans[i] = temp;
-
-         }
-
-         return ans;
-
+        }
+        
+        int each_have = n/k;
+        int extra_some = n%k;
+        
+        ptr = head;
+        
+        for(int i=0;i<k;i++){
+            int currLen = each_have;
+            if(extra_some){
+                currLen++;
+                extra_some--;
+            }
+            ans[i] = ptr;
+            ListNode*follow = ptr;
+            while(currLen--){
+                follow = ptr;
+                ptr = ptr->next;
+            }
+            follow->next = NULL;
+        }
+        
+        return ans;
+        
     }
 };
