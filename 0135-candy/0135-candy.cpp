fefa977be8beae8acac249520843614fd2289c4a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-09-13 11:00:02 +0530

    Time: 21 ms (37.33%), Space: 18.2 MB (24.67%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,30 +2,22 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int ans = n;
-        int i = 0;
-        for(i=1;i<n;){
-            if(ratings[i] == ratings[i-1]){
-                i++;
-                continue;
+        vector<int>left(n,1);
+        vector<int>right(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
             }
-            int peak = 0;
-            while(i<n && ratings[i] > ratings[i-1]){
-                peak++;
-                ans += peak;
-                i++;
-                if(i==n) return ans;
-            }
-            int valley = 0;
-            while(i<n && ratings[i] < ratings[i-1]){
-                valley++;
-                ans += valley;
-                i++;
-            }
-            ans -= min(peak,valley);
         }
-        return ans;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                right[i] = right[i+1] + 1;
+            }
+        }
+        int answer = 0;
+        for(int i=0;i<n;i++){
+            answer += max(left[i],right[i]);
+        }
+        return answer;
     }
 };
-
-  
