d282e3465b1b742c926731789591fa8b43d902bc
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-09-14 10:57:12 +0530

    Time: 19 ms (72.51%), Space: 14.9 MB (51.33%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -4,8 +4,8 @@ public:
     unordered_map<string,vector<string>>graph;
     vector<string>path;
     
-    void dfs(string &node){
-        while(graph[node].size() > 0){
+    void dfs(string node){
+        while(graph[node].size()){
             string nbr = graph[node].back();
             graph[node].pop_back();
             dfs(nbr);
@@ -14,16 +14,17 @@ public:
     }
     
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        int n = tickets.size();
-        for(int i=0;i<n;i++){
+        graph.clear();
+        path.clear();
+        for(int i=0;i<tickets.size();i++){
             graph[tickets[i][0]].push_back(tickets[i][1]);
         }
         for(auto &itr : graph){
             sort(itr.second.rbegin(),itr.second.rend());
         }
-        string src = "JFK";
-        dfs(src);
+        dfs("JFK");
         reverse(path.begin(),path.end());
         return path;
     }
 };
+
