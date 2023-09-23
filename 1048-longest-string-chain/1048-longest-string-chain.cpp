3ee848dfe0569cbe999e8de25d6e9b5f2ccb0738
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-09-23 09:47:41 +0530

    Time: 144 ms (39.75%), Space: 51.8 MB (31.77%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool static comp(const string&x, const string&y){
+    bool static comp(const string &x,const string &y){
         return x.length() < y.length();
     }
     
@@ -9,19 +9,13 @@ public:
         int n = words.size();
         sort(words.begin(),words.end(),comp);
         unordered_map<string,int>dp;
+        int maxLen = 0;
         for(int i=0;i<n;i++){
-            dp[words[i]] = 1;
-        }
-        int maxLen = 1;
-        for(int i=1;i<n;i++){
             string str = words[i];
-            int len = str.length();
-            for(int j=0;j<len;j++){
-                string pref = str.substr(0,j);
-                string suff = str.substr(j+1);
-                string curr = pref;
-                curr += suff;
-                dp[str] = max(dp[str], 1 + dp[curr]);
+            for(int j=0;j<str.length();j++){
+                string pstr = str.substr(0,j);
+                pstr += str.substr(j+1);
+                dp[str] = max(dp[str], 1 + dp[pstr]);
                 maxLen = max(maxLen, dp[str]);
             }
         }
