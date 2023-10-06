41e174ecd2c73d61e1eb1a65f3d5e3172bc2054f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-06 10:50:50 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (17.22%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if(n==1) return 1;
+        if(n==1) return 0;
         if(n==2) return 1;
         if(n==3) return 2;
         if(n==4) return 4;
-        if(n==5) return 6;
-        if(n==6) return 9;
-        return 3*integerBreak(n-3);
+        if(n%3==0) return pow(3,n/3);
+        else if(n%3==1) return pow(3,(n-4)/3)*4;
+        else return pow(3,(n-2)/3)*2;
     }
 };
