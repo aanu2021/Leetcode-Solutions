d164e8998ce0c37478f7f0c9c3f305f01d908707
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-08 20:22:44 +0530

    Time: 30 ms (66.32%), Space: 13.6 MB (22.88%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -37,14 +37,14 @@ public:
         }
         dp.clear();
         dp = vector<vector<int>>(m+1,vector<int>(n+1,0));
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                dp[i][j] = (nums1[i]*nums2[j]) + dp[i+1][j+1];
-                dp[i][j] = max(dp[i][j], dp[i+1][j]);
-                dp[i][j] = max(dp[i][j], dp[i][j+1]);
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j] = (nums1[i-1]*nums2[j-1]) + dp[i-1][j-1];
+                dp[i][j] = max(dp[i][j], dp[i-1][j]);
+                dp[i][j] = max(dp[i][j], dp[i][j-1]);
             }
         }
-        return dp[0][0];
+        return dp[m][n];
         // int ans = func(nums1,nums2,0,0,m,n);
         // return ans;
     }
