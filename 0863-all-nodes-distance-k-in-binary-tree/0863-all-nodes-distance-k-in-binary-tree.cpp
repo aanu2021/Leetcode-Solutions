8b8e90a7a6d7ca0641694a54bb49711dd9c349d6
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-10 00:48:12 +0530

    Time: 3 ms (83.71%), Space: 13 MB (78.47%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -3,41 +3,39 @@ public:
     
     vector<int>answer;
     
-    void add_subtree(TreeNode* &root,int k){
+    void add_subtree(TreeNode*& root,int dist){
         if(!root) return;
-        if(k==0){
+        if(dist == 0){
             answer.push_back(root->val);
             return;
         }
-        add_subtree(root->left,k-1);
-        add_subtree(root->right,k-1);
+        add_subtree(root->left,dist-1);
+        add_subtree(root->right,dist-1);
     }
     
-    int traverse(TreeNode* &root,TreeNode* &target,int k){
+    int dfs(TreeNode*& root,TreeNode*& target,int k){
         if(!root) return -1;
-        if(root->val == target->val){
+        if(root == target){
             add_subtree(root,k);
             return k-1;
         }
-        int dist = traverse(root->left,target,k);
+        int dist = dfs(root->left,target,k);
         if(dist > -1){
             if(dist == 0) answer.push_back(root->val);
             add_subtree(root->right,dist-1);
-            return dist - 1;
+            return dist-1;
         }
-        dist = traverse(root->right,target,k);
+        dist = dfs(root->right,target,k);
         if(dist > -1){
             if(dist == 0) answer.push_back(root->val);
             add_subtree(root->left,dist-1);
-            return dist - 1;
+            return dist-1;
         }
         return -1;
     }
     
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        answer.clear();
-        if(!root) return answer;
-        traverse(root,target,k);
+        dfs(root,target,k);
         return answer;
     }
 };
