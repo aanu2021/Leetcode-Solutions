9a8fdcc793fb2190c3c2034c7bf14aff64fdec2f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-11 10:55:01 +0530

    Time: 287 ms (30.67%), Space: 80.5 MB (94.92%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -3,19 +3,18 @@ public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
         int n = flowers.size();
         int m = people.size();
-        priority_queue<int,vector<int>,greater<int>>pq;
-        vector<int>answer(m,0);
         vector<pair<int,int>>vec(m);
         for(int i=0;i<m;i++){
             vec[i] = {people[i],i};
         }
-        sort(flowers.begin(),flowers.end());
         sort(vec.begin(),vec.end());
+        sort(flowers.begin(),flowers.end());
+        vector<int>answer(m,0);
         int j = 0;
+        priority_queue<int,vector<int>,greater<int>>pq;
         for(int i=0;i<m;i++){
             while(j<n && flowers[j][0] <= vec[i].first){
-                pq.push(flowers[j][1]);
-                j++;
+                pq.push(flowers[j][1]); j++;
             }
             while(!pq.empty() && pq.top() < vec[i].first){
                 pq.pop();
