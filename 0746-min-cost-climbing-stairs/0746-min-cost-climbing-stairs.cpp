4444e7c89da5745b1daf3fdd39ae96db83e4cf8b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-13 10:57:37 +0530

    Time: 4 ms (61.59%), Space: 14 MB (26.23%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int>dp(n+2,1e9);
+        dp[n] = 0;
+        dp[n+1] = 0;
+        for(int i=n-1;i>=0;i--){
+            dp[i] = cost[i] + min(dp[i+1], dp[i+2]);
+        }
+        return min(dp[0], dp[1]);
+    }
+};
