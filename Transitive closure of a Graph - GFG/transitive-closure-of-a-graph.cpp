e0563437c2b2542500ba65b70d392c6ce8382856
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-17 00:49:46 +0530

    Added solution - LeetHub

---
+++ Transitive closure of a Graph - GFG/transitive-closure-of-a-graph.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// Back-end complete function Template for C++
+
+class Solution{
+public:
+    vector<vector<int>> transitiveClosure(int n, vector<vector<int>> grid)
+    {
+        
+        for(int i=0;i<n;i++){
+            grid[i][i] = 1;
+        }
+        
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(grid[i][k] && grid[k][j]){
+                        grid[i][j] = 1;
+                    }
+                }
+            }
+        }
+        return grid;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<vector<int>> graph(N, vector<int>(N, -1));
+        for(int i = 0;i < N;i++)
+            for(int j=0;j<N;j++)
+            cin>>graph[i][j];
+        
+        Solution ob;
+        vector<vector<int>> ans = ob.transitiveClosure(N, graph);
+        for(int i = 0;i < N;i++)
+            {for(int j = 0;j < N;j++)
+                cout<<ans[i][j]<<" ";
+        cout<<"\n";}
+    }
+    return 0;
+}
+// } Driver Code Ends
