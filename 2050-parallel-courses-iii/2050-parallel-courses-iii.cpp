a4297b46b9b335c05660b1def8cbccbe58a3d75f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-18 20:16:56 +0530

    Time: 290 ms (96.22%), Space: 132 MB (71.58%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -1,68 +1,32 @@
 class Solution {
 public:
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
-        
-        vector<int>dist(n+1,0);
-        
-        vector<vector<int>>graph(n+1);
-        
-        vector<int>indegree(n+1,0);
-        
-        
-        for(int i=0;i<relations.size();++i){
-            
-            graph[relations[i][0]].push_back(relations[i][1]);
-            
-            indegree[relations[i][1]]++;
-            
-        }
-        
-        
+        vector<int>indegree(n,0);
+        vector<int>dist(n,0);
         queue<int>q;
-        
-        
-        for(int i=1;i<=n;++i){
-            
-            if(indegree[i]==0){
-                
-                dist[i]=time[i-1];
-                
-                q.push(i);
-                
-            }
-            
+        vector<vector<int>>graph(n);
+        for(int i=0;i<relations.size();i++){
+            int u = relations[i][0];
+            int v = relations[i][1];
+            u--; v--;
+            graph[u].push_back(v);
+            indegree[v]++;
         }
-        
-        
-        // for(int i=1;i<=n;++i){
-        //     cout<<dist[i]<<" ";
-        // }cout<<"\n";
-        
-        
+        for(int i=0;i<n;i++){
+            dist[i] = time[i];
+            if(indegree[i]==0) q.push(i);
+        }
         while(!q.empty()){
-            
-            int node=q.front();
+            int node = q.front();
             q.pop();
-            
-            for(int nbr:graph[node]){
-                
-                dist[nbr]=max(dist[nbr],time[nbr-1]+dist[node]);
-                
+            for(int &nbr : graph[node]){
+                dist[nbr] = max(dist[nbr], dist[node] + time[nbr]);
                 indegree[nbr]--;
-                
                 if(indegree[nbr]==0){
-                    
                     q.push(nbr);
-                    
                 }
-                
             }
-            
         }
-        
-        int maxi=*max_element(dist.begin(),dist.end());
-        
-        return maxi;
-        
+        return *max_element(dist.begin(),dist.end());
     }
 };
