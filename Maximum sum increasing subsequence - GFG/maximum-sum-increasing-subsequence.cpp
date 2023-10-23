4ed2d165eba799432226d6ac89bdbc86a8a2ca3c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-23 09:23:19 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -12,8 +12,6 @@ class Solution{
 	    vector<int>dp(n,0);
 	    for(int i=0;i<n;i++){
 	        dp[i] = arr[i];
-	    }
-	    for(int i=1;i<n;i++){
 	        for(int j=0;j<i;j++){
 	            if(arr[i] > arr[j]){
 	                dp[i] = max(dp[i], dp[j] + arr[i]);
