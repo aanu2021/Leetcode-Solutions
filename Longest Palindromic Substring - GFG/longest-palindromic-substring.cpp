b64d96f54b494e73d3f83e5c9792e00a7b0b81dd
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-10-27 13:56:38 +0530

    Added solution - LeetHub

---
+++ Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+
+    vector<int>findLPS(string s){
+        int n = s.length();
+        s = "#" + s + "*";
+        vector<int>p(n+2, 0);
+        int l = 1, r = 1;
+        for(int i=1;i<=n;i++){
+            p[i] = max(0, min(r-i, p[l+(r-i)]));
+            while(s[i-p[i]] == s[i+p[i]]){
+                p[i]++;
+            }
+            if(i + p[i] > r){
+                l = i - p[i];
+                r = i + p[i];
+            }
+        }
+        return p;
+    }
+
+    string longestPalindrome(string s){
+        int n = s.length();
+        int startIdx = 0, maxLen = 0;
+        vector<int>a = findLPS(s);
+        for(int i=0;i<a.size();i++){
+            int currLen = 2*a[i] - 1;
+            if(currLen > maxLen){
+                maxLen = currLen;
+                startIdx = i-a[i];
+            }
+        }
+        string t = "?";
+        for(int i=0;i<s.length();i++){
+            t += s[i];
+            t += "?";
+        }
+        vector<int>b = findLPS(t);
+        for(int i=0;i<b.size();i++){
+            int currLen = (b[i]/2)*2;
+            if(i%2 && currLen > maxLen){
+                maxLen = currLen;
+                startIdx = (i/2) - (currLen/2);
+            }
+        }
+        return s.substr(startIdx, maxLen);
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.longestPalindrome(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
