dd34784a9ada7d81642ad6ee50c795d33c4aa241
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-11-13 23:58:00 +0530

    Time: 6 ms (76.39%), Space: 39.4 MB (96.56%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -2,10 +2,10 @@ class Solution {
     
     public boolean isSafe(char board[][],int row,int col,char num){
         for(int i=0;i<9;i++){
-            if(board[i][col] == num) return false;
+            if(board[row][i] == num) return false;
         }
-        for(int j=0;j<9;j++){
-            if(board[row][j] == num) return false;
+        for(int i=0;i<9;i++){
+            if(board[i][col] == num) return false;
         }
         int smi = (row/3)*3;
         int smj = (col/3)*3;
@@ -22,7 +22,7 @@ class Solution {
             for(int j=0;j<9;j++){
                 if(board[i][j] == '.'){
                     for(char num='1';num<='9';num++){
-                        if(isSafe(board,i,j,num)){
+                        if(isSafe(board,i,j,num) == true){
                             board[i][j] = num;
                             if(solve(board) == true) return true;
                             board[i][j] = '.';
