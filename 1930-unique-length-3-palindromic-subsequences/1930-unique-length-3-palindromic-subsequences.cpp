d3c2d2874dd6c390e3ec3c513521f656b0a32fb5
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-11-14 11:20:47 +0530

    Time: 44 ms (95.89%), Space: 23.3 MB (20.32%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -2,38 +2,31 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         int n = s.length();
-        vector<vector<int>>prefix(n,vector<int>(26,0));
-        vector<vector<int>>suffix(n,vector<int>(26,0));
-        vector<vector<int>>dp(26,vector<int>(26,0));
-        for(int i=0;i<n;i++){
-            prefix[i][s[i]-'a']++;
-            suffix[i][s[i]-'a']++;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<26;j++){
-                prefix[i][j] += prefix[i-1][j];
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<26;j++){
-                suffix[i][j] += suffix[i+1][j];
-            }
-        }
-        for(int i=0;i<n;i++){
-            char ch = s[i];
-            int x = (int)(ch-'a');
-            for(int j=0;j<26;j++){
-                int leftOcc = (i-1 >= 0 ? prefix[i-1][j] : 0);
-                int rightOcc = (i+1 < n ? suffix[i+1][j] : 0);
-                if(leftOcc > 0 && rightOcc > 0){
-                    dp[x][j]++;
-                }
-            }
-        }
         int answer = 0;
-        for(int i=0;i<26;i++){
-            for(int j=0;j<26;j++){
-                if(dp[i][j]) answer++;
+        unordered_map<char,vector<int>>indexMap;
+        for(int i=0;i<n;i++){
+            indexMap[s[i]].push_back(i);
+        }
+        for(char c1='a';c1<='z';c1++){
+            for(char c2='a';c2<='z';c2++){
+                if(c1 == c2){
+                    if(indexMap[c1].size() >= 3){
+                        answer++;
+                    }
+                }
+                else{
+                    if(indexMap[c1].size() < 2 || indexMap[c2].size() == 0) continue;
+                    int sz = indexMap[c1].size();
+                    int firstIndex = indexMap[c1][0];
+                    int lastIndex  = indexMap[c1][sz-1];
+                    int idx = lower_bound(indexMap[c2].begin(),indexMap[c2].end(),firstIndex) - indexMap[c2].begin();
+                    if(idx < indexMap[c2].size()){
+                        int indexHave = indexMap[c2][idx];
+                        if(indexHave > firstIndex && indexHave < lastIndex){
+                            answer++;
+                        }
+                    }
+                }
             }
         }
         return answer;
