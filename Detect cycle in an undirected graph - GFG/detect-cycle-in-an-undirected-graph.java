4115eeeab9235fe8289b6655cea84d9451a2160c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-11-16 11:01:32 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -32,6 +32,15 @@ class GFG {
 // } Driver Code Ends
 
 
+class Pair{
+    int node;
+    int parent;
+    Pair(int node, int parent){
+        this.node = node;
+        this.parent = parent;
+    }
+}
+
 class Solution {
     // Function to detect cycle in an undirected graph.
     
@@ -46,12 +55,35 @@ class Solution {
         return false;
     }
     
-    public boolean isCycle(int n, ArrayList<ArrayList<Integer>> adj) {
+    public boolean isCyclic2(int n, ArrayList<ArrayList<Integer>> adj){
         boolean visited[] = new boolean[n];
         for(int i=0;i<n;i++){
             if(visited[i] == true) continue;
-            if(isCyclic(i,-1,adj,visited) == true) return true;
+            Queue<Pair>q = new LinkedList<>();
+            q.add(new Pair(i,-1));
+            visited[i] = true;
+            while(!q.isEmpty()){
+                int node = q.peek().node;
+                int parent = q.peek().parent;
+                q.remove();
+                for(int nbr : adj.get(node)){
+                    if(nbr == parent) continue;
+                    if(visited[nbr] == true) return true;
+                    visited[nbr] = true;
+                    q.add(new Pair(nbr,node));
+                }
+            }
         }
         return false;
+    }
+    
+    public boolean isCycle(int n, ArrayList<ArrayList<Integer>> adj) {
+        return isCyclic2(n,adj);
+        // boolean visited[] = new boolean[n];
+        // for(int i=0;i<n;i++){
+        //     if(visited[i] == true) continue;
+        //     if(isCyclic(i,-1,adj,visited) == true) return true;
+        // }
+        // return false;
     }
 }
