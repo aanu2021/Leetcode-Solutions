d18cfc7417738d345a7ef68f33a413c6bc46ae1d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-11-16 10:53:43 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    
+    public boolean isCyclic(int u,int p,ArrayList<ArrayList<Integer>> adj, boolean visited[]){
+        visited[u] = true;
+        for(int v : adj.get(u)){
+            if(v == p) continue;
+            if(visited[v] == true) return true;
+            boolean flag = isCyclic(v,u,adj,visited);
+            if(flag) return true;
+        }
+        return false;
+    }
+    
+    public boolean isCycle(int n, ArrayList<ArrayList<Integer>> adj) {
+        boolean visited[] = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(visited[i] == true) continue;
+            if(isCyclic(i,-1,adj,visited) == true) return true;
+        }
+        return false;
+    }
+}
