8bc655e88ed1f46905e85d246b37de954b637348
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-11-18 10:31:23 +0530

    Time: 175 ms (41.56%), Space: 99.2 MB (85.20%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -3,6 +3,12 @@ public:
     
     typedef long long ll;
     
+    ll findOps(vector<int>&nums,int l,int r,ll currSum){
+        ll ops = (ll)((ll)nums[r]*(ll)(r-l+1));
+        ops -= currSum;
+        return ops;
+    }
+    
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
@@ -10,7 +16,7 @@ public:
         ll currSum = 0LL;
         while(r < n){
             currSum += (ll)nums[r];
-            while(l <= r && (((ll)nums[r]*(ll)(r-l+1))- currSum) > k){
+            while(l <= r && findOps(nums,l,r,currSum) > k){
                 currSum -= (ll)nums[l];
                 l++;
             }
