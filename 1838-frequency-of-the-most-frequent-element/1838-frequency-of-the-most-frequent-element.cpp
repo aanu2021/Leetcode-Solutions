097c6d3de95771fe4ea6c49e7c562194ebbb2c92
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-11-18 10:30:12 +0530

    Time: 180 ms (27.72%), Space: 99.5 MB (25.44%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -6,35 +6,17 @@ public:
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        int maxFreq = 0;
-        vector<ll>prefix(n,0LL);
-        prefix[0] = (ll)nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1] + (ll)nums[i];
-        }
-        for(int i=0;i<n;i++){
-            int j = i;
-            while(j<n && nums[j] == nums[i]){
-                j++;
+        int l = 0, r = 0, maxLen = 0;
+        ll currSum = 0LL;
+        while(r < n){
+            currSum += (ll)nums[r];
+            while(l <= r && (((ll)nums[r]*(ll)(r-l+1))- currSum) > k){
+                currSum -= (ll)nums[l];
+                l++;
             }
-            j--;
-            int currFreq = 0;
-            int low = 0, high = j;
-            while(low <= high){
-                int mid = (low + high)/2;
-                ll currSum = prefix[j] - (mid - 1 >= 0 ? prefix[mid-1] : 0LL);
-                ll reqOps = ((ll)(j-mid+1)*(ll)nums[j]) - currSum;
-                if(reqOps <= k){
-                    currFreq = (j-mid+1);
-                    high = mid - 1;
-                }
-                else{
-                    low = mid + 1;
-                }
-            }
-            maxFreq = max(maxFreq, currFreq);
-            i = j;
+            maxLen = max(maxLen, r - l + 1);
+            r++;
         }
-        return maxFreq;
+        return maxLen;
     }
 };
