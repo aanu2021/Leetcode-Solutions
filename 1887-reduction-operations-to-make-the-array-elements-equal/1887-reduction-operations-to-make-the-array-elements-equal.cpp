331f67c4f01a68f36dae9bf9332e7c927436eedd
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-11-19 11:00:26 +0530

    Time: 214 ms (46.93%), Space: 83.4 MB (52.76%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -5,17 +5,7 @@ public:
         sort(nums.rbegin(),nums.rend());
         int answer = 0;
         int prevCnt = 0;
-        int startIdx = n;
         for(int i=0;i<n;i++){
-            if(nums[i] == nums[0]){
-                prevCnt++;
-            }
-            else{
-                startIdx = i;
-                break;
-            }
-        }
-        for(int i=startIdx;i<n;i++){
             answer += prevCnt;
             int j = i;
             while(j<n && nums[j]==nums[i]){
