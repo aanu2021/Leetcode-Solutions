88727ef013edd5fe28c025b30f3912515ab69599
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-12-09 11:04:03 +0530

    Time: 4 ms (33.83%), Space: 8.6 MB (81.67%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,64 +1,16 @@
 class Solution {
 public:
     
-    vector<int>answer;
-    
-    void inorder(TreeNode* &root){
+    void inorder(TreeNode* root,vector<int>& answer){
         if(!root) return;
-        inorder(root->left);
+        inorder(root->left, answer);
         answer.push_back(root->val);
-        inorder(root->right);
-    }
-    
-    void inorder2(TreeNode* &root){
-        stack<TreeNode*>S;
-        TreeNode*node = root;
-        while(node){
-            S.push(node);
-            node = node->left;
-        }
-        while(!S.empty()){
-            auto curr = S.top(); S.pop();
-            answer.push_back(curr->val);
-            if(curr->right){
-                curr = curr->right;
-                while(curr){
-                    S.push(curr);
-                    curr = curr->left;
-                }
-            }
-        }
-    }
-    
-    void inorder3(TreeNode* &root){
-        TreeNode*node = root;
-        while(node){
-            if(!node->left){
-                answer.push_back(node->val);
-                node = node->right;
-            }
-            else{
-                TreeNode * curr = node->left;
-                while(curr && curr->right && curr->right != node){
-                    curr = curr->right;
-                }
-                if(!curr->right){
-                    curr->right = node;
-                    node = node->left;
-                }
-                else{
-                    answer.push_back(node->val);
-                    curr->right = NULL;
-                    node = node->right;
-                }
-            }
-        }
+        inorder(root->right, answer);
     }
     
     vector<int> inorderTraversal(TreeNode* root) {
-        if(!root) return {};
-        answer.clear();
-        inorder3(root);
+        vector<int>answer;
+        inorder(root,answer);
         return answer;
     }
 };
