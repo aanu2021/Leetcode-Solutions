68c802f2c163fa8eff5c1d33b048737abc2ba389
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-12-21 20:04:48 +0530

    Time: 16 ms (58.66%), Space: 17.6 MB (90.17%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,51 +1,11 @@
 class Solution {
 public:
-    
-    // O(N) Time 
-    // O(N) Space
-    
-    /*
-    int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int>left(n,1);
-        vector<int>right(n,1);
-        for(int i=1;i<n;i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i] = left[i-1] + 1;
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i] > ratings[i+1]){
-                right[i] = right[i+1] + 1;
-            }
-        }
-        int answer = 0;
-        for(int i=0;i<n;i++){
-            answer += max(left[i],right[i]);
-        }
-        return answer;
-    }
-    */
-    
-    // O(N) Time
-    // O(1) Space
-    
-    /*
-    
-    1. Peak calculation is important.
-    2. peak = max(left_peak,right_valley)
-    3. valley elements have exactly 1 candy
-    4. When two equal elements are there, both resets to 1
-    5. In our algorithm peak and valley values are calculated twice,        therefore we need to subtract the minimum among them.
-    
-    */
-    
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         int answer = n;
         int i = 1;
         for(;i<n;){
-            if(ratings[i]==ratings[i-1]){
+            if(ratings[i] == ratings[i-1]){
                 i++;
                 continue;
             }
@@ -54,7 +14,7 @@ public:
                 peak++;
                 answer += peak;
                 i++;
-                if(i==n) return answer;
+                if(i == n) return answer;
             }
             int valley = 0;
             while(i<n && ratings[i] < ratings[i-1]){
@@ -67,3 +27,5 @@ public:
         return answer;
     }
 };
+
+
