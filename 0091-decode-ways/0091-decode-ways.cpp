17c0824f96336d627052db0531e5d0b8a48b2695
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2023-12-25 11:31:43 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (44.81%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.length();
-        if(n==0 || s[0]=='0') return 0;
+        if(s[0] == '0') return 0;
         vector<int>dp(n+1,0);
         dp[0] = 1;
         s = "*" + s;
         for(int i=1;i<=n;i++){
-            char curr = s[i];
-            char prev = s[i-1];
-            if(curr>'0') dp[i]+=dp[i-1];
-            if(prev=='1' || (prev=='2' && curr<='6')){
-                dp[i]+=dp[i-2];
+            if(s[i] >= '1' && s[i] <= '9'){
+                dp[i] += dp[i-1];
+            }
+            if(i-2 >= 0){
+                if((s[i-1] == '1' && (s[i] >= '0' && s[i] <= '9')) || (s[i-1] == '2' && (s[i] >= '0' && s[i] <= '6'))){
+                    dp[i] += dp[i-2];
+                }
             }
         }
         return dp[n];
