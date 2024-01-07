5f0e5846df0a03d8f975d0a6e7c83fe53149d6a7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-07 11:48:14 +0530

    Time: 1052 ms (31.38%), Space: 285.5 MB (12.78%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -4,36 +4,39 @@ public:
     typedef long long ll;
     
     int numberOfArithmeticSlices(vector<int>& nums) {
-        
         int n = nums.size();
-        
-        vector<unordered_map<ll,ll>>dp(n);
-        
-        ll answer = 0LL;
-        
+        vector<unordered_map<ll,int>>dp(n);
+        int answer = 0;
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                ll diff = (ll)nums[i] - (ll)nums[j];
-                int cnt = 0;
+                ll diff = (ll)nums[j] - (ll)nums[i];
                 if(dp[j].find(diff) != dp[j].end()){
-                    cnt = dp[j][diff];
+                    answer += dp[j][diff];
                 }
-                answer += cnt;
-                dp[i][diff] += (ll)(1 + cnt);
+                dp[i][diff] += (1 + dp[j][diff]);
             }
         }
-        
-        return (int)answer;
-        
+        return answer;
     }
 };
 
 /*
 
 2 4 6 8 10
+0 1 2 3  4
     
-atleast three elements  
-
-dp[i][diff] = number of arithmetic subsequence ending at index i, 
+dp[1][2] = 1
+dp[2][2] = 2    
+dp[2][4] = 1    
+dp[3][2] = 3 
+dp[3][4] = 1
+dp[3][6] = 1 
+dp[4][2] = 4 
+dp[4][4] = 2 
+dp[4][6] = 1
+dp[4][8] = 1    
+    
+answer = 1 + 2 + 3 + 1 = 7 
 
 */
+    
