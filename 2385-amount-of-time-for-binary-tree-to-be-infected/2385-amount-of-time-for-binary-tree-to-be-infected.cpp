e21ee0ccc77802593f268f1aec10020319ffff5b
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-10 12:36:46 +0530

    Time: 129 ms (96.40%), Space: 93.4 MB (97.80%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -4,8 +4,8 @@ public:
     void add_subtree(TreeNode* root, int lvl, int &maxTimer){
         if(!root) return;
         maxTimer = max(maxTimer, lvl);
-        add_subtree(root->left,lvl+1,maxTimer);
-        add_subtree(root->right,lvl+1,maxTimer);
+        add_subtree(root->left, lvl + 1, maxTimer);
+        add_subtree(root->right, lvl + 1, maxTimer);
     }
     
     int dfs(TreeNode* root, int start, int &maxTimer){
@@ -17,13 +17,13 @@ public:
         int dist = dfs(root->left, start, maxTimer);
         if(dist > -1){
             maxTimer = max(maxTimer, dist);
-            add_subtree(root->right, dist+1, maxTimer);
+            add_subtree(root->right, dist + 1, maxTimer);
             return dist + 1;
         }
         dist = dfs(root->right, start, maxTimer);
         if(dist > -1){
             maxTimer = max(maxTimer, dist);
-            add_subtree(root->left, dist+1, maxTimer);
+            add_subtree(root->left, dist + 1, maxTimer);
             return dist + 1;
         }
         return -1;
@@ -31,7 +31,7 @@ public:
     
     int amountOfTime(TreeNode* root, int start) {
         int maxTimer = 0;
-        dfs(root,start,maxTimer);
+        dfs(root, start, maxTimer);
         return maxTimer;
     }
 };
