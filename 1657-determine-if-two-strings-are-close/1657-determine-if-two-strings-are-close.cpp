8fd9d59fbc1b7b718677aa5b2f5ac111add6b37f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-14 17:25:35 +0530

    Time: 66 ms (90.42%), Space: 21 MB (78.40%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.length()!=word2.length()) return false;
-        vector<int>freq1(26,0);
-        vector<int>freq2(26,0);
-        for(char ch:word1){
+        if(word1.length() != word2.length()) return false;
+        vector<int>freq1(26, 0);
+        vector<int>freq2(26, 0);
+        for(char ch : word1){
             freq1[ch-'a']++;
         }
-        for(char ch:word2){
+        for(char ch : word2){
             freq2[ch-'a']++;
         }
-        vector<int>v1,v2;
+        vector<int>v1, v2;
         for(int i=0;i<26;i++){
-            if(freq1[i]==0 && freq2[i]==0) continue;
-            if(freq1[i]==0 || freq2[i]==0) return false;
+            if(freq1[i] == 0 && freq2[i] == 0) continue;
+            if(freq1[i] == 0 || freq2[i] == 0) return false;
             v1.push_back(freq1[i]);
             v2.push_back(freq2[i]);
         }
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-        for(int i=0;i<v1.size();i++){
-            if(v1[i]!=v2[i]) return false;
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
+        for(int i=0;i<freq1.size();i++){
+            if(freq1[i] != freq2[i]) return false;
         }
         return true;
     }
 };
+
+
+
