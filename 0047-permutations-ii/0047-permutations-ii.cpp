3aed3fa8db1d6c7dedcdf5fe7a2e305f1899e4a9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-17 10:10:14 +0530

    Time: 3 ms (93.71%), Space: 11.4 MB (32.59%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     
-    vector<vector<int>>answer;
-    
-    void recursion(vector<int>&nums,int index,int n){
-        if(index==n){
+    void func(vector<int>& nums, int index, int n, vector<vector<int>>& answer){
+        if(index == n){
             answer.push_back(nums);
             return;
         }
@@ -12,19 +10,17 @@ public:
         for(int i=index;i<n;i++){
             if(S.find(nums[i]) != S.end()) continue;
             S.insert(nums[i]);
-            swap(nums[i],nums[index]);
-            recursion(nums,index+1,n);
-            swap(nums[i],nums[index]);
+            swap(nums[i], nums[index]);
+            func(nums, index+1, n, answer);
+            swap(nums[i], nums[index]);
         }
     }
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
-        answer.clear();
+        vector<vector<int>>answer;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        recursion(nums,0,n);
+        func(nums,0,n,answer);
         return answer;
-        
     }
 };
