ed0f8344934bcc470636f514210a53135a64040f
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-18 09:16:46 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (88.87%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1 || n == 2) return n;
+        int a = 1, b = 2;
+        for(int i=3;i<=n;i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
