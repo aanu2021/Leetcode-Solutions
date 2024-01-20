c688eff0ef42cda469eb3ca94dfb45c5a7778740
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-20 09:06:41 +0530

    Time: 60 ms (94.07%), Space: 45.2 MB (14.28%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -2,55 +2,43 @@ class Solution {
 public:
     
     typedef long long ll;
-    
     const ll M = 1e9 + 7;
-    
-    ll mod(ll a){
-        return ((a%M)+M)%M;
-    }
-    
-    ll mul(ll a,ll b){
-        return mod(mod(a)*mod(b));
-    }
-    
-    ll add(ll a,ll b){
-        return mod(mod(a)+mod(b));
-    }
     
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-        vector<int>left(n,-1);
-        vector<int>right(n,n);
+        vector<int>ps(n, -1);
+        vector<int>ns(n, n);
         stack<int>S;
         for(int i=0;i<n;i++){
-            while(!S.empty() && arr[S.top()]>=arr[i]){
+            while(!S.empty() && arr[S.top()] > arr[i]){
                 S.pop();
             }
-            if(!S.empty()){
-                left[i] = S.top();
-            }
+            if(!S.empty()) ps[i] = S.top();
             S.push(i);
         }
-        while(!S.empty()){
-            S.pop();
-        }
+        while(!S.empty()) S.pop();
         for(int i=n-1;i>=0;i--){
-            while(!S.empty() && arr[S.top()]>arr[i]){
+            while(!S.empty() && arr[S.top()] >= arr[i]){
                 S.pop();
             }
-            if(!S.empty()){
-                right[i] = S.top();
-            }
+            if(!S.empty()) ns[i] = S.top();
             S.push(i);
         }
-        ll ans = 0LL;
+        ll total = 0LL;
         for(int i=0;i<n;i++){
-            ll l=(ll)(i-left[i]);
-            ll r=(ll)(right[i]-i);
-            ll muli = mul(l,r);
-            ll curr = mul(muli,(ll)arr[i]);
-            ans = add(ans,curr);
+            ll left = (ll)(i-ps[i]);
+            ll right = (ll)(ns[i]-i);
+            total += (left*right*(ll)arr[i]);
+            total %= M;
         }
-        return ans;
+        return total;
     }
 };
+
+
+     
+     
+     
+     
+     
+    
