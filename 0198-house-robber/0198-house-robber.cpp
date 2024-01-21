d1156bc29d007862726d5f6db80fb9dc400963f4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-21 08:36:12 +0530

    Time: 3 ms (37.68%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,12 +3,13 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int>dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int first = nums[0];
+        int second = max(nums[0], nums[1]);
         for(int i=2;i<n;i++){
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+            int third = max(second, first + nums[i]);
+            first = second;
+            second = third;
         }
-        return dp[n-1];
+        return second;
     }
 };
