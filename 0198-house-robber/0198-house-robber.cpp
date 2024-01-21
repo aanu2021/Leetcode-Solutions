31bf7f5423387aba99a5968486e75a003a4e0170
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-21 08:34:34 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,40 +1,14 @@
 class Solution {
 public:
-    
-    int func(vector<int>&nums,int i,int n,vector<int>&dp){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int ans = func(nums,i+1,n,dp);
-        ans = max(ans,nums[i]+func(nums,i+2,n,dp));
-        return dp[i] = ans;
-    }
-    
-    int rob2(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n,-1);
-        int ans = func(nums,0,n,dp);
-        return ans;
-    }
-    
-    int rob3(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n+2,0);
-        for(int i=n-1;i>=0;i--){
-            dp[i] = max(dp[i+1],nums[i]+dp[i+2]);
-        }
-        return dp[0];
-    }
-    
     int rob(vector<int>& nums) {
         int n = nums.size();
-        int prev = 0,prev_prev = 0;
-        for(int i=n-1;i>=0;i--){
-            int skip = prev;
-            int pick = prev_prev+nums[i];
-            prev_prev = prev;
-            prev = max(skip,pick);
+        if(n == 1) return nums[0];
+        vector<int>dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
         }
-        return prev;
+        return dp[n-1];
     }
-    
 };
