ef8ad076599567622cf1fbcf573d03c385257678
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-01-27 19:18:51 +0530

    Time: 666 ms (51.50%), Space: 256.8 MB (17.50%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
+    
+    int calculateSum(vector<vector<int>>&grid, int row, int c1, int c2)
+    {
+        return (grid[row][c2] - (c1 > 0 ? grid[row][c1-1] : 0));
+    }
+    
     int numSubmatrixSumTarget(vector<vector<int>>& grid, int target) {
         
-        int m=grid.size();
-        int n=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         
         for(int i=0;i<m;i++){
-            
             for(int j=1;j<n;j++){
-                
-                grid[i][j]+=grid[i][j-1];
-                
+                grid[i][j] += grid[i][j-1];
             }
-            
         }
         
-        int ans=0;
-        
+        int answer = 0;
         for(int c1=0;c1<n;c1++){
-            
             for(int c2=c1;c2<n;c2++){
-                
-                map<int,int>mp;
-                
-                mp[0]=1;
-                
-                int sum=0;
-                
+                unordered_map<int,int>freq;
+                freq[0] = 1;
+                int currSum = 0;
                 for(int row=0;row<m;row++){
-                    
-                    sum+=grid[row][c2]-(c1-1>=0 ? grid[row][c1-1] : 0);
-                    
-                    if(mp.find(sum-target)!=mp.end()){
-                        
-                        ans+=mp[sum-target];
-                        
-                    }
-                    
-                    mp[sum]++;
-                    
+                    currSum += calculateSum(grid,row,c1,c2);
+                    answer += freq[currSum-target];
+                    freq[currSum]++;
                 }
-                
             }
-            
         }
-        
-        return ans;
-        
+        return answer;
     }
 };
+
+   
