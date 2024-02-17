9054e234cb8c5bc4100aefbc588ddf3c9efa9d12
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-02-17 09:55:00 +0530

    Time: 58 ms (98.91%), Space: 57.5 MB (30.10%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,117 +1,40 @@
-    
-/*
-
-
- Well , the initial approach that hits our brain , is to use the 
- bricks first to jump from i-th building to (i+1)-th building , 
- and when we have no such sufficient brocks to make the current 
- jump to the next building , then use the ladder for make these 
- possible.
- 
- Wait wait wait , don't start to code , first think deeply is the
- above mentioned algo full proof for all the cases or not .
- 
- Brain-Storming Example : [1,5,4,6,8]
- 
-                          bricks = 4   ladder = 1
-                          
- Try to do a dry run on these example with the discussed algo ,
- what we will figure out that it is not optimal forever , it might 
- happen then if we use the ladders first , then we are left with 
- sufficient amount of bricks , which will help to make maximum 
- number of jumps in future.
- 
- Hmmmmmm , quite tricky na
- 
- Let's try to make some observations , 
- 
- Obs 1. Ladder is independent of height difference b/w two 
- consecutive buildings , i.e if we have atleast one ladder as of 
- now , we can jump a height of 1 unit , 10 unit , 15 unit , 
- ......., 1000 unit and so on.
- 
- Obs 2. But bricks count is bounded and we need to spend them 
- greedily. 
- 
- Obs 3. What if we use our ladder(is we have currently) for those 
- consecutive height differences , which are larger ones 
- comparatably than the others.
- 
- Like , Height Differences are given 4,6,3,7,9  and we have ladder
- count 2 initially , then it is more optimal to use that for first 
- two maximum differences , like for prefix [4,6,3] maximum ones 
- are [6 and 9] , in the same manner for the prefix [4,6,3,7] 2 
- (ladder_cnt) Maximum ones are [6 and 7].
- 
- 
- 
- Algorithm :
- 
- We will initially use those ladders as long as possible , then 
- the time will appear , when we have zero numbers of ladders , we 
- have only option left , i.e to use the bricks but before doing so
- , we will check whether the current height difference is the not
- among the ladder_cnt Maximum differences till the current prefix 
- or not.
- 
- For maintaing the previous differences from minimum to 
- maximum(sorted order), the DS that we will use is Minheap / 
- Priority Queue.
- 
-
-Approach : Greedy
- 
-T.C : O(N*logN)
-S.C : O(N)
-
-
-*/
-    
-
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladder) {
-        
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int n = heights.size();
-        priority_queue<int,vector<int>,greater<int>>pq;
         int i = 0;
+        priority_queue<int>pq;
         for(i=0;i<n-1;){
-            int curr_height_diff = heights[i+1] - heights[i];
-            if(curr_height_diff <= 0){
-                i++;
-                continue;
+            int diff = heights[i+1] - heights[i];
+            if(diff < 0){
+                i++; continue;
+            }
+            if(bricks >= diff){
+                bricks -= diff;
+                pq.push(diff);
+                i++; continue;
             }
             else{
-                pq.push(curr_height_diff);
-                if(pq.size() <= ladder){
-                    i++;
+                if(ladders > 0){
+                    if(!pq.empty() && pq.top() > diff){
+                        bricks += pq.top();
+                        pq.pop();
+                        bricks -= diff;
+                        pq.push(diff);
+                    }
+                    ladders--;
+                    i++; continue;
                 }
                 else{
-                    if(pq.size() > ladder && pq.top() <= bricks){
-                        bricks -= pq.top();
-                        pq.pop();
-                        i++;
-                    }
-                    else{
-                        return i;
-                    }
+                    break;
                 }
             }
         }
-        
-        return n-1;
-        
+        return i;
     }
 };
 
-// 4 2 7 6 9 14 12
-// 0 1 2 3 4 5  6
+
+
     
-// i = 4
-// htd = 5
-    
-// ladders = 1
-// bricks = 2    
-    
-// pq = { 5 }    
     
