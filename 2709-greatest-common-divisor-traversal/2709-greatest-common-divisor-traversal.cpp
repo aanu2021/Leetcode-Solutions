80ce0c848aec5e8de17797220a87e79d28b19caf
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-02-25 11:00:39 +0530

    Time: 475 ms (67.07%), Space: 173.5 MB (43.71%) - LeetHub

--- 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
@@ -10,7 +10,7 @@ class DSU{
              Parent[i] = i;
              Rank[i] = 1;
          }
-     }  
+     }
      int find(int x){
          if(Parent[x] == x) return x;
          return Parent[x] = find(Parent[x]);
@@ -27,47 +27,38 @@ class DSU{
                  if(Rank[lx] == Rank[ly]) Rank[lx]++;
              }
          }
-     } 
-    
+     }
 };
 
 class Solution {
 public:
     bool canTraverseAllPairs(vector<int>& nums) {
         int n = nums.size();
+        int MAX = *max_element(nums.begin(),nums.end());
         unordered_map<int,int>freq;
-        unordered_map<int,int>indexMap;
-        for(int &ele : nums) freq[ele]++;
+        for(int i=0;i<n;i++) freq[nums[i]]++;
         if(freq[1] > 0){
-            if(n==1) return true;
+            if(n == 1) return true;
             else return false;
         }
-        vector<int>vec;
-        for(auto &itr : freq) if(itr.second > 0) vec.push_back(itr.first);
-        for(int i=0;i<vec.size();i++){
-            indexMap[vec[i]] = i;
-        }
-        n = vec.size();
-        int MAX = *max_element(vec.begin(),vec.end());
-        DSU obj(n);
+        DSU obj(MAX+1);
         for(int i=2;i<=MAX;i++){
-            int pidx = -1;
+            int leader = -1;
             for(int j=i;j<=MAX;j+=i){
                 if(freq[j] == 0) continue;
-                int idx = indexMap[j];
-                if(pidx == -1) pidx = idx;
-                obj.Union(pidx, idx);
+                if(leader == -1) leader = j;
+                obj.Union(leader, j);
             }
         }
-        int prevIndex = -1;
+        int leader = -1;
         for(int i=0;i<n;i++){
-            int idx = indexMap[vec[i]];
-            int pidx = obj.find(idx);
-            if(prevIndex == -1){
-                prevIndex = pidx;
+            int ele = nums[i];
+            int pele = obj.find(ele);
+            if(leader == -1){
+                leader = pele;
             }
             else{
-                if(prevIndex != pidx) return false;
+                if(leader != pele) return false;
             }
         }
         return true;
