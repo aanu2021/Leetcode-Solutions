fa20748d6f5f4d3732c22fa2244725f7dbc1fead
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-03-17 10:03:20 +0530

    Time: 22 ms (8.06%), Space: 20.6 MB (29.62%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,62 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
         int n = intervals.size();
-        int idx = n;
-        
-        for(int i=0;i<n;i++){
-            if(intervals[i][0] >= newInterval[0]){
-                idx = i;
-                break;
-            }
-        }
-        
         vector<vector<int>>answer;
-        
-        // New Interval would be the first interval, among all the intervals.
-        
-        if(idx == 0){
-            vector<int>curr = newInterval;
-            for(int i=0;i<n;i++){
-                if(intervals[i][0] <= curr[1]){
-                    curr[1] = max(curr[1], intervals[i][1]);
-                }
-                else{
-                    answer.push_back(curr);
-                    curr = intervals[i];
-                }
-            }
-            answer.push_back(curr);
-            return answer;
-        }
-        
-        // New Interval would be in between interval, among all the intervals.
-        
-        else{
-            for(int i=0;i<idx-1;i++){
-                answer.push_back(intervals[i]);
-            }
-            vector<int>curr = intervals[idx-1];
-            if(newInterval[0] <= curr[1]){
-                curr[1] = max(curr[1],newInterval[1]);
+        vector<int>v = intervals[0];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0] <= v[1]){
+                v[1] = max(v[1], intervals[i][1]);
             }
             else{
-                answer.push_back(curr);
-                curr = newInterval;
+                answer.push_back(v);
+                v = intervals[i];
             }
-            for(int i=idx;i<n;i++){
-                if(intervals[i][0] <= curr[1]){
-                    curr[1] = max(curr[1], intervals[i][1]);
-                }
-                else{
-                    answer.push_back(curr);
-                    curr = intervals[i];
-                }
-            }
-            answer.push_back(curr);
-            return answer;
         }
-        
+        answer.push_back(v);
+        return answer;
     }
 };
