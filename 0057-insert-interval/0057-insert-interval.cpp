8c9349d04575556106de679754447051c764fc85
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-03-17 10:23:33 +0530

    Time: 7 ms (92.69%), Space: 20.5 MB (58.08%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,63 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
         int n = intervals.size();
-        vector<vector<int>>answer;
-        vector<int>v = intervals[0];
-        for(int i=1;i<n;i++){
-            if(intervals[i][0] <= v[1]){
-                v[1] = max(v[1], intervals[i][1]);
-            }
-            else{
-                answer.push_back(v);
-                v = intervals[i];
+        if(n == 0){
+            return {newInterval};
+        }
+        // Base Cases...
+        if(intervals[n-1][1] < newInterval[0]){
+            intervals.push_back(newInterval);
+            return intervals;
+        }
+        if(newInterval[1] < intervals[0][0]){
+            intervals.insert(intervals.begin(),newInterval);
+            return intervals;
+        }
+        int L = newInterval[0];
+        int R = newInterval[1];
+        int index = n;
+        for(int i=0;i<n;i++){
+            if(intervals[i][0] > newInterval[1]){
+                index = i;
+                break;
             }
         }
-        answer.push_back(v);
+        // cout<<index<<"\n";
+        // index -> position where we should place newInterval
+        int leftIndex = -1;
+        int rightIndex = n;
+        for(int i=index-1;i>=0;i--){
+            if(L <= intervals[i][0]){
+                L = min(L, intervals[i][0]);
+                R = max(R, intervals[i][1]);
+                continue;
+            }
+            else if(L >= intervals[i][0] && L <= intervals[i][1]){
+                L = min(L, intervals[i][0]);
+                R = max(R, intervals[i][1]);
+                leftIndex = i-1;
+                break;
+            }
+            else{
+                leftIndex = i;
+                break;
+            }
+        }
+        // cout<<L<<" "<<R<<"\n";
+        // cout<<leftIndex<<"\n";
+        vector<vector<int>>answer;
+        for(int i=0;i<=leftIndex;i++){
+            answer.push_back(intervals[i]);
+        }
+        answer.push_back({L,R});
+        for(int i=index;i<n;i++){
+            answer.push_back(intervals[i]);
+        }
         return answer;
     }
 };
+
+// 0 1 2 3 x 4 5 6
+                 
+    
