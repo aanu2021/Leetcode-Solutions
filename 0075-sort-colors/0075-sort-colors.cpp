650da60755a0993477b3a7992b9f8c3ca97c50df
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-04-21 00:15:00 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (5.20%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,15 +5,14 @@ public:
         int low = 0, mid = 0, high = n-1;
         while(mid <= high){
             if(nums[mid] == 0){
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
+                swap(nums[low], nums[mid]);
+                low++; mid++;
             }
             else if(nums[mid] == 1){
                 mid++;
             }
             else{
-                swap(nums[mid],nums[high]);
+                swap(nums[mid], nums[high]);
                 high--;
             }
         }
