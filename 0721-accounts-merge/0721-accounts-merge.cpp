f94f88f8d971d0844a2214b015df9d441e17908d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-05-15 10:39:10 +0530

    Time: 81 ms (43.37%), Space: 44.3 MB (30.23%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,53 +1,46 @@
 class DSU{
-  
-  public:
-  
-  vector<int>Parent;
-  vector<int>Rank;
-  
-  DSU(int n){
-      Parent.resize(n);
-      Rank.resize(n);
-      for(int i=0;i<n;i++){
-          Parent[i] = i;
-          Rank[i] = 1;
-      }
-  }
-  
-  int find(int x){
-      if(Parent[x]==x) return x;
-      return Parent[x] = find(Parent[x]);
-  }
-  
-  void Union(int x,int y){
-      int lx = find(x);
-      int ly = find(y);
-      if(lx != ly){
-          if(Rank[lx] < Rank[ly]){
-              Parent[lx] = ly;
-          }
-          else{
-              Parent[ly] = lx;
-              if(Rank[lx] == Rank[ly]){
-                  Rank[lx]++;
-              }
-          }
-      }
-  }
-    
+   private:
+    vector<int>Parent;
+    vector<int>Rank;
+   public: 
+     DSU(int n){
+         Parent.resize(n);
+         Rank.resize(n);
+         for(int i=0;i<n;i++){
+             Parent[i] = i;
+             Rank[i] = 1;
+         }
+     }
+     int find(int x){
+         if(Parent[x] == x) return x;
+         return Parent[x] = find(Parent[x]);
+     }
+     void Union(int x, int y){
+         int lx = find(x);
+         int ly = find(y);
+         if(lx != ly){
+             if(Rank[lx] > Rank[ly]){
+                 Parent[ly] = lx;
+             }
+             else{
+                 Parent[lx] = ly;
+                 if(Rank[lx] == Rank[ly]) Rank[lx]++;
+             }
+         }
+     }
 };
 
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        // store the final user to email groups
-        vector<vector<string>>ans;
-        int uid = 0;
-        // unique email id for emails
-        unordered_map<string,int>eid;
-        // email corresponding to their users
-        unordered_map<int,string>etn;
         int n = accounts.size();
+        int uid = 0;
+        vector<vector<string>>answer;
+        
+        unordered_map<string,int>eid;
+        unordered_map<int,string>etn;
+        unordered_map<int,unordered_set<string>>lte;
+        
         for(int i=0;i<n;i++){
             for(int j=1;j<accounts[i].size();j++){
                 if(eid.find(accounts[i][j]) == eid.end()){
@@ -60,37 +53,33 @@ public:
         DSU obj(uid);
         
         for(int i=0;i<n;i++){
-            string user = accounts[i][0];
             for(int j=1;j<accounts[i].size();j++){
-                obj.Union(eid[accounts[i][1]],eid[accounts[i][j]]);
+                obj.Union(eid[accounts[i][1]], eid[accounts[i][j]]);
             }
         }
-                              
-        // Leader to Emails                      
-        unordered_map<int,unordered_set<string>>lte;
-                            
+        
         for(int i=0;i<n;i++){
-            string user = accounts[i][0];
-            int leader_id = obj.find(eid[accounts[i][1]]);
             for(int j=1;j<accounts[i].size();j++){
+                int leader_id = obj.find(eid[accounts[i][j]]);
                 lte[leader_id].insert(accounts[i][j]);
             }
         }
-                              
         
         for(auto &itr : lte){
-            int leaderValue = itr.first;
-            string user = etn[leaderValue];
+            int leader_id = itr.first;
+            string name = etn[leader_id];
             vector<string>vec;
-            for(auto &it : itr.second){
-                vec.push_back(it);
+            auto it = itr.second;
+            for(auto iter : it){
+                vec.push_back(iter);
             }
             sort(vec.begin(),vec.end());
             reverse(vec.begin(),vec.end());
-            vec.push_back(user);
+            vec.push_back(name);
             reverse(vec.begin(),vec.end());
-            ans.push_back(vec);
-        }       
-        return ans;                      
+            answer.push_back(vec);
+        }
+        
+        return answer;
     }
 };
