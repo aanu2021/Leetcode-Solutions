0538ebb4b4204ae3e1a9fa5c5c7458b71ec34343
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-05-25 08:17:37 +0530

    Time: 4 ms (32.03%), Space: 8.9 MB (77.56%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,39 +1,74 @@
+struct Node{
+    Node*links[26];
+    bool endWord;
+    Node(){
+        for(int i=0;i<26;i++){
+            links[i] = NULL;
+        }
+        endWord = false;
+    }
+    bool containsKey(char ch){
+        return (links[ch-'a'] != NULL);
+    }
+    Node * get(char ch){
+        return links[ch-'a'];
+    }
+    void put(char ch, Node * node){
+        links[ch-'a'] = node;
+    }
+    bool isEndWord(){
+        return endWord;
+    }
+    void setEndWord(){
+        endWord = true;
+    }
+};
+
 class Solution {
 public:
     
-    vector<string>answer;
-    unordered_set<string>S;
+    Node * root = new Node();
     
-    void recursion(string &s,int i,int n,vector<string>&solution){
-        if(i==n){
-            string word = "";
-            for(string &str : solution){
-                word += str;
-                word += " ";
+    void insertWord(string &str){
+        int n = str.length();
+        Node * node = root;
+        for(int i=0;i<n;i++){
+            if(!node->containsKey(str[i])){
+                node->put(str[i], new Node());
             }
-            word.pop_back();
-            answer.push_back(word);
+            node = node->get(str[i]);
+        }
+        node->setEndWord();
+    }
+    
+    void recursion(string &s, int i, int n, string solution, vector<string>&answer){
+        if(i == n){
+            solution.pop_back();
+            answer.push_back(solution);
+            return;
         }
         string str = "";
+        Node * node = root;
         for(int j=i;j<n;j++){
             str += s[j];
-            if(S.find(str) != S.end()){
-                solution.push_back(str);
-                recursion(s,j+1,n,solution);
-                solution.pop_back();
+            if(!node->containsKey(s[j])){
+                break;
+            }
+            node = node->get(s[j]);
+            if(node->isEndWord()){
+                recursion(s, j+1, n, solution + str + " ", answer);
             }
         }
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
-        S.clear();
-        answer.clear();
-        vector<string>solution;
-        for(string &str : wordDict){
-            S.insert(str);
+        for(string str : wordDict){
+            insertWord(str);
         }
-        recursion(s,0,n,solution);
+        vector<string>answer;
+        string solution = "";
+        recursion(s, 0, n, "", answer);
         return answer;
     }
 };
