f92115b48c421040d631138355ea39a46c5a309c
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-02 20:56:08 +0530

    Time: 84 ms (100.00%), Space: 31.6 MB (66.67%) - LeetHub

---
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        int n = s.length();
+        vector<vector<int>>bucket(26);
+        vector<int>removed(n, 0);
+        for(int i=0;i<n;i++){
+            if(s[i] == '*'){
+                for(int j=0;j<26;j++){
+                    if(bucket[j].size() > 0){
+                        removed[i] = 1;
+                        removed[bucket[j].back()] = 1;
+                        bucket[j].pop_back();
+                        break;
+                    }
+                }
+            }
+            else{
+                bucket[s[i]-'a'].push_back(i);
+            }
+        }
+        string res = "";
+        for(int i=0;i<n;i++){
+            if(!removed[i]){
+                res += s[i];
+            }
+        }
+        return res;
+    }
+};
+
+
+
