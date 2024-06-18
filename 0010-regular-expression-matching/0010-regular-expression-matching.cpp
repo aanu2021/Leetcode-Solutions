11e777c3f1f009b2432954a7db07f5a5b008973e
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-18 20:56:26 +0530

    Time: 3 ms (70.24%), Space: 8.4 MB (64.73%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -7,15 +7,12 @@ public:
         s = "#" + s;
         p = "#" + p;
         
-        vector<vector<bool>>dp(m+1, vector<bool>(n+1, false));
         vector<bool>prev(n+1, false);
         vector<bool>curr(n+1, false);
-        dp[0][0] = true;
         prev[0] = true;
         
         for(int i=1;i<=n;i++){
             if(p[i] == '*'){
-                dp[0][i] = dp[0][i-2];
                 prev[i] = prev[i-2];
             }
         }
@@ -23,14 +20,11 @@ public:
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s[i] == p[j] || p[j] == '.'){
-                    dp[i][j] = dp[i-1][j-1];
                     curr[j] = prev[j-1];
                 }
                 else if(p[j] == '*'){
-                    dp[i][j] = dp[i][j-2];
                     curr[j] = curr[j-2];
                     if(p[j-1] == '.' || s[i] == p[j-1]){
-                        dp[i][j] = dp[i][j] || dp[i-1][j];
                         curr[j] = curr[j] || prev[j];
                     }
                 }
