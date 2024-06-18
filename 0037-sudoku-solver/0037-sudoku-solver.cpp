9f421683f2bfab4e07ce26f01cd68b31fb61a181
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-18 19:34:05 +0530

    Time: 10 ms (73.26%), Space: 7.7 MB (37.37%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     
-    bool isPossible(vector<vector<char>>&board,int row,int col,char num){
+    bool isSafe(vector<vector<char>>&board, int row, int col, char num){
         for(int i=0;i<9;i++){
-            if(board[row][i]==num) return false;
+            if(board[i][col] == num) return false;
         }
-        for(int i=0;i<9;i++){
-            if(board[i][col]==num) return false;
+        for(int j=0;j<9;j++){
+            if(board[row][j] == num) return false;
         }
-        int sr = (row/3)*3;
-        int sc = (col/3)*3;
+        int smi = (row/3)*3;
+        int smj = (col/3)*3;
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                if(board[i+sr][j+sc]==num) return false;
+                if(board[i+smi][j+smj] == num) return false;
             }
         }
         return true;
     }
     
     bool solve(vector<vector<char>>&board){
-        for(int row=0;row<9;row++){
-            for(int col=0;col<9;col++){
-                if(board[row][col]=='.'){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
                     for(char num='1';num<='9';num++){
-                        if(isPossible(board,row,col,num)){
-                            board[row][col] = num;
-                            if(solve(board)){
-                                return true;
-                            }
-                            board[row][col] = '.';
+                        if(isSafe(board, i, j, num)){
+                            board[i][j] = num;
+                            if(solve(board)) return true;
+                            board[i][j] = '.';
                         }
                     }
                     return false;
