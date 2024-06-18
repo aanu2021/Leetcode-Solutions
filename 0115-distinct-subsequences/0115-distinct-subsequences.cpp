83147113910c9cf2a49945235c73f4e369f8051d
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-18 21:34:52 +0530

    Time: 18 ms (62.18%), Space: 19.8 MB (62.05%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,15 +4,14 @@ public:
         int m = s.length();
         int n = t.length();
         vector<vector<unsigned int>>dp(m+1, vector<unsigned int>(n+1, 0));
-        dp[0][0] = 1;
-        for(int i=1;i<=m;i++){
+        for(int i=0;i<=m;i++){
             dp[i][0] = 1;
         }
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 dp[i][j] = dp[i-1][j];
                 if(s[i-1] == t[j-1]){
-                    dp[i][j]  += dp[i-1][j-1];
+                    dp[i][j] += dp[i-1][j-1];
                 }
             }
         }
