a62c68af6044f3d043eec24c0065426f5c4c7561
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-18 20:04:26 +0530

    Time: 7 ms (30.78%), Space: 13.2 MB (98.76%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     
-    vector<int>answer;
-    
-    void add_subtree(TreeNode*& root,int dist){
+    void addSubtree(TreeNode* root, int k, vector<int>&answer){
         if(!root) return;
-        if(dist == 0){
-            answer.push_back(root->val);
-            return;
-        }
-        add_subtree(root->left,dist-1);
-        add_subtree(root->right,dist-1);
+        if(k < 0) return;
+        if(k == 0) answer.push_back(root->val);
+        addSubtree(root->left, k-1, answer);
+        addSubtree(root->right, k-1, answer);
     }
     
-    int dfs(TreeNode*& root,TreeNode*& target,int k){
+    int func(TreeNode* root, TreeNode* target, int k, vector<int>&answer){
         if(!root) return -1;
         if(root == target){
-            add_subtree(root,k);
+            addSubtree(root, k, answer);
             return k-1;
         }
-        int dist = dfs(root->left,target,k);
-        if(dist > -1){
-            if(dist == 0) answer.push_back(root->val);
-            add_subtree(root->right,dist-1);
-            return dist-1;
+        int L = func(root->left, target, k, answer);
+        if(L >= 0){
+            if(L == 0) answer.push_back(root->val);
+            addSubtree(root->right, L-1, answer);
+            return L-1;
         }
-        dist = dfs(root->right,target,k);
-        if(dist > -1){
-            if(dist == 0) answer.push_back(root->val);
-            add_subtree(root->left,dist-1);
-            return dist-1;
+        int R = func(root->right, target, k, answer);
+        if(R >= 0){
+            if(R == 0) answer.push_back(root->val);
+            addSubtree(root->left, R-1, answer);
+            return R-1;
         }
         return -1;
     }
     
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        dfs(root,target,k);
+        if(!root) return {};
+        vector<int>answer;
+        func(root, target, k, answer);
         return answer;
     }
 };
