642eb10026fdcbf59cb4921e1ef47eb8b20661c2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-18 23:33:52 +0530

    Time: 6 ms (32.13%), Space: 8.8 MB (68.37%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,74 +1,30 @@
-struct Node{
-    Node*links[26];
-    bool endWord;
-    Node(){
-        for(int i=0;i<26;i++){
-            links[i] = NULL;
-        }
-        endWord = false;
-    }
-    bool containsKey(char ch){
-        return (links[ch-'a'] != NULL);
-    }
-    Node * get(char ch){
-        return links[ch-'a'];
-    }
-    void put(char ch, Node * node){
-        links[ch-'a'] = node;
-    }
-    bool isEndWord(){
-        return endWord;
-    }
-    void setEndWord(){
-        endWord = true;
-    }
-};
-
 class Solution {
 public:
     
-    Node * root = new Node();
+    unordered_set<string>S;
     
-    void insertWord(string &str){
-        int n = str.length();
-        Node * node = root;
-        for(int i=0;i<n;i++){
-            if(!node->containsKey(str[i])){
-                node->put(str[i], new Node());
-            }
-            node = node->get(str[i]);
-        }
-        node->setEndWord();
-    }
-    
-    void recursion(string &s, int i, int n, string solution, vector<string>&answer){
+    void func(string &s, int i, int n, string sub, vector<string>&answer){
         if(i == n){
-            solution.pop_back();
-            answer.push_back(solution);
+            sub.pop_back();
+            answer.push_back(sub);
             return;
         }
         string str = "";
-        Node * node = root;
         for(int j=i;j<n;j++){
             str += s[j];
-            if(!node->containsKey(s[j])){
-                break;
-            }
-            node = node->get(s[j]);
-            if(node->isEndWord()){
-                recursion(s, j+1, n, solution + str + " ", answer);
+            if(S.find(str) != S.end()){
+                func(s, j+1, n, sub + str + " ", answer);
             }
         }
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
-        for(string str : wordDict){
-            insertWord(str);
+        for(string &str : wordDict){
+            S.insert(str);
         }
         vector<string>answer;
-        string solution = "";
-        recursion(s, 0, n, "", answer);
+        func(s, 0, n, "", answer);
         return answer;
     }
 };
