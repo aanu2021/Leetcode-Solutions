d1e049954ffd2cfd96b14f40fddfff9441c92fe4
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-19 01:12:09 +0530

    Time: 34 ms (65.24%), Space: 23.8 MB (44.09%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,76 +1,70 @@
 class DSU{
-  
-  public:
-  
-  vector<int>Parent;
-  vector<int>Size;
-  
-  DSU(int n){
-      Parent.resize(n);
-      Size.resize(n);
-      for(int i=0;i<n;i++){
-          Parent[i] = i;
-          Size[i] = 1;
-      }
-  }
-  
-  int find(int x){
-      if(Parent[x]==x) return x;
-      return Parent[x] = find(Parent[x]);
-  }
-  
-  void Union(int x,int y){
-      int lx = find(x);
-      int ly = find(y);
-      if(lx != ly){
-          if(Size[lx] > Size[ly]){
-              Size[lx] += Size[ly];
-              Parent[ly] = lx;
-          }
-          else{
-              Size[ly] += Size[lx];
-              Parent[lx] = ly;
-          }
-      }
-  }
-    
-  int getSize(int x){
-      int lx = find(x);
-      return Size[lx];
-  }  
-    
+   private:
+    vector<int>Parent;
+    vector<int>Size;
+   public:
+     DSU(int n){
+         Parent = vector<int>(n);
+         Size = vector<int>(n);
+         for(int i=0;i<n;i++){
+             Parent[i] = i;
+             Size[i] = 1;
+         }
+     }
+     int find(int x){
+         if(Parent[x] == x) return x;
+         return Parent[x] = find(Parent[x]);
+     }
+     void Union(int x, int y){
+         int lx = find(x);
+         int ly = find(y);
+         if(lx != ly){
+             if(Size[lx] > Size[ly]){
+                 Size[lx] += Size[ly];
+                 Parent[ly] = lx;
+             }
+             else{
+                 Size[ly] += Size[lx];
+                 Parent[lx] = ly;
+             }
+         }
+     }
+     int getSize(int x){
+         int p = find(x);
+         return Size[p];
+     }
 };
-
 
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
+        unordered_map<int,vector<int>>rowMap;
+        unordered_map<int,vector<int>>colMap;
         DSU obj(n);
-        unordered_map<int,vector<int>>row;
-        unordered_map<int,vector<int>>col;
         for(int i=0;i<n;i++){
-            row[stones[i][0]].push_back(i);
-            col[stones[i][1]].push_back(i);
+            int x = stones[i][0];
+            int y = stones[i][1];
+            rowMap[x].push_back(i);
+            colMap[y].push_back(i);
         }
-        int totalRemoval = 0;
-        for(auto &itr : row){
-            int leader = itr.second[0];
-            for(auto &ele : itr.second){
-                obj.Union(leader,ele);
+        for(auto &itr : rowMap){
+            for(auto &it : itr.second){
+                obj.Union(itr.second[0], it);
             }
         }
-        for(auto &itr : col){
-            int leader = itr.second[0];
-            for(auto &ele : itr.second){
-                obj.Union(leader,ele);
+        for(auto &itr : colMap){
+            for(auto &it : itr.second){
+                obj.Union(itr.second[0], it);
             }
         }
+        int total = 0;
         for(int i=0;i<n;i++){
-            if(obj.find(i) == i){
-                totalRemoval += (obj.getSize(i) - 1);
+            int p = obj.find(i);
+            if(p == i){
+                total += obj.getSize(i) - 1;
             }
         }
-        return totalRemoval;
+        return total;
     }
 };
