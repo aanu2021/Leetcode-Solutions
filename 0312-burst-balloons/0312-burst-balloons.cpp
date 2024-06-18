8a2c217cbbb0e3382d48915ac8ee962753a7f442
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-19 00:02:12 +0530

    Time: 171 ms (51.73%), Space: 11.9 MB (79.15%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<int>arr;
-        arr.push_back(1);
-        for(int i=0;i<n;i++){
-            arr.push_back(nums[i]);
-        }
-        arr.push_back(1);
-        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        nums.push_back(1);
+        reverse(nums.begin(),nums.end());
+        nums.push_back(1);
+        reverse(nums.begin(),nums.end());
+        vector<vector<int>>dp(n+2, vector<int>(n+2, 0));
         for(int i=1;i<=n;i++){
-            dp[i][i] = (arr[i-1]*arr[i]*arr[i+1]);
+            dp[i][i] = (nums[i-1]*nums[i]*nums[i+1]);
         }
         for(int L=2;L<=n;L++){
             for(int i=1;i<=n-L+1;i++){
                 int j = i+L-1;
                 for(int k=i;k<=j;k++){
-                    dp[i][j] = max(dp[i][j], (arr[i-1]*arr[k]*arr[j+1]) + dp[i][k-1] + dp[k+1][j]);
+                    int currScore = nums[i-1]*nums[k]*nums[j+1];
+                    currScore += dp[i][k-1];
+                    currScore += dp[k+1][j];
+                    dp[i][j] = max(dp[i][j], currScore);
                 }
             }
         }
