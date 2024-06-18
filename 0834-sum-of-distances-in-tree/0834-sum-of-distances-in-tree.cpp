e98be66c5dc22d89c6c9bb815aed90c97de30de0
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-19 00:53:30 +0530

    Time: 232 ms (49.10%), Space: 97.5 MB (59.75%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     
     vector<vector<int>>graph;
-    vector<int>subtree;
     vector<int>subtree_cnt;
+    vector<int>subtree_sum;
     vector<int>dp;
     
     void precompute(int u, int p){
@@ -12,44 +12,42 @@ public:
             if(v == p) continue;
             precompute(v, u);
             subtree_cnt[u] += subtree_cnt[v];
-            subtree[u] += (subtree_cnt[v] + subtree[v]);
+            subtree_sum[u] += subtree_sum[v] + subtree_cnt[v];
         }
     }
     
     void dfs(int u, int p, int par){
-        int N = subtree_cnt[0];
-        dp[u] += (N-subtree_cnt[u]) + par;
+        dp[u] = subtree_sum[u] + (subtree_cnt[0] - subtree_cnt[u]) + par;
         for(int v : graph[u]){
             if(v == p) continue;
-            dp[u] += (subtree_cnt[v] + subtree[v]);
-        }
-        for(int v : graph[u]){
-            if(v == p) continue;
-            int partial = dp[u] - subtree[v] - subtree_cnt[v];
+            int partial = dp[u] - subtree_sum[v] - subtree_cnt[v];
             dfs(v, u, partial);
         }
     }
     
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        
         graph.clear();
-        subtree.clear();
         subtree_cnt.clear();
+        subtree_sum.clear();
         dp.clear();
         
         graph = vector<vector<int>>(n);
-        subtree = vector<int>(n, 0);
         subtree_cnt = vector<int>(n, 0);
+        subtree_sum = vector<int>(n, 0);
         dp = vector<int>(n, 0);
         
         for(int i=0;i<edges.size();i++){
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
+            int u = edges[i][0];
+            int v = edges[i][1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
         }
         
         precompute(0, -1);
-        
         dfs(0, -1, 0);
         
         return dp;
+        
     }
 };
