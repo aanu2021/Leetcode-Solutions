972100a4289e46f22d696c9b89ef37bcf50bbeb2
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-19 00:22:39 +0530

    Time: 31 ms (62.48%), Space: 20 MB (41.67%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     
-    int dp[202][202];
-    
     int dx[4] = {0,0,1,-1};
     int dy[4] = {1,-1,0,0};
     
-    int func(vector<vector<int>>&grid,int i,int j,int m,int n,int prev){
-        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] <= prev) return 0;
-        
+    int func(vector<vector<int>>&grid, int i, int j, int m, int n, vector<vector<int>>&dp){
+        if(i < 0 || j < 0 || i >= m || j >= n) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        
-        int currLen = 0;
-        
-        for(int dir = 0 ; dir < 4 ; dir++){
+        int maxLen = 1;
+        for(int dir=0;dir<4;dir++){
             int ni = i + dx[dir];
             int nj = j + dy[dir];
-            currLen = max(currLen,func(grid,ni,nj,m,n,grid[i][j]));
+            if(ni >= 0 && nj >= 0 && ni < m && nj < n){
+                if(grid[ni][nj] > grid[i][j]){
+                    maxLen = max(maxLen, 1 + func(grid, ni, nj, m, n, dp));
+                }
+            }
         }
-        
-        return dp[i][j] = 1 + currLen;
-        
+        return dp[i][j] = maxLen;
     }
     
     int longestIncreasingPath(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        memset(dp,-1,sizeof(dp));
+        vector<vector<int>>dp(m, vector<int>(n, -1));
         int maxLen = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(dp[i][j] == -1){
-                    maxLen = max(maxLen,func(grid,i,j,m,n,-1));
+                    maxLen = max(maxLen, func(grid, i, j, m, n, dp));
                 }
             }
         }
