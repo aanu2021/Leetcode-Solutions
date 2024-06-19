73c7c5784bd5519a520c60b63d86b8b6ce37dda7
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-06-19 10:02:18 +0530

    Time: 118 ms (33.92%), Space: 69 MB (65.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,67 +1,41 @@
 class Solution {
 public:
     
-    bool isPossible(vector<int>&nums,int mid,int m,int k){
-        
-        int n=nums.size();
-        
-        int currsum=0;
-        
-        int cnt=0;
-        
+    typedef long long ll;
+    
+    bool canMake(vector<int>&nums, int n, int mid, int m, int k){
+        int cnt = 0;
+        int consecutive = 0;
         for(int i=0;i<n;i++){
-            
-            if(nums[i]>mid){
-                continue;
+            if(nums[i] > mid){
+                consecutive = 0;
             }
-            
-            int j=i;
-            
-            currsum=0;
-            
-            while(j<n && nums[j]<=mid && currsum<k){
-                currsum++;
-                j++;
+            else{
+                consecutive++;
+                if(consecutive == k){
+                    cnt++; consecutive = 0;
+                }
             }
-            
-            if(currsum==k){
-                
-                cnt++;
-                
-            }
-            
-            i=j;
-            i--;
-            
         }
-        
-        return cnt>=m;
-        
+        return cnt >= m;
     }
     
     int minDays(vector<int>& nums, int m, int k) {
-        
-        int n=nums.size();
-        
-        int low=*min_element(nums.begin(),nums.end());
-        int high=*max_element(nums.begin(),nums.end());
-        
-        int ans=-1;
-        
-        while(low<=high){
-            
-            int mid=(low+high)/2;
-            
-            if(isPossible(nums,mid,m,k)==true){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
+        int n = nums.size();
+        ll prod = (ll)((ll)m*(ll)k);
+        if(prod > n) return -1;
+        int low = 0, high = *max_element(nums.begin(),nums.end());
+        int answer = high;
+        while(low <= high){
+            int mid = low + ((high - low)/2);
+            if(canMake(nums, n, mid, m, k)){
+                answer = mid;
+                high = mid - 1;
             }
-            
+            else{
+                low = mid + 1;
+            }
         }
-        
-        return ans;
-        
+        return answer;
     }
 };
