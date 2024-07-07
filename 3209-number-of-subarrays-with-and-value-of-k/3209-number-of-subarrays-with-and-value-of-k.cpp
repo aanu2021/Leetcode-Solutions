0358ae868f5009b9c5940ad84ae608d1946daeca
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-07-07 12:50:14 +0530

    Time: 790 ms (25.00%), Space: 385.3 MB (12.50%) - LeetHub

---
+++ 3209-number-of-subarrays-with-and-value-of-k/3209-number-of-subarrays-with-and-value-of-k.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    typedef long long ll;
+    
+    unordered_map<ll,ll>p;
+    
+    long long countSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        ll answer = 0LL;
+        
+        p.clear();
+        
+        for(int i=0;i<nums.size();i++){
+            unordered_map<ll,ll>c;
+            c[nums[i]] = 1LL;
+            if(nums[i] < k || nums[i] > k){
+                
+            }
+            else{
+                answer++;
+            }
+            for(auto &itr : p){
+                ll pval = itr.first & (ll)nums[i];
+                if(pval == (ll)k) answer += itr.second;
+                c[pval] += itr.second;
+            }
+            p = c;
+        }
+        
+        return answer;
+    }
+};
+
+// 1 1 2
+// 0 1 2
+
+// 1 + 2 + 3 = 6
+    
+    
+    
+// k = 1
+// k = 0    
+    
