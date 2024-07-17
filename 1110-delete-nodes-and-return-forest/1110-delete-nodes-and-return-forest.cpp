74d56ea5a420639cc7c1d474e61fd52bb344b7a9
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-07-17 11:06:07 +0530

    Time: 16 ms (56.71%), Space: 27.4 MB (20.14%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -6,34 +6,27 @@ public:
     
     void func(TreeNode* &root){
         if(!root) return;
-        
         func(root->left);
         func(root->right);
-        
         if(S.find(root->val) != S.end()){
-            if(root->left){
-                result.push_back(root->left);
-            }
-            if(root->right){
-                result.push_back(root->right);
-            }
+            if(root->left) result.push_back(root->left);
+            if(root->right) result.push_back(root->right);
             root = NULL;
         }
     }
     
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         
+        if(!root) return {};
+        
         S.clear();
         result.clear();
         
-        for(int &ele : to_delete){
-            S.insert(ele);
-        }
+        for(int &ele : to_delete) S.insert(ele);
         
         func(root);
-        if(root){
-            result.push_back(root);
-        }
+        
+        if(root) result.push_back(root);
         
         return result;
     }
