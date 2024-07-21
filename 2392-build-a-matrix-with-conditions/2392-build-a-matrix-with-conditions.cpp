aac1693d0944702e3fa5f81c469f375b1d48278a
Anumoy Nandy <91496248+aanu2021@users.noreply.github.com>
2024-07-21 20:20:07 +0530

    Time: 78 ms (98.03%), Space: 61.1 MB (74.93%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,97 +1,54 @@
 class Solution {
 public:
     
-    vector<int>topo_sort(int k,vector<vector<int>>&edges){
-        
+    vector<int>topo_sort(int k, vector<vector<int>>&edges){
+        int m = edges.size();
         vector<vector<int>>graph(k+1);
-        
-        vector<int>indegree(k+1,0);
-        
-        for(int i=0;i<edges.size();++i){
-            
-            graph[edges[i][0]].push_back(edges[i][1]);
-            
-            indegree[edges[i][1]]++;
-            
+        vector<int>indegree(k+1, 0);
+        for(int i=0;i<m;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            graph[u].push_back(v);
+            indegree[v]++;
         }
         
-        vector<int>ans;
-        
         queue<int>q;
-        
-        for(int i=1;i<=k;++i){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+        vector<int>topo;
+        for(int i=1;i<=k;i++){
+            if(indegree[i] == 0) q.push(i);
         }
         
         while(!q.empty()){
-            
-            int node=q.front();
-            q.pop();
-            
-            ans.push_back(node);
-            
-            for(int nbr:graph[node]){
-                
+            int node = q.front(); q.pop();
+            topo.push_back(node);
+            for(int &nbr : graph[node]){
                 indegree[nbr]--;
-                
-                if(indegree[nbr]==0){
-                    
-                    q.push(nbr);
-                    
-                }
-                
+                if(indegree[nbr] == 0) q.push(nbr);
             }
-            
         }
         
-        if(ans.size()!=k){
-            
-            return {};
-            
-        }
-        
-        return ans;
-        
+        if(topo.size() != k) return {};
+        else return topo;
     }
     
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
         
-        vector<int>row_topo=topo_sort(k,rowConditions);
+        vector<int>row_topo = topo_sort(k, rowConditions);
+        vector<int>col_topo = topo_sort(k, colConditions);
         
-        vector<int>col_topo=topo_sort(k,colConditions);
+        if(row_topo.size() == 0 || col_topo.size() == 0) return {};
+        vector<vector<int>>grid(k, vector<int>(k, 0));
+        unordered_map<int,int>rowMap,colMap;
         
-        if(row_topo.size()==0 || col_topo.size()==0){
-            
-            return {};
-            
+        for(int i=0;i<k;i++){
+            rowMap[row_topo[i]] = i;
+            colMap[col_topo[i]] = i;
         }
         
-        vector<int>index_row(k+1,-1);
-        vector<int>index_col(k+1,-1);
-        
-        for(int i=0;i<row_topo.size();++i){
-            
-            index_row[row_topo[i]] = i;
-            
+        for(int i=1;i<=k;i++){
+            grid[rowMap[i]][colMap[i]] = i;
         }
-        
-        for(int i=0;i<col_topo.size();++i){
-            
-            index_col[col_topo[i]] = i;
-            
-        }
-        
-        vector<vector<int>>ans(k,vector<int>(k,0));
-        
-        for(int i=1;i<=k;++i){
-            
-            ans[index_row[i]][index_col[i]] = i;
-            
-        }
-        
-        return ans;
+        return grid;
         
     }
 };
